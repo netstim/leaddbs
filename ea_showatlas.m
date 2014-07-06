7335237622869d7e354e045e1e13bce4620429bd
ahorn <mail@andreas-horn.de>
2014-07-06 11:52:08 +0200

    Sundy commit

--- ea_showatlas.m
+++ ea_showatlas.m
@@ -193,7 +193,7 @@ for atlas=1:length(atlases.names)
             if options.prefs.hullsmooth
                 nii.img = smooth3(nii.img,'gaussian',options.prefs.hullsmooth);
             end
-            fv=isosurface(X,Y,Z,permute(nii.img,[2,1,3]),max(nii.img(:))/3);
+            fv=isosurface(X,Y,Z,permute(nii.img,[2,1,3]),max(nii.img(:))/2);
             
             if ischar(options.prefs.hullsimplify)
                 
