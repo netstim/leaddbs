fb91293b399fd7cfb437194e7d10559c9de57a1b
ahorn <mail@andreas-horn.de>
2015-09-12 11:26:16 +0200

    Final implementation of necessary fieldtrip code into lead-dbs for simbio modeling.

--- ea_genvat_simbio_ws.m
+++ ea_genvat_simbio_ws.m
@@ -2556,7 +2556,7 @@ try
     dir = diag([1,1,1]);
     for i=1:size(pos,1)
         locpos = repmat(pos(i,:),3,1);
-        rhs = sb_rhs_venant(locpos,dir,vol);
+        rhs = ea_sb_rhs_venant(locpos,dir,vol);
         lf((3*(i-1)+1):(3*(i-1)+3),:) = (vol.transfer * rhs)';
     end    
     lf = lf';
@@ -2564,6 +2564,88 @@ catch
   warning('an error occurred while running simbio');
   rethrow(lasterror)
 end
+
+function rhs = ea_sb_rhs_venant(pos,dir,vol);
+
+% SB_RHS_VENANT
+%
+% $Id: sb_rhs_venant.m 8776 2013-11-14 09:04:48Z roboos $
+
+%find node closest to source position
+next_nd = ea_sb_get_next_nd(pos,vol.pos);
+%find nodes neighbouring closest node
+if isfield(vol,'tet')
+    ven_nd = ea_sb_get_ven_nd(next_nd,vol.tet);
+elseif isfield(vol,'hex')
+    ven_nd = ea_sb_get_ven_nd(next_nd,vol.hex);
+else
+    error('No connectivity information given!');
+end
+%calculate rhs matrix
+loads = ea_sb_calc_ven_loads(pos,dir,ven_nd,vol.pos);
+%assign values in sparse matrix
+i = reshape(ven_nd',[],1);
+j = reshape(repmat([1:size(ven_nd,1)],size(ven_nd,2),1),[],1);
+loads = reshape(loads',[],1);
+j = j(i~=0);
+loads = loads(i~=0);
+i = i(i~=0);
+rhs = sparse(i,j,loads,size(vol.pos,1),size(pos,1));
+
+
+function next_nd = ea_sb_get_next_nd(pos,node);
+next_nd = zeros(size(pos,1),1);
+for i=1:size(pos,1)
+    [dist, next_nd(i)] = min(sum(bsxfun(@minus,node,pos(i,:)).^2,2));
+end
+
+function loads = ea_sb_calc_ven_loads(pos,dir,ven_nd,node);
+
+% SB_CALC_VEN_LOADS
+%
+% $Id: sb_calc_ven_loads.m 8776 2013-11-14 09:04:48Z roboos $
+
+%aref setzen
+aref = 20;
+%lambda setzen
+lambda = 10e-6;
+%r setzen
+r = 1;
+
+loads = zeros(size(pos,1),size(ven_nd,2));
+for i=1:size(pos,1);
+    x = bsxfun(@minus,node(ven_nd(i,ven_nd(i,:)~=0),:),pos(i,:))./aref;
+    X = zeros(9,size(x,1));
+    X(1:3:7,:) = ones(3,size(x,1));
+    X(2:3:8,:) = x';
+    X(3:3:9,:) = (x.^2)';
+    T = zeros(9,1);
+    T(1:3:7) = 0;
+    T(2:3:8) = dir(i,:)./aref;
+    T(3:3:9) = 0;
+    W = zeros(size(x,1)*3,size(x,1));
+    W(1:size(x,1),:) = diag(x(:,1).^r);
+    W(size(x,1)+1:2*size(x,1),:) = diag(x(:,2).^r);
+    W(size(x,1)*2+1:3*size(x,1),:) = diag(x(:,3).^r);
+    tmp = ((X')*X + lambda*(W')*W) \ X'*T;
+    loads(i,ven_nd(i,:)~=0) = tmp;
+end
+
+
+function ven_nd = ea_sb_get_ven_nd(next_nd,elem);
+ven_nd = zeros(size(next_nd,1),1);
+for i=1:size(next_nd,1)
+    [tmp1,tmp2] = find(elem == next_nd(i));
+    tmp = unique(elem(tmp1,:));
+    %tmp = tmp(tmp~=next_nd(i)); seems like this is not done in the
+    %original.
+    if(length(tmp) > size(ven_nd,2))
+        ven_nd = [ven_nd, zeros(size(ven_nd,1),length(tmp)-size(ven_nd,2))];
+    end
+    ven_nd(i,1:length(tmp)) = tmp;
+end
+
+
 
 function [output] = ea_ft_transform_geometry(transform, input)
 
