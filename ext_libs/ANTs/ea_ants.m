631c46b7d329474d7a69773bdf1cdd92e174d484
ahorn <mail@andreas-horn.de>
2015-11-20 20:40:32 +0100

    Try with ants affine..

--- ext_libs/ANTs/ea_ants.m
+++ ext_libs/ANTs/ea_ants.m
@@ -1,5 +1,5 @@
 function ea_ants(fixedimage, movingimage, outputname)
-% Wrapper for ANTs
+% Wrapper for ANTs nonlinear registration
 
 if fileparts(movingimage)
     volumedir = [fileparts(movingimage), filesep];
@@ -10,21 +10,10 @@ end
 ttries=ea_detct2anatattempts(volumedir); % how many coregistration attempts have been made in the past..
 
 % first attempt..
-fixparams{1} =[' 3' ...
-             ' -t affine[ 0.1 ]' ...
-             ' --regularization Gauss[3,0]' ...
-             ' --number-of-iterations 10x10x5' ...
-             ' --rigid-affine true'...
-             ' --do-rigid true'];
+fixparams{1} =[' -d 3 -t a'];
 
 % second attempt..
-fixparams{2} = [' 3' ...
-             ' -t affine[ 0.1 ]' ...
-             ' --regularization Gauss[3,0]' ...
-             ' --number-of-iterations 10x10x5' ...
-             ' --initial-affine ', volumedir, 'ct2anat',num2str(ttries),'.txt'...
-             ' --rigid-affine true'...
-             ' --do-rigid true'];
+fixparams{2} = [' -d 3 -t a'];
 
 if ttries>2
     ttries=2;
@@ -34,26 +23,15 @@ end
 basedir = [fileparts(mfilename('fullpath')), filesep];
 
 if ispc
-    ANTS = [basedir, 'ANTS.exe'];
-    Warp = [basedir, 'WarpImageMultiTransform.exe'];
+    ANTS = [basedir, 'antsRegistration.exe'];
 
 elseif isunix
-    ANTS = [basedir, 'ANTS.', computer];
-    Warp = [basedir, 'WarpImageMultiTransform.', computer];
+    ANTS = [basedir, 'antsRegistration.', computer];
 end
 
 ea_libs_helper
-system([ANTS, fixparams{ttries}, ...
-        ' -m PR[', fixedimage,',', movingimage,',1,2]' ...
-        ' --output-naming ', outputname]);
+system([ANTS,' -m ',movingimage,' -f ',fixedimage, ' -o ',outputname,fixparams{ttries}]);
 
-outputbase = outputname(1:end-4);
-system([Warp ...
-        ' 3 ', movingimage, ' ', outputname, ...
-        ' -R ', fixedimage, ...
-        ' ', outputbase, 'Warp.nii' ...
-        ' ', outputbase, 'Affine.txt']);
 
-delete([outputbase, 'Warp.nii']);
-delete([outputbase, 'InverseWarp.nii']);
+    keyboard
 movefile([outputbase, 'Affine.txt'], [volumedir, 'ct2anat', num2str(ttries+1), '.txt']);
