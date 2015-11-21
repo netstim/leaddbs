188591c6489c5b21a85a753da780883af43ebbcb
ahorn <mail@andreas-horn.de>
2015-11-21 11:39:02 +0100

    Antsrecommit

--- ext_libs/ANTs/ea_ants.m
+++ ext_libs/ANTs/ea_ants.m
@@ -10,13 +10,16 @@ end
 ttries=ea_detct2anatattempts(volumedir); % how many coregistration attempts have been made in the past..
 
 % first attempt..
-fixparams{1} =[' -d 3 -t a'];
+fixparams{1} =[' -d 3 -t a -v 1 -c [10000x0x0,1.e-8,20] -s 4x2x1vox'];
+
+fixparams{1} =[' -d 3 -v 1 -t translation[ 0.1 ]  -c [10,1.e-8,20]   -s 4x2x1vox  -f 6x4x2 -l 1  -t rigid[ 0.1 ] -c [10,1.e-8,20]  -s 4x2x1vox   -f 3x2x1 -l 1  -t affine[ 0.1 ]   -c [10,1.e-8,20] -s 4x2x1vox']
+
 
 % second attempt..
-fixparams{2} = [' -d 3 -t a'];
+%fixparams{2} = [' -d 3 -t a -v 1 -c 1e-6 -s 1e-6'];
 
-if ttries>2
-    ttries=2;
+if ttries>1
+    ttries=1;
 end
 
 
@@ -30,7 +33,7 @@ elseif isunix
 end
 
 ea_libs_helper
-system([ANTS,' -m ',movingimage,' -f ',fixedimage, ' -o ',outputname,fixparams{ttries}]);
+system([ANTS,' -m ',movingimage,'.gz',' -f ',fixedimage,'.gz', ' -o ',outputname,fixparams{ttries}]);
 
 
     keyboard
