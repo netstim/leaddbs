f30089ffde0a6c753be33a5b5e938d69dc295eec
Ningfei Li <ningfei.li@gmail.com>
2015-12-02 13:55:13 +0100

    Tweak ea_update

--- ea_update.m
+++ ea_update.m
@@ -1,4 +1,4 @@
-function update=ea_update(varargin)
+function ea_update(varargin)
 % lead update
 
 local=ea_getvsn('local');
@@ -23,68 +23,74 @@ earoot=[fileparts(which('lead')),filesep];
 
 updurl = 'http://www.lead-dbs.org/release/download.php';
 if update
-    if update==1 % full update
-        id='lead_dbs';
-    elseif update==2 % incremental update
-        id=['updates_',strrep(local,'.',''),'-',strrep(web,'.','')];
-    end
-
-    disp('*** Updating LEAD-DBS. Please do not quit MATLAB.');
-    mkdir([earoot,'tmp'])
-    disp('Downloading code...');
     try
-        webopts=weboptions('Timeout',5);
-        websave([earoot,'tmp',filesep,'updates.zip'],updurl,'id',id,webopts);
-    catch
-        try
-            urlwrite([updurl,'?id=',id],[earoot,'tmp',filesep,'updates.zip'],'Timeout',5);
-        catch
-            info='Unknow error occurred, please retry later!';
-            display(info);
-            msgbox(info,'Update','Error')
-            return
+        if update==1 % full update
+            id='lead_dbs';
+        elseif update==2 % incremental update
+            id=['updates_',strrep(local,'.',''),'-',strrep(web,'.','')];
         end
-    end
-    disp('Extracting code...');
-    try
-        unzip([earoot,'tmp',filesep,'updates.zip'],[earoot,'tmp',filesep]);
-    catch
-        system(['unzip -q ',earoot,'tmp',filesep,'updates.zip -d ', earoot,'tmp']);
-    end
-    delete([earoot,'tmp',filesep,'updates.zip']);
-    
-    if update==2 % delete files during incremental updating
-        dels=textscan(fopen([earoot,'tmp',filesep,id,filesep,'DELETE']),'%s');
-        for f=1:length(dels{1})
-            if isdir([earoot,dels{1}{f}])
-                rmdir([earoot,dels{1}{f}],'s')
-            else
-                delete([earoot,dels{1}{f}])
+
+        disp('*** Updating LEAD-DBS. Please do not quit MATLAB.');
+        mkdir([earoot,'tmp'])
+        disp('Downloading code...');
+        try
+            webopts=weboptions('Timeout',5);
+            websave([earoot,'tmp',filesep,'updates.zip'],updurl,'id',id,webopts);
+        catch
+            try
+                urlwrite([updurl,'?id=',id],[earoot,'tmp',filesep,'updates.zip'],'Timeout',5);
+            catch
+                info='Download error! Please retry later.';
+                disp(info);
+                msgbox(info,'Update','Error')
+                return
             end
         end
-        delete([earoot,'tmp',filesep,id,filesep,'DELETE'])
+
+        disp('Extracting code...');
+        try
+            unzip([earoot,'tmp',filesep,'updates.zip'],[earoot,'tmp',filesep]);
+        catch
+            system(['unzip -q ',earoot,'tmp',filesep,'updates.zip -d ', earoot,'tmp']);
+        end
+        delete([earoot,'tmp',filesep,'updates.zip']);
+
+        disp('Deleting outdated code...');
+        try
+            if update==2 % delete files during incremental updating
+                dels=textscan(fopen([earoot,'tmp',filesep,id,filesep,'DELETE']),'%s');
+                for f=1:length(dels{1})
+                    if isdir([earoot,dels{1}{f}])
+                        rmdir([earoot,dels{1}{f}],'s')
+                    else
+                        delete([earoot,dels{1}{f}])
+                    end
+                end
+                delete([earoot,'tmp',filesep,id,filesep,'DELETE'])
+            end
+        catch
+            disp('Error while deleting some files. You may ignore this.');
+        end
+
+        disp('Copying new code...');
+        copyfile([earoot,'tmp',filesep,id,filesep,'*'],earoot,'f');
+        copyfile([earoot,'tmp',filesep,id,filesep,'.version.txt'],earoot,'f');
+        disp('Cleaning up...');
+        rmdir([earoot,'tmp'],'s')
+        disp('Done.');
+
+        disp('Restarting LEAD-DBS.');
+        lead;
+        disp('*** Update finished.');
+    catch
+        info=sprintf(['Failed to update!\n',...
+                      'Alternatively, you can download the latest verion from: http://www.lead-dbs.org/release/download.php']);
+        disp(info);
+        msgbox(info,'Update','Error');
     end
-    
-    disp('copying code to the right place...');
-    copyfile([earoot,'tmp',filesep,id,filesep,'*'],earoot,'f');
-    copyfile([earoot,'tmp',filesep,id,filesep,'.version.txt'],earoot,'f');
-    disp('Cleaning up...');
-    rmdir([earoot,'tmp'],'s')
-    disp('Done.');
-
-    disp('Restarting LEAD-DBS.');
-    lead;
-    disp('*** Update finished.');
-
 else
     info=sprintf(['LEAD-DBS aleady up-to-date!\n',...
                   'Alternatively, you can re-download the latest verion from: http://www.lead-dbs.org/release/download.php']);
     disp(info);
-    msgbox(info,'Update','Help')
+    msgbox(info,'Update','Help');
 end
-
-
-
-
-
-
