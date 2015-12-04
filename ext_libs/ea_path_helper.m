3b2e84e08a23eea984b247c9ae6309906796f4b8
Ningfei Li <ningfei.li@gmail.com>
2015-12-04 14:42:52 +0100

    Fix ea_path_helper
    Signed-off-by: Ningfei Li <ningfei.li@gmail.com>

--- ext_libs/ea_path_helper.m
+++ ext_libs/ea_path_helper.m
@@ -1,12 +1,12 @@
 function filepath = ea_path_helper(filepath)
 % handle special characters in the path for cli compatibility
 
+if isempty(fileparts(filepath))
+    filepath = ['.', filesep, filepath];
+end
+
 if ispc
     filepath = ['"',filepath,'"'];
 else
     filepath = regexprep(filepath,'[() &]', '\\$0');
-end
-
-if isempty(fileparts(filepath))
-    filepath = ['.', filesep, filepath];
 end
