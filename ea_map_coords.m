e165051bbd1036b46d8692c877439127c55f3ca3
ahorn <mail@andreas-horn.de>
2016-02-10 11:11:25 -0500

    Added simpler interpolation to map_coords on HD warping fields.

--- ea_map_coords.m
+++ ea_map_coords.m
@@ -152,78 +152,12 @@ inds = coord(1:3, :);
 %     warning('map_coords:hdw_rounding',...
 %         'target voxel coords rounded for HDW')
 % end
-W = nifti(hdwim);
-
+Vx=spm_vol([hdwim,',1,1']);
+Vy=spm_vol([hdwim,',1,2']);
+Vz=spm_vol([hdwim,',1,3']);
 for i = 1:size(coord, 2)
     ind = inds(:, i);
-    
-               % linearly interpolate: 
-    targ(1,1,1,:)=squeeze(W.dat(floor(ind(1)), floor(ind(2)), floor(ind(3)), 1, 1:3));
-    seed(1,1,1,:)=[floor(ind(1)), floor(ind(2)), floor(ind(3))];
-    vec(1,1,1,:)=squeeze(targ(1,1,1,:))-squeeze(seed(1,1,1,:));
-    weight(1,1,1,:)=1-(pdist([squeeze(seed(1,1,1,:))';ind'])/sqrt(3));
-    
-    targ(1,1,2,:)=squeeze(W.dat(floor(ind(1)), floor(ind(2)), ceil(ind(3)), 1, 1:3));
-    seed(1,1,2,:)=[floor(ind(1)), floor(ind(2)), ceil(ind(3))];
-    vec(1,1,2,:)=squeeze(targ(1,1,2,:))-squeeze(seed(1,1,2,:));
-    weight(1,1,2,:)=1-(pdist([squeeze(seed(1,1,2,:))';ind'])/sqrt(3));
-    
-    targ(1,2,1,:)=squeeze(W.dat(floor(ind(1)), ceil(ind(2)), floor(ind(3)), 1, 1:3));
-    seed(1,2,1,:)=[floor(ind(1)), ceil(ind(2)), floor(ind(3))];
-    vec(1,2,1,:)=squeeze(targ(1,2,1,:))-squeeze(seed(1,2,1,:));
-        weight(1,2,1,:)=1-(pdist([squeeze(seed(1,2,1,:))';ind'])/sqrt(3));
-
-    targ(2,1,1,:)=squeeze(W.dat(ceil(ind(1)), floor(ind(2)), floor(ind(3)), 1, 1:3));
-    seed(2,1,1,:)=[ceil(ind(1)), floor(ind(2)), floor(ind(3))];
-    vec(2,1,1,:)=squeeze(targ(2,1,1,:))-squeeze(seed(2,1,1,:));
-        weight(2,1,1,:)=1-(pdist([squeeze(seed(2,1,1,:))';ind'])/sqrt(3));
-
-    targ(1,2,2,:)=squeeze(W.dat(floor(ind(1)), ceil(ind(2)), ceil(ind(3)), 1, 1:3));
-    seed(1,2,2,:)=[floor(ind(1)), ceil(ind(2)), ceil(ind(3))];
-    vec(1,2,2,:)=squeeze(targ(1,2,2,:))-squeeze(seed(1,2,2,:));
-        weight(1,2,2,:)=1-(pdist([squeeze(seed(1,2,2,:))';ind'])/sqrt(3));
-
-    targ(2,1,2,:)=squeeze(W.dat(ceil(ind(1)), floor(ind(2)), ceil(ind(3)), 1, 1:3));
-    seed(2,1,2,:)=[ceil(ind(1)), floor(ind(2)), ceil(ind(3))];
-    vec(2,1,2,:)=squeeze(targ(2,1,2,:))-squeeze(seed(2,1,2,:));
-        weight(2,1,2,:)=1-(pdist([squeeze(seed(2,1,2,:))';ind'])/sqrt(3));
-
-    targ(2,2,1,:)=squeeze(W.dat(ceil(ind(1)), ceil(ind(2)), floor(ind(3)), 1, 1:3));
-    seed(2,2,1,:)=[ceil(ind(1)), ceil(ind(2)), floor(ind(3))];
-    vec(2,2,1,:)=squeeze(targ(2,2,1,:))-squeeze(seed(2,2,1,:));
-        weight(2,2,1,:)=1-(pdist([squeeze(seed(2,2,1,:))';ind'])/sqrt(3));
-
-    targ(2,2,2,:)=squeeze(W.dat(ceil(ind(1)), ceil(ind(2)), ceil(ind(3)), 1, 1:3));
-    seed(2,2,2,:)=[ceil(ind(1)), ceil(ind(2)), ceil(ind(3))];
-    vec(2,2,2,:)=squeeze(targ(2,2,2,:))-squeeze(seed(2,2,2,:));
-        weight(2,2,2,:)=1-(pdist([squeeze(seed(2,2,2,:))';ind'])/sqrt(3));
-
-%     rests=ind-floor(ind);
-%     rrests=1-rests;
-    
-    weightsum=sum([weight(1,1,1,:)
-        weight(1,1,2,:)
-        weight(1,2,1,:)
-        weight(2,1,1,:)
-        weight(1,2,2,:)
-        weight(2,1,2,:)
-        weight(2,2,1,:)
-        weight(2,2,2,:)]);
-    
-    coord(1:3,i)=ind+...
-        sum([squeeze(vec(1,1,1,:))*weight(1,1,1,:),...
-        squeeze(vec(1,1,2,:))*weight(1,1,2,:),...
-        squeeze(vec(1,2,1,:))*weight(1,2,1,:),...
-        squeeze(vec(2,1,1,:))*weight(2,1,1,:),...
-        squeeze(vec(1,2,2,:))*weight(1,2,2,:),...
-        squeeze(vec(2,1,2,:))*weight(2,1,2,:),...
-        squeeze(vec(2,2,1,:))*weight(2,2,1,:),...
-        squeeze(vec(2,2,2,:))*weight(2,2,2,:)],2)/weightsum;
-    
-% 
-%    
-
-
-%% original code:
-%                coord(1:3, i) = squeeze(W.dat(ind(1), ind(2), ind(3), 1, 1:3));
+    coord(1,i)=spm_sample_vol(Vx,ind(1),ind(2),ind(3),1);
+    coord(2,i)=spm_sample_vol(Vy,ind(1),ind(2),ind(3),1);
+    coord(3,i)=spm_sample_vol(Vz,ind(1),ind(2),ind(3),1);
 end
