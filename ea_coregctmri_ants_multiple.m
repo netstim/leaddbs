79f52d52daffda50b3d01c64fd2dc9c510426f15
andreashorn <mail@andreas-horn.de>
2016-03-22 13:21:46 -0400

    Removed keyboard as per nl's mail

---
+++ ea_coregctmri_ants_multiple.m
@@ -0,0 +1,30 @@
+function varargout=ea_coregctmri_ants_multiple(options)
+% This function uses the BRAINSfit to register postop-CT to preop-MR.
+% __________________________________________________________________________________
+% Copyright (C) 2015 Charite University Medicine Berlin, Movement Disorders Unit
+% Andreas Horn
+
+if ischar(options) % return name of method.
+    varargout{1}='Advanced Normalization Tools (ANTs) - multiple runs';
+    varargout{2}={'SPM8','SPM12'};
+    varargout{3}=['nan']; % suggestion for alpha-parameter.
+    return
+end
+disp('Interpolating preoperative anatomical image');
+ea_normalize_reslicepretra(options);
+disp('Done.');
+disp('Coregistering postop CT to preop MRI...');
+ea_ants([options.root,options.patientname,filesep,options.prefs.prenii_unnormalized],...
+    [options.root,options.patientname,filesep,options.prefs.rawctnii_unnormalized],...
+    [options.root,options.patientname,filesep,options.prefs.ctnii_coregistered])
+ea_ants([options.root,options.patientname,filesep,options.prefs.prenii_unnormalized],...
+    [options.root,options.patientname,filesep,options.prefs.rawctnii_unnormalized],...
+    [options.root,options.patientname,filesep,options.prefs.ctnii_coregistered])
+ea_ants([options.root,options.patientname,filesep,options.prefs.prenii_unnormalized],...
+    [options.root,options.patientname,filesep,options.prefs.rawctnii_unnormalized],...
+    [options.root,options.patientname,filesep,options.prefs.ctnii_coregistered])
+ea_ants([options.root,options.patientname,filesep,options.prefs.prenii_unnormalized],...
+    [options.root,options.patientname,filesep,options.prefs.rawctnii_unnormalized],...
+    [options.root,options.patientname,filesep,options.prefs.ctnii_coregistered])
+disp('Coregistration done.');
+
