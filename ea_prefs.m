2275bb8da5661c2619ced0eb5ec16e4d9643b097
siobhanewert <siobhanewert@gmx.net>
2016-04-19 22:26:54 -0400

    ud

--- ea_prefs.m
+++ ea_prefs.m
@@ -5,7 +5,6 @@ function prefs=ea_prefs(patientname)
 % folder.
 
 % load loaded prefs (-> prefs.lp)
-
 try
 load([fileparts(which('lead')),filesep,'ea_prefs']);
 prefs.lp=lp;
@@ -13,14 +12,14 @@ end
 
 %% general settings:
 prefs.pp.do=1; % use parallel processing if available.
-prefs.pp.csize=2; % specify how many clusters to recruit.
+prefs.pp.csize=4; % specify how many clusters to recruit.
 prefs.pp.profile='local'; % specify parallel processing profile.
 
 %% general file handling:
-prefs.prenii_unnormalized=['anat.nii']; % (preoperative) anatomical MR image
-prefs.tranii_unnormalized=['postop_tra.nii']; % postoperative primary anatomical MR image
-prefs.sagnii_unnormalized=['postop_sag.nii']; % postoperative optional anatomical MR image (saggital planes), for visualization only.
-prefs.cornii_unnormalized=['postop_cor.nii']; % postoperative optional anatomical MR image (coronar planes), for visualization only.
+prefs.prenii_unnormalized=['anat.nii']; % not needed if schoenecker normalization is used.
+prefs.tranii_unnormalized=['postop_tra.nii'];
+prefs.sagnii_unnormalized=['postop_sag.nii'];
+prefs.cornii_unnormalized=['postop_cor.nii'];
 prefs.rawctnii_unnormalized=['postop_ct.nii'];
 prefs.ctnii_coregistered=['rpostop_ct.nii'];
 
@@ -36,6 +35,10 @@ prefs.gtranii=['glpostop_tra.nii'];
 prefs.gcornii=['glpostop_cor.nii'];
 prefs.gsagnii=['glpostop_sag.nii'];
 prefs.gctnii=['glpostop_ct.nii'];
+
+
+
+
 
 %% connectome files:
 prefs.rest=['rest.nii']; % raw resting state fMRI data
@@ -56,13 +59,12 @@ prefs.lc.graphsurfc=[0.2081    0.1663    0.5292]; % default color for graph metr
 prefs.lc.matsurfc=[0.8 0.7 0.4]; % default color for matrix-level correlations 3D-visualizations.
 prefs.lc.seedsurfc=[0.8 0.1 0.1]; % default color for seed of matrix-level correlations 3D-visualizations.
 
-
 %% DTI-files:
 prefs.b0=['b0.nii'];
 prefs.fa=['fa.nii'];
 prefs.fa2anat=['fa2anat.nii'];
 prefs.FTR_unnormalized=['FTR.mat'];
-prefs.FTR_normalized=['wFTR_gqi.mat'];
+prefs.FTR_normalized=['wFTR.mat'];
 prefs.DTD=['DTD.mat'];
 prefs.HARDI='HARDI.mat';
 prefs.dti='dti.nii';
@@ -88,7 +90,7 @@ prefs.dicom.dicomfiles=1; % 1: delete DICOMs after conversion, 2: move DICOMs to
 %% Normalization:
 prefs.normalize.coreg='auto'; % set to 'manual' to include manual coregistration check steps.
 prefs.normalize.default='ea_normalize_ants_multimodal';
-prefs.normalize.inverse.customtpm=0; % set to 1 if custom TPM shall be built for inverse warpings.
+prefs.normalize.inverse.customtpm=1; % set to 1 if custom TPM shall be built for inverse warpings.
 
 %% Coregistration (CT/MR):
 prefs.ctcoreg.default='ea_coregctmri_ants';
@@ -106,6 +108,16 @@ prefs.lhullmethod=2; % set 2 to use isosurface, 1 for concavehull, 0 for convexh
 prefs.lhullsmooth=3; % set to smooth hulldata. Only applies if isosurface is used. Only odd numbers allowed. Set to 0 if you don't want to smooth.
 prefs.lhullsimplify='auto'; % 0.1 would reduce hulldata to 10% ? set to simplify hulldata. Set to 1 to not simplify. Only applies if isosurface is used.
 
+%% 2D-Export
+prefs.d2.useprepost='pre'; % can be 'post' or 'pre' to set the backdrop.
+prefs.d2.groupcolors='lead'; % can be 'maxdist' to use ea_distinguishable_colors by Timothy E. Holy, 'lead' to use a handpicked color set inspired by colorblender.com or 'lines' to use the matlab lines colormap (supports only seven colors).
+prefs.d2.isovolsmoothed='s'; % set to '' if you want to display the raw, unsmoothed version of the isovolume.
+prefs.d2.isovolcolormap='jet'; % color map to use for plotting of isovolume heatmap.
+prefs.d2.isovolsepcomb='combined'; % set to 'combined' to use the lr-combined isovolume and 'lr' to use the separate isovolumes.
+
+%% DICOM files:
+prefs.dicom.dicomfiles=0; % 1: delete DICOMs after conversion, 2: move DICOMs to pt/DICOM folder after conversion. 0: leave DICOMs where they were (not recommended: DICOMs will then be always re-imported from the import folder).
+
 
 %% fibers:
 
@@ -115,17 +127,6 @@ prefs.addfibers={}; % additional fibers to show.
 %% lead server:
 
 prefs.ls.autosave=0;
-prefs.firstrun='off'; 
-%prefs.ls.dir='/Volumes/DBS_Lokas/bg_rest/lead_server/';
-
-%% video export:
-% for help see documentation of CaptureFigVid
-% (http://www.mathworks.com/matlabcentral/fileexchange/41093-create-video-of-rotating-3d-plot/content/CaptureFigVid/CaptureFigVid.m)
-prefs.video.path=[-20,10;-110,10;-190,80;-290,10;-380,10];
-prefs.video.opts.FrameRate=24;
-prefs.video.opts.Duration=20;
-prefs.video.opts.Periodic=true;
-prefs.ls.dir='/PA/Neuro/_projects/lead/lead_server/';
 
 %% environment
-prefs.env.dev=1;
+prefs.env.dev=0;
