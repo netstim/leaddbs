5965e62062f61caafbca08bca37f6d352e858daa
andreashorn <mail@andreas-horn.de>
2016-05-17 16:41:06 +0200

    v1.4.8.7

--- connectomics/ea_perform_lc.m
+++ connectomics/ea_perform_lc.m
@@ -90,6 +90,7 @@ function ea_perform_ft_proxy(options)
 %#function ea_ft_mesotracking_reisert
 %#function ea_ft_deterministictracking_kroon
 %#function ea_ft_globaltracking_reisert
+
 eval([options.lc.struc.ft.method,'(options)']); % triggers the fibertracking function and passes the options struct to it.
 try load([options.root,options.patientname,filesep,'ea_ftmethod_applied']); end
 if exist('ft_method_applied','var')
