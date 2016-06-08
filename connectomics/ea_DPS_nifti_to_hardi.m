967c31b7419527c032012674256ac869764c1e69
andreashorn <mail@andreas-horn.de>
2016-06-07 20:23:09 -0400

    New import for Gibbstracker

---
+++ connectomics/ea_DPS_nifti_to_hardi.m
@@ -0,0 +1,29 @@
+function [mrs nii_orig] = DPS_nifti_to_hardi(niiName,bvecname,bvalname)
+
+nii_orig = load_untouch_nii(niiName);
+nii = nii_orig;
+
+mrs = ea_DPS_nifti_to_mrs(nii);
+
+if nargin == 1,
+    bvec = importdata([niiName(1:end-3), 'bvec']);
+    bval = importdata([niiName(1:end-3), 'bval']);
+else
+    bvec = importdata(bvecname);
+    bval = importdata(bvalname);    
+end
+
+T = 1;
+
+for k = 1:size(bval,2),
+    gdir = T*bvec(:,k);
+    gdir = gdir / (eps+norm(gdir));
+    tensor(:,:,k) = gdir*gdir' *bval(k);
+end;
+
+mrs.user.bfactor = bval;
+mrs.user.bDir = bvec;
+mrs.user.bTensor = tensor;
+
+end
+
---
+++ connectomics/ea_DPS_nifti_to_mrs.m
@@ -0,0 +1,27 @@
+function mrs = ea_DPS_nifti_to_mrs(nii)
+
+if isstr(nii),
+    nii = ea_load_untouch_nii(nii);
+end;
+
+nii.mat = [nii.hdr.hist.srow_x ; nii.hdr.hist.srow_y ; nii.hdr.hist.srow_z; 0 0 0 1];
+if ndims(nii.img) > 3
+    mrs = mrstruct_init( 'series3D', double(nii.img) );
+else
+    mrs = mrstruct_init( 'volume', double(nii.img) );
+end
+
+mrs.vox = sqrt( sum((nii.mat*diag([1 1 1 0])).^2) );
+mrs.edges = nii.mat;
+mrs.edges = diag([-1 -1 1 1 ]) * mrs.edges;
+
+
+%extract TR and TE
+if ~isempty(nii.hdr.hk.db_name)
+    tString = nii.hdr.hk.db_name;
+    names = regexp(tString, '?TR:(?<TR>.+)\sTE:(?<TE>.+)', 'names');
+    if ~isempty(names)
+        mrs.tr = str2double(names.TR);
+        mrs.te = str2double(names.TE);
+    end
+end
