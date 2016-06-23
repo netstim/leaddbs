1d3d503ab1e510311be6ca71175d209ef5f5ab48
andreashorn <mail@andreas-horn.de>
2016-06-23 18:34:16 -0400

    Moved some coregctmri functions to dev folder because they never work.

--- dev/ea_atlas2stl.m
+++ dev/ea_atlas2stl.m
@@ -1,8 +1,17 @@
-function ea_atlas2stl(atlasfile,ofn)
+function ea_atlas2stl(atlasnames,ofn)
 
-load(atlasfile);
-for mesh=1:numel(atlases.fv)
-    cfv(mesh)=atlases.fv{mesh};
+if ~iscell(atlasnames)
+    ea_error('Please specify atlas(es) in a cellstring');
+end
+cnt=1;
+earoot=ea_getearoot;
+for atl=1:length(atlasnames)
+    
+    load([earoot,'atlases',filesep,atlasnames{atl},filesep,'atlas_index.mat']);
+    for mesh=1:numel(atlases.fv)
+        cfv(cnt)=atlases.fv{mesh};
+        cnt=cnt+1;
+    end
 end
 cfv=ea_concatfv(cfv);
 
