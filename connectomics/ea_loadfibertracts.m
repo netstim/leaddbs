203eef2a0532092ab16c442c8b7583f3a3e910c5
Ningfei Li <ningfei.li@gmail.com>
2016-07-22 11:21:53 +0200

    Further fix of ea_convertfibs2newformat

--- connectomics/ea_loadfibertracts.m
+++ connectomics/ea_loadfibertracts.m
@@ -10,14 +10,14 @@ if strcmp(cfile(end-3:end),'.mat')
     idx=fibinfo.idx;
     if nargout>2
         try
-       voxmm=fibinfo.voxmm;
+            voxmm=fibinfo.voxmm;
         catch % assume voxel
             voxmm='vox';
         end
-       mat=[];
-       try
+        mat=[];
+        try
            mat=fibinfo.mat;
-       end
+        end
     end
 else
     ea_trk2ftr(cfile);
@@ -31,11 +31,8 @@ end
 function ea_trk2ftr(cfile)
 
 [pth,fn,~]=fileparts(cfile);
-
-
 [hdr,trks]=ea_trk_read([cfile]);
 ftr.vox=hdr.voxel_size;
-
 
 %V=spm_vol([spm('dir'),filesep,'canonical',filesep,'avg152T2.nii']); % assume MNI152 official to be voxel space of tracts
 V=spm_vol([ea_getearoot,'templates',filesep,'mni_hires.nii']);
@@ -43,10 +40,9 @@ for i=1:length(trks)
     ftr.curveSegCell{i}=[trks(i).matrix(:,1)/ftr.vox(1),trks(i).matrix(:,2)/ftr.vox(2),trks(i).matrix(:,3)/ftr.vox(3)];
 
     %% use conversion from DSI_studio:
-
-ftr.curveSegCell{i}(:,1)=78.0-ftr.curveSegCell{i}(:,1);
-ftr.curveSegCell{i}(:,2)=76.0-ftr.curveSegCell{i}(:,2);
-ftr.curveSegCell{i}(:,3)=-50.0+ftr.curveSegCell{i}(:,3);
+    ftr.curveSegCell{i}(:,1)=78.0-ftr.curveSegCell{i}(:,1);
+    ftr.curveSegCell{i}(:,2)=76.0-ftr.curveSegCell{i}(:,2);
+    ftr.curveSegCell{i}(:,3)=-50.0+ftr.curveSegCell{i}(:,3);
 
     %% use conversion from a file:
 %     ftr.curveSegCell{i}=[ftr.curveSegCell{i},ones(size(ftr.curveSegCell{i},1),1)]';
@@ -57,9 +53,9 @@ end
 save(fullfile(pth,[fn,'.mat']),'-struct','ftr','-v7.3');
 delete(cfile);
 
-function fibers=ea_convertfibs2newformat(fibinfo,cfile)
-    fn=fieldnames(fibinfo);
+function ftr=ea_convertfibs2newformat(fibinfo,cfile)
 
+fn=fieldnames(fibinfo);
 if isfield(fn,'normalized_fibers_mm')
     fibers=fibinfo.normalized_fibers_mm;
     voxmm='mm';
@@ -89,5 +85,8 @@ ea_dispercent(1,'end');
 
 fibers=[fibers,idxv];
 
+ftr.fibers=fibers;
+ftr.idx=idx;
+fibinfo.voxmm=voxmm;
 
 ea_savefibertracts(cfile,fibers,idx,'mm')
