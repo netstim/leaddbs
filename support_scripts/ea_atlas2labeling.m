890fa83de358ecf2bbb2b53b3e61cf2f03019cb7
andreashorn <mail@andreas-horn.de>
2016-08-04 16:32:30 -0400

    New support script to convert atlas 2 labeling

---
+++ support_scripts/ea_atlas2labeling.m
@@ -0,0 +1,69 @@
+function ea_atlas2labeling(atlasname,spacefile)
+    
+
+earoot=ea_getearoot;
+
+copyfile([earoot,'atlases',filesep,atlasname],[earoot,'atlases',filesep,atlasname,'_copy']);
+if ~exist('spacefile','var')
+spacefile=[earoot,'templates',filesep,'mni_hires.nii'];
+end
+
+copybase=[earoot,'atlases',filesep,atlasname,'_copy',filesep];
+
+subfs={'lh','rh','mixed','midline'};
+cnt=1;
+for subf=1:length(subfs)
+    subbase=[copybase,subfs{subf},filesep];
+    switch subfs{subf}
+        case 'lh'
+            append='_L';
+        case 'rh'
+            append='_R';
+        otherwise
+            append='';
+    end
+        
+   system(['gunzip ',subbase,'*.nii.gz']);
+   
+   nuclei=dir([subbase,'*.nii']);
+   
+   for nucleus=1:length(nuclei)
+       ea_conformspaceto(spacefile,[subbase,nuclei(nucleus).name],1);
+       [~,nuclname]=fileparts(nuclei(nucleus).name);
+       Astr{cnt}=[nuclname,append];
+       nii=ea_load_nii([subbase,nuclei(nucleus).name]);
+       if ~exist('Avol','var') % init
+           Avol=zeros(size(nii.img));
+       end
+       Avol(:,:,:,cnt)=nii.img;
+       cnt=cnt+1;
+   end
+    
+end
+
+Avol(isnan(Avol))=0;
+Avol(Avol<0.5)=0;
+[~,Fvol]=max(Avol,[],4);
+lab=nii;
+lab.img=Fvol;
+lab.fname=[earoot,'templates',filesep,'labeling',filesep,atlasname,'.nii'];
+ea_write_nii(lab);
+
+f=fopen([earoot,'templates',filesep,'labeling',filesep,atlasname,'.txt'],'w');
+for nucl=1:length(Astr)
+    fprintf(f,'%d %s\n',nucl,Astr{nucl});
+end
+fclose(f);
+
+rmdir([earoot,'atlases',filesep,atlasname,'_copy'],'s')
+
+
+
+
+
+
+
+
+
+
+
