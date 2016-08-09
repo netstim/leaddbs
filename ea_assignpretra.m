0a1b2e911c307677af17ac8afbf7660e4ac7b3c8
andreashorn <mail@andreas-horn.de>
2016-08-09 12:19:09 -0400

    Mistake in assigning primary anat file when pd present

--- ea_assignpretra.m
+++ ea_assignpretra.m
@@ -3,6 +3,7 @@ function [options,presentfiles]=ea_assignpretra(options)
 
 directory=[options.root,options.patientname,filesep];
 ptemps={'','_t1','_pd'};
+
 for prenii=1:length(options.prefs.rawpreniis)
 
     fexist(prenii)=exist([directory,options.prefs.rawpreniis{prenii}],'file');
@@ -16,18 +17,21 @@ end
 
 
 if ~exist([directory,'.pp'],'file') && ~exist([directory,'ea_normmethod_applied.mat'],'file') % only do this once, small hidden flag .pp inside patient folder will show this has been done before.
-    for f=find(fexist)
+    for fi=f
         % apply biasfieldcorrection and reorient/crop
-        ea_dcm2nii([directory,options.prefs.rawpreniis{f}]);
-        ea_bias_field_correction([directory,options.prefs.rawpreniis{f}])
+        ea_dcm2nii([directory,options.prefs.rawpreniis{fi}]);
+        ea_bias_field_correction([directory,options.prefs.rawpreniis{fi}])
     end
     fs=fopen([directory,'.pp'],'w');
+    fprintf(fs,'%s','dcmbiasfielddone');
+    fclose(fs);
 end
+
 try
     options.prefs.prenii_unnormalized=options.prefs.rawpreniis{f(1)};
     options.primarytemplate=ptemps{f(1)};
 catch
-    options.primarytemplate=options.prefs.prenii_unnormalized;
+    options.primarytemplate='';
 end
 
 presentfiles=options.prefs.rawpreniis(f);
