cd1c9ccc5aba56b5a785c95ad37248352f632386
andreashorn <mail@andreas-horn.de>
2016-08-09 11:13:06 -0400

    amend

--- ea_run_cluster.m
+++ ea_run_cluster.m
@@ -1,15 +1,20 @@
 function ea_run_cluster(~,~,clusterfunctionname,handles)
 
-
 leadfig=handles.leadfigure;
 ea_busyaction('on',leadfig,'lead');
-
 
 options=ea_handles2options(handles);
 options.macaquemodus=getappdata(handles.leadfigure,'macaquemodus');
 
 options.uipatdirs=getappdata(handles.leadfigure,'uipatdir');
 
-feval(eval(['@',clusterfunctionname]),options);
+for pat=1:length(options.uipatdirs)
+    % set patient specific options
+    options.root=[fileparts(uipatdirs{pat}),filesep];
+    [root,thispatdir]=fileparts(uipatdirs{pat});
+    options.patientname=thispatdir;
+    % run main function
+    feval(eval(['@',clusterfunctionname]),options);
+end
 
 ea_busyaction('off',leadfig,'lead');
