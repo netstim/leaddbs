2946bd73a1e37c220b6236bb5fa696c84f5bdb62
andreashorn <mail@andreas-horn.de>
2016-08-24 08:38:17 -0400

    Fix markers issue pointed out by PD

--- templates/electrode_models/ea_resolvecoords.m
+++ templates/electrode_models/ea_resolvecoords.m
@@ -39,7 +39,7 @@ for side=1:length(markers)
     
 M=[markers(side).head,1;markers(side).tail,1;markers(side).x,1;markers(side).y,1];
 E=[electrode.head_position,1;electrode.tail_position,1;electrode.x_position,1;electrode.y_position,1];
-X=ea_linsolve(E,M);
+X=linsolve(E,M);
 coords_mm=[electrode.coords_mm,ones(size(electrode.coords_mm,1),1)];
 coords{side}=X'*coords_mm';
 coords{side}=coords{side}(1:3,:)';
