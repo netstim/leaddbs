a7317544c524e36dc0066e39f3f75c63912c4f82
andreashorn <mail@andreas-horn.de>
2016-08-29 12:01:49 -0400

    home-folder prefs file renamed to .ea_prefs.m

--- ea_prefs.m
+++ ea_prefs.m
@@ -7,11 +7,11 @@ prefs=ea_prefs_default(patientname);
 home=ea_gethome;
 uid=['ea_prefs_',dash2sub(ea_generate_guid)];
 
-if ~exist([home,'ea_prefs.m'],'file');
-    copyfile([ea_getearoot,'ea_prefs_default.m'],[home,'ea_prefs.m']);
+if ~exist([home,'.ea_prefs.m'],'file');
+    copyfile([ea_getearoot,'ea_prefs_default.m'],[home,'.ea_prefs.m']);
 end
 try
-    copyfile([home,'ea_prefs.m'],[ea_getearoot,uid,'.m'])
+    copyfile([home,'.ea_prefs.m'],[ea_getearoot,uid,'.m'])
     uprefs=feval(uid,patientname);
     delete([ea_getearoot,uid,'.m']);
 end
