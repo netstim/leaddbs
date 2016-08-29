e36ffcd61486a3e525694c1986ba4cfa9fbbbdbc
andreashorn <mail@andreas-horn.de>
2016-08-29 14:37:14 -0400

    Amend

--- cluster/ea_run_Martinos_Launchpad_compile.m
+++ cluster/ea_run_Martinos_Launchpad_compile.m
@@ -10,6 +10,16 @@ options.spmdir=spm('dir');
 save([options.root,options.patientname,filesep,'job_',jobID],'options')
 setenv('ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS','1')
 
-cmdstring=['pbsubmit -q highio -O ',[options.root,options.patientname,filesep,'job_',jobID],'.out -E ',[options.root,options.patientname,filesep,'job_',jobID],'.err -c "matlab.new -singleCompThread -nodisplay -r "''ea_run runcluster ',[options.root,options.patientname,filesep,'job_',jobID],'''"'];
+cmdstring=['pbsubmit -q highio -O ',[options.root,options.patientname,filesep,'job_',jobID],'.out',...
+    ' -E ',[options.root,options.patientname,filesep,'job_',jobID],'.err',...
+    ' -c "',[ea_getearoot,filesep,'cluster',filesep,'ea_run_binary.',computer('arch')],'',...
+    ' ','/usr/pubsw/common/matlab/8.6',...
+    ' runcluster ',[options.root,options.patientname,filesep,'job_',jobID],'"'];
+
+if ~exist([ea_getearoot,'cluster',filesep,'ea_run_binary.',computer('arch')],'file');
+mcc('-m','ea_run','-o',['ea_run_binary']);
+movefile('ea_run_binary',['cluster',filesep,'ea_run_binary.',computer('arch')]);
+delete('run_ea_run_binary.sh');
+end
 keyboard
 system(cmdstring);
