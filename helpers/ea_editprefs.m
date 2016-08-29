1581a9945eb6412d5e9c072117ac8131c5a41e20
andreashorn <mail@andreas-horn.de>
2016-08-29 12:08:15 -0400

    Fix nonexisting user prefs.

--- helpers/ea_editprefs.m
+++ helpers/ea_editprefs.m
@@ -1,3 +1,6 @@
 function ea_editprefs(varargin)
 
+if ~exist([ea_gethome,'.ea_prefs.m'],'file')
+    copyfile([ea_getearoot,'ea_prefs_default.m'],[ea_gethome,'.ea_prefs.m']);
+end
 edit([ea_gethome,'.ea_prefs.m']);
