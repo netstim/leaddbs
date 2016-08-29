b65296dbd3df8642efc5bf1d6b5d11f8114064b9
andreashorn <mail@andreas-horn.de>
2016-08-29 13:43:41 -0400

    Clusterfunction that aims to precompile.

--- cluster/ea_run_Martinos_Launchpad_compile.m
+++ cluster/ea_run_Martinos_Launchpad_compile.m
@@ -1,4 +1,4 @@
-function ea_run_Martinos_Launchpad(options)
+function ea_run_Martinos_Launchpad_compile(options)
 
 % This is a function that runs code for each subject on a cluster. It needs
 % to be adapted to suit your needs. Then, you can "Export code" using
@@ -9,6 +9,7 @@ jobID=ea_generate_guid;
 options.spmdir=spm('dir');
 save([options.root,options.patientname,filesep,'job_',jobID],'options')
 setenv('ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS','1')
-cmdstring=['pbsubmit -q matlab -O ',[options.root,options.patientname,filesep,'job_',jobID],'.out -E ',[options.root,options.patientname,filesep,'job_',jobID],'.err -c "matlab.new -singleCompThread -nodisplay -r "''ea_run runcluster ',[options.root,options.patientname,filesep,'job_',jobID],'''"'];
 
+cmdstring=['pbsubmit -q highio -O ',[options.root,options.patientname,filesep,'job_',jobID],'.out -E ',[options.root,options.patientname,filesep,'job_',jobID],'.err -c "matlab.new -singleCompThread -nodisplay -r "''ea_run runcluster ',[options.root,options.patientname,filesep,'job_',jobID],'''"'];
+keyboard
 system(cmdstring);
