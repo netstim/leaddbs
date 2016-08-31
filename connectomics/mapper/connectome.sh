faeaa19a953031d1c6bf9ba8ca6f0ea096c6dfde
andreashorn <mail@andreas-horn.de>
2016-08-31 17:10:55 -0400

    Amend

--- connectomics/mapper/connectome.sh
+++ connectomics/mapper/connectome.sh
@@ -108,6 +108,7 @@ if [ -z $filename ]
 
         filebasename=$(basename "$filename")
         extension="${filebasename##*.}"
+        echo $extension
         if [ $extension == ".txt" ] && [ $command == "seed" ]; then # multiple seeds, read in and supply separately.
             while IFS='' read -r line || [[ -n "$line" ]]; do
                 cmd="/autofs/cluster/nimlab/connectomes/software/lead_dbs/connectomics/mapper/run_cs_conseed.sh /usr/pubsw/common/matlab/8.6 $dofMRI $dodMRI /autofs/cluster/nimlab/connectomes/ $line $command $writesingle $outputfolder $maskname"
