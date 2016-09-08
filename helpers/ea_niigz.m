71b26ea3678000972c0bbb2aff14fedb1e068f81
andreashorn <mail@andreas-horn.de>
2016-09-08 12:07:04 -0400

    No error but warning in duplicate .nii/.nii.gz pairs.

--- helpers/ea_niigz.m
+++ helpers/ea_niigz.m
@@ -15,7 +15,7 @@ nii=dir(fullfile(pth,[base,'.nii']));
 niigz=dir(fullfile(pth,[base,'.nii.gz']));
 
 if ~isempty(nii) && ~isempty(niigz)
-    ea_error(['Duplicate .nii/.nii.gz files detected. Aborting. ',fullfile(pth,base),'.']);
+    warning(['Duplicate .nii/.nii.gz files detected. ',fullfile(pth,base),'.']);
 elseif isempty(nii) && ~isempty(niigz)
     fn=fullfile(pth,[base,'.nii.gz']);
 elseif ~isempty(nii) && isempty(niigz)
