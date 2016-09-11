e5b6b3bfbafd0903c16377a70497edf7f4eca699
Ningfei Li <ningfei.li@gmail.com>
2016-09-11 19:53:34 +0200

    Fix ea_downloadasset Dataset installation from the menu button should work now (except for groupconnectome, still need to fix the error induced by non-exist dmri folder)

--- ea_checkinstall.m
+++ ea_checkinstall.m
@@ -17,38 +17,57 @@ switch cmd
     case 'bigbrain'
         checkf=[earoot,'templates',filesep,'bigbrain_2015_100um_bb.nii'];
         force=ea_alreadyinstalled(checkf,checkonly,robot);
-        if checkonly;            success=~force; return; end
-                if force==-1;      success=-1;       return; end
+        if checkonly;
+            success=~force;
+            return;
+        end
+        if force==-1;
+            success=-1;
+            return;
+        end
+
         if ~exist(checkf,'file') || force
             success=ea_downloadasset('Bigbrain 100um subcortical',...
                 [earoot,'templates',filesep,'bigbrain_2015_100um_bb.nii.gz'],...
-                'http://www.lead-dbs.org/release/data/bigbrain_2015_100um_bb.nii.gz');
+                'bigbrain');
         else
             disp('BigBrain is installed.')
         end
     case 'macaque'
         checkf=[earoot,'toolbox',filesep,'macaque'];
         force=ea_alreadyinstalled(checkf,checkonly,robot);
-                if checkonly;            success=~force; return; end
-                                if force==-1;      success=-1;       return; end
+        if checkonly;
+            success=~force;
+            return;
+        end
+        if force==-1;
+            success=-1;
+            return;
+        end
 
         if ~exist(checkf,'file') || force
             success=ea_downloadasset('Lead-DBS Macaque toolbox',...
                 [earoot,'toolbox',filesep,'macaque.zip'],...
-                'http://www.lead-dbs.org/release/download.php?id=macaque');
+                'macaque');
         else
             disp('Macaque toolbox is installed.')
         end
     case 'groupconnectome2013'
         checkf=[ea_getconnectomebase('dmri'),'Groupconnectome (Horn 2013) full.mat'];
         force=ea_alreadyinstalled(checkf,checkonly,robot);
-                if checkonly;            success=~force; return; end
-                                if force==-1;      success=-1;       return; end
+        if checkonly;
+            success=~force;
+            return;
+        end
+        if force==-1;
+            success=-1;
+            return;
+        end
 
         if ~exist(checkf,'file') || force
             success=ea_downloadasset('structural group connectome (Horn 2013)',...
                 [ea_getconnectomebase('dmri'),'groupconnectome2013.zip'],...
-                'http://www.lead-dbs.org/release/download.php?id=group');
+                'group');
         else
             disp('Group Connectome (Horn 2013) is installed.')
         end
@@ -57,17 +76,19 @@ switch cmd
 end
 
 
-function success=ea_downloadasset(assetname,destination,URL)
+function success=ea_downloadasset(assetname,destination,id)
+downloadurl = 'http://www.lead-dbs.org/release/download.php';
 success=1;
 disp(['Downloading ',assetname,'...'])
 try
-    websave(destination,URL);
+    webopts=weboptions('Timeout',5);
+    websave(destination,downloadurl,'id',id,webopts);
     unzip(destination);
 catch
     success=0;
 end
 try delete(destination); end
-try delete([destination,'.html']); end
+% try delete([destination,'.html']); end
 
 
 function force=ea_alreadyinstalled(checkf,checkonly,robot)
