68cb9b0ad609c37277e4dc39e3e276b5ccb6e18e
Ningfei Li <ningfei.li@gmail.com>
2016-10-18 20:56:14 +0200

    Update ea_dcm2niix dcm2niix binaries support custom output directory now

--- ext_libs/dcm2nii/ea_dcm2niix.m
+++ ext_libs/dcm2nii/ea_dcm2niix.m
@@ -1,48 +1,28 @@
-function ea_dcm2niix(inputimage,ofolder)
+function ea_dcm2niix(dicomdir, outdir)
 % Wrapper for dcm2niix
+
+if nargin < 2
+    outdir = dicomdir;
+end
+
+if ~exist(outdir, 'dir')
+    mkdir(outdir);
+end
 
 ea_libs_helper;
 
 basedir = [fileparts(mfilename('fullpath')), filesep];
 
 if ispc
-    dcm2nii = [basedir, 'dcm2niix.exe'];
+    dcm2niix = [basedir, 'dcm2niix.exe'];
 else
-    dcm2nii = [basedir, 'dcm2niix.', computer('arch')];
+    dcm2niix = [basedir, 'dcm2niix.', computer('arch')];
 end
 
-cmd=[dcm2nii, ' -o ',ofolder,' -z n -x y ',inputimage];
+cmd=[dcm2niix, ' -o ', outdir, ' -z n -x y ', dicomdir];
 
 if ~ispc
     system(['bash -c "', cmd, '"']);
 else
     system(cmd);
 end
-
-
-% -o option seems not to work in dcm2niix yet. Move files manually:
-try
-di=dir([inputimage,'*.nii']);
-for d=1:length(di)
-    movefile([inputimage,di(d).name],[ofolder,di(d).name]);
-end
-
-di=dir([inputimage,'*.bval']);
-for d=1:length(di)
-    movefile([inputimage,di(d).name],[ofolder,di(d).name]);
-end
-
-di=dir([inputimage,'*.bvec']);
-for d=1:length(di)
-    movefile([inputimage,di(d).name],[ofolder,di(d).name]);
-end
-end
-
-% try
-%     delete([pth,filesep,'o',fn,ext]);
-%     movefile([pth,filesep,'co',fn,ext],inputimage);
-% catch
-%     try
-%         movefile([pth,filesep,'c',fn,ext],inputimage);
-%     end
-% end
