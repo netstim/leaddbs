5077cf0c1dcf9cf544f23c7af94866184c707a79
Ningfei Li <ningfei.li@gmail.com>
2016-11-05 16:48:36 +0100

    Fix ea_restoreprefs UI callbacks are automatically passed two args
    Signed-off-by: Ningfei Li <ningfei.li@gmail.com>

--- helpers/ea_restoreprefs.m
+++ helpers/ea_restoreprefs.m
@@ -1,4 +1,4 @@
-function ea_restoreprefs
+function ea_restoreprefs(varargin)
 
 answer=questdlg('Do you really want to reset your preferences to default values?','Reset Lead-DBS preferences','Cancel','Sure','Cancel');
 if strcmp(answer,'Sure')
