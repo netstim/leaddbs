b1d3920b6d87efc6ed9d5b49b1799837d1cf875a
Ningfei Li <ningfei.li@gmail.com>
2016-11-20 14:50:50 +0100

    Add hotfix function for LEAD

---
+++ ea_hotfix.m
@@ -0,0 +1,80 @@
+function ea_hotfix(varargin)
+% lead hotfix
+
+local = ea_getvsn('local');
+web = ea_getvsn('web');
+
+% if local version unknow or outdated, skip applying hotfix
+hotfix = 0;
+if strcmp(local,web) % local version is the latest release, applicable for hotfix
+    hotfix = 1;
+end
+
+earoot=ea_getearoot;
+
+updurl = 'http://www.lead-dbs.org/release/download.php';
+if hotfix
+    try
+        disp('*** Updating LEAD. Please do not quit MATLAB.');
+        mkdir([earoot,'tmp'])
+        disp('Downloading hotfix...');
+        try
+            webopts=weboptions('Timeout',5);
+            websave([earoot,'tmp',filesep,'hotfix.zip'],updurl,'id','hotfix',webopts);
+        catch
+            try
+                urlwrite([updurl,'?id=hotfix'],[earoot,'tmp',filesep,'hotfix.zip'],'Timeout',5);
+            catch
+                info='Download error! Please retry later.';
+                disp(info);
+                msgbox(info,'Update','Error')
+                return
+            end
+        end
+
+        disp('Extracting code...');
+        try
+            unzip([earoot,'tmp',filesep,'hotfix.zip'],[earoot,'tmp',filesep]);
+        catch
+            system(['unzip -q ',earoot,'tmp',filesep,'hotfix.zip -d ', earoot,'tmp']);
+        end
+        delete([earoot,'tmp',filesep,'hotfix.zip']);
+
+        disp('Deleting outdated code...');
+        try
+            dfid = fopen([earoot,'tmp',filesep,'hotfix',filesep,'DELETE']);
+            dels=textscan(dfid,'%s');
+            fclose(dfid);
+            for f=1:length(dels{1})
+                if isdir([earoot,dels{1}{f}])
+                    rmdir([earoot,dels{1}{f}],'s')
+                else
+                    delete([earoot,dels{1}{f}])
+                end
+            end
+            delete([earoot,'tmp',filesep,'hotfix',filesep,'DELETE'])
+        catch
+            disp('Error while deleting some files. You may ignore this.');
+        end
+
+        disp('Copying new code...');
+        copyfile([earoot,'tmp',filesep,'hotfix',filesep,'*'],earoot,'f');
+        copyfile([earoot,'tmp',filesep,'hotfix',filesep,'.version.txt'],earoot,'f');
+        disp('Cleaning up...');
+        rmdir([earoot,'tmp'],'s')
+        disp('Done.');
+
+        disp('Restarting LEAD.');
+        lead;
+        disp('*** Update finished.');
+    catch
+        info=sprintf(['Hotfix does not exist or failed to apply hotfix!\n',...
+                      'Please wait for the next release.']);
+        disp(info);
+        msgbox(info,'Update','Error');
+    end
+else
+    info=sprintf('Local version is not applicable for hotfix!');
+    disp(info);
+    msgbox(info,'Update','Help');
+end
