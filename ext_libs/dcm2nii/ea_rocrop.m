23c85b2757481f7947cd7691220e225fd3e9e044
andreashorn <mail@andreas-horn.de>
2016-12-13 13:15:11 -0500

    Only try rocrop 10 times.

--- ext_libs/dcm2nii/ea_rocrop.m
+++ ext_libs/dcm2nii/ea_rocrop.m
@@ -14,11 +14,23 @@ ea_dcm2nii(inputimage, [pth, filesep, 'temp.nii']);
 tempf=dir([pth,filesep,'temp.nii']);
 tempfsize = tempf.bytes/1024;
 
+cnt=1; % try ten times, if doesnt work, continue.
+failed=0;
 % To be safe, retry when the file size smaller than 10 KB
 while tempfsize < 10
     disp('dcm2nii returns corrupted file, retrying...')
     ea_dcm2nii(inputimage, [pth, filesep, 'temp.nii']);
     tempf=dir([pth,filesep,'temp.nii']);
     tempfsize = tempf.bytes/1024;
+    cnt=cnt+1;
+    if cnt>10
+        failed=1;
+        warning('Giving up. Please check input files, manually. Attempting to continue.');
+       break 
+    end
 end
+if ~failed
 movefile([pth,filesep,'temp.nii'],inputimage);
+else
+    delete([pth,filesep,'temp.nii']);
+end
