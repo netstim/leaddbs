08f34c37123e400623408dadafb031922f5d06d1
siobhanewert <siobhanewert@gmx.net>
2016-12-14 21:51:44 -0500

    Added threshold variable as function input to modify brain outline estimate

--- ext_libs/fsl/ea_bet.m
+++ ext_libs/fsl/ea_bet.m
@@ -1,4 +1,4 @@
-function ea_bet(inputimage, outputmask, outputimage)
+function ea_bet(inputimage, outputmask, outputimage, fraintthreshold)
 % Wrapper for bet2 brain extraction
 
 % Do not output brain mask by default
@@ -7,8 +7,18 @@ if nargin < 2
 end
 
 % overwrite the input image
-if nargin < 3
+if ~exist('outputimage','var') 
     outputimage = inputimage;
+else
+    if isempty(outputimage)
+        outputimage=inputimage;
+    end
+end
+
+% If no threshold is set, the default fractional intensity threshold (0->1) is set
+% to default=0.5 (as set in FSL binary), smaller values give larger brain outline estimates
+if nargin < 4
+    fraintthreshold = 0.5;
 end
 
 inputimage = ea_path_helper(ea_niigz(inputimage));
@@ -32,6 +42,8 @@ cmd = [BET, ...
 if outputmask
     cmd = [cmd, ' -m'];
 end
+
+cmd = [cmd, ' -f ' ,num2str(fraintthreshold)];
 
 setenv('FSLOUTPUTTYPE','NIFTI');
 if ~ispc
