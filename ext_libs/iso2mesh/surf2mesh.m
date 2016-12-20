83d830b575c543121f567f4ebc3898baed872b56
andreashorn <mail@andreas-horn.de>
2016-12-20 11:33:56 -0500

    Decreased size tolerance once more.

--- ext_libs/iso2mesh/surf2mesh.m
+++ ext_libs/iso2mesh/surf2mesh.m
@@ -108,7 +108,8 @@ end
 
 
 if(isempty(cmdopt))
-  system([' "' mcpath('tetgen_latest') exesuff '" -A -T1e-09 -q2 -a ' num2str(maxvol) ' ' moreopt ' "' mwpath('post_vmesh.poly') '"']);
+
+  system([' "' mcpath('tetgen_latest') exesuff '" -A -T1e-10 -q2 -a ' num2str(maxvol) ' ' moreopt ' "' mwpath('post_vmesh.poly') '"']);
 else
   system([' "' mcpath('tetgen') exesuff '" ' cmdopt ' "' mwpath('post_vmesh.poly') '"']);
 end
@@ -121,11 +122,21 @@ try
     fprintf(1,'volume mesh generation is complete\n');
     
 catch
-    node=[];
-    elem=[];
-    face=[];
-    success=0;
+    keyboard
+    system([' "' mcpath('tetgen_latest') exesuff '" -A -d -T1e-10 -q2 -a ' num2str(maxvol) ' ' moreopt ' "' mwpath('post_vmesh.poly') '"']);
     
+    system([' "' mcpath('tetgen_latest') exesuff '" -A -T1e-10 -q2 -a ' num2str(maxvol) ' ' moreopt ' ' mwpath('post_vmesh.1.face'),' ',mwpath('post_vmesh.1.node') '']);
+    try
+        
+        [node,elem,face]=readtetgen(mwpath('post_vmesh.2'));
+        fprintf(1,'volume mesh generation is complete\n');
+    catch
+        
+        node=[];
+        elem=[];
+        face=[];
+        success=0;
+    end
 end
 
 function sweeptempdir
