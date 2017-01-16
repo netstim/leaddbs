7f715d8dab531ed2e7b1cd228a1bb880f6344b20
akapp <arikappel@gmail.com>
2017-01-16 14:42:46 -0500

    templates/space/mni_icbm2009b/

--- ea_showcortex.m
+++ ea_showcortex.m
@@ -60,11 +60,11 @@ for nativemni=nm % switch between native and mni space.
             % root=[options.root,options.patientname,filesep];
             % adir=[root,''];
             root=[options.root,options.patientname,filesep];
-            adir=[root,'templates/space/'];
+            adir=[root,'templates/space/mni_icbm2009b/'];
             reslice='yes';
         case 1 % template cortex in mni space
             root=[options.earoot,mcr];
-            adir=[root,'templates/space/'];
+            adir=[root,'templates/space/mni_icbm2009b/'];
             reslice='no';
         case 2 % patient cortex in native space
             root=[options.root,options.patientname,filesep];
