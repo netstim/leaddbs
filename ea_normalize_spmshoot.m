84e312dcbfd89687107cdf3b9e00b31e256d0799
andreashorn <mail@andreas-horn.de>
2017-01-16 16:33:13 -0500

    Amend

--- ea_normalize_spmshoot.m
+++ ea_normalize_spmshoot.m
@@ -86,27 +86,27 @@ end
 % matlabbatch{1}.spm.tools.dartel.warp1.settings.param(1).its = 3;
 % matlabbatch{1}.spm.tools.dartel.warp1.settings.param(1).rparam = [4 2 1e-06];
 % matlabbatch{1}.spm.tools.dartel.warp1.settings.param(1).K = 0;
-% matlabbatch{1}.spm.tools.dartel.warp1.settings.param(1).template = {[ea_space('dartel'),'shootmni_1.nii']};
+% matlabbatch{1}.spm.tools.dartel.warp1.settings.param(1).template = {[ea_space(options,'dartel'),'shootmni_1.nii']};
 % matlabbatch{1}.spm.tools.dartel.warp1.settings.param(2).its = 3;
 % matlabbatch{1}.spm.tools.dartel.warp1.settings.param(2).rparam = [2 1 1e-06];
 % matlabbatch{1}.spm.tools.dartel.warp1.settings.param(2).K = 0;
-% matlabbatch{1}.spm.tools.dartel.warp1.settings.param(2).template = {[ea_space('dartel'),'shootmni_2.nii']};
+% matlabbatch{1}.spm.tools.dartel.warp1.settings.param(2).template = {[ea_space(options,'dartel'),'shootmni_2.nii']};
 % matlabbatch{1}.spm.tools.dartel.warp1.settings.param(3).its = 3;
 % matlabbatch{1}.spm.tools.dartel.warp1.settings.param(3).rparam = [1 0.5 1e-06];
 % matlabbatch{1}.spm.tools.dartel.warp1.settings.param(3).K = 1;
-% matlabbatch{1}.spm.tools.dartel.warp1.settings.param(3).template = {[ea_space('dartel'),'shootmni_3.nii']};
+% matlabbatch{1}.spm.tools.dartel.warp1.settings.param(3).template = {[ea_space(options,'dartel'),'shootmni_3.nii']};
 % matlabbatch{1}.spm.tools.dartel.warp1.settings.param(4).its = 3;
 % matlabbatch{1}.spm.tools.dartel.warp1.settings.param(4).rparam = [0.5 0.25 1e-06];
 % matlabbatch{1}.spm.tools.dartel.warp1.settings.param(4).K = 2;
-% matlabbatch{1}.spm.tools.dartel.warp1.settings.param(4).template = {[ea_space('dartel'),'shootmni_4.nii']};
+% matlabbatch{1}.spm.tools.dartel.warp1.settings.param(4).template = {[ea_space(options,'dartel'),'shootmni_4.nii']};
 % matlabbatch{1}.spm.tools.dartel.warp1.settings.param(5).its = 3;
 % matlabbatch{1}.spm.tools.dartel.warp1.settings.param(5).rparam = [0.25 0.125 1e-06];
 % matlabbatch{1}.spm.tools.dartel.warp1.settings.param(5).K = 4;
-% matlabbatch{1}.spm.tools.dartel.warp1.settings.param(5).template = {[ea_space('dartel'),'shootmni_5.nii']};
+% matlabbatch{1}.spm.tools.dartel.warp1.settings.param(5).template = {[ea_space(options,'dartel'),'shootmni_5.nii']};
 % matlabbatch{1}.spm.tools.dartel.warp1.settings.param(6).its = 3;
 % matlabbatch{1}.spm.tools.dartel.warp1.settings.param(6).rparam = [0.25 0.125 1e-06];
 % matlabbatch{1}.spm.tools.dartel.warp1.settings.param(6).K = 6;
-% matlabbatch{1}.spm.tools.dartel.warp1.settings.param(6).template = {[ea_space('dartel'),'shootmni_6.nii']};
+% matlabbatch{1}.spm.tools.dartel.warp1.settings.param(6).template = {[ea_space(options,'dartel'),'shootmni_6.nii']};
 % matlabbatch{1}.spm.tools.dartel.warp1.settings.optim.lmreg = 0.01;
 % matlabbatch{1}.spm.tools.dartel.warp1.settings.optim.cyc = 3;
 % matlabbatch{1}.spm.tools.dartel.warp1.settings.optim.its = 3;
@@ -117,12 +117,12 @@ matlabbatch{1}.spm.tools.shoot.warp1.images = {
     {[directory,'rc2',options.prefs.prenii_unnormalized,',1']}
     {[directory,'rc3',options.prefs.prenii_unnormalized,',1']}
     }';
-matlabbatch{1}.spm.tools.shoot.warp1.templates = {[ea_space('dartel'),'shootmni_1.nii']
-    [ea_space('dartel'),'shootmni_2.nii']
-    [ea_space('dartel'),'shootmni_3.nii']
-    [ea_space('dartel'),'shootmni_4.nii']
-    [ea_space('dartel'),'shootmni_5.nii']
-    [ea_space('dartel'),'shootmni_6.nii']};
+matlabbatch{1}.spm.tools.shoot.warp1.templates = {[ea_space(options,'dartel'),'shootmni_1.nii']
+    [ea_space(options,'dartel'),'shootmni_2.nii']
+    [ea_space(options,'dartel'),'shootmni_3.nii']
+    [ea_space(options,'dartel'),'shootmni_4.nii']
+    [ea_space(options,'dartel'),'shootmni_5.nii']
+    [ea_space(options,'dartel'),'shootmni_6.nii']};
   jobs{1}=matlabbatch;                                            
 
 
