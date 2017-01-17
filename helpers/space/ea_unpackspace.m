04ff1b23961fef88ecb05eb2025c735e6229e882
andreashorn <mail@andreas-horn.de>
2017-01-17 18:37:56 -0500

    Amend

--- helpers/space/ea_unpackspace.m
+++ helpers/space/ea_unpackspace.m
@@ -1,7 +1,12 @@
 function ea_unpackspace
 
+disp(['Unpacking space ',ea_getspace,'...']);
+disp('This could take a while...');
 gunzip([ea_space,'*.nii.gz']);
 delete([ea_space,'*.nii.gz']);
-            ea_genauxspace([],[],ea_getspace);
+ea_genauxspace([],[],ea_getspace);
 
-            delete([ea_space,'packed']);
+delete([ea_space,'packed']);
+
+disp('Unpacking done.');
+
