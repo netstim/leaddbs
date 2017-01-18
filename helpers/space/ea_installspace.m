b23425c7afa6bc094b5018b0f3a7ca2843d442b5
andreashorn <mail@andreas-horn.de>
2017-01-18 17:32:27 -0500

    fixed install script for spaces

--- helpers/space/ea_installspace.m
+++ helpers/space/ea_installspace.m
@@ -5,15 +5,21 @@ disp(['Installing / Downloading space ',ea_getspace,'...']);
 disp('This could take a while...');
 downloadurl = 'http://www.lead-dbs.org/release/download.php';
     webopts=weboptions('Timeout',5);
-    destination=[ea_space,'data.zip'];
+    destination=[ea_space,'../data_download.zip'];
     try
         websave(destination,downloadurl,'id',ea_getspace,webopts);
     catch
         urlwrite([downloadurl,'?id=',ea_getspace],destination,'Timeout',5);
     end
 disp('Download done. Will now continue building/unpacking space.');
-unzip(destination,ea_space);
+
+space=ea_space;
+rmdir(space,'s');
+unzip(destination,space);
+[~,spacename]=fileparts(fileparts(space));
+movefile([space,spacename],[space(1:end-1),'_tmp']);
+rmdir(space,'s');
+movefile([space(1:end-1),'_tmp'],[space(1:end-1)]);
 delete(destination);
-delete([ea_space,'need_install']);
 ea_unpackspace;
 
