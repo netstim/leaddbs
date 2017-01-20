aeeaeeb5ccfbe18f2bbb5f65805afc509ad72ef8
Ningfei Li <ningfei.li@gmail.com>
2017-01-20 20:34:29 +0100

    Fix moving electrode templates in ea_compat_data.

--- ea_compat_data.m
+++ ea_compat_data.m
@@ -3,10 +3,9 @@ if strcmp(ea_getspace,'MNI_ICBM_2009b_NLIN_ASYM')
     if exist([ea_getearoot,'templates',filesep,'mni_hires_t1.nii'],'file');
         movefile([ea_getearoot,'templates'], [ea_getearoot,'templates_temp']);
         mkdir([ea_getearoot,'templates',filesep,'space',filesep]);
-        movefile([ea_getearoot,'templates_temp'],[ea_getearoot,'templates',filesep,'space',filesep,'MNI_ICBM_2009b_NLIN_ASYM']);
-        %rmdir([ea_getearoot,'templates',filesep,'space',filesep,'MNI_ICBM_2009b_NLIN_ASYM',filesep,'space'],'s')
-        movefile([ea_getearoot,'atlases'],[ea_getearoot,'templates',filesep,'space',filesep,'MNI_ICBM_2009b_NLIN_ASYM',filesep,'atlases']);
-        
+        movefile([ea_getearoot,'templates_temp'],ea_space);
+        movefile([ea_getearoot,'atlases'],[ea_space,'atlases']);
+
         movefile([ea_space,'mni_hires_t1.nii'],[ea_space,'t1.nii']);
         movefile([ea_space,'mni_hires_t2.nii'],[ea_space,'t2.nii']);
         movefile([ea_space,'mni_hires_pd.nii'],[ea_space,'pd.nii']);
@@ -18,17 +17,26 @@ if strcmp(ea_getspace,'MNI_ICBM_2009b_NLIN_ASYM')
         movefile([ea_space,'mni_hires_distal.nii'],[ea_space,'distal.nii']);
         movefile([ea_space,'mni_wires.mat'],[ea_space,'wires.mat']);
     end
-    
+
     if ~exist([ea_space,'norm_mapping.mat'],'file')
         spacedef=ea_gendefspacedef;
         save([ea_space,'ea_space_def.mat'],'spacedef');
-        
+
     end
-    
+
     if exist([ea_space,'distal.nii'],'file')
         movefile( [ea_space,'distal.nii'],[ea_space,'atlas.nii']);
     end
+
     if exist([ea_space,'TPM_Lorio_Draganski.nii'])
         movefile([ea_space,'TPM_Lorio_Draganski.nii'],[ea_getearoot,'templates',filesep,'TPM_Lorio_Draganski.nii']);
+    end
+
+    if exist([ea_space,'electrode_contacts'],'dir')
+        movefile( [ea_space,'electrode_contacts'],[ea_getearoot,'templates']);
+    end
+
+    if exist([ea_space,'electrode_models'],'dir')
+        movefile( [ea_space,'electrode_models'],[ea_getearoot,'templates']);
     end
 end
