19677ca17ae9d109dac6b6c59e9207c7ea189b6d
andreashorn <mail@andreas-horn.de>
2017-01-20 20:50:10 -0500

    Fix in compat_data

--- ea_compat_data.m
+++ ea_compat_data.m
@@ -1,9 +1,9 @@
-function ea_compat_data
 if strcmp(ea_getspace,'MNI_ICBM_2009b_NLIN_ASYM')
     if exist([ea_getearoot,'templates',filesep,'mni_hires_t1.nii'],'file');
         movefile([ea_getearoot,'templates'], [ea_getearoot,'templates_temp']);
         mkdir([ea_getearoot,'templates',filesep,'space',filesep]);
-        movefile([ea_getearoot,'templates_temp'],ea_space);
+        
+        movefile([ea_getearoot,'templates_temp'],[ea_getearoot,'templates',filesep,'space',filesep,ea_getspace]); % do not use ea_space in this line since wont be defined and result in error.
         movefile([ea_getearoot,'atlases'],[ea_space,'atlases']);
 
         movefile([ea_space,'mni_hires_t1.nii'],[ea_space,'t1.nii']);
@@ -11,14 +11,14 @@ if strcmp(ea_getspace,'MNI_ICBM_2009b_NLIN_ASYM')
         movefile([ea_space,'mni_hires_pd.nii'],[ea_space,'pd.nii']);
         movefile([ea_space,'mni_hires_fa.nii'],[ea_space,'fa.nii']);
         movefile([ea_space,'mni_hires_bb.nii'],[ea_space,'bb.nii']);
-        movefile([ea_space,'mni_hires_c1mask.nii'],[ea_space,'c1mask.nii']);
-        movefile([ea_space,'mni_hires_c2mask.nii'],[ea_space,'c2mask.nii']);
-        movefile([ea_space,'TPM_2009b.nii'],[ea_space,'TPM.nii']);
-        movefile([ea_space,'mni_hires_distal.nii'],[ea_space,'distal.nii']);
+        try movefile([ea_space,'mni_hires_c1mask.nii'],[ea_space,'c1mask.nii']); end
+        try movefile([ea_space,'mni_hires_c2mask.nii'],[ea_space,'c2mask.nii']); end
+        try movefile([ea_space,'TPM_2009b.nii'],[ea_space,'TPM.nii']); end
+        movefile([ea_space,'mni_hires_distal.nii'],[ea_space,'atlas.nii']);
         movefile([ea_space,'mni_wires.mat'],[ea_space,'wires.mat']);
     end
 
-    if ~exist([ea_space,'norm_mapping.mat'],'file')
+    if ~exist([ea_space,'ea_space_def.mat'],'file')
         spacedef=ea_gendefspacedef;
         save([ea_space,'ea_space_def.mat'],'spacedef');
 
@@ -28,7 +28,7 @@ if strcmp(ea_getspace,'MNI_ICBM_2009b_NLIN_ASYM')
         movefile( [ea_space,'distal.nii'],[ea_space,'atlas.nii']);
     end
 
-    if exist([ea_space,'TPM_Lorio_Draganski.nii'])
+    if exist([ea_space,'TPM_Lorio_Draganski.nii'],'file')
         movefile([ea_space,'TPM_Lorio_Draganski.nii'],[ea_getearoot,'templates',filesep,'TPM_Lorio_Draganski.nii']);
     end
 
