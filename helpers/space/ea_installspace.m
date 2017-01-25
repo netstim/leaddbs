2079233e09a3705177324ec22af7d18ba15036e5
andreashorn <mail@andreas-horn.de>
2017-01-25 09:28:25 -0500

    Fix compat ea_installspace reported by jt

--- helpers/space/ea_installspace.m
+++ helpers/space/ea_installspace.m
@@ -3,9 +3,9 @@ function ea_installspace
 disp(['Installing / Downloading space ',ea_getspace,'...']);
 disp('This could take a while...');
 downloadurl = 'http://www.lead-dbs.org/release/download.php';
-    webopts=weboptions('Timeout',5);
     destination=[ea_space,'../data_download.zip'];
     try
+        webopts=weboptions('Timeout',5);
         websave(destination,downloadurl,'id',ea_getspace,webopts);
     catch
         urlwrite([downloadurl,'?id=',ea_getspace],destination,'Timeout',5);
