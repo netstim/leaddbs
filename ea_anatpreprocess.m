3c943f09233e5eafae25e7fc874170bae76d2b5e
siobhanewert <siobhanewert@gmx.net>
2017-02-06 16:46:23 -0500

    Skip rocrop but run biasfield correction, images should be rocropped by default when DICOMs are imported using Lead DBS

--- ea_anatpreprocess.m
+++ ea_anatpreprocess.m
@@ -2,5 +2,5 @@ function ea_anatpreprocess(fpth)
 % Preprocess of anat image
 % Currently includes: reorientation & cropping and bias field correction
 
-ea_rocrop(fpth);
+%ea_rocrop(fpth);
 ea_bias_field_correction(fpth)
