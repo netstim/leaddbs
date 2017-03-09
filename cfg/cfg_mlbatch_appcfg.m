ea5de83d1a3d2a64d018ba088229f6d0212b0b89
andreashorn <mail@andreas-horn.de>
2017-03-09 13:48:19 -0500

    Deleted cfg folder

--- cfg/cfg_mlbatch_appcfg.m
+++
@@ -1,5 +0,0 @@
-function [cfg, def] = cfg_mlbatch_appcfg
-% Auto-configuration file for batch system. cfg_util will scan the MATLAB
-% path for cfg_mlbatch_appcfg files and evaluate all of them.
-cfg = ea_cfg; % impexp_cfg_NiftiMrStruct]
-def = [];
