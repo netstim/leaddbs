856d978ab692f55eee6c2e079e0259a732b2661c
Ningfei Li <ningfei.li@gmail.com>
2017-04-18 17:11:33 +0200

    Minor tweak of 'ea_anatomycontrol' figure.

--- ea_anatomycontrol.m
+++ ea_anatomycontrol.m
@@ -561,7 +561,6 @@ function cortexalpha_CreateFcn(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    empty - handles not created until after all CreateFcns called
 
-set(hObject,'Value')
 % Hint: slider controls usually have a light gray background.
 if isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor',[.9 .9 .9]);
