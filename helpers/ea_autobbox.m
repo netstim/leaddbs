5e9fa94fed4c577f3fe6a9a414452122d253b8ec
Ningfei Li <ningfei.li@gmail.com>
2017-05-02 15:25:28 +0200

    Add '.gz' image support to 'ea_autobbox'.

--- helpers/ea_autobbox.m
+++ helpers/ea_autobbox.m
@@ -2,7 +2,7 @@ function [bbox, BW] = ea_autobbox(image, margin)
 % Calculate the minimum bounding box and binary mask of the nifti image
 
 if nargin < 2
-    margin = 3;	% add margin to the calculated bounding box, unit in voxel
+    margin = 5;	% add margin to the calculated bounding box, unit in voxel
 end
 
 nii = load_nii(image);
@@ -14,5 +14,13 @@ BW = img > max(img(:)) * th;
 
 [i,j,k] = ind2sub(size(BW), find(BW));
 
-bbox = round([ea_vox2mm([min(i)-margin, min(j)-margin, min(k)-margin], image);
-              ea_vox2mm([max(i)+margin, max(j)+margin, max(k)+margin], image)]);
+if strcmp(image(end-2:end), '.gz')
+    gunzip(image);
+    transform = spm_get_space(image(1:end-3));
+    delete(image(1:end-3));
+else
+    transform = spm_get_space(image);
+end
+
+bbox = round([ea_vox2mm([min(i)-margin, min(j)-margin, min(k)-margin], transform);
+              ea_vox2mm([max(i)+margin, max(j)+margin, max(k)+margin], transform)]);
