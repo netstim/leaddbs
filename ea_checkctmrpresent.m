886de2221e0232c9cf8bd5bf50088d2235f8fe42
Ningfei Li <ningfei.li@gmail.com>
2017-06-06 14:07:45 +0200

    Fix statusbar.

--- ea_checkctmrpresent.m
+++ ea_checkctmrpresent.m
@@ -12,56 +12,61 @@ end
 prefs=ea_prefs(patientname);
 
 modality=zeros(2,1);
+
 % check for unnormalized images first:
 % CT
 if exist([uipatdir{1},filesep,prefs.rawctnii_unnormalized],'file')
-    try set(handles.statusone,'String','Raw CT file found. Please coregister CT to preoperative MR first.'); end
+    status = 'Raw CT file found. Please coregister CT to preoperative MR first.';
     modality(2)=1;
 end
 if exist([uipatdir{1},filesep,prefs.ctnii_coregistered],'file')
-    try set(handles.statusone,'String','Coregistered CT file found. Set normalize option.'); end
+    status = 'Coregistered CT file found. Set Normalization option.';
     modality(2)=1;
 end
+
 % MR
 if exist([uipatdir{1},filesep,prefs.tranii_unnormalized],'file')
-    try set(handles.statusone,'String','Unnormalized MR-volumes found. Set normalize option.'); end
+    status = 'Unnormalized MR-volumes found. Set Normalization option.';
     modality(1)=1;
 end
-% MR & CT
-if exist([uipatdir{1},filesep,prefs.tranii_unnormalized],'file') && exist([uipatdir{1},filesep,prefs.cornii_unnormalized],'file') && exist([uipatdir{1},filesep,prefs.rawctnii_unnormalized],'file')
-    try set(handles.statusone,'String','Unnormalized MR-volumes and raw CT volume found. Set normalize option or coregister CT.'); end
-    modality(1:2)=1;
-end
-if exist([uipatdir{1},filesep,prefs.tranii_unnormalized],'file') && exist([uipatdir{1},filesep,prefs.cornii_unnormalized],'file') && exist([uipatdir{1},filesep,prefs.ctnii_coregistered],'file')
-    try set(handles.statusone,'String','Unnormalized MR-volumes and coregistered CT file found. Set normalize option.'); end
-    modality(1:2)=1;
-end
 
+% MR & CT
+if exist([uipatdir{1},filesep,prefs.tranii_unnormalized],'file') && exist([uipatdir{1},filesep,prefs.rawctnii_unnormalized],'file')
+    status = 'Unnormalized MR-volumes and raw CT volume found. Set Normalization option or Coregister CT.';
+    modality(1:2)=1;
+end
+if exist([uipatdir{1},filesep,prefs.tranii_unnormalized],'file') && exist([uipatdir{1},filesep,prefs.ctnii_coregistered],'file')
+    status = 'Unnormalized MR-volumes and coregistered CT file found. Set Normalization option.';
+    modality(1:2)=1;
+end
 
 % Now check for normalized images:
 if exist([uipatdir{1},filesep,prefs.ctnii],'file')
     try
         load([uipatdir{1},filesep,'ea_normmethod_applied.mat'])
-        nmused=eval([norm_method_applied,'(''check'')']);
-        try set(handles.statusone,'String',['CT-volumes have been normalized with: ',nmused]); end
+        nmused=eval([norm_method_applied{1},'(''check'')']);
+        status = ['CT-volumes have been normalized with: ',nmused];
     catch
-        try set(handles.statusone,'String','Normalized CT-volumes found.'); end
+        status = 'Normalized CT-volumes found.';
     end
     modality(2)=1;
 end
 
-if exist([uipatdir{1},filesep,prefs.tranii],'file') && exist([uipatdir{1},filesep,prefs.cornii],'file')
+if exist([uipatdir{1},filesep,prefs.tranii],'file')
     try
         load([uipatdir{1},filesep,'ea_normmethod_applied.mat'])
-        nmused=eval([norm_method_applied,'(''check'')']);
-        try set(handles.statusone,'String',['MR-volumes have been normalized with: ',nmused]); end
+        nmused=eval([norm_method_applied{1},'(''check'')']);
+        status = ['MR-volumes have been normalized with: ',nmused];
     catch
-        try set(handles.statusone,'String','Normalized MR-volumes found.'); end
+        status = 'Normalized MR-volumes found.';
     end
     modality(1)=1;
 end
 
-if exist([uipatdir{1},filesep,prefs.tranii],'file') && exist([uipatdir{1},filesep,prefs.cornii],'file') && exist([uipatdir{1},filesep,prefs.ctnii],'file')
-    try set(handles.statusone,'String','Normalized MR- and CT-volumes found.'); end
+if exist([uipatdir{1},filesep,prefs.tranii],'file') && exist([uipatdir{1},filesep,prefs.ctnii],'file')
+    status = 'Normalized MR- and CT-volumes found.';
     modality(1:2)=1;
 end
+
+set(handles.statusone,'String',status);
+set(handles.statusone,'TooltipString',status);
