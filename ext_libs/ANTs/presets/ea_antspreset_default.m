6645150fc5c8b325c7c70b9efcd0702672907c57
andreashorn <mail@andreas-horn.de>
2017-07-02 21:10:08 +0200

    Refine last commit

--- ext_libs/ANTs/presets/ea_antspreset_default.m
+++ ext_libs/ANTs/presets/ea_antspreset_default.m
@@ -29,17 +29,17 @@ end
 % Convergence
 apref.convergence.rigid='[500x250x120x0,1e-6,10]'; % Rigid convergence params
 apref.convergence.affine='[500x250x120x0,1e-6,10]'; % Affine convergence params
-apref.convergence.syn='[200x50x25,1e-6,10]'; % SyN convergence params
-apref.convergence.scrf='[200x50x25,1e-6,10]'; % SyN subcortical focus stage convergence params
+apref.convergence.syn='[200x50x25x0,1e-6,10]'; % SyN convergence params
+apref.convergence.scrf='[200x50x25x0,1e-6,10]'; % SyN subcortical focus stage convergence params
 
 % Affine Convergence
-apref.shrinkfactors.rigid='12x8x4x2'; % Rigid shrink factors
-apref.shrinkfactors.affine='12x8x4x2'; % Affine shrink factors
-apref.shrinkfactors.syn='6x4x2'; % SyN shrink factors
-apref.shrinkfactors.scrf='4x2x1'; % SyN subcortical focus stage shrink factors
+apref.shrinkfactors.rigid='12x8x4x1'; % Rigid shrink factors
+apref.shrinkfactors.affine='12x8x4x1'; % Affine shrink factors
+apref.shrinkfactors.syn='6x4x2x1'; % SyN shrink factors
+apref.shrinkfactors.scrf='4x2x2x1'; % SyN subcortical focus stage shrink factors
 
 % Smoothing Sigmas
 apref.smoothingsigmas.rigid='4x3x2x1vox'; % Rigid Smoothing sigmas
 apref.smoothingsigmas.affine='4x3x2x1vox'; % Affine Smoothing sigmas
-apref.smoothingsigmas.syn='3x2x1vox'; % SyN Smoothing sigmas
-apref.smoothingsigmas.scrf='2x1x0vox'; % SyN subcortical focus stage smoothing sigmas
+apref.smoothingsigmas.syn='3x2x1x1vox'; % SyN Smoothing sigmas
+apref.smoothingsigmas.scrf='2x1x0x0vox'; % SyN subcortical focus stage smoothing sigmas
