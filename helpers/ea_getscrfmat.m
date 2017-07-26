0c8d9c02290cd0f3d28cd17e35d722d274ddc425
Chadwick Boulay <chadwick.boulay@gmail.com>
2017-07-26 10:58:00 -0400

    Fixes #109. Though the intention of the code change that introduced the bug is now lost.

--- helpers/ea_getscrfmat.m
+++ helpers/ea_getscrfmat.m
@@ -1,9 +1,10 @@
-function mat=ea_getscrfmat(directory)
+function mat=ea_getscrfmat(directory, varargin)
 
 load([directory,'scrf',filesep,'scrf_instore.mat']);
 mmat=ea_antsmat2mat(AffineTransform_float_3_3,fixed); % analytical solution
 
-if options.prefs.env.dev==1 % perform additional check    
+if ~isempty(varargin) && isstruct(varargin{1})...
+        && varargin{1}.prefs.env.dev==1 % perform additional check    
     mat=ea_antsmat2mat_empirical(directory); % do an extra empirical check
     if sum(abs(mmat(:)-mat(:)))<1e-06 % precision
         mat=mmat;
