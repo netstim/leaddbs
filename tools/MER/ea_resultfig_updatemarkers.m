7e4844e5e015727d4bf42c458a8d9a57fe38edb9
Chadwick Boulay <chadwick.boulay@gmail.com>
2017-08-10 23:07:42 -0400

    mer: Fixed marker handle/data mismatch after clearing figure.

--- tools/MER/ea_resultfig_updatemarkers.m
+++ tools/MER/ea_resultfig_updatemarkers.m
@@ -15,29 +15,25 @@ end
 
 set(0, 'CurrentFigure', resultfig); hold on;
 
-% Synchronize markers and handles.
-
-% Get a key for each handle and for each marker in merstruct
-uq_sides = unique(cat(2, {merstruct.Markers.side}, {merhandles.markers.side}));
-uq_labels = unique(cat(2, {merstruct.Markers.tract_label}, {merhandles.markers.label}));
-[~, h_side_keys] = ismember({merhandles.markers.side}, uq_sides);
-[~, h_label_keys] = ismember({merhandles.markers.label}, uq_labels);
-h_keys = [h_side_keys' h_label_keys' [merhandles.markers.depth]'];
-[~, s_side_keys] = ismember({merstruct.Markers.side}, uq_sides);
-[~, s_label_keys] = ismember({merstruct.Markers.tract_label}, uq_labels);
-s_keys = [s_side_keys' s_label_keys' [merstruct.Markers.depth]'];
-
 % Clear handles without matching keys in merstruct. e.g., after undo
+[s_keys, h_keys] = get_keys_for_markers_handles(merstruct.Markers, merhandles.markers);
 if ~isempty(h_keys)
-    [~, ia] = setdiff(h_keys, s_keys, 'rows');
+    if isempty(s_keys)
+        ia = 1:length(h_keys);
+    else
+        [~, ia] = setdiff(h_keys, s_keys, 'rows');
+    end
     for h_ix = 1:length(ia)
         h_id = ia(h_ix);
         delete(merhandles.markers(h_id).h);
         delete(merhandles.markers(h_id).tag);
     end
     merhandles.markers(ia) = [];
+end
     
-    % Update visibility of handles that do have matching keys in merstruct
+% Update visibility of handles that do have matching keys in merstruct
+[s_keys, h_keys] = get_keys_for_markers_handles(merstruct.Markers, merhandles.markers);
+if ~isempty(s_keys) && ~isempty(h_keys)
     ia = find(ismember(h_keys, s_keys, 'rows'));
     for h_ix = 1:length(ia)
         h_id = ia(h_ix);
@@ -47,13 +43,14 @@ if ~isempty(h_keys)
             set(hm.h, 'Visible', 'on');
         end
     end
-    
-    % Create new handles for merstruct markers without keys in merhandles.
-    [~, ia] = setdiff(s_keys, h_keys, 'rows');
-else
-    ia = 1:size(s_keys, 1);
 end
-
+    
+% Create new handles for merstruct markers without keys in merhandles.
+if isempty(h_keys)
+    ia = 1:size(s_keys, 1);  % All markers
+else
+    [~, ia] = setdiff(s_keys, h_keys, 'rows');  % Missing markers only.
+end
 for m_ix = 1:length(ia)
     m_id = ia(m_ix);
     marker = merstruct.Markers(m_id);
@@ -99,3 +96,14 @@ for m_ix = 1:length(ia)
     merhandles.markers(m_id) = new_hmark;
 end
 setappdata(handles.mercontrolfig, 'merhandles', merhandles);
+
+function [s_keys, h_keys] = get_keys_for_markers_handles(s_markers, h_markers)
+% Get a key for each marker in merstruct and for each marker handle.
+uq_sides = unique(cat(2, {s_markers.side}, {h_markers.side}));
+uq_labels = unique(cat(2, {s_markers.tract_label}, {h_markers.label}));
+[~, h_side_keys] = ismember({h_markers.side}, uq_sides);
+[~, h_label_keys] = ismember({h_markers.label}, uq_labels);
+h_keys = [h_side_keys' h_label_keys' [h_markers.depth]'];
+[~, s_side_keys] = ismember({s_markers.side}, uq_sides);
+[~, s_label_keys] = ismember({s_markers.tract_label}, uq_labels);
+s_keys = [s_side_keys' s_label_keys' [s_markers.depth]'];
