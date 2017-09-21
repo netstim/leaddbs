cebf01e9fae0b040f2186a59a00d911b786a9037
andreashorn <mail@andreas-horn.de>
2017-09-21 16:21:02 -0400

    Amend

--- ea_showelectrode.m
+++ ea_showelectrode.m
@@ -91,11 +91,8 @@ for side=1:length(options.sides)
             elrender{side}(cnt)=patch(electrode.insulation(ins));
             
             if isfield(elstruct,'group')
-                try
                 usecolor=elstruct.groupcolors(elstruct.group,:);
-                catch
-                    keyboard
-                end
+
             else
                 usecolor=elspec.lead_color;
             end
