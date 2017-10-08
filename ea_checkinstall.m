8f1fe031f54c6b977dd0bf6e270a9b3d990ec7f4
andreashorn <mail@andreas-horn.de>
2017-10-08 14:06:09 +0200

    Fixed install menu

--- ea_checkinstall.m
+++ ea_checkinstall.m
@@ -9,8 +9,8 @@ end
 
 switch cmd
     case 'list' % simply return list of installable datasets
-        success={'Redownload Data files','Apply Hotfix','Big Brain 100um subcortical (Amunts 2013)','Structural group connectome (Horn 2013)'};
-        commands={'leaddata','hotfix','bigbrain','groupconnectome2013'};
+        success={'Redownload Data files','Apply Hotfix','Big Brain 100um subcortical (Amunts 2013)','Structural group connectome 20 subjects Gibbs-tracker (Horn 2013)','Structural group connectome 169 NKI subjects Gibbs-tracker (Horn 2016)','Structural group connectome 32 Adult Diffusion HCP subjects GQI (Horn 2017)','Structural group connectome 90 PPMI PD-patients GQI (Ewert 2017)','Functional group connectome 74 PPMI PD-patients, 15 controls (Horn 2017)'};
+        commands={'leaddata','hotfix','bigbrain','groupconnectome2013','groupconnectome2016','groupconnectome2017','groupconnectome_ppmi2017','fgroupconnectome_ppmi2017'};
     case 'leaddata'
         checkf=[ea_space,'bb.nii'];
         force=ea_alreadyinstalled(checkf,checkonly,robot);
@@ -68,6 +68,82 @@ switch cmd
                 'group');
         else
             disp('Group Connectome (Horn 2013) is installed.')
+        end
+    case 'groupconnectome2016'
+        checkf=[ea_getconnectomebase('dmri'),'Gibbsconnectome_169 (Horn 2016)',filesep,'data.mat'];
+        force=ea_alreadyinstalled(checkf,checkonly,robot);
+        if checkonly
+            success=~force;
+            return;
+        end
+        if force==-1
+            success=-1;
+            return;
+        end
+        
+        if ~exist(checkf,'file') || force
+            success=ea_downloadasset('structural group connectome (Horn 2016)',...
+                [ea_getconnectomebase('dmri'),'groupconnectome2016.zip'],...
+                'group2016');
+        else
+            disp('Group Connectome (Horn 2016) is installed.')
+        end
+    case 'groupconnectome2017'
+        checkf=[ea_getconnectomebase('dmri'),'HCP_MGH_30fold_groupconnectome (Horn 2017)',filesep,'data.mat'];
+        force=ea_alreadyinstalled(checkf,checkonly,robot);
+        if checkonly
+            success=~force;
+            return;
+        end
+        if force==-1
+            success=-1;
+            return;
+        end
+        
+        if ~exist(checkf,'file') || force
+            success=ea_downloadasset('structural group connectome (Horn 2017)',...
+                [ea_getconnectomebase('dmri'),'groupconnectome2017.zip'],...
+                'group2017');
+        else
+            disp('Group Connectome (Horn 2017) is installed.')
+        end
+    case 'groupconnectome_ppmi2017'
+        checkf=[ea_getconnectomebase('dmri'),'PPMI_90 (Ewert 2017)',filesep,'data.mat'];
+        force=ea_alreadyinstalled(checkf,checkonly,robot);
+        if checkonly
+            success=~force;
+            return;
+        end
+        if force==-1
+            success=-1;
+            return;
+        end
+        
+        if ~exist(checkf,'file') || force
+            success=ea_downloadasset('structural group connectome (Ewert 2017)',...
+                [ea_getconnectomebase('dmri'),'groupconnectome_ppmi2017.zip'],...
+                'group2017_ppmi');
+        else
+            disp('Group Connectome (Ewert 2017) is installed.')
+        end
+    case 'fgroupconnectome_ppmi2017'
+        checkf=[ea_getconnectomebase('fmri'),'PPMI 74_15 (Horn 2017)',filesep,'dataset_info.mat'];
+        force=ea_alreadyinstalled(checkf,checkonly,robot);
+        if checkonly
+            success=~force;
+            return;
+        end
+        if force==-1
+            success=-1;
+            return;
+        end
+        
+        if ~exist(checkf,'file') || force
+            success=ea_downloadasset('functional group connectome (Horn 2017)',...
+                [ea_getconnectomebase('fmri'),'fgroupconnectome_ppmi2017.zip'],...
+                'fgroup2017_ppmi');
+        else
+            disp('Functional Group Connectome (Horn 2017) is installed.')
         end
     case 'hotfix'
         if ~checkonly
