ddb598d563cecdbef2b51631d772f87a28c1c851
Ningfei Li <ningfei.li@gmail.com>
2017-10-27 21:37:04 +0200

    Remove apps still under development from 'Open in...' menu.

--- helpers/ea_getsuiteapps.m
+++ helpers/ea_getsuiteapps.m
@@ -1,5 +1,12 @@
-function [apps,ids,accs]=ea_getsuiteapps
+function [apps, ids, accs] = ea_getsuiteapps
 
-apps={'Lead DBS','Lead Connectome','Lead Anatomy','Lead Connectome Mapper','Lead Group','Lead Group Connectome','Lead OR','Lead Predict'};
-ids={'lead_dbs','lead_connectome','lead_anatomy','lead_mapper','lead_group','lead_group_connectome','lead_or','lead_predict'};
-accs={'D','C','T','M','G','K','O','I'};
+apps = {'Lead DBS','Lead Connectome','Lead Anatomy','Lead Connectome Mapper','Lead Group'};
+ids = {'lead_dbs','lead_connectome','lead_anatomy','lead_mapper','lead_group'};
+accs = {'D','C','T','M','G'};
+
+prefs = ea_prefs;
+if prefs.env.dev
+    apps = [apps, 'Lead Group Connectome','Lead OR','Lead Predict'];
+    ids = [ids, 'lead_group_connectome','lead_or','lead_predict'];
+    accs = [accs, 'K','O','I'];
+end
