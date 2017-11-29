552acbfab421653e421d32f28f2147e9ea02a226
andreashorn <mail@andreas-horn.de>
2017-11-29 08:16:14 +0100

    Further addition to predict leave one out

--- helpers/annals2017_maps/ea_predict_leave_one_out.m
+++ helpers/annals2017_maps/ea_predict_leave_one_out.m
@@ -1,4 +1,4 @@
-function ea_predict_leave_one_out(uipatdirs,stimname,typemap,regressor,fdmri)
+function ea_predict_leave_one_out(uipatdirs,stimname,typemap,regressor,fdmri,dowhat)
 if size(regressor,1)<size(regressor,2)
     regressor=regressor';
 end
@@ -11,10 +11,16 @@ for pat=1:length(uipatdirs)
     fis.fmri{pat}=[uipatdirs{pat},filesep,'stimulations',filesep,stimname,filesep,fdmri.fmriconnectome,filesep,'vat_seed_compound_fMRI_func_seed_AvgR_Fz.nii'];
     fis.dmri{pat}=[uipatdirs{pat},filesep,'stimulations',filesep,stimname,filesep,fdmri.dmriconnectome,filesep,'vat_seed_compound_dMRI_struc_seed.nii'];
 end
+if ~exist('dowhat','var')
+    dowhat='both';
+end
+if ismember(dowhat,{'both','genmodels'});
+    ea_gen_models_leoo(uipatdirs,typemap,regressor,fdmri,fis);
+end
+if ismember(dowhat,{'both','predictoutcome'});
+    ea_predict_outcomes_leoo(uipatdirs,stimname,typemap,regressor,fdmri,fis);
+end
 
-ea_gen_models_leoo(uipatdirs,typemap,regressor,fdmri,fis);
-
-ea_predict_outcomes_leoo(uipatdirs,stimname,typemap,regressor,fdmri,fis);
 
 function ea_predict_outcomes_leoo(uipatdirs,stimname,typemap,regressor,fdmri,fis)
 load([ea_getearoot,'predict',filesep,'models',filesep,'horn2017_AoN',filesep,'modeldata.mat']); % here only needed to get a mask
@@ -24,23 +30,24 @@ for pat=1:length(uipatdirs)
     if ismember(fdmri.do,{'fmri','both'})
         model=ea_load_nii([odir,'fMRI_',typemap,'_leoo_',num2str(pat),'.nii']);
         patcon=ea_load_nii(fis.fmri{pat});
-        pred.fmri(pat)=corr(model.nii(modeldata.mask),patcon.nii(modeldata.mask),'rows','pairwise','type','spearman');
+        pred.fmri(pat)=corr(model.img(modeldata.mask),patcon.img(modeldata.mask),'rows','pairwise','type','spearman');
     end
     if ismember(fdmri.do,{'dmri','both'})
         model=ea_load_nii([odir,'dMRI_',typemap,'_leoo_',num2str(pat),'.nii']);
         patcon=ea_load_nii(fis.dmri{pat});
-        pred.dmri(pat)=corr(model.nii(modeldata.mask),patcon.nii(modeldata.mask),'rows','pairwise','type','spearman');
+        pred.dmri(pat)=corr(model.img(modeldata.mask),patcon.img(modeldata.mask),'rows','pairwise','type','spearman');
     end
 end
+
 if ismember(fdmri.do,{'fmri','both'})
-    [R_upd,p_upd,R,p,h]=ea_corrplot_gen([reg,pred.fmri],'Corr',{'Empirical Improvement';'Predicted Improvement'},[],[],'permutation_spearman');
+    [R_upd,p_upd,R,p,h]=ea_corrplot_gen([regressor,pred.fmri'],'Corr',{'Empirical Improvement';'Predicted Improvement'},[],[],'permutation_spearman');
     axis square
-    ea_screenshot([odir,'leoo_prediction_fmri.png']);
+    ea_screenshot([odir,'leoo_prediction_fmri_',typemap,'.png']);
 end
 if ismember(fdmri.do,{'dmri','both'})
-    [R_upd,p_upd,R,p,h]=ea_corrplot_gen([reg,pred.dmri],'Corr',{'Empirical Improvement';'Predicted Improvement'},[],[],'permutation_spearman');
+    [R_upd,p_upd,R,p,h]=ea_corrplot_gen([regressor,pred.dmri'],'Corr',{'Empirical Improvement';'Predicted Improvement'},[],[],'permutation_spearman');
     axis square
-    ea_screenshot([odir,'leoo_prediction_dmri.png']);
+    ea_screenshot([odir,'leoo_prediction_dmri_',typemap,'.png']);
 end
 save([odir,'leoo_prediction'],'pred');
 
