280b55cbb8d53262ad3590184b56638d50526aa2
andreashorn <mail@andreas-horn.de>
2017-12-04 15:59:43 +0000

    Close SPM figures when selecting electrodes manually.

--- ea_runmanual.m
+++ ea_runmanual.m
@@ -6,19 +6,19 @@ load([ea_getearoot,'templates',filesep,'electrode_models',filesep,options.elspec
 normdist=pdist([electrode.head_position;electrode.tail_position]);
 
 
-for side=options.sides    
+for side=options.sides
     spm('defaults', 'fmri');
     Fgraph = spm_figure('GetWin', 'Graphics');
     Finter = spm('FnUIsetup','Click on Tip', 0);
     
     figure(Fgraph); clf;
     
-   switch options.modality
-       case 1 % MRI
-    vol = spm_vol([directory,options.prefs.tranii_unnormalized]);
-       case 2 % CT
-    vol = spm_vol([directory,options.prefs.ctnii_coregistered]);
-   end       
+    switch options.modality
+        case 1 % MRI
+            vol = spm_vol([directory,options.prefs.tranii_unnormalized]);
+        case 2 % CT
+            vol = spm_vol([directory,options.prefs.ctnii_coregistered]);
+    end
     spm_orthviews('Reset');
     spm_orthviews('Image', vol);
     colormap('gray');
@@ -35,22 +35,26 @@ for side=options.sides
         markers(side).tail = spm_orthviews('Pos')';
         %spm_orthviews('Reset');
     end
-
-       markers(side).tail=markers(side).head+...
-          (markers(side).tail-markers(side).head)*...
-          normdist/norm(...
-          (markers(side).tail-markers(side).head));
-
-      % add x and y
-      normtrajvector=(markers(side).tail-markers(side).head)./norm(markers(side).tail-markers(side).head);
-        orth=null(normtrajvector)*(options.elspec.lead_diameter/2);
-        markers(side).x=markers(side).head+orth(:,1)';
-        markers(side).y=markers(side).head+orth(:,2)'; % corresponding points in reality
-
-end
-              [coords_mm,trajectory,markers]=ea_resolvecoords(markers,options,0);
-
-
-
     
+    markers(side).tail=markers(side).head+...
+        (markers(side).tail-markers(side).head)*...
+        normdist/norm(...
+        (markers(side).tail-markers(side).head));
+    
+    % add x and y
+    normtrajvector=(markers(side).tail-markers(side).head)./norm(markers(side).tail-markers(side).head);
+    orth=null(normtrajvector)*(options.elspec.lead_diameter/2);
+    markers(side).x=markers(side).head+orth(:,1)';
+    markers(side).y=markers(side).head+orth(:,2)'; % corresponding points in reality
+    
+end
+[coords_mm,trajectory,markers]=ea_resolvecoords(markers,options,0);
+
+
+close(Fgraph);
+close(Finter);
+
+
+
+
 
