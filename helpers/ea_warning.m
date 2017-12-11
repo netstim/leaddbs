a69ad4a0d729f1361b31cc22fbd19eceee45ce60
andreashorn <mail@andreas-horn.de>
2017-12-11 15:07:09 +0100

    Added capability to suppress warning dialogs to ea_warning.

--- helpers/ea_warning.m
+++ helpers/ea_warning.m
@@ -1,4 +1,12 @@
 function ea_warning(msg)
-
-errordlg(msg);
+% the warning dialog of this function may be silenced by adding
+% evalin('base','WARNINGSILENT=1;');
+% to the beginning of your code.
 warning(msg);
+try
+    ws=evalin('base','WARNINGSILENT');
+    if ws
+        return
+    end
+end
+errordlg(sprintf([msg, '\n\n[This warning dialog may be silenced by adding \n\n''evalin(''base'',''WARNINGSILENT=1;'');''\n\n to the beginning of your code.]']));
