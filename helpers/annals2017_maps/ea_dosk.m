c44a6091dc40339113cc3c2c7d567f9deadc4ee0
andreashorn <mail@andreas-horn.de>
2017-12-27 13:44:18 +0100

    Decoupled the sk operation suited for stats dMRI mapper files.

---
+++ helpers/annals2017_maps/ea_dosk.m
@@ -0,0 +1,16 @@
+function fina=ea_dosk(fina,mask)
+[pth,fn,ext]=fileparts(fina);
+nii=ea_load_nii(fina);
+nii.img(~mask)=nan;
+nii.img(mask)=ea_normal(nii.img(mask));
+nii.fname=fullfile(pth,['k',fn,ext]);
+ea_write_nii(nii);
+matlabbatch{1}.spm.spatial.smooth.data = {[fullfile(pth,['k',fn]),'.nii,1']};
+matlabbatch{1}.spm.spatial.smooth.fwhm = [2 2 2];
+matlabbatch{1}.spm.spatial.smooth.dtype = 0;
+matlabbatch{1}.spm.spatial.smooth.im = 0;
+matlabbatch{1}.spm.spatial.smooth.prefix = 's';
+spm_jobman('run',{matlabbatch});
+clear matlabbatch
+delete(fullfile(pth,['k',fn,ext]));
+fina=fullfile(pth,['sk',fn,ext]);
