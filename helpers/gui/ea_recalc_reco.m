02d2f9ad8f5cee63a9b421ad4d4f7abfe164b834
andreashorn <mail@andreas-horn.de>
2018-01-23 13:12:40 +0100

    Auto determine sides upon recalc reco.

--- helpers/gui/ea_recalc_reco.m
+++ helpers/gui/ea_recalc_reco.m
@@ -7,12 +7,20 @@ else
 end
 options.native=1;
 options.hybridsave=1;
-options.sides=1:2;
-
 for pt=1:length(uipatdirs)
+    
+
     options=ea_getptopts(uipatdirs{pt},options);
     disp(['Re-propagating reconstruction from native (postop) -> native (preop) -> template space: ', options.patientname]); 
     [~,~,markers,elmodel,manually_corrected]=ea_load_reconstruction(options);
+    options.sides=[];
+    for el=1:length(markers)
+        if ~isempty(markers(el).head)
+            options.sides(end+1)=el;
+        end
+    end
+    
+   
     options.elmodel=elmodel;
     options=ea_resolve_elspec(options);
     [coords_mm,trajectory,markers]=ea_resolvecoords(markers,options);
