fdb79c8a22b1f0770b9c51bc198971ca1cb6fe72
Ningfei Li <ningfei.li@gmail.com>
2018-01-29 17:16:26 +0100

    Fix 'Apply Patient Inverse Normalization to file'.
    Need to call 'ea_assignpretra' before applying normalization, otherwise
    'anat_t2.nii' will be used as reference image by default.

--- helpers/gui/ea_applynormtofile_menu.m
+++ helpers/gui/ea_applynormtofile_menu.m
@@ -1,11 +1,12 @@
-function ea_applynormtofile_menu(~,~,handles,useinverse)
+function ea_applynormtofile_menu(~, ~, handles, useinverse)
 
-[options.root,options.patientname]=fileparts(get(handles.patdir_choosebox,'String'));
-options.root=[options.root,filesep];
-options.earoot=ea_getearoot;
-options.prefs=ea_prefs(options.patientname);
+[options.root, options.patientname] = fileparts(get(handles.patdir_choosebox, 'String'));
+options.root = [options.root, filesep];
+options.earoot = ea_getearoot;
+options.prefs = ea_prefs(options.patientname);
+options = ea_assignpretra(options);
 
-[fis,path]=uigetfile({'*.nii';'*.nii.gz'},'Choose files to apply deformation to...',[options.root,options.patientname],'Multiselect','on');
+[fis, path] = uigetfile({'*.nii';'*.nii.gz'}, 'Choose files to apply deformation to...', [options.root, options.patientname], 'Multiselect', 'on');
 
 if ~ischar(fis) && ~iscell(fis)
     if ~fis
@@ -13,13 +14,15 @@ if ~ischar(fis) && ~iscell(fis)
     end
 end
 if ischar(fis)
-    fis={fis};
+    fis = {fis};
 end
 
+to = cell(1, length(fis));
+from = cell(1, length(fis));
 for fi=1:length(fis) 
-   to{fi}=[path,'gl',fis{fi}];
-   from{fi}=[path,fis{fi}];
+   to{fi} = [path, 'gl', fis{fi}];
+   from{fi} = [path, fis{fi}];
 end
 
-ea_apply_normalization_tofile(options,from,to,[options.root,options.patientname,filesep],useinverse);
+ea_apply_normalization_tofile(options, from, to, [options.root, options.patientname, filesep], useinverse);
 
