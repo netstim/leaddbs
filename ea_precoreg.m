a0300ec5a05e16649b206ae82faa18beda46b4ef
Till <till.dembek@uk-koeln.de>
2018-02-09 13:03:38 +0100

    ea_precoreg now works without calling spm_jobman and stores the transformation in ea_precoregtransformation.mat

--- ea_precoreg.m
+++ ea_precoreg.m
@@ -1,11 +1,18 @@
-function ea_precoreg(fname,tname)
+function ea_precoreg(fname,tname,options)
 % pre-coreg predominant anat to MNI (without reslicing).
-matlabbatch{1}.spm.spatial.coreg.estimate.ref = {[ea_space,tname,'.nii,1']};
-matlabbatch{1}.spm.spatial.coreg.estimate.source = {[fname,',1']};
-matlabbatch{1}.spm.spatial.coreg.estimate.other = {''};
-matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.cost_fun = 'nmi';
-matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.sep = [4 2];
-matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.tol = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
-matlabbatch{1}.spm.spatial.coreg.estimate.eoptions.fwhm = [7 7];
-spm_jobman('run',{matlabbatch});
-clear matlabbatch
+% set save_tmet = 1 if you want to store the initial MRI-transformation in
+% the lead folder
+save_tmat = 1;
+%%
+flags.cost_fun = 'nmi';
+flags.sep = [4 2];
+flags.tol = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
+flags.fwhm = [7 7];
+tmp = spm_coreg([ea_space,tname,'.nii,1'],[fname,',1'],flags);
+tmat = spm_matrix(tmp(:)');
+if save_tmat
+    save([options.root options.patientname filesep 'ea_precoregtransformation.mat'],'tmat');
+end
+anat_t1_vol = spm_vol(fname);
+anat_t1_vol.mat = spm_get_space(fname, inv(tmat) * anat_t1_vol.mat);
+spm_write_vol(anat_t1_vol,spm_read_vols(anat_t1_vol));
