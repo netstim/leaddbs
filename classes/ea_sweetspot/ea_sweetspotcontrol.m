992e723448ae3c7589bc3d91e9bb1c53ed0f3c19
Ningfei Li <ningfei.li@gmail.com>
2018-02-12 12:31:36 +0100

    Tweak 'ea_sweetspotcontrol'.

--- classes/ea_sweetspot/ea_sweetspotcontrol.m
+++ classes/ea_sweetspot/ea_sweetspotcontrol.m
@@ -22,7 +22,7 @@ function varargout = ea_sweetspotcontrol(varargin)
 
 % Edit the above text to modify the response to help ea_sweetspotcontrol
 
-% Last Modified by GUIDE v2.5 07-Feb-2018 14:15:38
+% Last Modified by GUIDE v2.5 12-Feb-2018 12:19:21
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -79,7 +79,7 @@ if ~obj.binary
     set(handles.roicontrol,'Name',obj.niftiFilename);
     handles.histax.XAxis.Limits=[min(nzeros),max(nzeros)];
 else
-    set(handles.threshtxt,'Visible','off');
+    set(handles.threshLabel,'Visible','off');
 end
 % button
 set(handles.colorchange,'BackgroundColor',obj.color);
@@ -87,101 +87,116 @@ set(0,'CurrentFigure',handles.roicontrol);
 set(handles.roicontrol,'name',obj.name);
 %% sliders:
 if ~obj.binary
-% threshold
-jSlider{1} = javax.swing.JSlider(0,100);
-javacomponent(jSlider{1},[0,130,200,45]);
-set(jSlider{1}, 'Value', getmaxminthresh(obj), 'MajorTickSpacing',0.1, 'PaintLabels',true);  % with labels, no ticks
-hjSlider{1} = handle(jSlider{1}, 'CallbackProperties');
-set(hjSlider{1}, 'MouseReleasedCallback', {@sliderthresholdchange,obj,handles});  %alternative
-set(hjSlider{1}, 'StateChangedCallback', {@sliderthresholdchangetxt,obj,handles});  %alternative
+    % threshold
+    jSlider{1} = javax.swing.JSlider(0,100);
+    threshLabelPos = get(handles.thresholdLabel, 'Position');
+    javacomponent(jSlider{1},[0,threshLabelPos(2)-45,200,45]);
+    set(jSlider{1}, 'Value', threshold2slider(obj),...
+        'Background',java.awt.Color(1,1,1),...
+        'MajorTickSpacing',0.1, 'PaintLabels',true);  % with labels, no ticks
+    hjSlider{1} = handle(jSlider{1}, 'CallbackProperties');
+    set(hjSlider{1}, 'MouseReleasedCallback', {@sliderThresholdChange,obj,handles});  %alternative
+    set(hjSlider{1}, 'StateChangedCallback', {@sliderThresholdChangeTxt,obj,handles});  %alternative
 else
-   obj.threshold=obj.max/2;
+    obj.threshold=obj.max/2;
 end
 
 % alpha
 set(0,'CurrentFigure',handles.roicontrol);
 jSlider{2} = javax.swing.JSlider(0,100);
-javacomponent(jSlider{2},[0,65,200,45]);
-set(jSlider{2}, 'Value', obj.alpha*100, 'MajorTickSpacing',0.1, 'PaintLabels',true);  % with labels, no ticks
+alphaLabelPos = get(handles.alphaLabel, 'Position');
+javacomponent(jSlider{2},[0,alphaLabelPos(2)-45,200,45]);
+set(jSlider{2}, 'Value', obj.alpha*100,...
+    'Background',java.awt.Color(1,1,1),...
+    'MajorTickSpacing',0.1, 'PaintLabels',true);  % with labels, no ticks
 hjSlider{2} = handle(jSlider{2}, 'CallbackProperties');
-set(hjSlider{2}, 'StateChangedCallback', {@slideralphachange,obj,handles});  %alternative
+set(hjSlider{2}, 'MouseReleasedCallback', {@sliderAlphaChange,obj,handles});  %alternative
+set(hjSlider{2}, 'StateChangedCallback', {@sliderAlphaChangeTxt,obj,handles});  %alternative
 
 % smooth
 set(0,'CurrentFigure',handles.roicontrol);
 jSlider{3} = javax.swing.JSlider(0,100);
-javacomponent(jSlider{3},[0,0,200,45]);
-set(jSlider{3}, 'Value', round(obj.smooth*2), 'MajorTickSpacing',0.1, 'PaintLabels',true);  % with labels, no ticks
+smoothLabelPos = get(handles.smoothLabel, 'Position');
+javacomponent(jSlider{3},[0,smoothLabelPos(2)-45,200,45]);
+set(jSlider{3}, 'Value', round(obj.smooth*2),...
+    'Background',java.awt.Color(1,1,1),...
+    'MajorTickSpacing',0.1, 'PaintLabels',true);  % with labels, no ticks
 hjSlider{3} = handle(jSlider{3}, 'CallbackProperties');
-set(hjSlider{3}, 'StateChangedCallback', {@slidersmoothchangetxt,obj,handles});  %alternative
-set(hjSlider{3}, 'MouseReleasedCallback', {@slidersmoothchange,obj,handles});  %alternative
+set(hjSlider{3}, 'MouseReleasedCallback', {@sliderSmoothChange,obj,handles});  %alternative
+set(hjSlider{3}, 'StateChangedCallback', {@sliderSmoothChangeTxt,obj,handles});  %alternative
 set(0,'CurrentFigure',obj.plotFigureH);
 
-cnt=4+size(obj.nii.X,1);
 % factors
+factorLabels = [handles.factorLabel1, handles.factorLabel2, handles.factorLabel3, handles.factorLabel4];
+cnt=4+size(obj.nii.X,1);
 fcnt=1;
 for f=size(obj.nii.X,1):-1:1
-   set(0,'CurrentFigure',handles.roicontrol);
+	set(0,'CurrentFigure',handles.roicontrol);
+	jSlider{cnt} = javax.swing.JSlider(0,100);
+    factorLabelPos = get(factorLabels(f), 'Position');
+    javacomponent(jSlider{cnt},[200,factorLabelPos(2)-45,325,45]);
+    set(jSlider{cnt}, 'Value', 100/length(obj.nii.factors),...
+        'Background',java.awt.Color(1,1,1),...
+        'MajorTickSpacing',0.1, 'PaintLabels',true);  % with labels, no ticks
 
-            jSlider{cnt} = javax.swing.JSlider(0,100);
-javacomponent(jSlider{cnt},[200,0+(fcnt-1)*65,200,45]);
-set(jSlider{cnt}, 'Value', 100/length(obj.nii.factors), 'MajorTickSpacing',0.1, 'PaintLabels',true);  % with labels, no ticks
-FactorSlider{f} = handle(jSlider{cnt}, 'CallbackProperties');
-set(FactorSlider{f}, 'MouseReleasedCallback', {@sliderfactorchange,obj,handles,f});  %alternative
-set(FactorSlider{f}, 'StateChangedCallback', {@sliderfactorchangelive,obj,handles,f});  %alternative   
-t = uicontrol(handles.roicontrol,'Style','text',...
-                'String',obj.nii.factors{f},...
-                'Position',[200,(0+(fcnt-1)*65)+50,200,12]);
-cnt=cnt-1;
-fcnt=fcnt+1;
+    FactorSlider{f} = handle(jSlider{cnt}, 'CallbackProperties');
+    set(FactorSlider{f}, 'MouseReleasedCallback', {@sliderFactorChange,obj,handles,f});  %alternative
+    set(FactorSlider{f}, 'StateChangedCallback', {@sliderFactorChangeLive,obj,handles,f});  %alternative
+
+    set(factorLabels(f), 'Visible', 'on', 'HorizontalAlignment', 'left', 'String',strrep(obj.nii.factors{f}, '_', ' '));
+
+    cnt=cnt-1;
+    fcnt=fcnt+1;
 end
 setappdata(handles.roicontrol,'FactorSlider',FactorSlider);
-    
 
-function thresh=getmaxminthresh(obj)
 
-thresh=obj.threshold-obj.min;
-thresh=thresh/obj.max;
-thresh=round(thresh*100); % slider only supports integers
+function sliderValue = threshold2slider(obj)
+threshold = (obj.threshold-obj.min)/(obj.max-obj.min);
+sliderValue = round(threshold*100); % slider only supports integers
 
-function slideralphachange(varargin)
+
+function sliderThresholdChange(varargin)
+slide = varargin{1};
+obj = varargin{3};
+obj.threshold = slide.Value / 100 * (obj.max-obj.min) + obj.min;
+
+
+function sliderThresholdChangeTxt(varargin)
 slide=varargin{1};
 obj=varargin{3};
 handles=varargin{4};
-obj.alpha=slide.Value/100;
-set(handles.alphatxt,'String',num2str(obj.alpha));
+thresholdValue = slide.Value / 100 * (obj.max-obj.min) + obj.min;
+set(handles.thresholdValue,'String',sprintf('%0.2f',thresholdValue));
 
-function sliderthresholdchange(varargin)
+
+function sliderAlphaChange(varargin)
+slide = varargin{1};
+obj = varargin{3};
+obj.alpha = slide.Value/100;
+
+
+function sliderAlphaChangeTxt(varargin)
+slide = varargin{1};
+handles = varargin{4};
+alpha = slide.Value/100;
+set(handles.alphaValue,'String',num2str(alpha));
+
+
+function sliderSmoothChange(varargin)
 slide=varargin{1};
 obj=varargin{3};
-tval=slide.Value;
-tval=tval/100;
-tval=tval*obj.max;
-obj.threshold=tval+obj.min;
+obj.smooth=(round(slide.Value/2));
 
-function sliderfactorchangelive(varargin)
 
+function sliderSmoothChangeTxt(varargin)
 slide=varargin{1};
-obj=varargin{3};
 handles=varargin{4};
-factor=varargin{5};
-FactorSlider=getappdata(handles.roicontrol,'FactorSlider');
+smooth=(round(slide.Value/2));
+set(handles.smoothValue,'String',[num2str(smooth),' Its.']);
 
-for slider=1:length(FactorSlider)
-tval(slider)=FactorSlider{slider}.Value;
-tval(slider)=tval(slider)/100;
-end
-% update fmix model:
 
-ofactors=1:length(obj.nii.factors); ofactors(factor)=[];
-tval(ofactors)=(tval(ofactors)/sum(tval(ofactors)))*(1-tval(factor));
-tval(isnan(tval))=eps;
-% scale remaining sliders accordingly:
-for osliders=ofactors
-    FactorSlider{osliders}.Value=tval(osliders)*100;
-end
-
-function sliderfactorchange(varargin)
-
+function sliderFactorChange(varargin)
 slide=varargin{1};
 obj=varargin{3};
 handles=varargin{4};
@@ -201,27 +216,25 @@ for osliders=ofactors
 end
 
 
-function sliderthresholdchangetxt(varargin)
+function sliderFactorChangeLive(varargin)
 slide=varargin{1};
 obj=varargin{3};
-   handles=varargin{4};
-tval=slide.Value;
-tval=tval/100;
-tval=tval*obj.max;
-tval=tval+obj.min;
-set(handles.theshtxt,'String',sprintf('%0.2f',tval));
-
-function slidersmoothchange(varargin)
-slide=varargin{1};
-
-obj=varargin{3};
-obj.smooth=(round(slide.Value/2));
-
-function slidersmoothchangetxt(varargin)
-slide=varargin{1};
 handles=varargin{4};
-smooth=(round(slide.Value/2));
-set(handles.smoothtxt,'String',[num2str(smooth),' Its.']);
+factor=varargin{5};
+FactorSlider=getappdata(handles.roicontrol,'FactorSlider');
+
+for slider=1:length(FactorSlider)
+    tval(slider)=FactorSlider{slider}.Value;
+    tval(slider)=tval(slider)/100;
+end
+% update fmix model:
+ofactors=1:length(obj.nii.factors); ofactors(factor)=[];
+tval(ofactors)=(tval(ofactors)/sum(tval(ofactors)))*(1-tval(factor));
+tval(isnan(tval))=eps;
+% scale remaining sliders accordingly:
+for osliders=ofactors
+    FactorSlider{osliders}.Value=tval(osliders)*100;
+end
 
 
 % --- Outputs from this function are returned to the command line.
@@ -255,7 +268,7 @@ function showhide_Callback(hObject, eventdata, handles)
 obj=getappdata(handles.roicontrol,'obj');
 switch get(hObject,'Value')
     case 1
-obj.visible='on';
+        obj.visible='on';
     case 0
-   obj.visible='off';
+        obj.visible='off';
 end
