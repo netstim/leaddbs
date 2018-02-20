1274c4b95a8ea5421bc7e4f4ea0d0780cfd8465e
Ningfei Li <ningfei.li@gmail.com>
2018-02-20 10:04:33 +0100

    Refine 'ea_gethome'.

--- helpers/ea_gethome.m
+++ helpers/ea_gethome.m
@@ -6,15 +6,11 @@ else
     home = getenv('HOME');
 end
 
-home = [home, filesep];
-
-% TP: Bug when home is pointing to a drive: i.e. C:\ then another filesep
-% is added at end to make it C:\\ causing potential error. Simple fix below:
-if strcmp(home(end-1), filesep)
-    home(end) = [];
+if ~strcmp(home(end), filesep)
+    home = [home, filesep];
 end
 
 if isdeployed
    mkdir([ctfroot, filesep, 'home', filesep]);
-   home=[ctfroot, filesep, 'home', filesep];
+   home = [ctfroot, filesep, 'home', filesep];
 end
--- helpers/space/ea_getspace.m
+++ helpers/space/ea_getspace.m
@@ -1,9 +1,9 @@
 function space=ea_getspace
 
 %prefs=ea_prefs('');
-home=ea_gethome;
-if ~exist([home,'.ea_prefs.mat'],'file')
-    copyfile([ea_getearoot,'common',filesep,'ea_prefs_default.mat'],[home,'.ea_prefs.mat'], 'f');
+home = ea_gethome;
+if ~exist([home,'.ea_prefs.mat'], 'file')
+    copyfile([ea_getearoot,'common',filesep,'ea_prefs_default.mat'], [home,'.ea_prefs.mat'], 'f');
 end
 load([home,'.ea_prefs.mat']);
-space=machine.space;
+space = machine.space;
