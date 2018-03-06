5159925f2ececb41c0d349a2886fdec37ce45f85
andreashorn <mail@andreas-horn.de>
2018-03-06 09:52:53 +0100

    Added dcm2nii select to lead_connectome figure

--- connectomics/lead_connectome.m
+++ connectomics/lead_connectome.m
@@ -22,7 +22,7 @@ function varargout = lead_connectome(varargin)
 
 % Edit the above text to modify the response to help leadfigure
 
-% Last Modified by GUIDE v2.5 28-Aug-2017 17:07:48
+% Last Modified by GUIDE v2.5 06-Mar-2018 09:52:32
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -710,3 +710,26 @@ function overwriteapproved_Callback(hObject, eventdata, handles)
 
 % Hint: get(hObject,'Value') returns toggle state of overwriteapproved
 ea_storeui(handles);
+
+
+% --- Executes on selection change in dcm2niiselect.
+function dcm2niiselect_Callback(hObject, eventdata, handles)
+% hObject    handle to dcm2niiselect (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = cellstr(get(hObject,'String')) returns dcm2niiselect contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from dcm2niiselect
+
+
+% --- Executes during object creation, after setting all properties.
+function dcm2niiselect_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to dcm2niiselect (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
