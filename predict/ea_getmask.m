a7d3d85e4d12c2b38861c1037ac2a347bc4f214f
andreashorn <mail@andreas-horn.de>
2018-03-09 15:00:42 +0100

    Added hd masks to modeldata

--- predict/ea_getmask.m
+++ predict/ea_getmask.m
@@ -6,12 +6,21 @@ if nargin
         case {'grey','','standard'}
             load([ea_getearoot,'predict',filesep,'models',filesep,'horn2017_AoN',filesep,'modeldata.mat']);
             mask=modeldata.mask;
+        case {'grey_hd','hd','standard_hd'}
+            load([ea_getearoot,'predict',filesep,'models',filesep,'horn2017_AoN',filesep,'modeldata.mat']);
+            mask=modeldata.mask_hd;
         case 'brain'
             load([ea_getearoot,'predict',filesep,'models',filesep,'horn2017_AoN',filesep,'modeldata.mat']);
             mask=modeldata.brainmask;
+        case 'brain_hd'
+            load([ea_getearoot,'predict',filesep,'models',filesep,'horn2017_AoN',filesep,'modeldata.mat']);
+            mask=modeldata.brainmask_hd;
         case 'cortex'
             load([ea_getearoot,'predict',filesep,'models',filesep,'horn2017_AoN',filesep,'modeldata.mat']);
             mask=modeldata.cortexmask;
+        case 'cortex_hd'
+            load([ea_getearoot,'predict',filesep,'models',filesep,'horn2017_AoN',filesep,'modeldata.mat']);
+            mask=modeldata.cortexmask_hd;
     end
 else
     load([ea_getearoot,'predict',filesep,'models',filesep,'horn2017_AoN',filesep,'modeldata.mat']);
