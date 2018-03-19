3ea85f43a600618c4e6de2d851003b67959a9f03
andreashorn <mail@andreas-horn.de>
2018-03-19 20:03:36 +0100

    Added high-res 5mm masks to modeldata

--- predict/ea_getmask.m
+++ predict/ea_getmask.m
@@ -3,24 +3,33 @@ function mask=ea_getmask(varargin)
 % predict suite.
 if nargin
     switch varargin{1}
-        case {'grey','','standard'}
+        case {'grey','','standard','grey_2'}
             load([ea_getearoot,'predict',filesep,'models',filesep,'horn2017_AoN',filesep,'modeldata.mat']);
             mask=modeldata.mask;
-        case {'grey_hd','hd','standard_hd'}
+        case {'grey_hd','hd','standard_hd','grey_1'}
             load([ea_getearoot,'predict',filesep,'models',filesep,'horn2017_AoN',filesep,'modeldata.mat']);
             mask=modeldata.mask_hd;
-        case 'brain'
+        case {'brain','brain_2'}
             load([ea_getearoot,'predict',filesep,'models',filesep,'horn2017_AoN',filesep,'modeldata.mat']);
             mask=modeldata.brainmask;
-        case 'brain_hd'
+        case {'brain_hd','brain_1'}
             load([ea_getearoot,'predict',filesep,'models',filesep,'horn2017_AoN',filesep,'modeldata.mat']);
             mask=modeldata.brainmask_hd;
-        case 'cortex'
+        case {'cortex','cortex_2'}
             load([ea_getearoot,'predict',filesep,'models',filesep,'horn2017_AoN',filesep,'modeldata.mat']);
             mask=modeldata.cortexmask;
-        case 'cortex_hd'
+        case {'cortex_hd','cortex_1'}
             load([ea_getearoot,'predict',filesep,'models',filesep,'horn2017_AoN',filesep,'modeldata.mat']);
             mask=modeldata.cortexmask_hd;
+        case {'cortex_5'}
+            load([ea_getearoot,'predict',filesep,'models',filesep,'horn2017_AoN',filesep,'modeldata.mat']);
+            mask=modeldata.cortexmask_hd5;
+        case {'brain_5'}
+            load([ea_getearoot,'predict',filesep,'models',filesep,'horn2017_AoN',filesep,'modeldata.mat']);
+            mask=modeldata.brainmask_hd5;
+        case {'grey_5'}
+            load([ea_getearoot,'predict',filesep,'models',filesep,'horn2017_AoN',filesep,'modeldata.mat']);
+            mask=modeldata.mask_hd5;
     end
 else
     load([ea_getearoot,'predict',filesep,'models',filesep,'horn2017_AoN',filesep,'modeldata.mat']);
