8868160bcd88a33742488db683c6039d6f64f638
neuromodulation_berlin <horn@neuromodulation.berlin>
2018-04-13 11:15:56 +0200

    Fixed PaCER typo.

--- ext_libs/PaCER/src/PaCER.m
+++ ext_libs/PaCER/src/PaCER.m
@@ -41,7 +41,7 @@ if(max(niiCT.voxsize) > 1)
     warning('Slice thickness is greater than 1 mm! Independent contact detection is most likly not possible. Forcing contactAreaCenter based method.');
     args.contactDetectionMethod = 'contactAreaCenter';
 elseif(max(niiCT.voxsize) > 0.7)
-    warning('Slice thickness is greater than 0.7 mm! Independet contact detection might not work reliable in this case. However, for certain electrode types with large contacts spacings you might be lucky.');
+    warning('Slice thickness is greater than 0.7 mm! Independent contact detection might not work reliable in this case. However, for certain electrode types with large contacts spacings you might be lucky.');
 end
 
 %% Run Algorithm
