6b17501d42bafc45bd0883c45f9723071a90dabd
neuromodulation_berlin <horn@neuromodulation.berlin>
2018-04-26 13:24:45 +0200

    Mod default ANTs preset for BSpline setting.

--- ext_libs/ANTs/presets/ea_antspreset_effective_lowvar_default.m
+++ ext_libs/ANTs/presets/ea_antspreset_effective_lowvar_default.m
@@ -22,16 +22,34 @@ switch normsettings.ants_strategy
     case 'SyN'
         apref.antsmode='SyN';
         apref.antsmode_suffix='[0.3,4,3]';
+        
+        apref.convergence.syn='[1000x500x250x0,1e-6,7]'; % SyN convergence params
+        apref.convergence.scrf='[200x50x10x0,1e-6,7]'; % SyN subcortical focus stage convergence params
+        apref.smoothingsigmas.syn='4x3x1x1vox'; % SyN Smoothing sigmas
+        apref.smoothingsigmas.scrf='2x2x1x1vox'; % SyN subcortical focus stage smoothing sigmas
+
+        apref.shrinkfactors.syn='8x4x4x1'; % SyN shrink factors
+        apref.shrinkfactors.scrf='4x4x2x1'; % SyN subcortical focus stage shrink factors
+        
+        
     case 'BSplineSyN'
         apref.antsmode='BSplineSyN';
-        apref.antsmode_suffix='[0.1,26,2,4]'; % as in example script in Tustison 2013
+        apref.antsmode_suffix='[0.1,26,0,3]';
+        
+        apref.convergence.syn='[100x70x50x20,1e-6,7]'; % SyN convergence params
+        apref.convergence.scrf='[100x70x50x20,1e-6,7]'; % SyN subcortical focus stage convergence params
+        
+        apref.smoothingsigmas.syn='3x2x1x0vox'; % SyN Smoothing sigmas
+        apref.smoothingsigmas.scrf='2x2x1x0vox'; % SyN subcortical focus stage smoothing sigmas
+        
+        apref.shrinkfactors.syn='6x4x2x1'; % SyN shrink factors
+        apref.shrinkfactors.scrf='6x4x2x1'; % SyN subcortical focus stage shrink factors
+
 end
 
 % Convergence
 apref.convergence.rigid='[1000x500x250x0,1e-7,10]'; % Rigid convergence params
 apref.convergence.affine='[1000x500x250x0,1e-7,10]'; % Affine convergence params
-apref.convergence.syn='[1000x500x250x0,1e-6,7]'; % SyN convergence params
-apref.convergence.scrf='[200x50x10x0,1e-6,7]'; % SyN subcortical focus stage convergence params
 
 % Shrink Factors (please keep in mind these refer to fixed space which is
 % typically 0.5 mm resolution in Lead-DBS whereas usually input images are
@@ -39,11 +57,7 @@ apref.convergence.scrf='[200x50x10x0,1e-6,7]'; % SyN subcortical focus stage con
 % of 1 at all.
 apref.shrinkfactors.rigid='12x8x4x1'; % Rigid shrink factors
 apref.shrinkfactors.affine='12x8x4x1'; % Affine shrink factors
-apref.shrinkfactors.syn='8x4x4x1'; % SyN shrink factors
-apref.shrinkfactors.scrf='4x4x2x1'; % SyN subcortical focus stage shrink factors
 
 % Smoothing Sigmas
 apref.smoothingsigmas.rigid='5x4x3x1vox'; % Rigid Smoothing sigmas
 apref.smoothingsigmas.affine='5x4x3x1vox'; % Affine Smoothing sigmas
-apref.smoothingsigmas.syn='4x3x1x1vox'; % SyN Smoothing sigmas
-apref.smoothingsigmas.scrf='2x2x1x1vox'; % SyN subcortical focus stage smoothing sigmas
