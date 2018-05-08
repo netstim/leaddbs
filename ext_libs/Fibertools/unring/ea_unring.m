448c8658f63f7d24cf69b83f2cb165dcd83a1986
Ningfei Li <ningfei.li@gmail.com>
2018-05-08 10:53:53 +0200

    Fix 'printf' in mesoft C++ code.

--- ext_libs/Fibertools/unring/ea_unring.m
+++ ext_libs/Fibertools/unring/ea_unring.m
@@ -8,18 +8,16 @@ function v = ea_unring(v,params)
 %                     maxW  right border of window used for TV computation (default 3)
 
 disp('Unring DTI...');
-if nargin == 1,
+if nargin == 1
     params = [1 3 20];
-end;
+end
 
 v = double(v);
 v = ringRm(v,params);
-
-
 
 %% add methods dump:
 cits={
     'Kellner, E., Dhital, B., Kiselev, V. G., & Reisert, M. (2015). Gibbs-ringing artifact removal based on local subvoxel-shifts. Magnetic Resonance in Medicine, n/a?n/a. http://doi.org/10.1002/mrm.26054'
     };
-ea_methods(options,['Diffusion-weighted acquisitions were corrected for Gibbs'' ringing artefacts following the approach described in (Kellner 2015; https://bitbucket.org/reisert/unring).'],cits);
+ea_methods(options,'Diffusion-weighted acquisitions were corrected for Gibbs'' ringing artefacts following the approach described in (Kellner 2015; https://bitbucket.org/reisert/unring).',cits);
     
