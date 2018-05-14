48a7a11d33819292f26494edfaaa93128a42c14a
Ningfei Li <ningfei.li@gmail.com>
2018-05-14 14:26:56 +0200

    Update mesoft make script.

--- ext_libs/mesoft/ccode/ft_make.m
+++ ext_libs/mesoft/ccode/ft_make.m
@@ -1,16 +1,20 @@
 if ismac
-    compflgs = '';
-    % compflgs = ' CXXFLAGS=''$CXXFLAGS -Wno-deprecated-register''';
+    compflags = '';
+%     compflags = ' CXXFLAGS=''$CXXFLAGS -Wno-deprecated-register''';
 elseif isunix
-    compflgs = ' COMPFLAGS=''$COMPFLAGS -static-libstdc++''';
+    compflags = ' COMPFLAGS=''$COMPFLAGS -static-libstdc++''';
+%     compflags = [' COMPFLAGS=''$COMPFLAGS -static-libstdc++''', ...
+%                 ' -DPARALLEL_OPENMP', ...
+%                 ' CXXFLAGS=''$CXXFLAGS -fopenmp''', ...
+%                 ' LINKLIBS=''$LINKLIBS -lgomp'''];
 elseif ispc
-    compflgs = ' COMPFLAGS="$COMPFLAGS /MT"';
+    compflags = ' COMPFLAGS="$COMPFLAGS /MT"';
 end
 
-eval(['mex' compflgs, ' AccumulateTrilin.cpp']);
-eval(['mex' compflgs, ' AccumulateTrilinWeighted.cpp']);
-eval(['mex' compflgs, ' BuildFibres.cpp']);
-eval(['mex' compflgs, ' pcRJMCMC.cpp']);
-eval(['mex' compflgs, ' printTOstderr.cpp']);
-eval(['mex' compflgs, ' reparametrize_arclen.cpp']);
-eval(['mex' compflgs, ' SelectCorticalFibers.cpp']);
+eval(['mex' compflags, ' AccumulateTrilin.cpp']);
+eval(['mex' compflags, ' AccumulateTrilinWeighted.cpp']);
+eval(['mex' compflags, ' BuildFibres.cpp']);
+eval(['mex' compflags, ' pcRJMCMC.cpp']);
+eval(['mex' compflags, ' printTOstderr.cpp']);
+eval(['mex' compflags, ' reparametrize_arclen.cpp']);
+eval(['mex' compflags, ' SelectCorticalFibers.cpp']);
