b67468285b31e27a615360c497ff7082f01c8aed
Ningfei Li <ningfei.li@gmail.com>
2018-05-15 12:57:11 +0200

    Update 'Gibbstracker' make script.
    Signed-off-by: Ningfei Li <ningfei.li@gmail.com>

--- ext_libs/Fibertools/Gibbstracker/make.m
+++ ext_libs/Fibertools/Gibbstracker/make.m
@@ -1,8 +1,17 @@
-mex AccumulateBilin.cpp
-mex AccumulateBilinWeighted.cpp
-mex anisoDiffusion.cpp
-mex anisoDiffusionHomogenous.cpp
-mex BuildFibres.cpp
-mex CreateConnectivityMatrixROI.cpp
-mex pcRJMCMC.cpp
-mex reparametrize_arclen.cpp
+if ismac
+    compflags = '';
+%     compflags = ' CXXFLAGS=''$CXXFLAGS -Wno-deprecated-register''';
+elseif isunix
+    compflags = ' COMPFLAGS=''$COMPFLAGS -static-libstdc++''';
+elseif ispc
+    compflags = ' COMPFLAGS="$COMPFLAGS /MT"';
+end
+
+eval(['mex' compflags, ' AccumulateBilin.cpp']);
+eval(['mex' compflags, ' AccumulateBilinWeighted.cpp']);
+eval(['mex' compflags, ' anisoDiffusion.cpp']);
+eval(['mex' compflags, ' anisoDiffusionHomogenous.cpp']);
+eval(['mex' compflags, ' BuildFibres.cpp']);
+eval(['mex' compflags, ' CreateConnectivityMatrixROI.cpp']);
+eval(['mex' compflags, ' pcRJMCMC.cpp']);
+eval(['mex' compflags, ' reparametrize_arclen.cpp']);
