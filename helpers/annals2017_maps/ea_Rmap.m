848f611bebc223bd9c8f8624a58377d8c1572b39
Ningfei Li <ningfei.li@gmail.com>
2018-07-12 11:22:01 +0200

    Fix 'ea_genX' parameters in 'ea_Rmap'.

--- helpers/annals2017_maps/ea_Rmap.m
+++ helpers/annals2017_maps/ea_Rmap.m
@@ -1,56 +1,55 @@
 function [R,Rperm,Rnaned,Rpermnaned]=ea_Rmap(varargin)
-    % creates a correlation nifti file given a set of images and a
-    % regressor ("R map" in Horn 2017 AoN)
+% creates a correlation nifti file given a set of images and a
+% regressor ("R map" in Horn 2017 AoN)
 
-    % ea_Rmap(fis,regressor,outputname,mask,sk,corrtype) % sk can be 'k','s','sk'
-    % for smoothing and normalization options. Mask only necessary if
-    % choosing 'k' option.
-    pthresh=0.05;
-    itercount=1000;
-    regressor=varargin{2};
-    output=varargin{3};
+% ea_Rmap(fis,regressor,outputname,mask,sk,corrtype) % sk can be 'k','s','sk'
+% for smoothing and normalization options. Mask only necessary if
+% choosing 'k' option.
 
-    if nargin<6
-        corrtype='Spearman';
-    else
-        corrtype=varargin{6};
-    end
-    [X,n]=ea_genX(varargin{1:6}); % accumulates all images into image matrix X.
-    X=X';
-    nnanix=~isnan(nansum(X,1)).*(abs(nansum(X,1)))>0;
-    if nargin>6
-        if strcmp(varargin{7},'permute')
-            Rperm=nan(1000,size(X,2));
-            regressorperm=repmat(regressor,1,itercount);
-            for i=1:itercount
-                regressorperm(:,i)=regressorperm(randperm(numel(regressor)),i);
-            end
-            Rperm(:,nnanix)=corr(regressorperm,X(:,nnanix),'type',corrtype,'rows','pairwise');
+pthresh=0.05;
+itercount=1000;
+regressor=varargin{2};
+output=varargin{3};
+
+if nargin<6
+    corrtype='Spearman';
+else
+    corrtype=varargin{6};
+end
+
+[X,n]=ea_genX(varargin{1:5}); % accumulates all images into image matrix X.
+X=X';
+nnanix=~isnan(nansum(X,1)).*(abs(nansum(X,1)))>0;
+
+if nargin>6
+    if strcmp(varargin{7},'permute')
+        Rperm=nan(1000,size(X,2));
+        regressorperm=repmat(regressor,1,itercount);
+        for i=1:itercount
+            regressorperm(:,i)=regressorperm(randperm(numel(regressor)),i);
         end
+        Rperm(:,nnanix)=corr(regressorperm,X(:,nnanix),'type',corrtype,'rows','pairwise');
     end
+end
 
+R=corr(regressor,X,'type',corrtype,'rows','pairwise');
+ea_exportmap(n,R,varargin{1:6});
 
-    R=corr(regressor,X,'type',corrtype,'rows','pairwise');
-    ea_exportmap(n,R,varargin{1:6});
+if exist('Rperm','var') % permutation test
+    Rpermnaned=[R;Rperm]; % for now, first entry is the unpermuted one.
+    sRd=sort([R;Rperm],1,'descend');
+    % delete values from Rpermnaned that are not significant (uncorrected):
+    delp=Rpermnaned<...
+        repmat(sRd(round((pthresh/2)*itercount),:),itercount+1,1);
+    deln=Rpermnaned>...
+        repmat(sRd(round((1-(pthresh/2))*itercount),:),itercount+1,1);
+    del=logical(delp.*deln);
+    Rpermnaned(del)=nan;
 
-    if exist('Rperm','var') % permutation test
-        Rpermnaned=[R;Rperm]; % for now, first entry is the unpermuted one.
-        sRd=sort([R;Rperm],1,'descend');
-        % delete values from Rpermnaned that are not significant (uncorrected):
-        delp=Rpermnaned<...
-            repmat(sRd(round((pthresh/2)*itercount),:),itercount+1,1);
-        deln=Rpermnaned>...
-            repmat(sRd(round((1-(pthresh/2))*itercount),:),itercount+1,1);
-        del=logical(delp.*deln);
-        Rpermnaned(del)=nan;
+    Rnaned=Rpermnaned(1,:);
+    Rpermnaned=Rpermnaned(2:end,:);
 
-        Rnaned=Rpermnaned(1,:);
-        Rpermnaned=Rpermnaned(2:end,:);
-
-        [pth,fn,ext]=fileparts(varargin{3});
-        varargin{3}=fullfile(pth,[fn,'_sig',ext]);
-        ea_exportmap(n,Rnaned,varargin{1:6});
-    end
-
-
-
+    [pth,fn,ext]=fileparts(varargin{3});
+    varargin{3}=fullfile(pth,[fn,'_sig',ext]);
+    ea_exportmap(n,Rnaned,varargin{1:6});
+end
--- helpers/annals2017_maps/ea_genX.m
+++ helpers/annals2017_maps/ea_genX.m
@@ -1,89 +1,97 @@
 function [X,n]=ea_genX(fis,regressor,output,mask,sk,ctype)
 
-    if ~exist('mask','var')
-       mask=nan; 
-    else
-       if isempty(mask)
-           mask=nan;
-       end
+if ~exist('mask','var')
+   mask=nan;
+else
+   if isempty(mask)
+       mask=nan;
+   end
+end
+
+% check wheter want to normalize or smooth data
+if exist('sk','var')
+    switch sk
+        case 'k'
+            dok=1; dos=0;
+        case 's'
+            dos=1; dok=0;
+        case 'sk'
+            dos=1; dok=1;
+        otherwise
+            dos=0; dok=0;
     end
-    
-    % check wheter want to normalize or smooth data
-    if exist('sk','var')
-        switch sk
-            case 'k'
-                dok=1; dos=0;
-            case 's'
-                dos=1; dok=0;
-            case 'sk'
-                dos=1; dok=1;
-            otherwise
-                dos=0; dok=0;
-        end
-    else
-        dos=0;
-        dok=0;
+else
+    dos=0;
+    dok=0;
+end
+
+tmpd=ea_getleadtempdir;
+
+if size(fis,2)==2 % left and right separate
+    origfis=fis;
+    clear fis
+    for f=1:size(origfis,1)
+        rnii=ea_load_nii(origfis{f,1});
+        lnii=ea_load_nii(origfis{f,2});
+        cnii=rnii;
+        cnii.img=ea_nanmean(cat(4,rnii.img,lnii.img),4);
+        guid=ea_generate_guid;
+        cnii.fname=[tmpd,guid,'.nii'];
+        ea_write_nii(cnii);
+        fis{f}=cnii.fname;
     end
-   
-    tmpd=ea_getleadtempdir;
-    
-    if size(fis,2)==2 % left and right separate
-        origfis=fis;
-        clear fis
-        for f=1:size(origfis,1)
-            rnii=ea_load_nii(origfis{f,1});
-            lnii=ea_load_nii(origfis{f,2});
-            cnii=rnii;
-            cnii.img=ea_nanmean(cat(4,rnii.img,lnii.img),4);
-            guid=ea_generate_guid;
-            cnii.fname=[tmpd,guid,'.nii'];
-            ea_write_nii(cnii);
-            fis{f}=cnii.fname;
-        end
-    end
-    
-    for f=1:length(fis)
-        if dok || dos
-            guid=ea_generate_guid;
-            [pth,fn,ext]=fileparts(fis{f});
-            if strcmp(ext,'.gz')
-                addgz='.gz';
-            else
-                addgz='';
-            end
-            copyfile(fis{f},[tmpd,guid,'.nii',addgz]);
-            if ~isempty(addgz)
-                gunzip([tmpd,guid,'.nii',addgz]);
-            end
-            nii=ea_load_nii([tmpd,guid,'.nii']);
-            if isnan(mask)
-                mask=1:numel(nii.img);
-                warning('If using k option should apply a mask');
-            end
-            nii.img(~mask)=nan;
-            if dok
-                nii.img(mask)=ea_normal(nii.img(mask),1,0,' ',0,1,'TRUE');
-            end
-            ea_write_nii(nii);
-            if dos
-                matlabbatch{1}.spm.spatial.smooth.data = {[tmpd,guid,'.nii,1']};
-                matlabbatch{1}.spm.spatial.smooth.fwhm = [2 2 2];
-                matlabbatch{1}.spm.spatial.smooth.dtype = 0;
-                matlabbatch{1}.spm.spatial.smooth.im = 0;
-                matlabbatch{1}.spm.spatial.smooth.prefix = 's';
-                spm_jobman('run',{matlabbatch});
-                clear matlabbatch
-                delete([tmpd,guid,'.nii']);
-                fis{f}=[tmpd,'s',guid,'.nii'];
-            end
-        end
-        n=ea_load_nii(fis{f});
-        if dos || dok % cleanup temp dir
-           delete(fis{f}); 
-        end
-        if ~exist('X','var')
-            X=n.img(:);
+end
+
+for f=1:length(fis)
+    if dok || dos
+        guid=ea_generate_guid;
+        [pth,fn,ext]=fileparts(fis{f});
+        if strcmp(ext,'.gz')
+            addgz='.gz';
         else
-            X=[X,n.img(:)];
+            addgz='';
+        end
+
+        copyfile(fis{f},[tmpd,guid,'.nii',addgz]);
+        if ~isempty(addgz)
+            gunzip([tmpd,guid,'.nii',addgz]);
+        end
+
+        nii=ea_load_nii([tmpd,guid,'.nii']);
+        if isnan(mask)
+            mask=1:numel(nii.img);
+            warning('If using k option should apply a mask');
+        end
+
+        nii.img(~mask)=nan;
+        if dok
+            nii.img(mask)=ea_normal(nii.img(mask),1,0,' ',0,1,'TRUE');
+        end
+
+        ea_write_nii(nii);
+
+        if dos
+            matlabbatch{1}.spm.spatial.smooth.data = {[tmpd,guid,'.nii,1']};
+            matlabbatch{1}.spm.spatial.smooth.fwhm = [2 2 2];
+            matlabbatch{1}.spm.spatial.smooth.dtype = 0;
+            matlabbatch{1}.spm.spatial.smooth.im = 0;
+            matlabbatch{1}.spm.spatial.smooth.prefix = 's';
+            spm_jobman('run',{matlabbatch});
+            clear matlabbatch
+            delete([tmpd,guid,'.nii']);
+            fis{f}=[tmpd,'s',guid,'.nii'];
         end
     end
+
+    n=ea_load_nii(fis{f});
+
+    if dos || dok % cleanup temp dir
+       delete(fis{f});
+    end
+
+    if ~exist('X','var')
+        X=n.img(:);
+    else
+        X=[X,n.img(:)];
+    end
+end
