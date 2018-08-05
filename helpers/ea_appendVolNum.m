946b352f01a87ae934d3e6a0eeaeb6a6b94ca1d5
Ningfei Li <ningfei.li@gmail.com>
2018-08-05 14:50:25 +0200

    Add function to append volnum to nii file path for the use in SPM.

---
+++ helpers/ea_appendVolNum.m
@@ -0,0 +1,47 @@
+function output = ea_appendVolNum(niifpath, volnum)
+% Append volume number to nifti file path for the use in SPM
+%
+% Example:
+%     ea_appendVolNum('A.nii') -> 'A.nii,1'
+%     ea_appendVolNum('A.nii', 1) -> 'A.nii,1'
+%     ea_appendVolNum('A.nii', [1,2]) -> {'A.nii,1'; 'A.nii,2'}
+%     ea_appendVolNum({'A.nii'}) -> {'A.nii,1'}
+%     ea_appendVolNum({'A.nii'}, 1) -> {'A.nii,1'}
+%     ea_appendVolNum({'A.nii'}, [1,2]) -> {'A.nii,1'; 'A.nii,2'}
+%     ea_appendVolNum({'A.nii', 'B.nii'}, [1,2]) -> {'A.nii,1'; 'B.nii,2'}
+%
+% Note: Output cell will be a N*1 cell array
+
+% Append ',1' by default
+if nargin < 2
+    volnum = 1;
+end
+
+% Return cell by default
+outputchar = 0;
+if ischar(niifpath)
+    niifpath = {niifpath};
+    if numel(volnum) == 1
+        outputchar = 1;
+    end
+end
+
+% Check the sizes of 'fname' and 'volnum'
+if numel(niifpath) == 1
+    niifpath = repmat(niifpath, numel(volnum), 1);
+elseif numel(volnum) == 1
+    volnum = repmat(volnum, numel(niifpath), 1);
+elseif numel(niifpath) ~= numel(volnum)
+    error('''volnum'' should match the size of ''fname''!')
+end
+
+% Append volume number
+output = cell(numel(niifpath), 1);
+for i=1:numel(output)
+    output{i} = [niifpath{i}, ',', num2str(volnum(i))];
+end
+
+% Return char as necessary
+if outputchar
+    output = output{1};
+end
