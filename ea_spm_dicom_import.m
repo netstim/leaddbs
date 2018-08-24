9b9d77f77c4e8fb3e35ce7176435d9ef8b803ee5
Ningfei Li <ningfei.li@gmail.com>
2018-08-24 16:46:04 +0200

    Add wrapper function for dcm2nii conversion using SPM.

---
+++ ea_spm_dicom_import.m
@@ -0,0 +1,38 @@
+function ea_spm_dicom_import(dicomdir, outdir)
+% Wrapper to convert DICOM to NIfTI using SPM
+
+if strcmp(dicomdir(end), filesep)
+    dicomdir = dicomdir(1:end-1);
+end
+
+if nargin < 2
+    outdir = fileparts(dicomdir);
+end
+
+dcm = ea_regexpdir(dicomdir, ['[^\', filesep, ']$'], 1);
+
+matlabbatch{1}.spm.util.import.dicom.data = dcm;
+matlabbatch{1}.spm.util.import.dicom.root = 'flat';
+matlabbatch{1}.spm.util.import.dicom.outdir = {outdir};
+matlabbatch{1}.spm.util.import.dicom.protfilter = '.*';
+matlabbatch{1}.spm.util.import.dicom.convopts.format = 'nii';
+matlabbatch{1}.spm.util.import.dicom.convopts.meta = 0;
+matlabbatch{1}.spm.util.import.dicom.convopts.icedims = 0;
+
+spm_jobman('run',{matlabbatch});
+clear matlabbatch
+
+% dcm = ea_searchdcm(dicomdir);
+
+% for i=1:length(dcm)
+%     matlabbatch{1}.spm.util.import.dicom.data = dcm{i};
+%     matlabbatch{1}.spm.util.import.dicom.root = 'flat';
+%     matlabbatch{1}.spm.util.import.dicom.outdir = {outdir};
+%     matlabbatch{1}.spm.util.import.dicom.protfilter = '.*';
+%     matlabbatch{1}.spm.util.import.dicom.convopts.format = 'nii';
+%     matlabbatch{1}.spm.util.import.dicom.convopts.meta = 0;
+%     matlabbatch{1}.spm.util.import.dicom.convopts.icedims = 0;
+%
+%     spm_jobman('run',{matlabbatch});
+%     clear matlabbatch
+% end
