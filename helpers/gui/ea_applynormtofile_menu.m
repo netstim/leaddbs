90386569176c9db82129909f5390683697cc3c8e
Ningfei Li <ningfei.li@gmail.com>
2018-11-06 12:28:19 +0100

    Fix apply normalization from menu.
    1. Set the default path of 'uigetfile' to patient folder (single patient
    mode) or parent folder of the patient folder (multiple patients mode)
    for transformation from [untouched] anchor space to template space.
    2. Fix workflow for forward and inverse transformation.
    3. Fix conflict between argument name and variable name:
    'untouchedanchor'.

--- helpers/gui/ea_applynormtofile_menu.m
+++ helpers/gui/ea_applynormtofile_menu.m
@@ -1,4 +1,4 @@
-function ea_applynormtofile_menu(~, ~, handles, useinverse, untouchedanchor, asoverlay,expdicom,fname)
+function ea_applynormtofile_menu(~, ~, handles, useinverse, untouchedanchor, asoverlay, expdicom, fname)
 if ~exist('untouchedanchor','var')
     untouchedanchor=0;
 end
@@ -24,7 +24,17 @@ else
 end
 
 if ~exist('fname','var')
-    [fis, path] = uigetfile({'*.nii';'*.nii.gz'}, 'Choose files to apply deformation to...', [ea_space], 'Multiselect', 'on');
+    if useinverse
+        defaultPath = ea_space;
+    else
+        if length(uipatdir) == 1
+            defaultPath = uipatdir{1};
+        else
+            defaultPath = fileparts(uipatdir{1});
+        end
+    end
+
+    [fis, path] = uigetfile({'*.nii';'*.nii.gz'}, 'Choose files to apply deformation to...', defaultPath, 'Multiselect', 'on');
 
     if ~ischar(fis) && ~iscell(fis)
         if ~fis
@@ -33,14 +43,76 @@ if ~exist('fname','var')
     end
 else
     [path,fis,ext]=fileparts(fname);
-    if ~isempty(path) % local file
-        path=[path,filesep];
+    if ~isempty(path)
+        path=[path, filesep];
+    else % local file
+        path=['.', filesep];
     end
     fis=[fis,ext];
 end
 
-for pt=1:length(uipatdir)
-    [options.root, options.patientname] = fileparts(uipatdir{pt});
+if useinverse % from template space to [untouched] achor space
+    for pt=1:length(uipatdir)
+        [options.root, options.patientname] = fileparts(uipatdir{pt});
+        options.root = [options.root, filesep];
+        options.earoot = ea_getearoot;
+        options.prefs = ea_prefs(options.patientname);
+        [options,presentfiles] = ea_assignpretra(options);
+        options.coregmr.method='SPM';
+
+        if ischar(fis)
+            fis = {fis};
+        end
+
+        to = cell(1, length(fis));
+        from = cell(1, length(fis));
+
+        for fi=1:length(fis)
+            to{fi} = [uipatdir{pt}, filesep, 'w', fis{fi}];
+            from{fi} = [path, fis{fi}];
+        end
+
+        ea_apply_normalization_tofile(options, from, to, [options.root, options.patientname, filesep], useinverse, interp);
+
+        if untouchedanchor % map from anchor to untouched anchor
+            ea_coreg2images(options,[options.root, options.patientname, filesep, presentfiles{1}],...
+                [options.root, options.patientname, filesep, 'raw_', presentfiles{1}],...
+                [options.root, options.patientname, filesep, 'rraw_', presentfiles{1}],...
+                to);
+            ea_delete([options.root, options.patientname, filesep, 'rraw_', presentfiles{1}]);
+
+            if asoverlay
+                untouchedanchorImage=ea_load_nii([options.root, options.patientname, filesep, 'raw_', presentfiles{1}]);
+                overlay=ea_load_nii(to{1});
+                fused=untouchedanchorImage;
+                fused.img(:)=zscore(fused.img(:));
+                fused.img=fused.img+overlay.img;
+                fused.img=ea_minmax(fused.img);
+                fused.img=fused.img*255;
+                fused.dt=[2,0];
+                [natpath,natfn,natext]=fileparts(untouchedanchorImage.fname);
+                fused.fname=fullfile(natpath,[natfn,'_overlay',natext]);
+                ea_write_nii(fused);
+            end
+
+            if expdicom
+                natpath=fileparts(untouchedanchorImage.fname);
+                [filename,pathname]=uigetfile('*.*','Select sample DICOM',[natpath,filesep,'DICOM']);
+                dicom_file=fullfile(pathname,filename);
+                merged_file=fused.fname;
+                newSeriesNumber=100;
+                newSeriesDescription='LeadDBS Plan';
+                mkdir(fullfile(natpath,'DICOM','LeadDBSExport'));
+                outputDirectory=fullfile(natpath,'DICOM','LeadDBSExport');
+                mergedImageVolume=1;
+                outputImagePosition=2;
+
+                uw_overlay_convert2dicom(dicom_file, merged_file, newSeriesNumber, newSeriesDescription, outputDirectory, mergedImageVolume, outputImagePosition);
+            end
+        end
+    end
+else % from [untouched] achor space to template space
+    [options.root, options.patientname] = fileparts(fileparts(path));
     options.root = [options.root, filesep];
     options.earoot = ea_getearoot;
     options.prefs = ea_prefs(options.patientname);
@@ -54,57 +126,18 @@ for pt=1:length(uipatdir)
     to = cell(1, length(fis));
     from = cell(1, length(fis));
 
-    if untouchedanchor && ~useinverse % need to map from untouched anchor anchor first
-        keyboard
+    for fi=1:length(fis)
+        to{fi} = [path, 'gl', fis{fi}];
+        from{fi} = [path, fis{fi}];
+    end
+
+    if untouchedanchor % map from untouched anchor to anchor first
         ea_coreg2images(options,[options.root, options.patientname, filesep, 'raw_', presentfiles{1}],...
             [options.root, options.patientname, filesep, presentfiles{1}],...
             [options.root, options.patientname, filesep, 'uraw_', presentfiles{1}],...
             from);
-    end
-
-    for fi=1:length(fis)
-        to{fi} = [uipatdir{pt},filesep, 'gl', fis{fi}];
-        if isempty(path)
-            from{fi} = [uipatdir{pt},filesep,fis{fi}];
-        else
-            from{fi} = [path, fis{fi}];
-        end
+        ea_delete([options.root, options.patientname, filesep, 'uraw_', presentfiles{1}]);
     end
 
     ea_apply_normalization_tofile(options, from, to, [options.root, options.patientname, filesep], useinverse, interp);
-
-    if untouchedanchor && useinverse % need to map from anchor to un touched (raw) anchor
-        ea_coreg2images(options,[options.root, options.patientname, filesep, presentfiles{1}],...
-            [options.root, options.patientname, filesep, 'raw_', presentfiles{1}],...
-            [options.root, options.patientname, filesep, 'rraw_', presentfiles{1}],...
-            to);
-        if asoverlay
-            untouchedanchor=ea_load_nii([options.root, options.patientname, filesep, 'raw_', presentfiles{1}]);
-            overl=ea_load_nii(to{1});
-            fused=untouchedanchor;
-            fused.img(:)=zscore(fused.img(:));
-            fused.img=fused.img+overl.img;
-            fused.img=ea_minmax(fused.img);
-            fused.img=fused.img*255;
-            fused.dt=[2,0];
-            [natpath,natfn,natext]=fileparts(untouchedanchor.fname);
-            fused.fname=fullfile(natpath,[natfn,'_overlay',natext]);
-            ea_write_nii(fused);
-        end
-
-        if expdicom
-            [natpath,natfn,natext]=fileparts(untouchedanchor.fname);
-            [filename,pathname]=uigetfile('*.*','Select sample DICOM',[natpath,filesep,'DICOM']);
-            dicom_file=fullfile(pathname,filename);
-            merged_file=fused.fname;
-            newSeriesNumber=100;
-            newSeriesDescription='LeadDBS Plan';
-            mkdir(fullfile(natpath,'DICOM','LeadDBSExport'));
-            outputDirectory=fullfile(natpath,'DICOM','LeadDBSExport');
-            mergedImageVolume=1;
-            outputImagePosition=2;
-
-            uw_overlay_convert2dicom(dicom_file, merged_file, newSeriesNumber, newSeriesDescription, outputDirectory, mergedImageVolume, outputImagePosition);
-        end
-    end
 end
