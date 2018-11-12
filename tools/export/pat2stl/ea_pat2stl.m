766c849dff4facd40d343d8616f053551ad97634
Ningfei Li <ningfei.li@gmail.com>
2018-11-12 16:35:59 +0100

    Also export 'combined_electrodes.stl'.

--- tools/export/pat2stl/ea_pat2stl.m
+++ tools/export/pat2stl/ea_pat2stl.m
@@ -1,17 +1,15 @@
 function ea_pat2stl(uipatdir,handles)
 
-
-    
 atlasset=get(handles.atlassetpopup,'String');
 atlasset=atlasset{get(handles.atlassetpopup,'Value')};
 
-
 cfv(1)=ea_atlas2stl({atlasset},[uipatdir,filesep,'export',filesep,'stl',filesep,'anatomy.stl']);
-%try % this is DBS specific.
-    cfv(2)=ea_electrode2stl([uipatdir,filesep],1,handles);
-    cfv(3)=ea_electrode2stl([uipatdir,filesep],2,handles);
-    cfv=ea_concatfv(cfv);
-    ea_stlwrite([uipatdir,filesep,'export',filesep,'stl',filesep,'combined_scene.stl'],cfv,'FACECOLOR',cfv.facevertexcdata);
-%end
 
+cfv(2)=ea_electrode2stl([uipatdir,filesep],1,handles);
+cfv(3)=ea_electrode2stl([uipatdir,filesep],2,handles);
 
+ecfv = ea_concatfv(cfv(2:3));
+ea_stlwrite([uipatdir,filesep,'export',filesep,'stl',filesep,'combined_electrodes.stl'],ecfv,'FACECOLOR',ecfv.facevertexcdata);
+
+scfv=ea_concatfv(cfv);
+ea_stlwrite([uipatdir,filesep,'export',filesep,'stl',filesep,'combined_scene.stl'],scfv,'FACECOLOR',scfv.facevertexcdata);
