c37601757da07cdb9ce1a34fc36b5d5e879debd6
Ningfei Li <ningfei.li@gmail.com>
2018-11-15 14:41:24 +0100

    Add discriminative fibers setting UI.

---
+++ ea_discfiberssetting.m
@@ -0,0 +1,150 @@
+function varargout = ea_discfiberssetting(varargin)
+% EA_DISCFIBERSSETTING MATLAB code for ea_discfiberssetting.fig
+%      EA_DISCFIBERSSETTING, by itself, creates a new EA_DISCFIBERSSETTING or raises the existing
+%      singleton*.
+%
+%      H = EA_DISCFIBERSSETTING returns the handle to a new EA_DISCFIBERSSETTING or the handle to
+%      the existing singleton*.
+%
+%      EA_DISCFIBERSSETTING('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in EA_DISCFIBERSSETTING.M with the given input arguments.
+%
+%      EA_DISCFIBERSSETTING('Property','Value',...) creates a new EA_DISCFIBERSSETTING or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before ea_discfiberssetting_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to ea_discfiberssetting_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help ea_discfiberssetting
+
+% Last Modified by GUIDE v2.5 15-Nov-2018 14:29:15
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @ea_discfiberssetting_OpeningFcn, ...
+                   'gui_OutputFcn',  @ea_discfiberssetting_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before ea_discfiberssetting is made visible.
+function ea_discfiberssetting_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to ea_discfiberssetting (see VARARGIN)
+
+% Choose default command line output for ea_discfiberssetting
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% select last selection
+prefs = ea_prefs('');
+discfibers = prefs.machine.lg.discfibers;
+set(handles.connthreshold, 'String', num2str(discfibers.connthreshold));
+set(handles.predthreshold, 'String', num2str(discfibers.predthreshold));
+set(handles.showpositiveonly, 'Value', discfibers.showpositiveonly);
+
+% UIWAIT makes ea_discfiberssetting wait for user response (see UIRESUME)
+% uiwait(handles.discfiberssetting);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = ea_discfiberssetting_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+function connthreshold_Callback(hObject, eventdata, handles)
+% hObject    handle to connthreshold (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of connthreshold as text
+%        str2double(get(hObject,'String')) returns contents of connthreshold as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function connthreshold_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to connthreshold (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+function predthreshold_Callback(hObject, eventdata, handles)
+% hObject    handle to predthreshold (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of predthreshold as text
+%        str2double(get(hObject,'String')) returns contents of predthreshold as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function predthreshold_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to predthreshold (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in showpositiveonly.
+function showpositiveonly_Callback(hObject, eventdata, handles)
+% hObject    handle to showpositiveonly (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of showpositiveonly
+
+
+% --- Executes on button press in savesetting.
+function savesetting_Callback(hObject, eventdata, handles)
+% hObject    handle to savesetting (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+prefs=ea_prefs('');
+discfibers = prefs.machine.lg.discfibers;
+discfibers.connthreshold = str2double(get(handles.connthreshold,'String'));
+discfibers.predthreshold = str2double(get(handles.predthreshold,'String'));
+discfibers.showpositiveonly = get(handles.showpositiveonly, 'Value');
+
+ea_setprefs('lg.discfibers', discfibers);
+
+delete(handles.discfiberssetting);
