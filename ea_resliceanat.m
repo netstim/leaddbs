fa8d152ac927dca3709340a3a54f82eb9d1fb3a7
Ningfei Li <ningfei.li@gmail.com>
2018-11-21 15:40:04 +0100

    'ea_resliceanat' method switched to 'ResampleImageBySpacing'.
    More robust than the SPM based 'reslice_nii', which sometimes doesn't
    actually do interpolation or sometimes makes the output image unusable
    for other toolsets like FSL.

--- ea_resliceanat.m
+++ ea_resliceanat.m
@@ -8,6 +8,7 @@ dim=V.mat(logical(eye(4)));
 dim=abs(dim(1:3));
 if any(dim>0.7)
     fprintf('\nInterpolating preoperative anatomical image...')
-    ea_reslice_nii([directory,options.prefs.prenii_unnormalized],[directory,options.prefs.prenii_unnormalized],[0.7 0.7 0.7],0,[],1);
+    ea_resample_image_by_spacing([directory,options.prefs.prenii_unnormalized],[0.7 0.7 0.7],0,5,0,[directory,options.prefs.prenii_unnormalized]);
+    % ea_reslice_nii([directory,options.prefs.prenii_unnormalized],[directory,options.prefs.prenii_unnormalized],[0.7 0.7 0.7],0,[],1);
     fprintf('\n\n');
 end
