b829836e055cb1d49053eb5ba18012e7b4aaf62e
Ningfei Li <ningfei.li@gmail.com>
2019-01-30 12:48:30 +0100

    discfibers control figure now supports showing positive/negative/both.

--- connectomics/ea_discfiberscontrol.m
+++ connectomics/ea_discfiberscontrol.m
@@ -22,16 +22,16 @@ function varargout = ea_discfiberscontrol(varargin)
 
 % Edit the above text to modify the response to help ea_discfiberscontrol
 
-% Last Modified by GUIDE v2.5 12-Feb-2018 12:19:21
+% Last Modified by GUIDE v2.5 30-Jan-2019 11:26:57
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
-    'gui_Singleton',  gui_Singleton, ...
-    'gui_OpeningFcn', @ea_discfiberscontrol_OpeningFcn, ...
-    'gui_OutputFcn',  @ea_discfiberscontrol_OutputFcn, ...
-    'gui_LayoutFcn',  [] , ...
-    'gui_Callback',   []);
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @ea_discfiberscontrol_OpeningFcn, ...
+                   'gui_OutputFcn',  @ea_discfiberscontrol_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
 if nargin && ischar(varargin{1})
     gui_State.gui_Callback = str2func(varargin{1});
 end
@@ -62,59 +62,144 @@ guidata(hObject, handles);
 % uiwait(handles.discfiberscontrol);
 
 resultfig = varargin{1};
-predthreshold = round(getappdata(resultfig, 'predthreshold')*100);
-showpositiveonly = getappdata(resultfig, 'showpositiveonly');
-
-set(handles.showPositiveOnly, 'Value', showpositiveonly);
-set(handles.showPositiveOnly, 'Callback', {@showPositiveOnlyChange, handles, resultfig});
+showfibersset = getappdata(resultfig, 'showfibersset');
+pospredthreshold = round(getappdata(resultfig, 'pospredthreshold')*100);
+negpredthreshold = round(getappdata(resultfig, 'negpredthreshold')*100);
 
 % Set current figure to draw slider
 set(0, 'CurrentFigure', handles.discfiberscontrol);
 set(handles.discfiberscontrol, 'Name', '');
 
+% Bind showfibersset setting changed function
+set(handles.showfiberssetpanel, 'SelectionChangedFcn', {@showfiberssetChanged, handles, resultfig});
+
 %% threshold slider:
-jSlider = javax.swing.JSlider(0,100);
-threshLabelPos = get(handles.thresholdLabel, 'Position');
-javacomponent(jSlider,[threshLabelPos(1)-6,threshLabelPos(2)-45,300,45]);
-set(jSlider, 'Value', predthreshold,...
+jSlider{1} = javax.swing.JSlider(0,100);
+posthreshLabelPos = get(handles.posthresholdLabel, 'Position');
+javacomponent(jSlider{1},[posthreshLabelPos(1)-6,posthreshLabelPos(2)-45,300,45]);
+set(jSlider{1}, 'Name', 'posthresholdSlider', 'Value', pospredthreshold,...
     'Background', java.awt.Color(1,1,1),...
     'MajorTickSpacing', 0.1, 'PaintLabels',true);
-set(handles.thresholdValue,'String',sprintf('%d',predthreshold));
-hjSlider = handle(jSlider, 'CallbackProperties');
-set(hjSlider, 'MouseReleasedCallback', {@sliderThresholdChange, handles, resultfig});
-set(hjSlider, 'StateChangedCallback', {@sliderThresholdChangeTxt, handles});
+set(handles.posthresholdValue,'String',sprintf('%d',pospredthreshold));
+hjSlider{1} = handle(jSlider{1}, 'CallbackProperties');
+set(hjSlider{1}, 'MouseReleasedCallback', {@sliderThresholdChange, handles, resultfig});
+set(hjSlider{1}, 'StateChangedCallback', {@sliderThresholdChangeTxt, handles});
+
+jSlider{2} = javax.swing.JSlider(0,100);
+negthreshLabelPos = get(handles.negthresholdLabel, 'Position');
+javacomponent(jSlider{2},[negthreshLabelPos(1)-6,negthreshLabelPos(2)-45,300,45]);
+set(jSlider{2}, 'Name', 'negthresholdSlider', 'Value', negpredthreshold,...
+    'Background', java.awt.Color(1,1,1),...
+    'MajorTickSpacing', 0.1, 'PaintLabels',true);
+set(handles.negthresholdValue,'String',sprintf('%d',negpredthreshold));
+hjSlider{2} = handle(jSlider{2}, 'CallbackProperties');
+set(hjSlider{2}, 'MouseReleasedCallback', {@sliderThresholdChange, handles, resultfig});
+set(hjSlider{2}, 'StateChangedCallback', {@sliderThresholdChangeTxt, handles});
+
+setappdata(handles.discfiberscontrol, 'hjSlider', hjSlider);
+
+switch showfibersset
+    case 'positive'
+        set(handles.showposonly, 'Value', 1);
+        jSlider{1}.enable;
+        jSlider{2}.disable;
+    case 'negative'
+        set(handles.shownegonly, 'Value', 1);
+        jSlider{1}.disable;
+        jSlider{2}.enable;
+    case 'both'
+        set(handles.showboth, 'Value', 1);
+        jSlider{1}.enable;
+        jSlider{2}.enable;
+    otherwise
+        set(handles.showboth, 'Value', 1);
+        jSlider{1}.enable;
+        jSlider{2}.enable;
+end
 
 
-function sliderThresholdChange(hObject, eventdata, handles, resultfig)
-slider = hObject;
-sliderThresholdChangeTxt(hObject, eventdata, handles);
+% --- Outputs from this function are returned to the command line.
+function varargout = ea_discfiberscontrol_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
 
-% predthreshold and showpostiveonly values from control figure
-predthreshold = slider.Value/100;
-showpositiveonly = get(handles.showPositiveOnly, 'Value');
+% Get default command line output from handles structure
+varargout{1} = handles.output;
 
-updateDiscFibers(resultfig, predthreshold, showpositiveonly);
-set(0, 'CurrentFigure', handles.discfiberscontrol);
+
+function showfiberssetChanged(hObject, eventdata, handles, resultfig)
+% Update showfibersset setting
+hjSlider = getappdata(handles.discfiberscontrol, 'hjSlider');
+switch get(get(handles.showfiberssetpanel, 'SelectedObject'), 'Tag')
+    case 'showposonly'
+        showfibersset = 'positive';
+        hjSlider{1}.enable;
+        hjSlider{2}.disable;
+    case 'shownegonly'
+        showfibersset = 'negative';
+        hjSlider{1}.disable;
+        hjSlider{2}.enable;
+    case 'showboth'
+        showfibersset = 'both';
+        hjSlider{1}.enable;
+        hjSlider{2}.enable;
+end
+
+% predthreshold values remain the same as in the resultfig
+pospredthreshold = getappdata(resultfig, 'pospredthreshold');
+negpredthreshold = getappdata(resultfig, 'negpredthreshold');
+
+updateDiscFibers(resultfig, showfibersset, pospredthreshold, negpredthreshold)
 
 
 function sliderThresholdChangeTxt(hObject, eventdata, handles)
 slider = hObject;
-set(handles.thresholdValue,'String',sprintf('%d',slider.Value));
+
+if slider.Enabled
+    switch slider.Name
+        case 'posthresholdSlider'
+            set(handles.posthresholdValue,'String',sprintf('%d',slider.Value));
+        case 'negthresholdSlider'
+             set(handles.negthresholdValue,'String',sprintf('%d',slider.Value));
+    end
+end
 
 
-function showPositiveOnlyChange(hObject, eventdata, handles, resultfig)
-checkbox = hObject;
+function sliderThresholdChange(hObject, eventdata, handles, resultfig)
+slider = hObject;
 
-% predthreshold value remains the same as in the resultfig
-predthreshold = getappdata(resultfig, 'predthreshold');
+if slider.Enabled
+    sliderThresholdChangeTxt(hObject, eventdata, handles);
+    
+    % showfibersset setting from the control figure
+    switch get(get(handles.showfiberssetpanel, 'SelectedObject'), 'Tag')
+        case 'showposonly'
+            showfibersset = 'positive';
+        case 'shownegonly'
+            showfibersset = 'negative';
+        case 'showboth'
+            showfibersset = 'both';
+    end
 
-% Showpostiveonly value from control figure
-showpositiveonly = get(checkbox, 'Value');
+    switch slider.Name
+        case 'posthresholdSlider'
+            % negpredthreshold value remains the same as in the resultfig
+            pospredthreshold = slider.Value/100;
+            negpredthreshold = getappdata(resultfig, 'negpredthreshold');
+        case 'negthresholdSlider'
+            % pospredthreshold value remains the same as in the resultfig
+            pospredthreshold = getappdata(resultfig, 'pospredthreshold');
+            negpredthreshold = slider.Value/100;
+    end
 
-updateDiscFibers(resultfig, predthreshold, showpositiveonly)
+    updateDiscFibers(resultfig, showfibersset, pospredthreshold, negpredthreshold);
+    set(0, 'CurrentFigure', handles.discfiberscontrol);
+end
 
 
-function updateDiscFibers(resultfig, predthreshold, showpositiveonly)
+function updateDiscFibers(resultfig, showfibersset, pospredthreshold, negpredthreshold)
 % Delete previous discfibers and colorbar
 delete(getappdata(resultfig, 'discfibers'));
 delete(getappdata(resultfig, 'cbfig'));
@@ -129,15 +214,19 @@ tfibcell = getappdata(resultfig, 'fibcell');
 % Determine threshold
 posits = getappdata(resultfig, 'posits');
 negits = getappdata(resultfig, 'negits');
-posthresh=posits(round(length(posits)*predthreshold));
+posthresh=posits(round(length(posits)*pospredthreshold));
+negthresh=negits(round(length(negits)*negpredthreshold));
 
-if showpositiveonly
-    negthresh = negits(1)-eps;
-    disp(['Fiber colors: Positive (T = ',num2str(posthresh),' ~ ',num2str(max(posits)), ')']);
-else
-    negthresh=negits(round(length(negits)*predthreshold));
-    disp(['Fiber colors: Positive (T = ',num2str(posthresh),' ~ ',num2str(max(posits)), ...
-          '); Negative (T = ',num2str(min(negits)),' ~ ',num2str(negthresh),').']);
+switch showfibersset
+    case 'positive'
+        negthresh = negits(1)-eps;
+        disp(['Fiber colors: Positive (T = ',num2str(posthresh),' ~ ',num2str(posits(1)), ')']);
+    case 'negative'
+        posthresh = posits(1)+eps;
+        disp(['Fiber colors: Negative (T = ',num2str(negits(1)),' ~ ',num2str(negthresh), ')']);
+    case 'both'
+        disp(['Fiber colors: Positive (T = ',num2str(posthresh),' ~ ',num2str(posits(1)), ...
+          '); Negative (T = ',num2str(negits(1)),' ~ ',num2str(negthresh),').']);
 end
 
 % Remove tvals and fibers outside the thresholding range
@@ -161,9 +250,16 @@ colorbarThreshold = 0.60; % Percentage of the pos/neg color to be kept
 negUpperBound=ceil(size(map,1)/2*colorbarThreshold);
 poslowerBound=floor((size(map,1)-size(map,1)/2*colorbarThreshold));
 alphas=zeros(size(fibcolorInd,1),1);
-if ~showpositiveonly
-    alphas(round(fibcolorInd)<=negUpperBound) = 1;
+switch showfibersset
+    case 'positive'
+        alphas(round(fibcolorInd)>=poslowerBound) = 1;
+    case 'negative'
+        alphas(round(fibcolorInd)<=negUpperBound) = 1;
+    case 'both'
+        alphas(round(fibcolorInd)>=poslowerBound) = 1;
+        alphas(round(fibcolorInd)<=negUpperBound) = 1;
 end
+
 alphas(round(fibcolorInd)>=poslowerBound) = 1;
 fibalpha=mat2cell(alphas,ones(size(fibcolorInd,1),1));
 
@@ -185,19 +281,26 @@ setappdata(resultfig, 'discfibers', h);
 % Set colorbar tick positions and labels
 cbvals = tvals(logical(alphas));
 % cbvals=tvalsRescale(logical(alphas));
-if showpositiveonly
-    cbmap = map(ceil(length(map)/2+0.5):end,:);
-    tick = [poslowerBound, length(map)] - floor(length(map)/2) ;
-    poscbvals = sort(cbvals(cbvals>0));
-    ticklabel = [poscbvals(1), max(cbvals)];
-    ticklabel = arrayfun(@(x) num2str(x,'%.2f'), ticklabel, 'Uni', 0);
-else
-    cbmap = map;
-    tick = [1, negUpperBound, poslowerBound, length(map)];
-    poscbvals = sort(cbvals(cbvals>0));
-    negcbvals = sort(cbvals(cbvals<0));
-    ticklabel = [min(cbvals), negcbvals(end), poscbvals(1), max(cbvals)];
-    ticklabel = arrayfun(@(x) num2str(x,'%.2f'), ticklabel, 'Uni', 0);
+switch showfibersset
+    case 'positive'
+        cbmap = map(ceil(length(map)/2+0.5):end,:);
+        tick = [poslowerBound, length(map)] - floor(length(map)/2) ;
+        poscbvals = sort(cbvals(cbvals>0));
+        ticklabel = [poscbvals(1), poscbvals(end)];
+        ticklabel = arrayfun(@(x) num2str(x,'%.2f'), ticklabel, 'Uni', 0);
+    case 'negative'
+        cbmap = map(1:floor(length(map)/2-0.5),:);
+        tick = [1, negUpperBound];
+        negcbvals = sort(cbvals(cbvals<0));
+        ticklabel = [negcbvals(1), negcbvals(end)];
+        ticklabel = arrayfun(@(x) num2str(x,'%.2f'), ticklabel, 'Uni', 0);
+    case 'both'
+        cbmap = map;
+        tick = [1, negUpperBound, poslowerBound, length(map)];
+        poscbvals = sort(cbvals(cbvals>0));
+        negcbvals = sort(cbvals(cbvals<0));
+        ticklabel = [min(cbvals), negcbvals(end), poscbvals(1), max(cbvals)];
+        ticklabel = arrayfun(@(x) num2str(x,'%.2f'), ticklabel, 'Uni', 0);
 end
 
 % Plot colorbar
@@ -205,13 +308,7 @@ cbfig = ea_plot_colorbar(cbmap, [], 'v', '', tick, ticklabel);
 set(cbfig, 'NumberTitle', 'off');
 setappdata(resultfig, 'cbfig', cbfig);
 
-
-% --- Outputs from this function are returned to the command line.
-function varargout = ea_discfiberscontrol_OutputFcn(hObject, eventdata, handles)
-% varargout  cell array for returning output args (see VARARGOUT);
-% hObject    handle to figure
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-
-% Get default command line output from handles structure
-varargout{1} = handles.output;
+% Update appdata in resultfig
+setappdata(resultfig, 'showfibersset', showfibersset);
+setappdata(resultfig, 'pospredthreshold', pospredthreshold);
+setappdata(resultfig, 'negpredthreshold', negpredthreshold);
