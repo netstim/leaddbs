bd8f4395806a7aa7c321c731416b0aa20a3e79a2
andreashorn <mail@andreas-horn.de>
2019-03-15 13:02:06 +0100

    Compatibility issue fix to get mapping from rCT to CT.

--- dependency/ea_getrawct2preniimat.m
+++ dependency/ea_getrawct2preniimat.m
@@ -15,7 +15,13 @@ directory=[options.root,options.patientname,filesep];
 
 switch options.prefs.reco.mancoruse
     case 'postop'
+        try
         load([directory 'ea_coregctmethod_applied.mat']);
+        catch
+            tmat=eye(4);
+            postopct=[directory,options.prefs.ctnii_coregistered];
+            return
+        end
         postopct=[directory,options.prefs.rawctnii_unnormalized];
 
         switch coregct_method_applied{end}
