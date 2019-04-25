006e0f3ecf5b22bd1fcd92247805191e9838a3d1
Ningfei Li <ningfei.li@gmail.com>
2019-04-25 14:31:41 +0200

    Fix setting altaslable visibility.
    There will be 'GraphicsPlaceholder' in the 'atlaslabels' when there are
    unilateral structures.

--- ea_showatlas.m
+++ ea_showatlas.m
@@ -537,16 +537,17 @@ end
 
 
 function atlabelsvisible(hobj,ev,obj,onoff)
+labelInd = arrayfun(@(x) isa(x, 'matlab.graphics.primitive.Text'), obj);
 if isempty(hobj)
-    arrayfun(@(label) set(label,'Visible',onoff), obj);
+    arrayfun(@(label) set(label,'Visible',onoff), obj(labelInd));
 else
     toggleTag = arrayfun(@(t) t.Tag, hobj.Parent.Children(1:end-3), 'Uni', 0);
     toggleState = arrayfun(@(t) t.State, hobj.Parent.Children(1:end-3), 'Uni', 0);
 
     if strcmp(onoff, 'on')
-        arrayfun(@(label) set(label,'Visible',toggleState{strcmp(label.Tag, toggleTag)}), obj);
+        arrayfun(@(label) set(label,'Visible',toggleState{strcmp(label.Tag, toggleTag)}), obj(labelInd));
     else
-        arrayfun(@(label) set(label,'Visible',onoff), obj);
+        arrayfun(@(label) set(label,'Visible',onoff), obj(labelInd));
     end
 end
 
