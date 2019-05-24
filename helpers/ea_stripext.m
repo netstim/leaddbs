7e81edb5b311940381c979631a931e5056a2196f
Ningfei Li <ningfei.li@gmail.com>
2019-05-24 11:31:31 +0200

    Fix strip file extension.

--- helpers/ea_stripext.m
+++ helpers/ea_stripext.m
@@ -1,5 +1,5 @@
-function fname = ea_stripext(fname)
-% Strip file extension
+function fname = ea_stripext(fpath)
+% Extract file name from path, strip file extension
 % Note: for NIfTI image, '.nii' or '.nii.gz' will be stripped
 
-fname = regexprep(fname, '(\.nii|\.nii\.gz|\.[^\.\s]+)$', '');
+fname = regexp(fpath, ['([^\', filesep, '\f\n\r\t\v]+?)(?=(\.nii(\.gz)?|\.[^\.\s]+)?$)'], 'match', 'once');
