3529fceca2f3a96b6c46766e204c04be4160859b
Loic Marx <loic.marx@gmail.com>
2019-06-05 10:55:34 +0200

    add note provenance

--- test/base/clearGlobal.m
+++ test/base/clearGlobal.m
@@ -6,6 +6,11 @@ function clearGlobal(globalName)
 %
 % INPUTS:
 %    globalName:    The name of the global variable to clear.
+%
+% NOTE:
+%    this function has been uploaded from
+%    https://github.com/opencobra/cobratoolbox/blob/master/src/base/utilities/clearGlobal.m
+%    [e6b4efd]
 
     clearvars('-global',globalName);
 end
