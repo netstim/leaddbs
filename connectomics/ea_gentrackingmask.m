4ddcb553c1b00032d23d4bf8c5659b3b6488aad7
Ningfei Li <ningfei.li@gmail.com>
2019-06-05 12:10:13 +0200

    Fix generating tracking mask.
    Now will regenerate the mask when needed. Whether the coregistration was
    approved or not is also took into consideration.

--- connectomics/ea_gentrackingmask.m
+++ connectomics/ea_gentrackingmask.m
@@ -3,29 +3,89 @@ directory=[options.root,options.patientname,filesep];
 
 ea_newseg(directory,options.prefs.prenii_unnormalized,0,options);
 
-%% Coreg options.prefs.prenii_unnormalized to b0 (for label.mat and FTR-Normalization)
-copyfile([directory,options.prefs.prenii_unnormalized],[directory,'c',options.prefs.prenii_unnormalized]);
-copyfile([directory,'c2',options.prefs.prenii_unnormalized],[directory,'cc2',options.prefs.prenii_unnormalized]);
-ea_conformspaceto([directory,'c',options.prefs.prenii_unnormalized],[directory,'cc2',options.prefs.prenii_unnormalized],1,[],[],0); % make sure cc2 and anat are exactly in same space (even across software packages)
-ea_backuprestore([directory,'c',options.prefs.prenii_unnormalized]);
-affinefile = ea_coreg2images(options, ...
-    [directory,'c',options.prefs.prenii_unnormalized], ... % moving
-    [directory,options.prefs.b0], ... % fix
-    [directory,'c',options.prefs.prenii_unnormalized], ... % out
-    {[directory,'cc2',options.prefs.prenii_unnormalized]}, ... % other
-    1); % writeout transform
+b0_anat = [ea_stripext(options.prefs.b0),'_',ea_stripext(options.prefs.prenii_unnormalized)];
 
-% Rename saved transform for further use: canat2b0*.mat to anat2b0*.mat,
-% and b02canat2b0*.mat to b02anat*.mat
-[~, anat] = ea_niifileparts(options.prefs.prenii_unnormalized);
-if ~isempty(affinefile)
-    for i = 1:numel(affinefile)
-        movefile(affinefile{i}, strrep(affinefile{i}, ['c',anat], anat));
+if exist([directory,'ea_coreg_approved.mat'], 'file')
+    isapproved = load([directory,'ea_coreg_approved.mat']);
+    if isfield(isapproved, b0_anat)
+        isapproved = isapproved.(b0_anat);
+    end
+else
+    isapproved = 0;
+end
+
+if ~isapproved
+    fprintf(['\nCoregistration from b0 to anat not approved!\n', ...
+             'Will do the coregistration using the method chosen from GUI.\n\n'])
+    if exist([directory,'ea_coregmrmethod_applied.mat'], 'file')
+        coregmrmethod = load([directory,'ea_coregmrmethod_applied.mat']);
+    end
+    coregmrmethod.(b0_anat) = options.coregmr.method;
+    save([directory,'ea_coregmrmethod_applied.mat'],'-struct','coregmrmethod');
+    docoreg = 1;
+else
+    if exist([directory,'ea_coregmrmethod_applied.mat'], 'file')
+        coregmrmethod = load([directory,'ea_coregmrmethod_applied.mat']);
+        if isfield(coregmrmethod, b0_anat) && ~isempty(coregmrmethod.(b0_anat))
+            transform = [ea_stripext(options.prefs.prenii_unnormalized), '2', ea_stripext(options.prefs.b0), ...
+                         '_', lower(strrep(coregmrmethod.(b0_anat), 'Hybrid SPM & ', '')), '\d*\.(mat|h5)$'];
+            transform = ea_regexpdir(directory, transform, 0);
+
+            if isempty(transform) % Approved but the transformation file not found
+                fprintf(['\nThe coregistration between b0 and anat using ''%s'' has been approved.\n', ...
+                         'But the tranformation file doesn''t exist!\n', ...
+                         'Will redo the coregistration using ''%s''.\n\n'], ...
+                         coregmrmethod.(b0_anat), coregmrmethod.(b0_anat));
+                docoreg = 1;
+            else % Approved and the transformation file found
+                transform = transform{end};
+                fprintf(['\nThe coregistration between b0 and anat using ''%s'' has been approved.\n', ...
+                         'Will use this transform to generate the fiber tracking mask:\n', ...
+                         '%s\n\n'], coregmrmethod.(b0_anat), transform);
+                docoreg = 0;
+            end
+            options.coregmr.method = coregmrmethod.(b0_anat);
+        else % 'ea_coregmrmethod_applied.mat' found but method not set, redo coreg use method from GUI
+            coregmrmethod.(b0_anat) = options.coregmr.method;
+            save([directory,'ea_coregmrmethod_applied.mat'],'-struct','coregmrmethod');
+            docoreg = 1;
+        end
+    else % 'ea_coregmrmethod_applied.mat' not found, redo coreg use method from GUI
+        coregmrmethod.(b0_anat) = options.coregmr.method;
+        save([directory,'ea_coregmrmethod_applied.mat'],'-struct','coregmrmethod');
+        docoreg = 1;
     end
 end
 
-movefile([directory,'cc2',options.prefs.prenii_unnormalized],[directory,'trackingmask.nii']);
-delete([directory,'c',options.prefs.prenii_unnormalized]);
+if docoreg
+    copyfile([directory,options.prefs.prenii_unnormalized],[directory,'c',options.prefs.prenii_unnormalized]);
+    copyfile([directory,'c2',options.prefs.prenii_unnormalized],[directory,'cc2',options.prefs.prenii_unnormalized]);
+    ea_conformspaceto([directory,'c',options.prefs.prenii_unnormalized],[directory,'cc2',options.prefs.prenii_unnormalized],1,[],[],0);
+    ea_backuprestore([directory,'c',options.prefs.prenii_unnormalized]);
+
+    affinefile = ea_coreg2images(options, ...
+        [directory,'c',options.prefs.prenii_unnormalized], ... % moving
+        [directory,options.prefs.b0], ... % fix
+        [directory,'c',options.prefs.prenii_unnormalized], ... % out
+        {[directory,'cc2',options.prefs.prenii_unnormalized]}, ... % other
+        1); % writeout transform
+
+    % Rename saved transform for further use: canat2b0*.mat to anat2b0*.mat,
+    % and b02canat*.mat to b02anat*.mat
+    [~, anat] = ea_niifileparts(options.prefs.prenii_unnormalized);
+    if ~isempty(affinefile)
+        for i = 1:numel(affinefile)
+            movefile(affinefile{i}, strrep(affinefile{i}, ['c',anat], anat));
+        end
+    end
+
+    movefile([directory,'cc2',options.prefs.prenii_unnormalized],[directory,'trackingmask.nii']);
+    delete([directory,'c',options.prefs.prenii_unnormalized]);
+else
+    % Reuse approved coregistration
+    ea_apply_coregistration([directory,options.prefs.b0], [directory,'c2',options.prefs.prenii_unnormalized], ...
+                            [directory,'trackingmask.nii'], transform);
+end
 
 tr=ea_load_nii([options.root,options.patientname,filesep,'trackingmask.nii']);
 if threshold
@@ -34,12 +94,6 @@ if threshold
     ea_write_nii(tr);
 end
 
-% apply inverse to b0 to gen checkreg:
-coregmethod=options.coregmr.method;
-T=load([directory,'ea_coregmrmethod_applied.mat']);
-if isfield(T,['b0_',ea_stripext(options.prefs.prenii_unnormalized)])
-    coregmethod=T.(['b0_',ea_stripext(options.prefs.prenii_unnormalized)]);
-end
-ea_apply_coregistration([directory,options.prefs.b0],[directory,options.prefs.prenii_unnormalized],[directory,ea_stripext(options.prefs.b0),'_',options.prefs.prenii_unnormalized],coregmethod);
-
-
+% Transform anat to b0 to generate checkreg image:
+ea_apply_coregistration([directory,options.prefs.b0], [directory,options.prefs.prenii_unnormalized], ...
+                        [b0_anat,'.nii'], strrep(options.coregmr.method, 'Hybrid SPM & ', ''));
