93b5ad86882a6d7002dd7c837bc792180ec1dd42
Loic Marx <loic.marx@gmail.com>
2019-06-06 15:23:55 +0200

    note provenance for setGlobal

--- test/base/setGlobal.m
+++ test/base/setGlobal.m
@@ -7,6 +7,11 @@ function setGlobal(globalName,globalValue)
 % INPUTS:
 %    globalName:    A string representing the name of the global variable
 %    globalValue:   The value to set the global variable to
+%
+% NOTE:
+%    this function has been adapted from
+%    https://github.com/opencobra/cobratoolbox/blob/master/src/base/utilities/setGlobal.m
+%    [e6b4efd]
 
     eval([ globalName '_val = globalValue;']);
     eval(['global ' globalName]);
