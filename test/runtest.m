7c103cfc18c77b4ed20673d5b076578618d1fdb8
Loic Marx <loic.marx@gmail.com>
2019-06-07 16:02:49 +0200

    fix bug in path

--- test/runtest.m
+++ test/runtest.m
@@ -6,5 +6,4 @@
 % Run this file once (F5) to automatically make sure all leadDBS files (including subdirectories) are in your MATLAB path.
 leadDBSDir = fileparts(mfilename('fullpath')); 
 addpath(genpath(leadDBSDir));
-addpath(genpath([getenv('ARTENOLIS_SOFT_PATH)' '/tools/spm12'])) 
-savepath();
+addpath(genpath([getenv('ARTENOLIS_SOFT_PATH') '/tools/spm12']));
