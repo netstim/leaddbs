9c69c2793ce3e4db6acc734f31f460ee0d17b405
Ningfei Li <ningfei.li@gmail.com>
2019-06-11 15:56:47 +0200

    Optimize atlas2labeling conversion.

--- support_scripts/ea_atlas2labeling.m
+++ support_scripts/ea_atlas2labeling.m
@@ -1,14 +1,22 @@
-function ea_atlas2labeling(atlasname,spacefile)
+function ea_atlas2labeling(atlasname, spacefile)
 
-
-copyfile([ea_space([],'atlases'),atlasname], [ea_space([],'atlases'),atlasname,'_copy']);
 if ~exist('spacefile','var')
     spacefile = [ea_space,'t2.nii'];
 end
 
+copyfile([ea_space([],'atlases'),atlasname], [ea_space([],'atlases'),atlasname,'_copy']);
 copybase = [ea_space([],'atlases'), atlasname, '_copy', filesep];
 
+hdr = ea_fslhd(spacefile);
+volnum = length(ea_regexpdir([copybase, 'lh'], '.*\.nii(\.gz)?$', 0)) + ...
+         length(ea_regexpdir([copybase, 'rh'], '.*\.nii(\.gz)?$', 0)) + ...
+         length(ea_regexpdir([copybase, 'mixed'], '.*\.nii(\.gz)?$', 0)) + ...
+         length(ea_regexpdir([copybase, 'midline'], '.*\.nii(\.gz)?$', 0));
+Avol = zeros(hdr.dim1, hdr.dim2, hdr.dim3, volnum);
+Astr = cell(volnum, 1);
+
 subfs = {'lh', 'rh', 'mixed', 'midline'};
+
 cnt = 1;
 for subf=1:length(subfs)
     subbase = [copybase, subfs{subf}, filesep];
@@ -21,36 +29,40 @@ for subf=1:length(subfs)
             append = '';
     end
 
-   gzfiles = ea_regexpdir(subbase, '.*\.nii\.gz', 0);
-   if ~isempty(gzfiles)
-       gunzip(gzfiles);
-       ea_delete(gzfiles);
-   end
+    gzfiles = ea_regexpdir(subbase, '.*\.nii\.gz$', 0);
+    if ~isempty(gzfiles)
+        gunzip(gzfiles);
+        ea_delete(gzfiles);
+    end
 
-   nuclei = dir([subbase, '*.nii']);
+    niifiles = ea_regexpdir(subbase, '.*\.nii$', 0);
+    for i=1:length(niifiles)
+        disp(['Reslicing structures ', num2str(i,'%02d'), '/', num2str(length(niifiles),'%02d'), '...']);
+        ea_conformspaceto(spacefile, niifiles{i}, 1);
+    end
 
-   for nucleus=1:length(nuclei)
-       ea_conformspaceto(spacefile, [subbase,nuclei(nucleus).name], 1);
-       [~,nuclname] = fileparts(nuclei(nucleus).name);
-       Astr{cnt} = [nuclname,append];
-       nii = ea_load_nii([subbase, nuclei(nucleus).name]);
-       if ~exist('Avol','var') % init
-           Avol = zeros(size(nii.img));
-       end
-       Avol(:,:,:,cnt) = nii.img;
-       cnt = cnt+1;
-   end
-
+    for i=1:length(niifiles)
+        disp(['Stacking structures ', num2str(i,'%02d'), '/', num2str(length(niifiles),'%02d'), '...']);
+        [~, nuclname] = fileparts(niifiles{i});
+        Astr{cnt} = [nuclname, append];
+        nii = ea_load_nii(niifiles{i});
+        nii.img(isnan(nii.img)) = 0;
+        nii.img(nii.img<0.5) = 0;
+        Avol(:,:,:,cnt) = nii.img;
+        cnt = cnt+1;
+    end
 end
 
-Avol(isnan(Avol)) = 0;
-Avol(Avol<0.5) = 0;
 [~,Fvol] = max(Avol,[],4);
+clear Avol
+
 lab = nii;
 lab.img = Fvol;
 lab.fname = [ea_space([],'labeling'), atlasname, '.nii'];
+disp('Write out NIfTI..');
 ea_write_nii(lab);
 
+disp('Write out TXT..');
 f = fopen([ea_space([],'labeling'), atlasname, '.txt'], 'w');
 for nucl=1:length(Astr)
     fprintf(f,'%d %s\n', nucl, Astr{nucl});
@@ -58,3 +70,5 @@ end
 fclose(f);
 
 rmdir([ea_space([],'atlases'), atlasname, '_copy'], 's')
+
+disp('Done!');
