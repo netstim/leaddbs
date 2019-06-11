82c40b21c96266cd7c08b5e8e3d31f103433111f
Ningfei Li <ningfei.li@gmail.com>
2019-06-11 14:48:57 +0200

    Formatting.

--- support_scripts/ea_atlas2labeling.m
+++ support_scripts/ea_atlas2labeling.m
@@ -1,60 +1,60 @@
 function ea_atlas2labeling(atlasname,spacefile)
 
 
-copyfile([ea_space([],'atlases'),atlasname],[ea_space([],'atlases'),atlasname,'_copy']);
+copyfile([ea_space([],'atlases'),atlasname], [ea_space([],'atlases'),atlasname,'_copy']);
 if ~exist('spacefile','var')
-    spacefile=[ea_space,'t2.nii'];
+    spacefile = [ea_space,'t2.nii'];
 end
 
-copybase=[ea_space([],'atlases'),atlasname,'_copy',filesep];
+copybase = [ea_space([],'atlases'), atlasname, '_copy', filesep];
 
-subfs={'lh','rh','mixed','midline'};
-cnt=1;
+subfs = {'lh', 'rh', 'mixed', 'midline'};
+cnt = 1;
 for subf=1:length(subfs)
-    subbase=[copybase,subfs{subf},filesep];
+    subbase = [copybase, subfs{subf}, filesep];
     switch subfs{subf}
         case 'lh'
-            append='_L';
+            append = '_L';
         case 'rh'
-            append='_R';
+            append = '_R';
         otherwise
-            append='';
+            append = '';
     end
 
-   gzfiles = ea_regexpdir(subbase,'.*\.nii\.gz',0);
+   gzfiles = ea_regexpdir(subbase, '.*\.nii\.gz', 0);
    if ~isempty(gzfiles)
        gunzip(gzfiles);
        ea_delete(gzfiles);
    end
 
-   nuclei=dir([subbase,'*.nii']);
+   nuclei = dir([subbase, '*.nii']);
 
    for nucleus=1:length(nuclei)
-       ea_conformspaceto(spacefile,[subbase,nuclei(nucleus).name],1);
-       [~,nuclname]=fileparts(nuclei(nucleus).name);
-       Astr{cnt}=[nuclname,append];
-       nii=ea_load_nii([subbase,nuclei(nucleus).name]);
+       ea_conformspaceto(spacefile, [subbase,nuclei(nucleus).name], 1);
+       [~,nuclname] = fileparts(nuclei(nucleus).name);
+       Astr{cnt} = [nuclname,append];
+       nii = ea_load_nii([subbase, nuclei(nucleus).name]);
        if ~exist('Avol','var') % init
-           Avol=zeros(size(nii.img));
+           Avol = zeros(size(nii.img));
        end
-       Avol(:,:,:,cnt)=nii.img;
-       cnt=cnt+1;
+       Avol(:,:,:,cnt) = nii.img;
+       cnt = cnt+1;
    end
 
 end
 
-Avol(isnan(Avol))=0;
-Avol(Avol<0.5)=0;
-[~,Fvol]=max(Avol,[],4);
-lab=nii;
-lab.img=Fvol;
-lab.fname=[ea_space([],'labeling'),atlasname,'.nii'];
+Avol(isnan(Avol)) = 0;
+Avol(Avol<0.5) = 0;
+[~,Fvol] = max(Avol,[],4);
+lab = nii;
+lab.img = Fvol;
+lab.fname = [ea_space([],'labeling'), atlasname, '.nii'];
 ea_write_nii(lab);
 
-f=fopen([ea_space([],'labeling'),atlasname,'.txt'],'w');
+f = fopen([ea_space([],'labeling'), atlasname, '.txt'], 'w');
 for nucl=1:length(Astr)
-    fprintf(f,'%d %s\n',nucl,Astr{nucl});
+    fprintf(f,'%d %s\n', nucl, Astr{nucl});
 end
 fclose(f);
 
-rmdir([ea_space([],'atlases'),atlasname,'_copy'],'s')
+rmdir([ea_space([],'atlases'), atlasname, '_copy'], 's')
