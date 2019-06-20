0cca0213e1c11ae68026aebb0c3c315e17bca1c3
Andreas Husch <27815260+adhusch@users.noreply.github.com>
2019-06-20 14:27:39 +0200

    Update ea_coregctmri_itksnap.m

--- ea_coregctmri_itksnap.m
+++ ea_coregctmri_itksnap.m
@@ -5,27 +5,40 @@ function varargout=ea_coregctmri_itksnap(options)
 % Group
 % Andreas Husch
 
-if ischar(options) % return name of method.
+if ischar(options) % return name of method (e.g. used in the GUI)
     varargout{1}='ITK-SNAP (ANTs compatible ITK transform file)'; %TODO make sure matlab finds the path
-    varargout{2}={'SPM8','SPM12'};
-    varargout{3}=['nan']; % suggestion for alpha-parameter.
+    varargout{2}={'SPM8','SPM12'}; % check this
+    varargout{3}=['nan'];
     return
 end
 
 disp('Opening ITK-SNAP for coregistration of postop CT to preop MRI...')
 disp('Make sure to save transformation file as "postop_ct2anat_t1_ants1.txt"! Process will continue afer transformation file exists.');
-try
-    system(['/usr/local/bin/itksnap ' ...
-        '-g ' options.root,options.patientname,filesep,options.prefs.prenii_unnormalized ' ' ...
-        '-o ' options.root,options.patientname,filesep,options.prefs.rawctnii_unnormalized ' ' ...
-        '--cwd ' options.root,options.patientname,filesep] );
-    while ~exist([options.root,options.patientname,filesep,'postop_ct2anat_t1_ants1.txt'], 'file')
-        pause(2); % (semi) busy waiting..
-    end
-catch
-    warning('Coulnt run ITK-SNAP successfully! Make sure itksnap is available in your path (Launch ITK-SNAP and see in Help / Install Command Line Tools)');
+
+%% Get itksnap binary (should be in path by following ITKSNAP / Help / Install Comand Line Tools)
+if ispc
+    itksnap = 'itksnap.exe';
+else
+    itksnap = '/usr/local/bin/itksnap';
 end
 
+cmd = [itksnap ' ' ...
+    '-g "' options.root,options.patientname,filesep,options.prefs.prenii_unnormalized '" ' ...
+    '-o "' options.root,options.patientname,filesep,options.prefs.rawctnii_unnormalized '" ' ...
+    '--cwd "' options.root,options.patientname,filesep '"'];
+
+try
+    if ~ispc
+        system(['bash -c ''' cmd ''' &']); % running in background (&) is key to avoid crash!
+    else
+        system(cmd);
+    end
+    while ~exist([options.root,options.patientname,filesep,'postop_ct2anat_t1_ants1.txt'], 'file')
+        pause(2); % semi busy waiting ... ;-)
+    end
+catch
+    warning('Could not run ITK-SNAP successfully! Make sure itksnap is available in your path (Launch ITK-SNAP and see in Help / Install Command Line Tools)');
+end
 
 %% Save ITK .txt also as ANTS .mat
 disp('Generating .mat version of transform to be re-read by LeadDBS for internal use later...');
@@ -44,4 +57,5 @@ ea_apply_coregistration([options.root,options.patientname,filesep,options.prefs.
     [options.root,options.patientname,filesep,options.prefs.ctnii_coregistered]);
 disp('Coregistration done.');
 
-% alternative to using txt: /opt/ANTSGit/bin/ConvertTransformFile 3 postop_ct2anat_t1_ants1.txt postop_ct2anat_t1_ants1.mat --convertToAffineType
+
+
