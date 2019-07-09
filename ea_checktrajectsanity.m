2bc6f214bf8ffac481fcacba60c7a91a8d1f9437
Daniel Duarte Tojal <daniel.duarte.001@student.uni.lu>
2019-07-09 13:23:35 +0200

    Add documentation template

--- ea_checktrajectsanity.m
+++ ea_checktrajectsanity.m
@@ -1,4 +1,20 @@
 function res=checktrajectsanity(trajvector)
+% 
+%
+% USAGE:
+%
+%    res = checktrajectsanity(trajvector)
+%
+% INPUT:
+%    trajvector:
+%
+% OUTPUT:
+%    res:
+%
+% .. AUTHOR:
+%       - Andreas Horn, Original file
+%       - Ning Fey, Original file
+%       - Daniel Duarte, Documentation
 
 load('trajvectors');
 trajvectors=[trajvectors;trajvector];
