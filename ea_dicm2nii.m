a56764e5a0eb742f268856ef69164096fb696cbc
Daniel Duarte Tojal <daniel.duarte.001@student.uni.lu>
2019-07-09 14:27:44 +0200

    Add documentation template

--- ea_dicm2nii.m
+++ ea_dicm2nii.m
@@ -1,3 +1,17 @@
 function ea_dicm2nii(indir,outdir)
+%
+%
+% USAGE:
+%
+%    ea_dicm2nii(indir,outdir)
+%
+% INPUTS:
+%    indir:
+%    outdir:
+%
+% .. AUTHOR:
+%       - Andreas Horn, Original file
+%       - Ning Fey, Original file
+%       - Daniel Duarte, Documentation
 
 dicm2nii(indir, outdir, 0)
