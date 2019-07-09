2a0fc51b65f6862a3fc48e6e9030cae778fc7889
Daniel Duarte Tojal <daniel.duarte.001@student.uni.lu>
2019-07-09 11:20:11 +0200

    Add documentation template

--- ea_centroid.m
+++ ea_centroid.m
@@ -1,6 +1,21 @@
 function stats=ea_centroid(bw)
-% small replacement for the regionprops function (centroid only) which
+% Small replacement for the regionprops function (centroid only) which
 % requires the image processing toolbox.
+%
+% USAGE:
+%
+%    stats=ea_centroid(bw)
+%
+% INPUTS:
+%    bw:
+%
+% OUTPUTS:
+%    stats:
+%
+% .. AUTHOR:
+%       - Andreas Horn, Original file
+%       - Ning Fey, Original file
+%       - Daniel Duarte, Documentation
 
 [xx,yy]=find(bw);
 xy=[yy,xx];
