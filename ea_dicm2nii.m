cf4e968b45697c925cddaf496866b286de4d9edf
Daniel Duarte Tojal <daniel.duarte.001@student.uni.lu>
2019-07-16 11:23:35 +0200

    Update the function

--- ea_dicm2nii.m
+++ ea_dicm2nii.m
@@ -1,17 +1,12 @@
 function ea_dicm2nii(indir,outdir)
-%
+% Wrapper for dicm2nii (Xiangrui Li, https://github.com/xiangruili/dicm2nii)
 %
 % USAGE:
 %
 %    ea_dicm2nii(indir,outdir)
 %
 % INPUTS:
-%    indir:
-%    outdir:
-%
-% .. AUTHOR:
-%       - Andreas Horn, Original file
-%       - Ningfei Li, Original file
-%       - Daniel Duarte, Documentation
+%    indir: DICOM folder
+%    outdir: output folder for NIfTI files
 
 dicm2nii(indir, outdir, 0)
