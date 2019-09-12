f217efde6a9fc7fb2024df1c855c9bbb4e1c4404
Simon Oxenford <oxenfordsimon@gmail.com>
2019-09-12 16:02:07 +0200

    Make view transition steps variable

--- ea_defaultview_transition.m
+++ ea_defaultview_transition.m
@@ -12,29 +12,34 @@ togglestates_init.refreshview = 1;
 
 v_init = ea_view();
 
-steps = 50;
 
-xyzmm_diff = (togglestates.xyzmm - togglestates_init.xyzmm) / steps;
-xyztransparencies_diff = (togglestates.xyztransparencies - togglestates_init.xyztransparencies) / steps;
-v_az_diff = (v.az - v_init.az) / steps;
-v_el_diff = (v.el - v_init.el) / steps;
-v_camva_diff = (v.camva - v_init.camva) / steps;
-v_camup_diff = (v.camup - v_init.camup) / steps;
+xyzmm_diff = (togglestates.xyzmm - togglestates_init.xyzmm);
+xyztransparencies_diff = (togglestates.xyztransparencies - togglestates_init.xyztransparencies);
+v_az_diff = (v.az - v_init.az);
+v_el_diff = (v.el - v_init.el);
+v_camva_diff = (v.camva - v_init.camva);
+v_camup_diff = (v.camup - v_init.camup);
 v_out.camproj = 'orthographic';
-v_camtarget_diff = (v.camtarget - v_init.camtarget) / steps;
-v_campos_diff = (v.campos - v_init.campos) / steps;
+v_camtarget_diff = (v.camtarget - v_init.camtarget);
+v_campos_diff = (v.campos - v_init.campos);
 
+speed_factor = 60;
+steps = abs(v_camva_diff) / 40;
+steps = steps + max(abs(v_campos_diff)) / 800;
+steps = steps + max(abs(v_camtarget_diff)) / 500;
+
+steps = round(steps * speed_factor);
 
 for i = 1:steps
     %togglestates_init.xyzmm = togglestates_init.xyzmm + xyzmm_diff;
     %togglestates_init.xyztransparencies = togglestates_init.xyztransparencies + xyztransparencies_diff;
     %ea_anatomyslices(resultfig,togglestates_init,struct,[]);
-    v_out.az = v_init.az + v_az_diff * i;
-    v_out.el = v_init.el + v_el_diff * i;
-    v_out.camva = v_init.camva + v_camva_diff * i;
-    v_out.camup = v_init.camup + v_camup_diff * i;
-    v_out.camtarget = v_init.camtarget + v_camtarget_diff * i;
-    v_out.campos = v_init.campos + v_campos_diff * i;
+    v_out.az = v_init.az + v_az_diff / steps * i;
+    v_out.el = v_init.el + v_el_diff / steps * i;
+    v_out.camva = v_init.camva + v_camva_diff / steps * i;
+    v_out.camup = v_init.camup + v_camup_diff / steps * i;
+    v_out.camtarget = v_init.camtarget + v_camtarget_diff / steps * i;
+    v_out.campos = v_init.campos + v_campos_diff / steps * i;
     ea_view(v_out);
     drawnow
 end
