7533990d1730d53cb738a31e6f735357b6973f9e
Simon Oxenford <oxenfordsimon@gmail.com>
2019-09-20 09:13:07 +0200

    Change edit prefs call when deployed

--- helpers/gui/ea_editprefs.m
+++ helpers/gui/ea_editprefs.m
@@ -6,9 +6,20 @@ end
 if ~isdeployed
     edit([ea_gethome,'.ea_prefs.m']);
 else
-    try
-        system(['subl ' ea_gethome,'.ea_prefs.json']); % TODO: add more programs to open file
-    catch
-        warning(['Unable to open. File location: ', ea_gethome, '.ea_prefs.json']);
-    end
+    
+    msgbox('Open ~/.ea_prefs.json with a text editor to edit preferences.');
+
+%     prefs = ea_prefs;
+%     if ~isfield(prefs, 'textedit')
+%         mb = msgbox('Choose app to open .json file');
+%         uiwait(mb);
+%         file = uigetfile('/Applications/*.app');
+%         if file
+%             ea_injectprefstring('textedit',file(1:end-4));
+%             prefs = ea_prefs;
+%         else
+%             return
+%         end
+%     end
+%     system(['open -a ','''',prefs.textedit,'''',' ',fullfile(ea_gethome,'.ea_prefs.json')])    
 end
