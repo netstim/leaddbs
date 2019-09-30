5b057f3b07509a0d3a23b9849db5b746db9ae257
Ningfei Li <ningfei.li@gmail.com>
2019-09-30 16:15:09 +0200

    BUG: Fix disabling warning on using javacomponent.

--- helpers/gui/ea_javacomponent.m
+++ helpers/gui/ea_javacomponent.m
@@ -1,6 +1,6 @@
 function ea_javacomponent(varargin)
 % Warpper for javacomponent, warning disabled
 
-warnState = warning('off', 'MATLAB:ui:ea_javacomponent:FunctionToBeRemoved');
-javacomponent(varargin{:})
+warnState = warning('off', 'MATLAB:ui:javacomponent:FunctionToBeRemoved');
+javacomponent(varargin{:});
 warning(warnState);
