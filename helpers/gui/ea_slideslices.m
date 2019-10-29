9178b2f67ec783f65fb05662f2fa8b0e9a98fe4f
Simon Oxenford <oxenfordsimon@gmail.com>
2019-10-29 11:27:57 +0100

    BUG: Fix rotate and slidesclices in off state

--- helpers/gui/ea_slideslices.m
+++ helpers/gui/ea_slideslices.m
@@ -4,4 +4,5 @@ ea_distogrotate;
 hfig = h.Parent.Parent;
 set(hfig,'WindowButtonDownFcn', []); % reset button down function
 ax = findobj(hfig.Children,'Type','axes');
-set(findobj(ax.Children,'Type','surface'),'HitTest','on'); % enable click actions on surfaces (image slices)
+
+set(findobj(ax.Children,'Type','surface'),'HitTest',cmd); % enable/disable click actions on surfaces (image slices)
