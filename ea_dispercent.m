65852062d30f663de401ad8a8116120d41f38d97
Simon Oxenford <oxenfordsimon@gmail.com>
2019-12-12 16:42:31 +0100

    Add more precision to displayed percentage

--- ea_dispercent.m
+++ ea_dispercent.m
@@ -3,16 +3,16 @@ function ea_dispercent(varargin)
 % Copyright (C) 2014 Charite University Medicine Berlin, Movement Disorders Unit
 % Andreas Horn
 
-percent=round(varargin{1}*100);
+percent=varargin{1}*100;
 
 if nargin==2
     if strcmp(varargin{2},'end')
-        fprintf(1,[repmat('\b',1,(length(num2str(percent))+1)),'%d','%%'],percent);
+        fprintf(1,[repmat('\b',1,8),'%7.3f','%%'],percent);
         fprintf('\n');
         fprintf('\n');
     else
-        fprintf(1,[varargin{2},':     ']);
+        fprintf(1,[varargin{2},':        ']);
     end
 else
-    fprintf(1,[repmat('\b',1,(length(num2str(percent))+1)),'%d','%%'],percent);
+    fprintf(1,[repmat('\b',1,8),'%7.3f','%%'],percent);
 end
