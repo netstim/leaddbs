56ae39796aa102fc70f5ba5ff0d7c2169dade95d
Simon Oxenford <oxenfordsimon@gmail.com>
2019-12-13 10:04:46 +0100

    Change ea_mahalanobis_cov description

--- ext_libs/corr/ea_mahalanobis_cov.m
+++ ext_libs/corr/ea_mahalanobis_cov.m
@@ -1,34 +1,8 @@
 function result=ea_mahalanobis_cov(x,val)
 
-%MAHALANOBIS computes the (squared) distance of each observation in x
-%   from the location estimate (locvct) of the data, 
-%   relative to the shape of the data.  
+% This function is the same as ea_mahalanobis but only optimized for a
+% specific input. It is called from ea_mcdcov
 %
-% Required input arguments:
-%                  x : data matrix (n observations in rows, p variables in columns)
-%             locvct : location estimate of the data (p-dimensional vector)
-%      cov or invcov : scatter estimate of the data or the inverse of the scatter estimate (pxp matrix)
-%
-% I/O: result=mahalanobis(x,locvct,'cov',covmat)
-%   The user should only give the input arguments that have to change their default value.
-%   The name of the input arguments needs to be followed by their value.
-%   The order of the input arguments is of no importance.
-%
-% Examples:
-%   result=mahalanobis(x,loc,'cov',covx)
-%   result=mahalanobis(x,loc,'invcov',invcovx)
-%
-% Output:
-%   A row vector containing the squared distances of all the observations to locvct.
-%
-% This function is part of LIBRA: the Matlab Library for Robust Analysis,
-% available at: 
-%              http://wis.kuleuven.be/stat/robust.html
-%
-% Written by Katrien Van Driessen
-% Revisions by Sabine Verboven
-% Last update on 18/09/2003
-%
+% Is is equal to ea_mahalanobis(x,[0; 0],'cov',val) with size(val) = [2 1]
 
-%Initialisation
 result=sum(x*pinv(diag(val)).*x,2)'; 
