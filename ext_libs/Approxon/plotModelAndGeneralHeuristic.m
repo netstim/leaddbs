a781fa8432eeb1e82d0476ca0c39d73f8375efaf
Andreas Husch <27815260+adhusch@users.noreply.github.com>
2019-12-17 10:35:39 +0100

    Workaround to a avoid need for CurveFittingToolbox for appliation
    All plots replaced by manually created ones

--- ext_libs/Approxon/plotModelAndGeneralHeuristic.m
+++ ext_libs/Approxon/plotModelAndGeneralHeuristic.m
@@ -17,8 +17,24 @@ else
     fig = gcf(); %#ok<NASGU>
 end
 axes(ax);
-h = plot(logLinMod, [PW, D], T ,'Xlim', [10 240], 'YLim', [1 8], 'Parent', ax);
+%h = plot(activation_model_3v, [PW, D], T ,'Xlim', [10 240], 'YLim', [1 8], 'Parent', ax);
+x = linspace(10,240, 60);
+y = linspace(1,8, 60);
+[X,Y] = meshgrid(x,y);
+Z = logLinMod(X,Y);
+
+% plot model surface
+hs = surface(X,Y,Z,'Parent', ax);%, 'EdgeColor', 'none');
 hold(ax, 'on');
+
+% plot general heuristic
+[~,hc] = contour3(X,Y,Z, [0.2 0.2], 'r', 'LineWidth', 2, 'Parent', ax);
+
+% plot data by astrom at all where the model was fitted to as smaller blue
+hd = plot3(PW,D,T, 'o', 'MarkerSize', 5, ....
+    'MarkerEdgeColor', 'w', 'MarkerFaceColor', 'b', 'Parent', ax);
+
+% plot large red point for current parameters
 pointHandle = plot3(60,3.5,logLinMod(60,3.5), 'o', 'MarkerSize', 20, ....
     'MarkerEdgeColor', 'w', 'MarkerFaceColor', 'r', 'Parent', ax); % initial choosen parameters
 %vd = plot3(60,7.5,0.064, '*r', 'Parent', ax); % validation data point
@@ -34,13 +50,8 @@ grid(ax, 'on');
 view(ax, 157.2, 26.3 );
 caxis(ax, [0.0369 2])
 
-x = linspace(10,240);
-y = linspace(1,8);
-[X,Y] = meshgrid(x,y);
-Z = activation_model_3v(X,Y);
-[~,hc] = contour3(X,Y,Z, [0.2 0.2], 'r', 'LineWidth', 2, 'Parent', ax);
 
-legend( [h; pointHandle; hc], 'Model', 'Data (Aström et al.)', 'Choosen Parameters', 'Proposed General Heuristics of 0.2V/mm', 'Location', 'NorthEast', 'Interpreter', 'none' );
+legend( [hs; hd ; pointHandle; hc], 'Model', 'Data (Aström et al.)', 'Choosen Parameters', 'Proposed General Heuristics of 0.2V/mm', 'Location', 'NorthEast', 'Interpreter', 'none' );
 
 ax.Color = 'w';
 ax.XTick = 30:30:270;
