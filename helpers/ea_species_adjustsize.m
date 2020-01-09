8ccc5cc6b14530ff7ebb03f199c612fbc5d4f568
andreashorn <andreas.horn@charite.de>
2020-01-09 16:04:14 +0100

    Fix in adjusting sizes for species.

--- helpers/ea_species_adjustsize.m
+++ helpers/ea_species_adjustsize.m
@@ -1,7 +1,7 @@
 function rad=ea_species_adjustsize(rad)
-s=load([ea_space,'ea_space_def.mat']);
+sd=load([ea_space,'ea_space_def.mat']);
 
-if isfield(s.spacedef,'species')
+if isfield(sd.spacedef,'species')
     load([ea_getearoot,'common',filesep,'ea_species.mat']); % https://faculty.washington.edu/chudler/facts.html
     humanweight=species{1,2};
     for s=1:size(species,1)
@@ -10,12 +10,12 @@ if isfield(s.spacedef,'species')
         else
             scell={species{s,1}};
         end
-        if ismember(s.spacedef.species,scell)
+        if ismember(sd.spacedef.species,scell)
             break
         end
     end
-    if s==size(species,1) && ~ismember(s.spacedef.species)
-        ea_warning(['Specified species ',s.spacedef.species,' not found in database.']);
+    if s==size(species,1) && ~ismember(sd.spacedef.species)
+        ea_warning(['Specified species ',sd.spacedef.species,' not found in database.']);
     end
     factor=species{s,2}/humanweight;
 else
--- helpers/space/ea_genwires.m
+++ helpers/space/ea_genwires.m
@@ -2,7 +2,7 @@ function ea_genwires
 load([ea_space,'ea_space_def.mat'])
 
 nii=ea_load_nii([ea_space,spacedef.templates{1},'.nii']);
-wires=ea_detect_edges_3d(nii.img,0.2,0);
+wires=ea_detect_edges_3d(nii.img,0.2,1);
 wires=wires>0.1;
 wires=wires*255;
 %figure, imagesc(squeeze(wires(round(size(wires,1)/2),:,:)));
