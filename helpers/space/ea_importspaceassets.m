f1c6c4c0984eb271c9ea837e67752b7f41161eea
Ningfei Li <ningfei.li@gmail.com>
2020-01-20 12:17:18 +0100

    BUG: Fix square bracket in file/folder names.
    `[]` may induce conflict with ANTs command.

--- helpers/space/ea_importspaceassets.m
+++ helpers/space/ea_importspaceassets.m
@@ -66,9 +66,9 @@ for p=1:length(parcellation)
             ea_apply_normalization_tofile(options,from,to,directory,0,0);
        end
 
-       movefile(to{1},[ea_space([],'labeling'),parcellation{p},' [imported from ',fromspace,']','.nii']);
+       movefile(to{1},[ea_space([],'labeling'),parcellation{p},' - imported from ',fromspace,'.nii']);
        try
-           movefile([wlabeling,parcellation{p},'.txt'],[ea_space([],'labeling'),parcellation{p},' [imported from ',fromspace,']','.txt']);
+           movefile([wlabeling,parcellation{p},'.txt'],[ea_space([],'labeling'),parcellation{p},' - imported from ',fromspace,'.txt']);
        catch
            warning([wlabeling,parcellation{p},'.txt', ' not present. Labeling folder in source space seems inconsistent. Moving on.']);
        end
@@ -110,7 +110,7 @@ end
 
 
 for atlasset=1:length(asc)
-    if exist([ea_space([],'atlases'),asc{atlasset},' [imported from ',fromspace,']'],'dir')
+    if exist([ea_space([],'atlases'),asc{atlasset},' - imported from ',fromspace],'dir')
         disp([ea_space([],'atlases'),asc{atlasset},' already exists! Skipping...'])
         continue
     else
@@ -121,7 +121,7 @@ for atlasset=1:length(asc)
         rmdir([ea_space,fromspace,filesep,asc{atlasset}],'s');
         continue
     end
-    
+
     % for .mat case
     src = fullfile(ea_space,fromspace,'anat_t1.nii');
     invt = fullfile(ea_space,fromspace,'y_ea_inv_normparams.nii');
@@ -129,15 +129,15 @@ for atlasset=1:length(asc)
 
     atlroot=[ea_space,fromspace,filesep,asc{atlasset},filesep];
     load([atlroot,'atlas_index.mat'])
-    
+
     for atlas=1:length(atlases.names)
-        
+
         [~,~,ext] = fileparts(atlases.names{atlas});
-        
+
         switch ext
             case {'.nii', '.gz'}
                 wasgzip = strcmp(ext,'.gz');
-        
+
                 switch atlases.types(atlas)
                     case 1 % right hemispheric atlas.
                         nii{1}=[atlroot,'rh',filesep,atlases.names{atlas}];
@@ -171,7 +171,7 @@ for atlasset=1:length(asc)
                         delete(nii{n});
                     end
                 end
-        
+
             case '.mat'
                 atlas_full_dir = dir(fullfile(atlroot,'*',atlases.names{atlas}));
                 for n = 1:length(atlas_full_dir)
@@ -183,7 +183,7 @@ for atlasset=1:length(asc)
                     atl_load.fibers(:,1:3) = XYZ_dest_mm';
                     save(atl_name, '-struct', 'atl_load');
                 end
-        
+
         end
 
     end
@@ -193,8 +193,8 @@ for atlasset=1:length(asc)
 if ~exist(ea_space([],'atlases'),'dir')
     mkdir(ea_space([],'atlases'));
 end
-    movefile(atlroot,[ea_space([],'atlases'),asc{atlasset},' [imported from ',fromspace,']']);
-    options.atlasset=[asc{atlasset},' [imported from ',fromspace,']'];
+    movefile(atlroot,[ea_space([],'atlases'),asc{atlasset},' - imported from ',fromspace]);
+    options.atlasset=[asc{atlasset},' - imported from ',fromspace];
     ea_genatlastable([],ea_space([],'atlases'),options);
 
 
