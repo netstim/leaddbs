24a125efe3f388e584a12811fff1aa4ef9562d80
Chadwick Boulay <chadwick.boulay@gmail.com>
2020-02-07 14:14:10 -0500

    ea_read_bids - Add support for custom regex matching on filenames.

--- ea_read_bids.m
+++ ea_read_bids.m
@@ -1,126 +1,181 @@
-function options = ea_read_bids(options,bids_subject_folder,run)
+function options = ea_read_bids(options, bids_subject_folder, run)
 
 %% get prefs
-if ~exist('options','var') || isempty(options)
+if ~exist('options', 'var') || isempty(options)
     options.prefs = ea_prefs('');
 end
 
 %% Choose the BIDS subject folder to be imported and read folder structure
-if ~exist('bids_subject_folder','var')
+if ~exist('bids_subject_folder', 'var')
     bids_subject_folder = uigetdir;
 end
 
 % remove filesep if required
-if strcmp(bids_subject_folder(end),filesep)
+if strcmp(bids_subject_folder(end), filesep)
     bids_subject_folder(end)=[];
 end
 
 % separate bids root folder from id folder and get id:
-[bids_folder,id]=fileparts(bids_subject_folder);
+[bids_rawdata, id]=fileparts(bids_subject_folder);
 
 % at this point lead checks that the subject folder begins with 'sub', not sure this is necessary:
-if ~strcmp(id(1:3),'sub')
+if ~strcmp(id(1:3), 'sub')
     error('Not a valid BIDS subject folder')
 end
 
-%% Check for sessions -> Potentially add further sessions e.g. previous preop without contrast agent or ct. 
-
+%% Get BIDS session names for preop and postop
+% Note that user's can specify the same folder for both, or even
+% '' for one or both if no session subfolder is used, by modifying
+% their .ea_prefs file.
 if ~isfield(options.prefs,'bids_session_preop') 
     options.prefs.bids_session_preop = 'ses-preDBS';
 end
-
 if ~isfield(options.prefs,'bids_session_postop') 
-    options.prefs.bids_session_postop = 'ses-postDBS';% ADD THIS TO LEAD PREFS FILE
+    options.prefs.bids_session_postop = 'ses-postDBS';
 end
-
-%% get all preop folders
-preop_folder = dir([bids_subject_folder filesep options.prefs.bids_session_preop '*']);
-if ~isempty(preop_folder)
-    preop_folder = strcat([bids_subject_folder filesep],{preop_folder([preop_folder(:).isdir]).name}');
-else 
-    preop_folder = {bids_subject_folder};
-end
-
-%% Get all postop folders
-postop_folder = dir([bids_subject_folder filesep options.prefs.bids_session_postop '*']);
-if ~isempty(postop_folder)
-    postop_folder = strcat([bids_subject_folder filesep],{postop_folder([postop_folder(:).isdir]).name}');
-else 
-    postop_folder = {bids_subject_folder};
-end
-
+ses_preop = options.prefs.bids_session_preop;
+ses_postop = options.prefs.bids_session_postop;
 
 
 %% Create a lead derivatives folder in the BIDS root
-derivatives_folder = fullfile(bids_folder,'derivatives','leaddbs',id);
-if ~exist(derivatives_folder,'dir')
+% If subject's parent folder (bids_rawdata) is named 'rawdata',
+% then the parent of _that_ is the bids dataset root, and we want our
+% derivates folder to be a sister of rawdata. Otherwise we assume
+% the subject's parent folder is in fact the dataset root.
+[bids_dataset, rawdata] = fileparts(bids_rawdata);
+if ~strcmpi(rawdata, 'rawdata')
+    bids_dataset = bids_rawdata;
+end
+derivatives_folder = fullfile(bids_dataset, 'derivatives', 'leaddbs', id);
+if ~exist(derivatives_folder, 'dir')
     mkdir(derivatives_folder)
 end
 
-%% set patientdir to the newly created derivatives folder
+%% Define default lookup table to map BIDS names to lead-dbs names.
+% Each row is a lead-dbs<-->BIDS mapping
+% The columns are:
+% lead_dbs_sequence_name BIDS_ses BIDS_subfolder file_search_expression
+% Note that the .nii.gz extension is assumed if not provided.
+lead2bids_lookup = ...
+    {'prenii_unnormalized'          ses_preop    'anat'  '.*T2w'
+    'prenii_unnormalized_t1'        ses_preop    'anat'  '.*T1w'
+    'prenii_unnormalized_pd'        ses_preop    'anat'  '.*PD'
+    'prenii_unnormalized_t2star'    ses_preop    'anat'  '.*T2star'
+    'prenii_unnormalized_swi'       ses_preop    'anat'  '.*SWI'
+    'prenii_unnormalized_fgatir'    ses_preop    'anat'  '.*FGATIR'
+    'tranii_unnormalized'           ses_postop   'anat'  '.*tra.*T2w' % needs refinement
+    'sagnii_unnormalized'           ses_postop   'anat'  '.*sag.*T2w' % needs refinement
+    'cornii_unnormalized'           ses_postop   'anat'  '.*cor.*T2w' % needs refinement
+    'rawctnii_unnormalized'         ses_postop   'ct'	'.*ct'
+    'rest'                          ses_preop    'func'	'.*bold'
+    'b0'                            ses_preop    'dwi'	'.*b0'
+    'fa'                            ses_preop    'dwi'	'.*fa'
+    'fa2anat'                       ses_preop    'dwi'   '.*fa2anat'
+    'dti'                           ses_preop    'dwi'   '.*diff'
+    };
 
-options.uipatdirs = {derivatives_folder};
-
-% add the definitions of new sequences to the options for later lookup, add your gadolinium sequence as a new definition
-% e.g. options.prefs.prenii_unnormalized_t1gd = 'anat_t1gd.nii';
+% Any sequences that are not already defined by lead-dbs should be added
+% to the prefs.
 options.prefs.prenii_unnormalized_t2star = 'anat_t2star.nii';
 options.prefs.prenii_unnormalized_swi = 'anat_swi.nii';
 options.prefs.prenii_unnormalized_fgatir = 'anat_fgatir.nii';
 
-lead2bids_lookup = ...
-    {'prenii_unnormalized'          preop_folder    ['anat' filesep '*T2w.nii.gz']
-    'prenii_unnormalized_t1'        preop_folder    ['anat' filesep '*T1w.nii.gz']
-    'prenii_unnormalized_pd'        preop_folder    ['anat' filesep '*PD.nii.gz']
-    'prenii_unnormalized_t2star'    preop_folder    ['anat' filesep '*T2star.nii.gz']
-    'prenii_unnormalized_swi'       preop_folder    ['anat' filesep '*SWI.nii.gz']
-    'prenii_unnormalized_fgatir'    preop_folder    ['anat' filesep '*FGATIR.nii.gz']
-    %here you would add 'prenii_unnormalized_t1gd'      preop_folder    ['anat' filesep '*T1gd.nii.gz']
-    'tranii_unnormalized'           postop_folder   ['anat' filesep '*tra*T2w.nii.gz'] % needs refinement
-    'sagnii_unnormalized'           postop_folder   ['anat' filesep '*sag*T2w.nii.gz'] % needs refinement
-    'cornii_unnormalized'           postop_folder   ['anat' filesep '*cor*T2w.nii.gz'] % needs refinement
-    'rawctnii_unnormalized'         postop_folder   ['ct' filesep '*ct.nii.gz']
-    'rest'                          preop_folder    ['func' filesep '*bold.nii.gz']
-    'b0'                            preop_folder    ['dwi' filesep '*b0.nii.gz']
-    'fa'                            preop_folder    ['dwi' filesep '*fa.nii.gz']
-    'fa2anat'                       preop_folder    ['dwi' filesep '*fa2anat.nii.gz']
-    'dti'                           preop_folder    ['dwi' filesep '*diff.nii.gz']};
-
-for a =1 :size(lead2bids_lookup,1) % run through all lookup entries
-    for b = 1:length(lead2bids_lookup{a,2}) % run through the different pre- and postop folders folders (more than one possible)
-        files=[];
-        % find relevant files based on the look up table here it would be possible to implement a regex match
-        files = dir(fullfile(lead2bids_lookup{a,2}{b},lead2bids_lookup{a,3})); % lead2bids_lookup{a,2}{b} = folder, lead2bids_lookup{a,3} = filename
-        
-        for c=1:length(files) % run through the found files
-            if c==1
-                outname = fullfile(derivatives_folder,[options.prefs.(lead2bids_lookup{a,1})(1:end-4) '.nii.gz']); % rename the file for lead dbs
-            else
-                outname = fullfile(derivatives_folder,[options.prefs.(lead2bids_lookup{a,1})(1:end-4) '_' num2str(c-1) '.nii.gz']); % add a running number to the remaining files
-                warning(['More than 1 file for ' lead2bids_lookup{a,1} ' found!']) % warn that more than one file is found
-            end
-            
-             copyfile(fullfile(files(c).folder,files(c).name),outname); % copy the file
-             
-             gunzip(outname,derivatives_folder) % unzip the file 
-             delete(outname) % delete the zipped file
-    
-            if strcmp(lead2bids_lookup{a,1},'dti') % check if it is a dti file and copy the bval and bvec files
-                if exist(fullfile(files(c).folder,[files(c).name(1:end-7) '.bval']),'file') % check for presence of bval file
-                    copyfile(fullfile(files(c).folder,[files(c).name(1:end-7) '.bval']),fullfile(options.prefs.patientdir,[options.prefs.(lead2bids_lookup{a,1})(1:end-4) '.bval']));
-                else
-                    warning('No .bval file found for dMRI image.'); % warn if no bval file is found
-                end
-                if exist(fullfile(files(c).folder,[files(c).name(1:end-7) '.bvec']),'file') % check if bvec file is present
-                    copyfile(fullfile(files(c).folder,[files(c).name(1:end-7) '.bvec']),fullfile(options.prefs.patientdir,[options.prefs.(lead2bids_lookup{a,1})(1:end-4) '.bvec']));
-                else
-                    warning('No .bvec file found for dMRI image.'); % warn if no bvec file is found
+% Append/override with options.prefs.lead2bids_lookup.
+% Triple-nested-loop feels bad. This could be a perf problem.
+if isfield(options.prefs, 'lead2bids_lookup')
+    for a = 1:size(lead2bids_lookup, 1)
+        for b = 1:size(options.prefs.lead2bids_lookup)
+            if strcmpi(lead2bids_lookup{a, 1}, options.prefs.lead2bids_lookup{b, 1})
+                for col_ix = 1:length({options.prefs.lead2bids_lookup{b, :}})
+                    lead2bids_lookup{a, col_ix} = options.prefs.lead2bids_lookup{b, col_ix};
                 end
             end
         end
     end
 end
 
-cd(options.uipatdirs{1}) % this should change to the derivatives folder. Andy wanted to correct this, not sure this works.
+copy_aux_files = {
+    'dti'   {'bvec', 'bval'}
+};
+
+%% Find images in BIDS structure, copy and gunzip to derivatives_folder
+% search for each type of file defined in lead2bids_lookup
+for a = 1:size(lead2bids_lookup, 1)
+    % Identify all session folders. Usually only 1.
+    ses_folder = dir([bids_subject_folder filesep lead2bids_lookup{a, 2} '*']);
+    ses_folder = ses_folder(~ismember({ses_folder.name}, {'.', '..'}));
+    if ~isempty(ses_folder)
+        ses_folder = strcat([bids_subject_folder filesep], {ses_folder([ses_folder(:).isdir]).name}');
+    else 
+        ses_folder = {bids_subject_folder};
+    end
+    
+    % Fixup the extension on the filename regexp, if necessary.
+    pattern = lead2bids_lookup{a, 4};
+    if (length(pattern) > 7)  && (strcmpi(pattern(end-6:end), '.nii.gz'))
+        pattern = pattern(1:end-7);
+    end
+    if (length(pattern) < 9) || (~strcmpi(pattern(end-8:end), '\.nii\.gz'))
+        pattern = [pattern '\.nii\.gz'];
+    end
+    
+    % Search all session folders, storing matching files
+    files = [];
+    for b = 1:length(ses_folder)
+        search_dir = fullfile(ses_folder{b}, lead2bids_lookup{a, 3});
+        found_nifti = dir(fullfile(search_dir, '*.nii.gz'));
+        match_nifti = regexpi({found_nifti.name}, pattern, 'match', 'once');
+        match_nifti = found_nifti(~cellfun(@isempty, match_nifti));
+        if ~isempty(match_nifti)
+            files = [files match_nifti];
+        end
+    end
+        
+    % Process (copy and gunzip) matching files
+    if length(files) > 1
+        warning(['More than 1 file for ' lead2bids_lookup{a, 1} ' found!'])
+    end
+    for c = 1:length(files)
+        inname_noext = files(c).name(1:end-7);
+        % rename the file for lead dbs
+        suffix = '';
+        if c > 1
+            suffix = ['_' num2str(c-1)];  % Multiple-matches get appended with _number.
+        end
+        outname_noext = options.prefs.(lead2bids_lookup{a,1})(1:end-4);
+        outname = fullfile(derivatives_folder, [outname_noext suffix '.nii.gz']);
+        
+        % Copy, gunzip, delete gzip
+        copyfile(fullfile(files(c).folder, files(c).name), outname);
+        gunzip(outname, derivatives_folder)
+        delete(outname)
+
+        % Copy auxiliary files. See copy_aux_files table above.
+        for aux_ix = 1:size(copy_aux_files, 1)
+            if strcmpi(lead2bids_lookup{a, 1}, copy_aux_files{aux_ix, 1})
+                for aux_f_ix = 1:length(copy_aux_files{aux_ix, 2})
+                    aux_f_ext = ['.' copy_aux_files{aux_ix, 2}{aux_f_ix}];
+                    aux_f_src = fullfile(files(c).folder, [inname_noext aux_f_ext]);
+                    if exist(aux_f_src, 'file')
+                        aux_f_dest = fullfile(options.prefs.patientdir, [outname_noext aux_f_ext]);
+                        copyfile(aux_f_src, aux_f_dest);
+                    else
+                        warning(['No ' aux_f_ext ' file found for ' lead2bids_lookup{a, 1} '.']);
+                    end
+                end
+            end
+        end
+        
+    end
+    
+end
+
+%% set patientdir to the newly created derivatives folder
+
+options.uipatdirs = {derivatives_folder};
+% Change to the derivatives folder. Still not working.
+% Andy wanted to correct this.
+cd(options.uipatdirs{1}) % this should 
 
 if exist('run','var') && run % check if lead dbs should run, not sure this is working correctly
 
