6a5b418ae32f91df42053880c1664c51cc5e1ef8
Gabriel Gonzalez-Escamilla <gabriellbk@gmail.com>
2020-02-17 09:05:57 +0100

    Update ea_apply_normalization_tofile.m
    for SPM normalization tempdir is not defined.
    This can be the current directory (so that the folder is always writable as suggested in the file) or "tmp=ea_getleadtempdir;" but may not always be writable/work.

--- ea_apply_normalization_tofile.m
+++ ea_apply_normalization_tofile.m
@@ -58,6 +58,7 @@ switch ea_whichnormmethod(directory)
         ea_fsl_apply_normalization(options,from,to,useinverse,refim,'',interp);
 
     otherwise % SPM part here
+        tempdir = pwd;% ensure using a writable folder (not necessarily tempdir)
         for fi=1:length(from) % assume from and to have same length (must have for this to work)
             if strcmp(from{fi}(end-2:end),'.gz') % .gz support
                 wasgz = 1;
