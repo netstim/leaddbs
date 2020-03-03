ee1ec22c6eb4e2ac737509302b1ffbbc37808391
Ningfei Li <ningfei.li@gmail.com>
2020-03-03 23:13:15 +0100

    ENH: Add visualization for St. Jude directed electrodes mdoels.

--- templates/electrode_models/ea_elspec_stjude_directed_15.m
+++ templates/electrode_models/ea_elspec_stjude_directed_15.m
@@ -55,3 +55,78 @@ filename = [fileparts(mfilename('fullpath')),'/StJude_Directed_15_Components/fin
 [node,~,face]=readtetgen(filename);
 save('stjude_directed_15_vol.mat','face','node')
 clear node face
+
+%% visualize
+if nargin
+    vizz=0;
+else
+    vizz=1;
+end
+
+options.elmodel = 'St. Jude Directed 6173 (long)';
+options = ea_resolve_elspec(options);
+elspec = options.elspec;
+
+if vizz
+    X = eye(4);
+    aData = 1;
+
+    figure;
+    for ins=1:length(electrode.insulation)
+        vs=X*[electrode.insulation(ins).vertices,ones(size(electrode.insulation(ins).vertices,1),1)]';
+        electrode.insulation(ins).vertices=vs(1:3,:)';
+        elrender=patch('Faces',electrode.insulation(ins).faces,'Vertices',electrode.insulation(ins).vertices);
+        specsurf(elrender,elspec.lead_color,aData);
+    end
+
+    for con=1:length(electrode.contacts)
+        vs=X*[electrode.contacts(con).vertices,ones(size(electrode.contacts(con).vertices,1),1)]';
+        electrode.contacts(con).vertices=vs(1:3,:)';
+        elrender=patch('Faces',electrode.contacts(con).faces,'Vertices',electrode.contacts(con).vertices);
+        specsurf(elrender,elspec.contact_color,aData);
+    end
+
+    axis equal
+    view(0,0);
+end
+
+
+function specsurf(varargin)
+
+surfc=varargin{1};
+color=varargin{2};
+if nargin==3
+    aData=varargin{3};
+end
+
+len=get(surfc,'ZData');
+
+cd=zeros([size(len),3]);
+cd(:,:,1)=color(1);
+try % works if color is denoted as 1x3 array
+    cd(:,:,2)=color(2);cd(:,:,3)=color(3);
+catch % if color is denoted as gray value (1x1) only
+    cd(:,:,2)=color(1);cd(:,:,3)=color(1);
+end
+
+cd=cd+0.01*randn(size(cd));
+
+set(surfc,'FaceColor','interp');
+set(surfc,'CData',cd);
+
+try % for patches
+    Vertices=get(surfc,'Vertices');
+    cd=zeros(size(Vertices));
+    cd(:)=color(1);
+    set(surfc,'FaceVertexCData',cd);
+end
+set(surfc,'AlphaDataMapping','none');
+
+set(surfc,'FaceLighting','phong');
+set(surfc,'SpecularColorReflectance',0);
+set(surfc,'SpecularExponent',10);
+set(surfc,'EdgeColor','none')
+
+if nargin==3
+    set(surfc,'FaceAlpha',aData);
+end
