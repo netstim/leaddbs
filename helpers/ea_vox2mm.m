87255be8b60005bf6a503d8f4870c06031d14299
Ningfei Li <ningfei.li@gmail.com>
2020-03-04 15:56:46 +0100

    REF: Use ea_get_affine instead of spm_get_space.

--- helpers/ea_vox2mm.m
+++ helpers/ea_vox2mm.m
@@ -3,7 +3,7 @@ function mm=ea_vox2mm(vox, transform)
 % coords need to be row vector: N*3
 
 if ischar(transform)
-    transform = spm_get_space(transform);
+    transform = ea_get_affine(transform);
 end
 
 % vox = round(vox);
