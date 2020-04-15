1d0a40b19d48ac610c585d3303a81b3d1bed8f27
Ningfei Li <ningfei.li@gmail.com>
2020-04-15 09:02:19 +0200

    ENH: Add function to generate random permutation.

---
+++ helpers/ea_shuffle.m
@@ -0,0 +1,20 @@
+function [xPerm, permInd] = ea_shuffle(X, N, Sel)
+% Generate N random permutations of input vector X
+% Can also shuffle only the [Sel]ected elements in X
+% Return the shuffled X and permutation indices as row vectors
+
+if ~exist('Sel', 'var')
+    Sel = 1:length(X);
+end
+
+if iscolumn(X)
+    X = X';
+end
+
+xPerm = repmat(X, N, 1);
+permInd = repmat(1:length(X), N, 1);
+
+for i=1:N
+    permInd(i, Sel) = Sel(randperm(length(Sel)));
+    xPerm(i, :) = xPerm(i, permInd(i, :));
+end
