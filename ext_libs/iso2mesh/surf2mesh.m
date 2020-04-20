7f5d34e32d5a8bdaeebb282d599a6a7d6faa1455
Andreas Horn <andreas.horn@charite.de>
2020-04-20 15:04:10 +0200

    Further reliability for VTA generation

--- ext_libs/iso2mesh/surf2mesh.m
+++ ext_libs/iso2mesh/surf2mesh.m
@@ -116,7 +116,11 @@ end
 
 if(isempty(cmdopt)) % default run
     
-    system([' "' mcpath('tetgen') exesuff '" -A -T1e-5 -pq1/0 -a -Y ' num2str(maxvol) ' ' moreopt ' "' mwpath('post_vmesh.poly') '"',cmdsuffix]);
+    for tolerance=[8,10,5]
+        if ~exist(mwpath('post_vmesh.1.node'),'file') % check if outputs are there
+            system([' "' mcpath('tetgen') exesuff '" -A -T1e-',num2str(tolerance),' -pq1/0 -a -Y ' num2str(maxvol) ' ' moreopt ' "' mwpath('post_vmesh.poly') '"',cmdsuffix]);
+        end
+    end
     
 else
 	system([' "' mcpath('tetgen') exesuff '"  -A -T1e-5 -q4 -a -Y ' cmdopt ' "' mwpath('post_vmesh.poly') '"',cmdsuffix]);
