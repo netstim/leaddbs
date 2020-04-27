efc556c916afb95b44b51f93ca5cef51a061f831
Andreas Horn <andreas.horn@charite.de>
2020-04-27 21:04:06 +0200

    Added writevta

---
+++ vatmodel/ea_write_vta_nii.m
@@ -0,0 +1,216 @@
+function [vatfv,vatvolume,radius]=ea_write_vta_nii(midpts,gradient,options)
+% define midpoints of quiver field
+vatgrad(side).x=midpts(indices,1); vatgrad(side).y=midpts(indices,2); vatgrad(side).z=midpts(indices,3);
+
+gradvis=gradient(indices,:);
+mag_gradvis=sqrt(sum(gradvis'.^2,1))';
+nmag_gradvis=mag_gradvis; % copy to get normalized version
+nmag_gradvis(nmag_gradvis>thresh)=thresh;
+nmag_gradvis=(nmag_gradvis-min(nmag_gradvis(:)))/max(nmag_gradvis-min(nmag_gradvis(:))); % norm from 1 - 0
+nmag_gradvis=nmag_gradvis/5; % largest grad vector will be 1/50 mm long
+
+% now apply scaling to gradvis:
+gradvis=gradvis.*repmat(nmag_gradvis,1,3);
+gradvis=gradvis./repmat(mag_gradvis,1,3);
+vatgrad(side).qx=gradvis(:,1); vatgrad(side).qy=gradvis(:,2); vatgrad(side).qz=gradvis(:,3);
+
+%figure, quiver3(midpts(:,1),midpts(:,2),midpts(:,3),gradient(:,1),gradient(:,2),gradient(:,3))
+
+
+% calculate electric field ET by calculating midpoints of each
+% mesh-connection and setting difference of voltage to these points.
+
+vat.pos=midpts;
+
+%plot3(midpts(:,1),midpts(:,2),midpts(:,3),'g.');
+
+setappdata(resultfig,'vatgrad',vatgrad);
+
+ngrad=sqrt(sum(gradient'.^2,1));
+vat.ET=ngrad; % vol.cond(vol.tissue).*ngrad; would be stromstaerke.
+% reload elstruct to make sure to take correct one (native vs. template)
+[coords_mm,trajectory,markers]=ea_load_reconstruction(options);
+elstruct(1).coords_mm=coords_mm;
+elstruct(1).coords_mm=ea_resolvecoords(markers,options);
+elstruct(1).trajectory=trajectory;
+elstruct(1).name=options.patientname;
+elstruct(1).markers=markers;
+if options.prefs.machine.vatsettings.horn_removeElectrode
+    vat = jr_remove_electrode(vat,elstruct,mesh,side,elspec);
+end
+ea_dispt('Preparing VAT...');
+
+vat.tET=vat.ET>thresh;
+vat.tpos=vat.pos(vat.tET,:);
+%nvat.tpos=nvat.pos(vat.tET,:);
+outliers=ea_removeoutliers(vat.tpos,mean(dpvx,1),voltix,constvol);
+vat.tpos(outliers,:)=[];
+%nvat.tpos(outliers,:)=[];
+if vizz
+    figure, plot3(vat.tpos(:,1),vat.tpos(:,2),vat.tpos(:,3),'r.');
+    [coords_mm,trajectory,markers,elmodel,manually_corrected,coords_acpc]=ea_load_reconstruction(options);
+    hold on
+    plot3(trajectory{side}(:,1),trajectory{side}(:,2),trajectory{side}(:,3),'k*');
+     plot3(nvat.tpos(:,1),nvat.tpos(:,2),nvat.tpos(:,3),'m.');
+     toptions=options; toptions.native=1;
+         [coords_mm,trajectory,markers,elmodel,manually_corrected,coords_acpc]=ea_load_reconstruction(toptions);
+    plot3(trajectory{side}(:,1),trajectory{side}(:,2),trajectory{side}(:,3),'g*');
+
+end
+
+% the following will be used for volume 2 isosurf creation as well as
+% volumetrics of the vat in mm^3.
+ea_dispt('Calculating interpolant on scattered FEM mesh data...');
+F=scatteredInterpolant(vat.pos(:,1),vat.pos(:,2),vat.pos(:,3),vat.ET','linear','none');
+
+ea_dispt('Converting to equispaced image data...');
+res=100;
+gv=cell(3,1); spacing=zeros(3,1);
+try
+    for dim=1:3
+%         maxdist=max([dpvx(dim)-(min(round(vat.tpos(:,dim)))-3);...
+%         (max(round(vat.tpos(:,dim)))+3)-dpvx(dim)]);
+%         gv{dim}=linspace(dpvx(dim)-maxdist,dpvx(dim)+maxdist,res);
+        gv{dim}=linspace(min(round(vat.tpos(:,dim)))-5,max(round(vat.tpos(:,dim)))+5,res);
+        spacing(dim)=abs(gv{dim}(1)-gv{dim}(2));
+    end
+catch
+    varargout{1}=nan;
+    varargout{2}=nan;
+    varargout{3}=nan;
+    return
+end
+
+ea_dispt('Creating nifti header for export...');
+% create nifti
+chun1=randperm(res); chun2=randperm(res); chun3=randperm(res);
+Vvat.mat=mldivide([(chun1);(chun2);(chun3);ones(1,res)]',[gv{1}(chun1);gv{2}(chun2);gv{3}(chun3);ones(1,res)]')';
+Vvat.dim=[res,res,res];
+Vvat.dt=[4,0];
+Vvat.n=[1 1];
+Vvat.descrip='lead dbs - vat';
+if ~exist([options.root,options.patientname,filesep,'stimulations',filesep,ea_nt(options)],'file')
+    mkdir([options.root,options.patientname,filesep,'stimulations',filesep,ea_nt(options)]);
+end
+
+ea_dispt('Filling data with values from interpolant...');
+eeg = F(gv);
+eeg(isnan(eeg))=0;
+eeg(eeg>options.prefs.vat.efieldmax)=options.prefs.vat.efieldmax; % upperlimit files to 10000.
+
+%figure, plot3(F.Points(:,1),F.Points(:,2),F.Points(:,3),'r.')
+%hold on
+%plot3(vat.pos(:,1),vat.pos(:,2),vat.pos(:,3),'b.')
+% e-field in matrix form.
+
+ea_dispt('Calculating output file data...');
+eg=eeg;
+eg=eg>thresh;
+% binary e-field - "vat"
+
+neeg=eeg;
+neeg(~eg)=nan;
+
+neeg(neeg>0)=ea_normal(neeg(neeg>0),1,0,' ',0,1,'TRUE');%
+% normalized e-field (zscored).
+neeg(~isnan(neeg))=neeg(~isnan(neeg))-min(neeg(~isnan(neeg)));
+neeg(~isnan(neeg))=neeg(~isnan(neeg))/sum(neeg(~isnan(neeg))); % 0-1 distributed.
+
+[xg,yg,zg] = meshgrid(gv{1},gv{2},gv{3});
+
+XYZmax=[max(yg(eg>0)),max(xg(eg>0)),max(zg(eg>0))]; % x and y need to be permuted here (should be correct but wouldnt matter anyways since only serves to calc radius)
+try
+    radius=ea_pdist([XYZmax;dpvx]);
+catch
+    keyboard
+end
+
+
+%eg=smooth3(eg,'gaussian',[25 25 25]);
+ea_dispt('Calculating volume...');
+
+vatvolume=sum(eg(:))*spacing(1)*spacing(2)*spacing(3); % returns volume of vat in mm^3
+S.volume(side)=vatvolume;
+
+
+
+ea_dispt('Writing files...');
+
+% determine stimulation name:
+if ~exist([options.root,options.patientname,filesep,'stimulations',filesep,ea_nt(options),stimname],'file')
+    mkdir([options.root,options.patientname,filesep,'stimulations',filesep,ea_nt(options),stimname]);
+end
+
+switch side
+    case 1
+        Vvat.fname=[options.root,options.patientname,filesep,'stimulations',filesep,ea_nt(options),stimname,filesep,'vat_right.nii'];
+        Vvate=Vvat; Vvatne=Vvat;
+        Vvate.fname=[options.root,options.patientname,filesep,'stimulations',filesep,ea_nt(options),stimname,filesep,'vat_efield_right.nii'];
+        Vvatne.fname=[options.root,options.patientname,filesep,'stimulations',filesep,ea_nt(options),stimname,filesep,'vat_efield_gauss_right.nii'];
+    case 2
+        Vvat.fname=[options.root,options.patientname,filesep,'stimulations',filesep,ea_nt(options),stimname,filesep,'vat_left.nii'];
+        Vvate=Vvat; Vvatne=Vvat;
+        Vvate.fname=[options.root,options.patientname,filesep,'stimulations',filesep,ea_nt(options),stimname,filesep,'vat_efield_left.nii'];
+        Vvatne.fname=[options.root,options.patientname,filesep,'stimulations',filesep,ea_nt(options),stimname,filesep,'vat_efield_gauss_left.nii'];
+end
+%save(stimfile,'S');
+ea_savestimulation(S,options);
+% setappdata(lgfigure,'curS',S);
+
+%spm_write_vol(Vvat,flipdim(eg,3));
+
+Vvate.img=eeg; %permute(eeg,[2,1,3]);
+Vvate.dt=[16,0];
+ea_write_nii(Vvate);
+
+Vvatne.img=neeg; %permute(neeg,[2,1,3]);
+ea_write_nii(Vvatne);
+
+Vvat.img=eg; %permute(eg,[1,2,3]);
+ea_write_nii(Vvat);
+
+ea_dispt('Calculating isosurface to display...');
+vatfv=isosurface(xg,yg,zg,permute(Vvat.img,[2,1,3]),0.75);
+
+caps=isocaps(xg,yg,zg,permute(Vvat.img,[2,1,3]),0.5);
+
+vatfv.faces=[vatfv.faces;caps.faces+size(vatfv.vertices,1)];
+vatfv.vertices=[vatfv.vertices;caps.vertices];
+
+
+try
+    vatfv=ea_smoothpatch(vatfv,1,35);
+catch
+    try
+        cd([ea_getearoot,'ext_libs',filesep,'smoothpatch']);
+        mex ea_smoothpatch_curvature_double.c -v
+        mex ea_smoothpatch_inversedistance_double.c -v
+        mex ea_vertex_neighbours_double.c -v
+        vatfv=ea_smoothpatch(vatfv);
+    catch
+        warndlg('Patch could not be smoothed. Please supply a compatible Matlab compiler to smooth VTAs.');
+    end
+end
+% new save by Till to save VAT and quiver in seperate .mat-file for quick
+% visualization
+switch side
+    case 1
+        vatfvname=[options.root,options.patientname,filesep,'stimulations',filesep,ea_nt(options),stimname,filesep,'vat_right.mat'];
+    case 2
+        vatfvname=[options.root,options.patientname,filesep,'stimulations',filesep,ea_nt(options),stimname,filesep,'vat_left.mat'];
+end
+vatgrad = vatgrad(side);
+save(vatfvname,'vatfv','vatgrad','vatvolume');
+
+%% new vta.nii save, filled and eroded/dilated by 3 voxels.
+Vvat.img=imfill(Vvat.img,'holes');
+SE = strel('sphere',3);
+Vvat.img = imerode(Vvat.img,SE);
+Vvat.img = imdilate(Vvat.img,SE);
+ea_write_nii(Vvat);
+
+%% old vta.nii which lead to slight systematic shifts
+% Vvat.img=surf2vol(vatfv.vertices,vatfv.faces,gv{1},gv{2},gv{3});
+% Vvat.img=imfill(Vvat.img,'holes');
+% Vvat.fname = [Vvat.fname(1:end-4) '_old.nii'];
+% ea_write_nii(Vvat);
