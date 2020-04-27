99c26f8d083adc242250ff5ac575de12cae76b84
Andreas Horn <andreas.horn@charite.de>
2020-04-27 11:59:51 +0200

    Add SyN stage for dMRI GUI mockup

--- connectomics/lead_connectome.m
+++ connectomics/lead_connectome.m
@@ -22,7 +22,7 @@ function varargout = lead_connectome(varargin)
 
 % Edit the above text to modify the response to help leadfigure
 
-% Last Modified by GUIDE v2.5 29-Dec-2019 14:35:15
+% Last Modified by GUIDE v2.5 27-Apr-2020 11:59:34
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -996,3 +996,12 @@ function use_internal_upsampling_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 % Hint: get(hObject,'Value') returns toggle state of use_internal_upsampling
+
+
+% --- Executes on button press in addSyndMRI.
+function addSyndMRI_Callback(hObject, eventdata, handles)
+% hObject    handle to addSyndMRI (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hint: get(hObject,'Value') returns toggle state of addSyndMRI
