90634739817e86ecc074e724517a1aa195d200a4
Ningfei Li <ningfei.li@gmail.com>
2020-06-07 10:43:37 +0200

    REF: Only display percentile in dispercent.

--- ea_dispercent.m
+++ ea_dispercent.m
@@ -7,7 +7,7 @@ percent=varargin{1}*100;
 
 if nargin==2
     if strcmp(varargin{2},'end')
-        fprintf(1,[repmat('\b',1,8),'%7.3f','%%'],percent);
+        fprintf(1,[repmat('\b',1,8),'%7.2f','%%'],percent);
         fprintf('\n');
         fprintf('\n');
     else
