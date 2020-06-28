dd8b0a2569b24ecf42381203de8df6e1b625f4e8
Ningfei Li <ningfei.li@gmail.com>
2020-06-28 21:09:47 +0200

    REF: Warn when both show pos and neg fibs are chosen in case dogroup.
    Will return. In case dogroup (Color by Group Variable), the colormap will
    be problematic if both show positive and negative fibers are checked.

--- connectomics/discfibers/ea_disctract.m
+++ connectomics/discfibers/ea_disctract.m
@@ -374,14 +374,18 @@ classdef ea_disctract < handle
                 colormap(gray);
                 gradientLevel = 1024;
                 if dogroups
-                    if ~obj.posvisible && obj.negvisible
+                    if obj.posvisible && ~obj.negvisible
+                        fibcmap{group} = ea_colorgradient(gradientLevel, [1,1,1], linecols(group,:));
+                        cmapind = round(normalize(allvals,'range',[1,gradientLevel]));
+                        alphaind = normalize(allvals, 'range');
+                    elseif ~obj.posvisible && obj.negvisible
                         fibcmap{group} = ea_colorgradient(gradientLevel, linecols(group,:), [1,1,1]);
                         cmapind = round(normalize(allvals,'range',[1,gradientLevel]));
                         alphaind = normalize(-allvals, 'range');
                     else
-                        fibcmap{group} = ea_colorgradient(gradientLevel, [1,1,1], linecols(group,:));
-                        cmapind = round(normalize(allvals,'range',[1,gradientLevel]));
-                        alphaind = normalize(allvals, 'range');
+                        warndlg(sprintf(['Please choose either "Show Positive Fibers" or "Show Negative Fibers".',...
+                            '\nShow both positive and negative fibers is not supported when "Color by Group Variable" is on.']));
+                        return;
                     end
                 else
                     if obj.posvisible && obj.negvisible
