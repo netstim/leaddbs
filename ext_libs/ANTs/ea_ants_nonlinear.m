09ed5443cfd30bb056842d09a9af38d1b9e97584
Ningfei Li <ningfei.li@gmail.com>
2020-07-30 12:32:17 +0200

    REF: Add image details to error message.

--- ext_libs/ANTs/ea_ants_nonlinear.m
+++ ext_libs/ANTs/ea_ants_nonlinear.m
@@ -71,7 +71,7 @@ if slabsupport
                     try
                         AllMX = AllMX.*mnii.img;
                     catch
-                        ea_error('Multispectral acquisitions are not co-registered & resliced to anchor-modality. Please run co-registration first!');
+                        ea_error(sprintf('Multispectral acquisitions are not co-registered & resliced to anchor-modality. Please run co-registration first!\n%s', movingimage{mov}));
                     end
                 end
                 sums(mov) = sum(mnii.img(:));
