be98293b54f04a4e9dbae0917c89e7304c8aba8c
Ningfei Li <ningfei.li@gmail.com>
2020-08-28 12:17:18 +0200

    ENH: Add function to calc X and Y markers norm vector.

---
+++ helpers/ea_calcxy.m
@@ -0,0 +1,12 @@
+function [x, y] = ea_calcxy(head, tail)
+% Calculate X and Y norm vector based on the head and tail of the lead
+%
+% The X and Y axis orientation will be the same as in the null model of the
+% directed lead
+
+trajvector=diff([head; tail]);
+normtrajvector = trajvector/norm(trajvector);
+y = [0 normtrajvector(3) -normtrajvector(2)];
+x = -cross(normtrajvector,y);
+y = y/norm(y);
+x = x/norm(x);
