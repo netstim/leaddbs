2f4c96571fcda23bfb41eb4f41e04771ab2c594f
Ningfei Li <ningfei.li@gmail.com>
2020-08-31 11:48:20 +0200

    REF: Refine spacedef generating function.

--- helpers/space/ea_gendefspacedef.m
+++ helpers/space/ea_gendefspacedef.m
@@ -1,11 +1,10 @@
 function spacedef=ea_gendefspacedef
 
-spacedef.norm_mapping={{'t1'},{'t1'}
-            {'t2'},{'t2'}
-            {'pd'},{'pd'}
-            {'fa'},{'fa'}
-            {'*'},{'t1'}
-            };
-        spacedef.templates={'t1','t2','pd'};
-        spacedef.hasfa=1;
-        spacedef.ver=1.0;
+spacedef.norm_mapping={'t1','t1'
+    't2','t2'
+    'pd','pd'
+    'fa','fa'
+    '*','t1'};
+spacedef.templates={'t1','t2','pd'};
+spacedef.hasfa=1;
+spacedef.ver=1.0;
