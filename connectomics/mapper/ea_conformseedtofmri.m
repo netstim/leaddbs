6d2f5c2ef68cbb97a46a6f857ba4053b0e715a7b
Ningfei Li <ningfei.li@gmail.com>
2020-10-16 21:12:06 +0200

    REF: Refine var names.

--- connectomics/mapper/ea_conformseedtofmri.m
+++ connectomics/mapper/ea_conformseedtofmri.m
@@ -1,21 +1,21 @@
-function s = ea_conformseedtofmri(dataset,s)
+function seed = ea_conformseedtofmri(dataset,seed)
 td = tempdir;
-ref = [td,'tmpspace.nii'];
-seed = [td,'tmpseed.nii'];
+tmpref = [td,'tmpspace.nii'];
+tmpseed = [td,'tmpseed.nii'];
 
 % Write out ref into temp file
-dataset.vol.space.fname = ref;
+dataset.vol.space.fname = tmpref;
 ea_write_nii(dataset.vol.space);
 
 % Write out seed into temp file
-s.fname = seed;
-ea_write_nii(s);
+seed.fname = tmpseed;
+ea_write_nii(seed);
 
 % Conform space by coregistration using SPM with trilinear interpolation
 options.coregmr.method='SPM';
-ea_coreg2images(options,seed,ref,seed,[],[],[],1);
+ea_coreg2images(options,tmpseed,tmpref,tmpseed,[],[],[],1);
 
 % Load resliced seed file and clean up
-s=ea_load_nii(seed);
-delete(ref);
-delete(seed);
+seed=ea_load_nii(tmpseed);
+delete(tmpref);
+delete(tmpseed);
