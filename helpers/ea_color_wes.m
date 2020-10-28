ecf0e50c886324c8373406a3168ff551b7bb5371
Ningfei Li <ningfei.li@gmail.com>
2020-10-28 11:54:17 +0100

    REF: Refine var name.

--- helpers/ea_color_wes.m
+++ helpers/ea_color_wes.m
@@ -1,4 +1,4 @@
-function pal=ea_color_wes(set)
+function colors=ea_color_wes(set, len)
 % source: http://opencolor.tools/palettes/wesanderson/
 % similar or same palettes found in R here: https://github.com/karthik/wesanderson
 % different sets here https://prafter.com/color/
@@ -7,54 +7,55 @@ if ~exist('set','var')
     s=randperm(length(sets));
     set=sets{s(1)};
 end
+
 switch set
     case 'all'
-        pal=[];
+        colors=[];
         for s=1:length(sets)
-            pal=[pal;ea_color_wes(sets{s})];
+            colors=[colors;ea_color_wes(sets{s})];
         end
     case 'lifeaquatic'
-        pal(1,:)=ea_hex2rgb('#1DACE8');
-        pal(2,:)=ea_hex2rgb('#1C366B');
-        pal(3,:)=ea_hex2rgb('#F24D29');
-        pal(4,:)=ea_hex2rgb('#E5C4A1');
-        pal(5,:)=ea_hex2rgb('#C4CFD0');
+        colors(1,:)=ea_hex2rgb('#1DACE8');
+        colors(2,:)=ea_hex2rgb('#1C366B');
+        colors(3,:)=ea_hex2rgb('#F24D29');
+        colors(4,:)=ea_hex2rgb('#E5C4A1');
+        colors(5,:)=ea_hex2rgb('#C4CFD0');
     case 'royaltenenbaums'
-        pal(1,:)=ea_hex2rgb('#9A872D');
-        pal(2,:)=ea_hex2rgb('#F5CDB6');
-        pal(3,:)=ea_hex2rgb('#F7B0AA');
-        pal(4,:)=ea_hex2rgb('#FDDDA4');
-        pal(5,:)=ea_hex2rgb('#76A08A');
+        colors(1,:)=ea_hex2rgb('#9A872D');
+        colors(2,:)=ea_hex2rgb('#F5CDB6');
+        colors(3,:)=ea_hex2rgb('#F7B0AA');
+        colors(4,:)=ea_hex2rgb('#FDDDA4');
+        colors(5,:)=ea_hex2rgb('#76A08A');
     case 'budapest'
-        pal(1,:)=ea_hex2rgb('#D8A49B');
-        pal(2,:)=ea_hex2rgb('#C7CEF6');
-        pal(3,:)=ea_hex2rgb('#7496D2');
+        colors(1,:)=ea_hex2rgb('#D8A49B');
+        colors(2,:)=ea_hex2rgb('#C7CEF6');
+        colors(3,:)=ea_hex2rgb('#7496D2');
     case 'moonrisekingdom'
-        pal(1,:)=ea_hex2rgb('#B62A3D');
-        pal(2,:)=ea_hex2rgb('#EDCB64');
-        pal(3,:)=ea_hex2rgb('#B5966D');
-        pal(4,:)=ea_hex2rgb('#DAECED');
-        pal(5,:)=ea_hex2rgb('#CECD7B');
+        colors(1,:)=ea_hex2rgb('#B62A3D');
+        colors(2,:)=ea_hex2rgb('#EDCB64');
+        colors(3,:)=ea_hex2rgb('#B5966D');
+        colors(4,:)=ea_hex2rgb('#DAECED');
+        colors(5,:)=ea_hex2rgb('#CECD7B');
     case 'fantasticmrfox'
-        pal(1,:)=ea_hex2rgb('#F8DF4F');
-        pal(2,:)=ea_hex2rgb('#A35E60');
-        pal(3,:)=ea_hex2rgb('#541F12');
-        pal(4,:)=ea_hex2rgb('#CC8B3C');
-        pal(5,:)=ea_hex2rgb('#E8D2B9');
+        colors(1,:)=ea_hex2rgb('#F8DF4F');
+        colors(2,:)=ea_hex2rgb('#A35E60');
+        colors(3,:)=ea_hex2rgb('#541F12');
+        colors(4,:)=ea_hex2rgb('#CC8B3C');
+        colors(5,:)=ea_hex2rgb('#E8D2B9');
     case 'darjeeling'
-        pal(1,:)=ea_hex2rgb('#AEA8A8');
-        pal(2,:)=ea_hex2rgb('#CB9E23');
-        pal(3,:)=ea_hex2rgb('#957A6D');
-        pal(4,:)=ea_hex2rgb('#AC6E49');
+        colors(1,:)=ea_hex2rgb('#AEA8A8');
+        colors(2,:)=ea_hex2rgb('#CB9E23');
+        colors(3,:)=ea_hex2rgb('#957A6D');
+        colors(4,:)=ea_hex2rgb('#AC6E49');
     case 'hotelchevalier'
-        pal(1,:)=ea_hex2rgb('#456355');
-        pal(2,:)=ea_hex2rgb('#FCD16B');
-        pal(3,:)=ea_hex2rgb('#D3DDDC');
-        pal(4,:)=ea_hex2rgb('#C6B19D');
+        colors(1,:)=ea_hex2rgb('#456355');
+        colors(2,:)=ea_hex2rgb('#FCD16B');
+        colors(3,:)=ea_hex2rgb('#D3DDDC');
+        colors(4,:)=ea_hex2rgb('#C6B19D');
     case 'rushmore'
-        pal(1,:)=ea_hex2rgb('#DBB165');
-        pal(2,:)=ea_hex2rgb('#DEB18B');
-        pal(3,:)=ea_hex2rgb('#2E604A');
-        pal(4,:)=ea_hex2rgb('#27223C');
-        pal(5,:)=ea_hex2rgb('#D1362F');
+        colors(1,:)=ea_hex2rgb('#DBB165');
+        colors(2,:)=ea_hex2rgb('#DEB18B');
+        colors(3,:)=ea_hex2rgb('#2E604A');
+        colors(4,:)=ea_hex2rgb('#27223C');
+        colors(5,:)=ea_hex2rgb('#D1362F');
 end
