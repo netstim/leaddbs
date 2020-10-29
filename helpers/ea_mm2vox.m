f0408c71a4eda0f0fbd5646eaaa9e3497d25152c
Ningfei Li <ningfei.li@gmail.com>
2020-10-29 09:44:02 +0100

    ENH: mm2vox and vox2mm also support zero-based coordinates now.

--- helpers/ea_mm2vox.m
+++ helpers/ea_mm2vox.m
@@ -1,9 +1,12 @@
-function vox=ea_mm2vox(mm, transform)
+function vox=ea_mm2vox(mm, transform, base)
 % converts mm-coordinates to voxel-coordinates
 % coords need to be row vector: N*3
 
 if ischar(transform)
-    transform = ea_get_affine(transform);
+    if ~exist('base', 'var')
+        base = 1; % Use one-based indexing by default
+    end
+    transform = ea_get_affine(transform, base);
 end
 
 vox = [mm, ones(size(mm,1),1)] / transform';
--- helpers/ea_vox2mm.m
+++ helpers/ea_vox2mm.m
@@ -1,9 +1,12 @@
-function mm=ea_vox2mm(vox, transform)
+function mm=ea_vox2mm(vox, transform, base)
 % converts voxel-coordinates to mm-coordinates
 % coords need to be row vector: N*3
 
 if ischar(transform)
-    transform = ea_get_affine(transform);
+    if ~exist('base', 'var')
+        base = 1; % Use one-based indexing by default
+    end
+    transform = ea_get_affine(transform, base);
 end
 
 % vox = round(vox);
