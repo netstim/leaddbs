b3d27cda1c7abf272587178b6e2d1fa8a0265ffa
Ningfei Li <ningfei.li@gmail.com>
2020-10-29 12:47:24 +0100

    REF: sub2space uses strrep now.

--- helpers/ea_sub2space.m
+++ helpers/ea_sub2space.m
@@ -1,8 +1,2 @@
 function str=ea_sub2space(str) % replaces subscores with spaces
-if ~iscell(str)
-    str(str=='_')=' ';
-else
-    for c=1:length(str)
-    	str{c}(str{c}=='_')=' ';
-    end
-end
+str = strrep(str, '_', ' ');
