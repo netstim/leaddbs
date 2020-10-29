52d720a190a7161de76fb8885d4cf22dd2134e99
Ningfei Li <ningfei.li@gmail.com>
2020-10-29 12:08:31 +0100

    STYLE: Cosmetic.

--- ea_calc_distance.m
+++ ea_calc_distance.m
@@ -9,13 +9,9 @@ function varargout=ea_calc_distance(varargin)
 % Copyright (C) 2014 Charite University Medicine Berlin, Movement Disorders Unit
 % Andreas Horn
 
-
 priordist=varargin{1};
-
 trajvector=varargin{2};
 mat=varargin{3};
-
-
 
 newtrajvector=trajvector*mat; % i.e. the vector between 0,0,0 and some point in direction of trajvector.
 % stretch factor in trajectory as found by mat.
@@ -26,25 +22,18 @@ dist_mm=priordist*normfactor;
 varargout{2}=dist_mm;
 varargout{1}=normfactor;
 
-
 % calculate voxel distances if nargin > 3.
-
 if nargin>3
     img=varargin{4};
     V=spm_vol(img);
     XYZ_mm=[0,0,0,1;trajvector,1]';
 
-    XYZ_vx= V.mat \ XYZ_mm;
-    XYZ_vx=XYZ_vx(1:3,:);
-    
-    
+    XYZ_vx = V.mat \ XYZ_mm;
+    XYZ_vx = XYZ_vx(1:3,:);
+
     trajvox=diff(XYZ_vx')';
-    
+
     worldtovoxfactor=norm(trajvox)/norm(trajvector);
     dist_vox=priordist*worldtovoxfactor*normfactor;
     varargout{3}=dist_vox;
 end
-
-
-
-
