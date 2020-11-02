6d6dce52a0d0284c927d3ed48ba153245f3dafdf
Ningfei Li <ningfei.li@gmail.com>
2020-11-02 10:55:08 +0100

    BUG: Fix rotating y marker.
    Rotate only when head is anterior.

--- ea_save_reconstruction.m
+++ ea_save_reconstruction.m
@@ -119,7 +119,7 @@ if ~reco.props(options.sides(1)).manually_corrected
             plot3(reco.mni.markers(side).y(:,1),reco.mni.markers(side).y(:,2),reco.mni.markers(side).y(:,3),'g*');
         end
 
-        if reco.mni.markers(side).head(2)<reco.mni.markers(side).y(2) % FIX ME need to check whether > or < is correct here.
+        if reco.mni.markers(side).head(2)>reco.mni.markers(side).y(2) % FIX ME need to check whether > or < is correct here.
             reco.mni.markers(side).y=reco.mni.markers(side).head+(reco.mni.markers(side).head-reco.mni.markers(side).y); % 180 deg flip
             corrected=1;
         end
