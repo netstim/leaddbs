8ac64de0088a3d336d0f0eeee36736304bc8cfc2
Andreas Horn <andreas.horn@charite.de>
2020-11-06 14:14:37 +0100

    Refine struct2roi

--- classes/ea_roi/ea_struct2roi.m
+++ classes/ea_roi/ea_struct2roi.m
@@ -2,7 +2,7 @@ function roi=ea_struct2roi(s,resultfig,ht)
 
 s.plotFigureH=resultfig;
 s.htH=ht;
-roi=ea_roi(s.niftiFilename,s);
+roi=ea_roi;
 props = fieldnames(s);
 for p = 1:numel(props)
     if ~ismember(props{p},{'controlH','plotFigureH','patchH','toggleH','htH','Tag'})
