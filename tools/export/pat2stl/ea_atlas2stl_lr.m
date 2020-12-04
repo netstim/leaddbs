20e788cacca6cb948f9efb19b6987ff119f344a4
Ningfei Li <ningfei.li@gmail.com>
2020-12-05 00:02:25 +0100

    STYLE: Formatting.

--- tools/export/pat2stl/ea_atlas2stl_lr.m
+++ tools/export/pat2stl/ea_atlas2stl_lr.m
@@ -5,30 +5,28 @@ if ~iscell(atlasnames)
 end
 earoot=ea_getearoot;
 for atl=1:length(atlasnames)
-
     load([ea_space([],'atlases'),atlasnames{atl},filesep,'atlas_index.mat']);
     for side=1:2
         cnt=1;
         clear cfv
-    for mesh=1:length(atlases.names)
-        cfv(cnt).vertices=atlases.roi{mesh,side}.fv.vertices;
-        cfv(cnt).faces=atlases.roi{mesh,side}.fv.faces;
-        cfv(cnt).facevertexcdata=atlases.roi{mesh,side}.cdat';
-        if isempty(cfv(cnt).facevertexcdata) % fiber atlas
-            cfv(cnt).facevertexcdata=repmat(atlases.roi(mesh,side).color,size(cfv(cnt).faces,1),1);
+        for mesh=1:length(atlases.names)
+            cfv(cnt).vertices=atlases.roi{mesh,side}.fv.vertices;
+            cfv(cnt).faces=atlases.roi{mesh,side}.fv.faces;
+            cfv(cnt).facevertexcdata=atlases.roi{mesh,side}.cdat';
+            if isempty(cfv(cnt).facevertexcdata) % fiber atlas
+                cfv(cnt).facevertexcdata=repmat(atlases.roi(mesh,side).color,size(cfv(cnt).faces,1),1);
+            end
+
+            cnt=cnt+1;
         end
 
-        cnt=cnt+1;
-    end
+        cfv=ea_concatfv(cfv);
 
-    cfv=ea_concatfv(cfv);
+        cfv=ea_mapcolvert2face(cfv);
 
-    cfv=ea_mapcolvert2face(cfv);
-
-    [pth,fn,ext]=fileparts(ofn);
-    sofn=fullfile(pth,[fn,'_',num2str(side),ext]);
-    ea_stlwrite(sofn,cfv,'FACECOLOR',cfv.facevertexcdata);
-
+        [pth,fn,ext]=fileparts(ofn);
+        sofn=fullfile(pth,[fn,'_',num2str(side),ext]);
+        ea_stlwrite(sofn,cfv,'FACECOLOR',cfv.facevertexcdata);
     end
 end
 
