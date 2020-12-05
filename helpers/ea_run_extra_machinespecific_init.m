f079cbc85eb72f9539d09cdf1936c4d902623106
oprienrico <repos@opri.it>
2020-12-05 14:11:17 -0500

    bugfix for unix platform (static linked libstdc). Now it is set as a new platform specific option: "prefs.platform.unix.load_shipped_libstdcpp6=false"

--- helpers/ea_run_extra_machinespecific_init.m
+++ helpers/ea_run_extra_machinespecific_init.m
@@ -15,7 +15,7 @@ if prefs.platform.unix.load_shipped_libstdcpp6
     % Include this line if libstdc++.so.6 is needed.
     % However it is preferrable to fix it at system level (e.g. using package build-essentials).
     % Install the matlab-support package and choose to use the system libraries for gcc.
-    addpath(fullfile(earoot,'ext_libs\support\unix'));
+    addpath(fullfile(earoot,'ext_libs/support/unix'));
 else
-    rmpath(fullfile(earoot,'ext_libs\support\unix'));
+    rmpath(fullfile(earoot,'ext_libs/support/unix'));
 end
