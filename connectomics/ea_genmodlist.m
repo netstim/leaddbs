6cfdc6286f5715a4c329aeb124fa818c761fc4bc
Ningfei Li <ningfei.li@gmail.com>
2020-12-07 00:00:46 +0100

    REF: Refine var name.

--- connectomics/ea_genmodlist.m
+++ connectomics/ea_genmodlist.m
@@ -1,7 +1,7 @@
-function [modlist, sf] = ea_genmodlist(directory,selectedparc,options)
+function [modlist, type] = ea_genmodlist(directory,selectedparc,options)
 cnt=1;
 modlist=cell(0);
-sf=[];
+type=[];
 
 % check for canonical fiber sets
 fdfibs=dir(ea_getconnectomebase('dmri'));
@@ -9,7 +9,7 @@ for fdf=1:length(fdfibs)
     if fdfibs(fdf).isdir && ~strcmp(fdfibs(fdf).name(1),'.')
         [~,fn]=fileparts(fdfibs(fdf).name);
         modlist{cnt}=fn;
-        sf(cnt)=1;
+        type(cnt)=1;
         cnt=cnt+1;
     end
 end
@@ -22,7 +22,7 @@ for fdf=1:length(fc)
             [~,fn]=fileparts(fc(fdf).name);
             for ds=1:length(d.dataset.subsets)
                 modlist{cnt}=[fn,' > ',d.dataset.subsets(ds).name];
-                sf(cnt)=2;
+                type(cnt)=2;
                 cnt=cnt+1;
             end
     end
@@ -33,7 +33,7 @@ if exist('directory','var') && ~isempty(directory)
     % check if pat-specific fibertracts are present:
     if exist([directory,'connectomes',filesep,'dMRI',filesep,options.prefs.FTR_normalized],'file')
         modlist{cnt}='Patient''s fiber tracts';
-        sf(cnt)=1;
+        type(cnt)=1;
         cnt=cnt+1;
     end
 
@@ -41,7 +41,7 @@ if exist('directory','var') && ~isempty(directory)
     % check if rest_tc are present:
     if exist([directory,'connectomics',filesep,selectedparc,filesep,'rest_tc.mat'],'file')
         modlist{cnt}='Patient''s fMRI time courses';
-        sf(cnt)=2;
+        type(cnt)=2;
         cnt=cnt+1;
     end
 
@@ -50,7 +50,7 @@ if exist('directory','var') && ~isempty(directory)
     for ff=1:length(ffis)
         [~, restfname] = fileparts(ffis(ff).name);
         modlist{cnt} = ['Patient''s fMRI - ', restfname];
-        sf(cnt)=2;
+        type(cnt)=2;
         cnt=cnt+1;
     end
 
@@ -64,11 +64,11 @@ if exist('options','var')
         for fo=1:length(dfo)
             if dfo(fo).isdir && ~strcmp(dfo(fo).name(1),'.')
                 if ~isempty(dir([stimdir,dfo(fo).name,filesep,'vat_seed_*func_seed_AvgR.nii']))
-                    sf(cnt)=2; % fMRI result
+                    type(cnt)=2; % fMRI result
                     modlist{cnt}=['Precomputed: ',dfo(fo).name];
                     cnt=cnt+1;
                 elseif ~isempty(dir([stimdir,dfo(fo).name,filesep,'vat_seed_*struc_seed.nii']))
-                    sf(cnt)=1; % dMRI result
+                    type(cnt)=1; % dMRI result
                     modlist{cnt}=['Precomputed: ',dfo(fo).name];
                     cnt=cnt+1;
                 end
