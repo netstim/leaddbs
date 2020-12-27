aef9e2fa415115308f0065368bbb093acb785d18
Andreas Horn <andreas.horn@charite.de>
2020-12-27 21:42:39 +0100

    ea_corr to support nans & infs

--- helpers/ea_corr.m
+++ helpers/ea_corr.m
@@ -3,17 +3,32 @@ function varargout=ea_corr(X,Y,corrtype)
 if ~exist('corrtype','var')
     corrtype='pearson';
 end
+
+if any(isnan(X)) || any(isnan(Y))
+    notnan=(~isnan(X)).*(~isnan(Y));
+else
+    notnan=ones(length(X),1);
+end
+if any(isinf(X)) || any(isinf(Y))
+    notinf=(~isinf(X)).*(~isinf(Y));
+else
+    notinf=ones(length(X),1);
+end
+
+notnan=logical(notnan.*notinf);
+
 switch lower(corrtype)
     case 'pearson'
-        [varargout{1},varargout{2}]=corr(X,Y,'rows','pairwise','type','Pearson');
+        [varargout{1},varargout{2}]=corr(X(notnan),Y(notnan),'type','Pearson');
     case 'spearman'
-        [varargout{1},varargout{2}]=corr(X,Y,'rows','pairwise','type','Spearman');
+        [varargout{1},varargout{2}]=corr(X(notnan),Y(notnan),'type','Spearman');
     case 'kendall'
-        [varargout{1},varargout{2}]=corr(X,Y,'rows','pairwise','type','Kendall');
+        [varargout{1},varargout{2}]=corr(X(notnan),Y(notnan),'type','Kendall');
     case 'bend'
-        [varargout{1},varargout{2}]=ea_bendcorr(X,Y);
-    case 'skipped spearman'
-        [varargout{1}]=ea_skipped_correlation(X,Y,'Spearman');
+        [varargout{1},varargout{2}]=ea_bendcorr(X(notnan),Y(notnan));
+    case {'skipped spearman','skipped'}
+        [varargout{1}]=ea_skipped_correlation(X(notnan),Y(notnan),'Spearman');
     case 'skipped pearson'
-        [varargout{1}]=ea_skipped_correlation(X,Y,'Pearson');
+        [varargout{1}]=ea_skipped_correlation(X(notnan),Y(notnan),'Pearson');
 end
+
