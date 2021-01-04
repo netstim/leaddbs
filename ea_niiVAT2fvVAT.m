2717d523f14d64a3bfd7f98e1909f88f547ee4a9
Ningfei Li <ningfei.li@gmail.com>
2021-01-04 23:12:34 +0100

    ENH: Add smooth to niivat2fvvat.

--- ea_niiVAT2fvVAT.m
+++ ea_niiVAT2fvVAT.m
@@ -2,12 +2,27 @@ function vatfv = ea_niiVAT2fvVAT(nii)
 start = nii.mat * [1;1;1;1];
 niisize = size(nii.img);
 stop = nii.mat * [niisize,1]';
+gv = cell(1,3);
 for dim=1:3
     gv{dim}=linspace(start(dim),stop(dim),niisize(dim));
 end
+
 [xg,yg,zg] = meshgrid(gv{1},gv{2},gv{3});
 vatfv=isosurface(xg,yg,zg,permute(nii.img,[2,1,3]),0.75);
 caps=isocaps(xg,yg,zg,permute(nii.img,[2,1,3]),0.5);
 vatfv.faces=[vatfv.faces;caps.faces+size(vatfv.vertices,1)];
 vatfv.vertices=[vatfv.vertices;caps.vertices];
+
+try
+    vatfv=ea_smoothpatch(vatfv,1,35);
+catch
+    try
+        cd([ea_getearoot,'ext_libs',filesep,'smoothpatch']);
+        mex ea_smoothpatch_curvature_double.c -v
+        mex ea_smoothpatch_inversedistance_double.c -v
+        mex ea_vertex_neighbours_double.c -v
+        vatfv=ea_smoothpatch(vatfv);
+    catch
+        warndlg('Patch could not be smoothed. Please supply a compatible Matlab compiler to smooth VTAs.');
+    end
 end
