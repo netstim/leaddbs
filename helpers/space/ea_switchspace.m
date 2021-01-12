36fff05478e43aa6bdc34a2613288176ef59f6fd
Ningfei Li <ningfei.li@gmail.com>
2021-01-12 11:52:24 +0100

    ENH: Set togglestates and view when switching space.

--- helpers/space/ea_switchspace.m
+++ helpers/space/ea_switchspace.m
@@ -10,12 +10,27 @@ else
 end
 if strcmp(answ,'Sure')
     ea_storemachineprefs('space',spacename);
+
+    % Get space definitions
+    spacedef = ea_getspacedef;
+
+    % Check if there's space specific togglestates setting
+    if ~isfield(spacedef, 'togglestates') % Fallback
+        mnispace = [ea_getearoot,'templates',filesep,'space',filesep,'MNI_ICBM_2009b_NLIN_ASYM'];
+        load([mnispace, filesep, 'ea_space_def.mat'], 'spacedef');
+    end
+    ea_storemachineprefs('togglestates', spacedef.togglestates);
+
+    % Check if there's space specific view setting
+    if ~isfield(spacedef, 'view') % Fallback
+        mnispace = [ea_getearoot,'templates',filesep,'space',filesep,'MNI_ICBM_2009b_NLIN_ASYM'];
+        load([mnispace, filesep, 'ea_space_def.mat'], 'spacedef');
+    end
+    ea_storemachineprefs('view', spacedef.view);
+
     if ~exist('mute','var')
         disp('Restarting Lead Neuroimaging Suite...');
         close all force
         lead;
     end
 end
-
-
-
