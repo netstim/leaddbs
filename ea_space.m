55f0dc69c09fc6cb6c37e45eed098d2c76f9f058
Ningfei Li <ningfei.li@gmail.com>
2021-01-12 11:50:18 +0100

    STYLE: Formatting.

--- ea_space.m
+++ ea_space.m
@@ -4,36 +4,37 @@ spacename=ea_getspace; % as default for now
 if ~exist('cmd','var')
     cmd='space';
 end
+
 if ~exist('native','var')
     native=0; % additional native variable will be used if not working in native space but still wanting to e.g. list atlases from native space. Is not important.
 end
+
 if ~exist('options','var')
-   options=struct; 
+   options=struct;
 end
+
 if ~isfield(options,'native')
     options.native=0;
 end
+
 if exist('native','var') % overwrite options.native with native
-   options.native=native; 
+   options.native=native;
 end
-    
 
 switch cmd
     case 'space'
-        
         path=[ea_getearoot,'templates',filesep,'space',filesep,spacename,filesep];
-        
     case 'atlases'
         if options.native || native
             path=[options.root,options.patientname,filesep,'atlases',filesep];
         else
             path=[ea_getearoot,'templates',filesep,'space',filesep,spacename,filesep,'atlases',filesep];
         end
-    case {'subcortical','schoenecker'} 
+    case {'subcortical','schoenecker'}
         path=[ea_getearoot,'templates',filesep,'space',filesep,spacename,filesep,'subcortical',filesep];
     case 'cortex'
         path=[ea_getearoot,'templates',filesep,'space',filesep,spacename,filesep,'cortex',filesep];
-        
+
     case 'labeling'
         path=[ea_getearoot,'templates',filesep,'space',filesep,spacename,filesep,'labeling',filesep];
 
