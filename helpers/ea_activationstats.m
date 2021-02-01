286757cb5c5a4e5d7f522b3d2e8f888bcde610cd
Ningfei Li <ningfei.li@gmail.com>
2021-02-01 21:20:36 +0100

    DOC: Update comments.

--- helpers/ea_activationstats.m
+++ helpers/ea_activationstats.m
@@ -6,21 +6,21 @@ if isfile([axonActivationFolder, filesep, 'axonActivation_left.mat'])
     load([axonActivationFolder, filesep, 'axonActivation_left.mat'], 'fibers', 'idx');
 end
 
-% Activated pathways
+% Activated axons
 axonIdx = unique(fibers(fibers(:,5)==1,4));
 if isempty(axonIdx)
     axonIdx = 0;
 end
 fprintf('\nLeft side, Activated axons: %d out of %d\n', length(axonIdx), length(idx));
 
-% Activated pathways
+% Non-Activated axons
 axonIdx = unique(fibers(fibers(:,5)==0,4));
 if isempty(axonIdx)
     axonIdx = 0;
 end
 fprintf('\nLeft side, Non-activated axons: %d out of %d\n', length(axonIdx), length(idx));
 
-% Activated pathways
+% Damaged axons
 axonIdx = unique(fibers(fibers(:,5)==-1,4));
 if isempty(axonIdx)
     axonIdx = 0;
@@ -32,21 +32,21 @@ if isfile([axonActivationFolder, filesep, 'axonActivation_right.mat'])
     load([axonActivationFolder, filesep, 'axonActivation_right.mat'], 'fibers', 'idx');
 end
 
-% Activated pathways
+% Activated axons
 axonIdx = unique(fibers(fibers(:,5)==1,4));
 if isempty(axonIdx)
     axonIdx = 0;
 end
 fprintf('\n\nRight side, Activated axons: %d out of %d\n', length(axonIdx), length(idx));
 
-% Activated pathways
+% Non-activated axons
 axonIdx = unique(fibers(fibers(:,5)==0,4));
 if isempty(axonIdx)
     axonIdx = 0;
 end
 fprintf('\nRight side, Non-activated axons: %d out of %d\n', length(axonIdx), length(idx));
 
-% Activated pathways
+% Damaged axons
 axonIdx = unique(fibers(fibers(:,5)==-1,4));
 if isempty(axonIdx)
     axonIdx = 0;
