8d416c734297994d5da867793bcc708f090fc5e4
Andreas Horn <andreas.horn@charite.de>
2021-04-18 20:05:37 +0200

    Added export functions networkmapping

--- helpers/ea_space2sub.m
+++ helpers/ea_space2sub.m
@@ -1,10 +1,14 @@
 function str=ea_space2sub(str) % replaces subscores with spaces
 if ~iscell(str)
 str(str==' ')='_';
+str(str==')')='';
+str(str=='(')='';
 else
     for c=1:length(str)
         
       str{c}(str{c}==' ')='_';
-  
+      str{c}(str{c}=='(')='';
+      str{c}(str{c}==')')='';
+      
     end
 end
--- helpers/stats/ea_glmplot.m
+++ helpers/stats/ea_glmplot.m
@@ -1,4 +1,4 @@
-function [h,pv,Rsquared,F,mdl,bb,dev,allstats]=ea_glmplot(X,y,labels,distribution,group1,group2)
+function [h,pv,Rsquared,F,mdl,bb,dev,allstats]=ea_glmplot(X,y,labels,distribution,group1,group2,colors,markers,switchxy)
 
 if ~(size(y,2)==1)
     ea_warning('Assuming X and y were switched. Switching variables.');
@@ -44,17 +44,51 @@ else
     end
 end
 
+if exist('colors', 'var') && ~isempty(colors)
+    map = colors;
+    if isnumeric(map) && ~isempty(group1)
+        if size(map,1) ~= numel(unique(group1.idx))
+            error('Number of custom colors doesn''t match number of categories!');
+        end
+    end
+else
+    map = 'lch';
+end
+
+if exist('markers', 'var') && ~isempty(markers)
+    if ~isempty(group2)
+        if numel(markers) < numel(unique(group2.idx))
+            error('Number of custom markers is less than number of categories!');
+        end
+    end
+else
+    markers = {'o' 's' 'd' '^' 'v' '>' '<' 'p' 'h' '*' '+' 'x'};
+end
+
+if ischar(map)
+    colorOptions = {'map', map};
+else
+    colorOptions = {'map', map, 'n_color', size(map,1), 'n_lightness', 1};
+end
+
+
 mdl=fitglm(X,y,'distribution',distribution);
 
 [bb,dev,allstats]=glmfit(X,y,distribution);
 
 yhat=predict(mdl,X);
-g=gramm('x',yhat,'y',y); % data needs to be put in "reversed" for gramm.
+if exist('switchxy','var') && switchxy
+    g=gramm('x',y,'y',yhat); % data needs to be put in "reversed" for gramm.
+else
+    g=gramm('x',yhat,'y',y); % data needs to be put in "reversed" for gramm.
+end
 if isempty(group1) && isempty(group2)
     g.geom_point();
+    g.set_color_options(colorOptions{:});
 else
     g.set_color_options('chroma',0,'lightness',30);
 end
+g.set_point_options('markers', markers, 'base_size', 7);
 g.stat_glm('distribution',distribution,'fullrange','false','fullrange','false');
 
 
@@ -72,7 +106,7 @@ h=figure('Position',[100 100 550 550]);
 g.draw();
 if ~isempty(group2) && ~isempty(group1)
     g.update('marker',group2.idx,'color',group1.idx);
-    g.set_color_options();
+    g.set_color_options(colorOptions{:});
     g.set_names('marker',group2.tag,'color',group1.tag,'x',labels{2},'y',labels{3});
     g.geom_point();
     g.draw();
@@ -86,7 +120,7 @@ elseif ~isempty(group2) && isempty(group1)
     set(h,'Position',[100 100 650 550]);
 elseif isempty(group2) && ~isempty(group1)
     g.update('color',group1.idx);
-    g.set_color_options();
+    g.set_color_options(colorOptions{:});
     g.set_names('color',group1.tag,'x',labels{2},'y',labels{3});
     g.geom_point();
     g.draw();
