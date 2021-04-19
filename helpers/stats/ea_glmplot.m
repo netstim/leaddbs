da65d68356c61eea534764c1bd234cfdf1b9092b
Ningfei Li <ningfei.li@gmail.com>
2021-04-19 08:41:17 +0200

    STYLE: Cosmetic.

--- helpers/stats/ea_glmplot.m
+++ helpers/stats/ea_glmplot.m
@@ -6,8 +6,6 @@ if ~(size(y,2)==1)
     y=X;
     X=Xn;
 end
-    
-    
 
 if ~exist('labels','var')
     labels={'','X','Y'};
@@ -31,7 +29,6 @@ else
         group1=group1s;
     end
 end
-
 
 if ~exist('group2','var')
     group2=[];
@@ -71,39 +68,42 @@ else
     colorOptions = {'map', map, 'n_color', size(map,1), 'n_lightness', 1};
 end
 
-
 mdl=fitglm(X,y,'distribution',distribution);
-
 [bb,dev,allstats]=glmfit(X,y,distribution);
-
 yhat=predict(mdl,X);
+
 if exist('switchxy','var') && switchxy
     g=gramm('x',y,'y',yhat); % data needs to be put in "reversed" for gramm.
 else
     g=gramm('x',yhat,'y',y); % data needs to be put in "reversed" for gramm.
 end
+
 if isempty(group1) && isempty(group2)
     g.geom_point();
     g.set_color_options(colorOptions{:});
 else
     g.set_color_options('chroma',0,'lightness',30);
 end
+
 g.set_point_options('markers', markers, 'base_size', 7);
 g.stat_glm('distribution',distribution,'fullrange','false','fullrange','false');
-
 
 [~,~,~,~,stats]=regress(y,ea_addone(X));
 %g.geom_abline();
 %g.stat_cornerhist('edges',[ea_nanmean(X)-1*ea_nanstd(X):0.2:ea_nanmean(X)+1*ea_nanstd(X)],'aspect',0.6,'location',max(X));
 pstr='p';
-pv=stats(3); F=stats(2);
+pv=stats(3);
+F=stats(2);
 Rsquared=mdl.Rsquared.Ordinary;
+
 g.set_title([labels{1},' [R2 = ',sprintf('%.2f',Rsquared),'; F-stat = ',sprintf('%.2f',F),'; ',pstr,' = ',sprintf('%.3f',pv),']'],'FontSize',20);
 g.set_names('x',labels{2},'y',labels{3});
 g.set_text_options('base_size',22);
 g.no_legend();
+
 h=figure('Position',[100 100 550 550]);
 g.draw();
+
 if ~isempty(group2) && ~isempty(group1)
     g.update('marker',group2.idx,'color',group1.idx);
     g.set_color_options(colorOptions{:});
@@ -129,4 +129,3 @@ end
 
 set([g.results.geom_point_handle],'MarkerSize',7);
 set([g.results.geom_point_handle],'MarkerEdgeColor','w');
-
