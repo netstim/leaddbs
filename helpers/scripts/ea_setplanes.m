e9dc9adeca47d493402c56dc1dcecb5b0113907c
Ningfei Li <ningfei.li@gmail.com>
2021-04-21 16:33:40 +0200

    REF: Refine value setting logic, add doc.

--- helpers/scripts/ea_setplanes.m
+++ helpers/scripts/ea_setplanes.m
@@ -1,17 +1,26 @@
 function ea_setplanes(xx,yy,zz,options)
 
+% Get 3D visualization figure handle
 H = findall(0,'type','figure');
 resultfig = H(contains({H(:).Name},{'Electrode-Scene'}));
 resultfig = resultfig(1); % take the first if there are many.
-togglestates=getappdata(resultfig,'togglestates');
-setXYZ=[xx,yy,zz];
-togglestates.xyztoggles=~isnan(setXYZ);
-setXYZ(~togglestates.xyztoggles)=togglestates.xyzmm(~togglestates.xyztoggles);
-togglestates.xyzmm=setXYZ;
+
+% Set non-nan values to xyzmm
+togglestates = getappdata(resultfig,'togglestates');
+setXYZ = [xx,yy,zz];
+togglestates.xyzmm(~isnan(setXYZ)) = setXYZ(~isnan(setXYZ));
+
+% Flag to refresh view
 togglestates.refreshview=1;
+
+% Options for ea_anatomyslices input
 if ~exist('options','var')
     options=struct;
 end
+
+% Set togglestates
 setappdata(resultfig,'togglestates',togglestates);
 
+
+% Update slices
 ea_anatomyslices(resultfig, togglestates, options, []);
