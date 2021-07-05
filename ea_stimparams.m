6433858f6fb5ccaa397d49722795041ee9d25b0b
Ningfei Li <ningfei.li@gmail.com>
2021-07-05 10:50:39 +0200

    BUG: Fix missing stimFolder in native mode.

--- ea_stimparams.m
+++ ea_stimparams.m
@@ -3513,5 +3513,6 @@ if hObject.Value
     numContacts = options.elspec.numel;
     stimLabel = getappdata(handles.stimfig, 'stimlabel');
     stimFolder = [options.root,options.patientname,filesep,'stimulations',filesep,ea_nt(~handles.estimateInTemplate.Value),stimLabel];
+    ea_mkdir(stimFolder);
     ea_addStimSet(numContacts, stimFolder, hObject);
 end
