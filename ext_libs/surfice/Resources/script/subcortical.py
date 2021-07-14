ad32d6db96775075f97caebd0acb1883eeabe517
Ningfei Li <ningfei.li@gmail.com>
2021-07-14 22:23:33 +0200

    EXT[Surf-Ice]: Clean up resources.
    Also add missing python35.zip for Windows.

--- ext_libs/surfice/Resources/script/subcortical.py
+++ ext_libs/surfice/Resources/script/subcortical.py
@@ -3,21 +3,36 @@ gl.resetdefaults()
 kframesperrotation = 180
 gl.meshload('BrainMesh_ICBM152_smoothed.lh.mz3')
 gl.meshcurv()
-gl.shadername('hidecurves')
 gl.overlayload('CIT168.mz3')
 gl.azimuthelevation(250, 35)
 gl.meshhemisphere(-1)
-for i in range(1, kframesperrotation * 5):
+for i in range(0, kframesperrotation * 7):
+  rot = (i / kframesperrotation)
   if ((i % kframesperrotation) == 0):
-    rot = (i / kframesperrotation)
-    if rot == 1:
-        gl.shadername('outline')
+    shader = 'Default'
+    if rot == 0:
+        shader = 'hidecurves'
+    elif rot == 1:
+        shader = 'outline'
     elif rot == 2:
-        gl.shadername('wire')
+        shader = 'wire'
     elif rot == 3:
-        gl.shadername('squares')
-        gl.shaderambientocclusion(0)
+        shader = 'squares'
+    elif rot == 4:
+        shader = 'xGaps'
     else :
-        gl.shadername('xGaps')
-  gl.azimuth( int(round(360.0/kframesperrotation)))
+        shader = 'MatCap'
+    gl.shadername(shader)
+    print('shader: ' + shader)
+  azimuth = round((i * 360.0/kframesperrotation) % 360)
+  xrayObj= 1.0
+  if (rot == 5):
+    xrayObj = abs(180 - azimuth) / 180
+  xrayOver= 0.0
+  if (rot == 6):
+    xrayOver = 1.0 - abs(180 - azimuth) / 180
+  gl. shaderxray(xrayObj, xrayOver);
+  gl.azimuthelevation(int(azimuth), 35);
   gl.wait(20)
+  #if (azimuth == 270):
+  #  gl.savebmp(str(rot)+'.png')
