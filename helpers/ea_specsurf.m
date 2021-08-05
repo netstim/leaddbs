b0dbfeb072695c1afd9647d4cc4766dddca0896c
Ningfei Li <ningfei.li@gmail.com>
2021-08-05 14:29:38 +0200

    BUG: Fix setting Vertex CData.
    Refine logic.

--- helpers/ea_specsurf.m
+++ helpers/ea_specsurf.m
@@ -2,7 +2,7 @@ function ea_specsurf(varargin)
 
 surfc = varargin{1};
 color = varargin{2};
-if nargin==3
+if nargin>=3
     aData = varargin{3};
 end
 
@@ -19,24 +19,48 @@ catch % if color is denoted as gray value (1x1) only
     cd(:,:,3) = color(1);
 end
 
+set(surfc,'AlphaDataMapping','none');
+
 cd = cd+0.01*randn(size(cd));
+set(surfc,'CData',cd);
+
+set(surfc,'EdgeColor','none');
+
+if nargin>=3
+    set(surfc,'FaceAlpha',aData);
+end
 
 set(surfc,'FaceColor','interp');
-set(surfc,'CData',cd);
+set(surfc,'FaceLighting','phong');
 
 try % for patches
     Vertices = get(surfc,'Vertices');
     cd = zeros(size(Vertices));
-    cd(:) = color(1);
+    cd = repmat(color, size(cd,1), size(cd,2)/size(color,2));
     set(surfc,'FaceVertexCData',cd);
 end
 
-set(surfc,'AlphaDataMapping','none');
-set(surfc,'FaceLighting','phong');
-set(surfc,'SpecularColorReflectance',0);
-set(surfc,'SpecularExponent',10);
-set(surfc,'EdgeColor','none')
-
-if nargin==3
-    set(surfc,'FaceAlpha',aData);
+if nargin>3
+    switch varargin{4} % material
+        case 'metal'
+            set(surfc,'AmbientStrength',0.2); %0.1;
+            set(surfc,'DiffuseStrength',0.3); %0.2;
+            set(surfc,'SpecularColorReflectance',0.1);
+            set(surfc,'SpecularExponent',20);
+            set(surfc,'SpecularStrength',1.0);
+            % met=load([ea_getearoot,'icons',filesep,'metal.mat']);
+            % ea_patchtexture(surfc,met.X);
+        case 'insulation'
+            set(surfc,'AmbientStrength',0.4);
+            set(surfc,'DiffuseStrength',0.35);
+            set(surfc,'SpecularColorReflectance',1.0);
+            set(surfc,'SpecularExponent',3);
+            set(surfc,'SpecularStrength',0.21);
+    end
+else % default
+    set(surfc,'AmbientStrength',0.3);
+    set(surfc,'DiffuseStrength',0.4);
+    set(surfc,'SpecularColorReflectance',0);
+    set(surfc,'SpecularExponent',3);
+    set(surfc,'SpecularStrength',0.21);
 end
