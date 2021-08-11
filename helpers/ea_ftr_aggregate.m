2d953b7d91c31a059fd4e267bc5c7d64871f5150
Ningfei Li <ningfei.li@gmail.com>
2021-08-11 14:40:37 +0200

    STYLE: Formatting.

--- helpers/ea_ftr_aggregate.m
+++ helpers/ea_ftr_aggregate.m
@@ -16,6 +16,7 @@ function [fibers, idx] = ea_ftr_aggregate(ftrFiles, outputFile, sel, type, filte
 if ~exist('filtermask','var')
     filtermask='';
 end
+
 fibers = [];
 idx = [];
 
@@ -42,36 +43,33 @@ end
 % Reset fibers(:, 4)
 fibers(:, 4) = repelem(1:numel(idx), idx);
 
-
 % filter
 if ~isempty(filtermask)
-   [~,~,ext]=fileparts(filtermask);
-   if strcmp(ext,'.gz')
-       tmpfn=tempname;
-       copyfile(filtermask,[tmpfn,'.nii.gz']);
-       gunzip([tmpfn,'.nii.gz']);
-       ea_delete([tmpfn,'.nii.gz']);
-       filtermask=[tmpfn,'.nii'];
-   end
-   filter=ea_open_vol(filtermask); 
-   fibvox=fibers;
-   fibvox(:,4)=1;
-   fibvox=(filter.mat\fibvox');
-   ids=spm_sample_vol(filter,double(fibvox(1,:)),double(fibvox(2,:)),double(fibvox(3,:)),1);
-   fibers=fibers(logical(ids),:);
-   
-   % recompute idx:
-   [~,iax,iac]=unique(fibers(:,4));
-   clear idx
-   for fib=1:length(iax)-1
-   idx(fib,1)=iax(fib+1)-iax(fib);
-   end
-   % add last entry
-   idx(fib+1,1)=sum(fibers(:,4)==max(fibers(:,4)));
-   fibers(:,4)=iac; % make sure no single fiber was completely erased and is not accounted for.
+    [~,~,ext]=fileparts(filtermask);
+    if strcmp(ext,'.gz')
+        tmpfn=tempname;
+        copyfile(filtermask,[tmpfn,'.nii.gz']);
+        gunzip([tmpfn,'.nii.gz']);
+        ea_delete([tmpfn,'.nii.gz']);
+        filtermask=[tmpfn,'.nii'];
+    end
+    filter=ea_open_vol(filtermask); 
+    fibvox=fibers;
+    fibvox(:,4)=1;
+    fibvox=(filter.mat\fibvox');
+    ids=spm_sample_vol(filter,double(fibvox(1,:)),double(fibvox(2,:)),double(fibvox(3,:)),1);
+    fibers=fibers(logical(ids),:);
+    
+    % recompute idx:
+    [~,iax,iac]=unique(fibers(:,4));
+    clear idx
+    for fib=1:length(iax)-1
+        idx(fib,1)=iax(fib+1)-iax(fib);
+    end
+    % add last entry
+    idx(fib+1,1)=sum(fibers(:,4)==max(fibers(:,4)));
+    fibers(:,4)=iac; % make sure no single fiber was completely erased and is not accounted for.
 end
-
-
 
 % Use other meta info from the first FTR file
 ftr = load(ftrFiles{1});
