683fd7b9aa373257c05730e35fa687303d779bd5
Ningfei Li <ningfei.li@gmail.com>
2021-09-01 22:24:49 +0200

    STYLE: Cleanup legacy code, formatting.

--- ea_options2handles.m
+++ ea_options2handles.m
@@ -1,13 +1,12 @@
 function ea_options2handles(options,handles)
 
-
-%% set handles
+% set handles
 set(handles.dicomcheck,'Value',options.dicomimp.do);
 set(handles.normalize_checkbox,'Value',options.normalize.do);
 if options.normalize.methodn>length(handles.normmethod,'String')
-set(handles.normmethod,'Value',1);
+    set(handles.normmethod,'Value',1);
 else
-set(handles.normmethod,'Value',options.normalize.methodn);
+    set(handles.normmethod,'Value',options.normalize.methodn);
 end
 set(handles.normcheck,'Value',options.normalize.check);
 
@@ -33,9 +32,7 @@ else
     set(handles.left_checkbox,'Value',0);
 end
 
-
 set(handles.doreconstruction_checkbox,'Value',options.doreconstruction);
-
 
 if options.automask
     set(handles.maskwindow_txt,'String','auto')
@@ -55,4 +52,3 @@ set(handles.targetpopup,'Value',options.entrypointn);
 set(handles.electrode_model_popup,'Value',options.elmodeln);
 set(handles.atlassetpopup,'Value',options.atlassetn);
 set(handles.exportservercheck,'Value',options.d3.autoserver);
-
