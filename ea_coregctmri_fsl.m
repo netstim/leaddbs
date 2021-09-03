cfbede905dd945afe29c149f75f703a4792c4a78
Ningfei Li <ningfei.li@gmail.com>
2021-09-03 17:06:27 +0200

    STYLE: Formatting.

--- ea_coregctmri_fsl.m
+++ ea_coregctmri_fsl.m
@@ -6,14 +6,11 @@ function varargout=ea_coregctmri_fsl(options)
 
 if ischar(options) % return name of method.
     varargout{1}='FSL FLIRT';
-    varargout{2}={'SPM8','SPM12'};
-    varargout{3}=['nan']; % suggestion for alpha-parameter.
     return
 end
 
-disp('Coregistering postop CT to preop MRI...');
+disp('Coregistering post-op CT to pre-op MRI...');
 ea_flirt([options.subj.preopAnat.(options.subj.AnchorModality).coreg],...
-          [options.subj.postopAnat.(options.subj.postopModality).preproc],...
-          [options.subj.postopAnat.(options.subj.postopModality).coreg],1);
+    [options.subj.postopAnat.(options.subj.postopModality).preproc],...
+    [options.subj.postopAnat.(options.subj.postopModality).coreg],1);
 disp('Coregistration done.');
-
