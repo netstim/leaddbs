f7c9c0b6f6b14461fcd78513b84a3c2cd6fd828c
Ningfei Li <ningfei.li@gmail.com>
2021-09-03 17:06:27 +0200

    STYLE: Formatting.

--- ea_coregctmri_ants_multiple_refine.m
+++ ea_coregctmri_ants_multiple_refine.m
@@ -6,12 +6,10 @@ function varargout=ea_coregctmri_ants_multiple_refine(options)
 
 if ischar(options) % return name of method.
     varargout{1}='Advanced Normalization Tools (ANTs), multiple runs + Subcortical Refine';
-    varargout{2}={'SPM8','SPM12'};
-    varargout{3}=['nan']; % suggestion for alpha-parameter.
     return
 end
 
-disp('Coregistering postop CT to preop MRI...');
+disp('Coregistering post-op CT to pre-op MRI...');
 ea_ants([options.subj.preopAnat.(options.subj.AnchorModality).coreg],...
     [options.subj.postopAnat.(options.subj.postopModality).preproc],...
     [options.subj.postopAnat.(options.subj.postopModality).coreg]);
