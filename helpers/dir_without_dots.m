d5458f89fcf5815d3b767d2091a251189da1ee16
nrajamani3 <nrajamani3@gatech.edu>
2021-09-06 09:53:08 +0200

    ENH:Migrate legacy dir to BIDS dir

---
+++ helpers/dir_without_dots.m
@@ -0,0 +1,25 @@
+function listing = dir_without_dots(varargin)
+
+if nargin == 0
+    name = '.';
+elseif nargin == 1
+    name = varargin{1};
+else
+    error('Too many input arguments.')
+end
+
+listing = dir(name);
+
+inds = [];
+n    = 0;
+k    = 1;
+
+while n < 2 && k <= length(listing)
+    if any(strcmp(listing(k).name, {'.', '..'}))
+        inds(end + 1) = k;
+        n = n + 1;
+    end
+    k = k + 1;
+end
+
+listing(inds) = [];
