0e6e6704f9e151ec0cb4d262fab90cd3bc2a52a1
Ningfei Li <ningfei.li@gmail.com>
2021-09-07 22:49:38 +0200

    DOC: Add doc strings.
    Also a few cosmetic changes and cleanup.

--- helpers/ea_checkcoregallmri.m
+++ helpers/ea_checkcoregallmri.m
@@ -2,6 +2,7 @@ function ea_checkcoregallmri(options,usebrainmask,usefa)
 if ~exist('usebrainmask','var')
     usebrainmask=0; % by default dont use brain mask.
 end
+
 if ~exist('usefa','var')
     usefa=1; % by default use it.
 end
