33af1ff316398267155d06060b2e7b3f24b94afb
Ningfei Li <ningfei.li@gmail.com>
2021-09-08 14:34:58 +0200

    REF: Refactor spm coregmr function.
    1. Clearer logic.
    2. Clearer verbose.
    3. Robust file renaming (movefile) after reslice.

--- ea_coregmr_spm.m
+++ ea_coregmr_spm.m
@@ -1,20 +1,31 @@
 function ea_coregmr_spm(options, fixed, moving, out, doreslice)
 % Wrapper for SPM registration of post-op MRI
 
-costfuns={'nmi','mi','ecc','ncc'};
-cfundo=[2,1];
-for costfun=1:length(cfundo)
-    if costfun==length(cfundo) % only at final stage apply refining if set
-        ea_spm_coreg(options, moving, fixed, ...
-            costfuns{cfundo(costfun)},doreslice,{''},options.prefs.mrcoreg.writeoutcoreg);
-    else % dont reslice, dont refine (not last pass).
-        ea_spm_coreg(options, moving, fixed, ...
-            costfuns{cfundo(costfun)},0,{''},options.prefs.mrcoreg.writeoutcoreg);
-    end
-    disp(['*** Coregistration pass (',costfuns{cfundo(costfun)},') completed.']);
+% Available cost functions
+% 'mi'  - Mutual Information
+% 'nmi' - Normalised Mutual Information
+% 'ecc' - Entropy Correlation Coefficient
+% 'ncc' - Normalised Cross Correlation
+costfuns = {'mi', 'nmi'};
+
+for i=1:length(costfuns)-1
+    % Estimate without reslice
+    ea_spm_coreg(options, moving, fixed, ...
+        costfuns{i}, 0, {''}, options.prefs.mrcoreg.writeoutcoreg);
+    disp(['*** Coregistration (Estimate) completed with cost function ''',costfuns{i},'''.']);
 end
 
+% Estimate and reslice if necessary
+ea_spm_coreg(options, moving, fixed, ...
+    costfuns{end}, doreslice, {''}, options.prefs.mrcoreg.writeoutcoreg);
+
+% Verbose and move file
 if doreslice
-    try movefile(strrep(moving, [filesep 'anat' filesep], [filesep 'anat' filesep 'r']), out); end
+    spmOutput = regexprep(moving, ['([^\', filesep, ']+\.nii(\.gz)?$)'], 'r$1');
+    if isfile(spmOutput) && ~strcmp(spmOutput, out)
+        movefile(spmOutput, out);
+    end
+    disp(['*** Coregistration (Estimate and Reslice) completed with cost function ''',costfuns{end},'''.']);
+else
+    disp(['*** Coregistration (Estimate) completed with cost function ''', costfuns{end},'''.']);
 end
-
