e6f475739b04d58fd313034c9ec9081bd66dc109
Ningfei Li <ningfei.li@gmail.com>
2021-09-09 23:35:00 +0200

    REF: Refactor coreg MR functions.
    1. Refine initializing coreg method popupmenu.
    2. Refine core function, using ea_coregimages.
    3. Refine coreg method in options struct and stored in json.

--- ea_coregpostopmr_brainsfit.m
+++
@@ -1,9 +0,0 @@
-function varargout = ea_coregpostopmr_brainsfit(options, fixed, moving, out)
-% Wrapper for BRAINSFit registration of post-op MRI
-
-if ischar(options) % return name of method.
-    varargout{1} = 'BRAINSFit (Johnson 2007)';
-    return
-end
-
-ea_brainsfit(fixed, moving, out, options.prefs.mrcoreg.writeoutcoreg);
