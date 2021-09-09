445904b1f8e591aa432654b05da6ce046f44e35c
Ningfei Li <ningfei.li@gmail.com>
2021-09-09 23:35:00 +0200

    REF: Refactor coreg MR functions.
    1. Refine initializing coreg method popupmenu.
    2. Refine core function, using ea_coregimages.
    3. Refine coreg method in options struct and stored in json.

--- ea_init_coregmrpopup.m
+++ ea_init_coregmrpopup.m
@@ -1,10 +1,15 @@
 function ea_init_coregmrpopup(handles, defaultmethod)
 % Initialize MR coregistration methods popupmenu.
 
-% Get functions and names
-funcs = ea_regexpdir(ea_getearoot, 'ea_coregpostopmr_.*.m', 0);
-funcs = regexp(funcs, '(ea_coregpostopmr_.*)(?=\.m)', 'match', 'once');
-names = cellfun(@(x) eval([x, '(''prompt'');']), funcs, 'Uni', 0);
+% Set methods
+names = {'ANTs (Avants 2008)'
+    'BRAINSFit (Johnson 2007)'
+    'FLIRT (Jenkinson 2001 & 2002)'
+    'FLIRT BBR (Greve and Fischl 2009)'
+    'Hybrid SPM & ANTs'
+    'Hybrid SPM & BRAINSFIT'
+    'Hybrid SPM & FLIRT'
+    'SPM (Friston 2007)'};
 
 % Set names to popupmenu
 set(handles.coregmrmethod, 'String', names);
