af5d9f6489c079e82c3d18ff18543a8cbfbc441a
Ningfei Li <ningfei.li@gmail.com>
2021-09-09 23:40:45 +0200

    REF: Refine coregimages function.
    1. Cleanup legacy doc.
    2. Fix unnecessary input var check.
    3. More robust method detection.
    4. Add code to conform space in case needed.

--- ea_coregimages.m
+++ ea_coregimages.m
@@ -3,9 +3,7 @@ function affinefile = ea_coregimages(options,moving,fixed,ofile,otherfiles,write
 %
 % 1. Moving image will keep untouched unless the output points to the same
 %    image.
-% 2. Use 'ea_backuprestore' before this function in case you want to
-%    backup/restore the moving image to/from the 'raw_' prefixed image.
-% 3. 'otherfiles' will always be overwritten. If you don't want them to be
+% 2. 'otherfiles' will always be overwritten. If you don't want them to be
 %    overwritten, use 'ea_apply_coregistration' afterwards to apply the
 %    returned forward transform ('affinefile{1}') to 'otherfiles' rather
 %    than supply 'otherfiles' parameter here.
@@ -23,9 +21,7 @@ if ~exist('writeoutmat','var') || isempty(writeoutmat)
     writeoutmat = 0;
 end
 
-if ~exist('affinefile','var') || isempty(affinefile)
-    affinefile = {};
-end
+affinefile = {};
 
 if ~exist('msks','var') || isempty(msks)
     msks={};
@@ -34,11 +30,51 @@ end
 % Only effective for SPM, 4th degree BSpline by default.
 % ANTs and BRAINSFit use Linear interpolation, FSL use Sinc interpolation.
 if ~exist('interp','var') || isempty(interp)
-    interp=4;
+    interp = 4;
 end
 
 switch lower(options.coregmr.method)
-    case 'spm' % SPM
+    case lower({'ANTs (Avants 2008)', 'ANTs'})
+        affinefile = ea_ants(fixed,...
+            moving,...
+            ofile,writeoutmat,otherfiles,msks);
+    case lower({'BRAINSFit (Johnson 2007)', 'BRAINSFit'})
+        affinefile = ea_brainsfit(fixed,...
+            moving,...
+            ofile,writeoutmat,otherfiles);
+    case lower({'FLIRT (Jenkinson 2001 & 2002)', 'FLIRT'})
+        affinefile = ea_flirt(fixed,...
+            moving,...
+            ofile,writeoutmat,otherfiles);
+    case lower({'FLIRT BBR (Greve and Fischl 2009)', 'FLIRT BBR', 'FLIRTBBR'})
+        affinefile = ea_flirtbbr(fixed,...
+            moving,...
+            ofile,writeoutmat,otherfiles);
+    case lower({'Hybrid SPM & ANTs', 'HybridSPMANTs'})
+        % Copy moving image to out image first, since SPM will change
+        % the header of the moving image.
+        copyfile(moving, ofile);
+        ea_spm_coreg(options,ofile,fixed,'nmi',0,otherfiles,writeoutmat)
+        affinefile = ea_ants(fixed,...
+            ofile,...
+            ofile,writeoutmat,otherfiles);
+    case lower({'Hybrid SPM & BRAINSFIT', 'HybridSPMBRAINSFIT'})
+        % Copy moving image to out image first, since SPM will change
+        % the header of the moving image.
+        copyfile(moving, ofile);
+        ea_spm_coreg(options,ofile,fixed,'nmi',0,otherfiles,writeoutmat)
+        affinefile = ea_brainsfit(fixed,...
+            ofile,...
+            ofile,writeoutmat,otherfiles);
+    case lower({'Hybrid SPM & FLIRT', 'HybridSPMFLIRT'})
+        % Copy moving image to out image first, since SPM will change
+        % the header of the moving image.
+        copyfile(moving, ofile);
+        ea_spm_coreg(options,ofile,fixed,'nmi',0,otherfiles,writeoutmat)
+        affinefile = ea_flirt(fixed,...
+            ofile,...
+            ofile,writeoutmat,otherfiles);
+    case lower({'SPM (Friston 2007)', 'SPM'})
         affinefile = ea_spm_coreg(options,moving,fixed,'nmi',1,otherfiles,writeoutmat,interp);
 
         [fpth, fname, ext] = ea_niifileparts(moving);
@@ -52,47 +88,14 @@ switch lower(options.coregmr.method)
             spmoutput = [fileparts(fpth), filesep, 'r', fname, ext];
             movefile(spmoutput, [fpth, ext]);
         end
-
-    case 'fsl flirt' % FSL FLIRT
-        affinefile = ea_flirt(fixed,...
-            moving,...
-            ofile,writeoutmat,otherfiles);
-    case 'fsl bbr' % FSL BBR
-        affinefile = ea_flirtbbr(fixed,...
-            moving,...
-            ofile,writeoutmat,otherfiles);
-    case 'ants' % ANTs
-        affinefile = ea_ants(fixed,...
-            moving,...
-            ofile,writeoutmat,otherfiles,msks);
-    case 'brainsfit' % BRAINSFit
-        affinefile = ea_brainsfit(fixed,...
-            moving,...
-            ofile,writeoutmat,otherfiles);
-    case 'hybrid spm & ants' % Hybrid SPM -> ANTs
-        % Copy moving image to out image first, since SPM will change
-        % the header of the moving image.
-        copyfile(moving, ofile);
-        ea_spm_coreg(options,ofile,fixed,'nmi',0,otherfiles,writeoutmat)
-        affinefile = ea_ants(fixed,...
-            ofile,...
-            ofile,writeoutmat,otherfiles);
-    case 'hybrid spm & fsl' % Hybrid SPM -> FSL
-        % Copy moving image to out image first, since SPM will change
-        % the header of the moving image.
-        copyfile(moving, ofile);
-        ea_spm_coreg(options,ofile,fixed,'nmi',0,otherfiles,writeoutmat)
-        affinefile = ea_flirt(fixed,...
-            ofile,...
-            ofile,writeoutmat,otherfiles);
-    case 'hybrid spm & brainsfit' % Hybrid SPM -> Brainsfit
-        % Copy moving image to out image first, since SPM will change
-        % the header of the moving image.
-        copyfile(moving, ofile);
-        ea_spm_coreg(options,ofile,fixed,'nmi',0,otherfiles,writeoutmat)
-        affinefile = ea_brainsfit(fixed,...
-            ofile,...
-            ofile,writeoutmat,otherfiles);
+    otherwise
+        warning('Coregistrion method not recognized...');
+        return;
 end
 
-% ea_conformspaceto(fixed, ofile); % fix qform/sform issues.
+% Reslice images if needed (fix qform/sform issues)
+V1 = ea_open_vol(fixed);
+V2 = ea_open_vol(ofile);
+if ~isequal(V1.mat, V2.mat)
+    ea_conformspaceto(fixed, ofile, 1);
+end
