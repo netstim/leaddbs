dfe7548dbeb9d07f0f203b87608b6df304afd323
Ningfei Li <ningfei.li@gmail.com>
2021-09-09 14:28:34 +0200

    REF: Refactor coreg methods names.

--- ea_coregpostopmr_flirt.m
+++ ea_coregpostopmr_flirt.m
@@ -1,4 +1,9 @@
-function ea_coregpostopmr_flirt(options, fixed, moving, out)
+function varargout = ea_coregpostopmr_flirt(options, fixed, moving, out)
 % Wrapper for FSL FLIRT registration of post-op MRI
+
+if ischar(options) % return name of method.
+    varargout{1} = 'FLIRT (Jenkinson 2001 & 2002)';
+    return
+end
 
 ea_flirt(fixed, moving, out, options.prefs.mrcoreg.writeoutcoreg);
