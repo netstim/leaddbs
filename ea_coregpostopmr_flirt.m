ac13cbd57c60f24a037593403ae93109ccd156e8
Ningfei Li <ningfei.li@gmail.com>
2021-09-09 23:35:00 +0200

    REF: Refactor coreg MR functions.
    1. Refine initializing coreg method popupmenu.
    2. Refine core function, using ea_coregimages.
    3. Refine coreg method in options struct and stored in json.

--- ea_coregpostopmr_flirt.m
+++
@@ -1,9 +0,0 @@
-function varargout = ea_coregpostopmr_flirt(options, fixed, moving, out)
-% Wrapper for FSL FLIRT registration of post-op MRI
-
-if ischar(options) % return name of method.
-    varargout{1} = 'FLIRT (Jenkinson 2001 & 2002)';
-    return
-end
-
-ea_flirt(fixed, moving, out, options.prefs.mrcoreg.writeoutcoreg);
