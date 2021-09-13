06c86c9ef8fca11d1f08c0d19f0c3d3edea9f89f
Ningfei Li <ningfei.li@gmail.com>
2021-09-13 07:47:42 +0200

    ENH: Add function to check if file name is BIDS-like.

--- helpers/rmBIDSEntity.m
+++ helpers/rmBIDSEntity.m
@@ -1,6 +1,10 @@
 function filePath = rmBIDSEntity(filePath, entity)
 % Remove BIDS entity from file path
 
+if ~isBIDSFileName(filePath)
+    error('Seems not a BIDS-like file name.')
+end
+
 if ischar(entity)
     entity = {entity};
 end
