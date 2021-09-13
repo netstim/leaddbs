a5decd3b66f2fc1c3128d86d081f2d3bf4a17a22
Ningfei Li <ningfei.li@gmail.com>
2021-09-13 16:15:13 +0200

    REF: Add options to turn off diary.

--- ea_coregpostopmr.m
+++ ea_coregpostopmr.m
@@ -24,8 +24,10 @@ if isempty(moving)
 end
 
 % Setup log
-ea_mkdir(fileparts(options.subj.coreg.log.logBaseName));
-diary([options.subj.coreg.log.logBaseName, 'MR', datestr(now, 'yyyymmddTHHMMss'), '.log']);
+if options.prefs.diary
+    ea_mkdir(fileparts(options.subj.coreg.log.logBaseName));
+    diary([options.subj.coreg.log.logBaseName, 'MR', datestr(now, 'yyyymmddTHHMMss'), '.log']);
+end
 
 % Do coregistration
 for i=1:length(moving)
@@ -34,4 +36,6 @@ end
 
 ea_dumpmethod(options, 'coreg');
 
-diary off;
+if options.prefs.diary
+    diary off;
+end
