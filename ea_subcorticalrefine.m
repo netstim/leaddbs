f088f0afdee24714e70669c9882c48f1270cdbfb
Ningfei Li <ningfei.li@gmail.com>
2021-09-17 13:56:10 +0200

    REF: Refactor checkreg function.
    Rename function.
    Fix field check bug in reglock check.

--- ea_subcorticalrefine.m
+++ ea_subcorticalrefine.m
@@ -266,7 +266,7 @@ function back_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 options=getappdata(handles.scrf,'options');
-ea_checkcoreg(options);
+ea_checkreg(options);
 closescrf(handles);
 
 
