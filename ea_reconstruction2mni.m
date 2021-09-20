d21b1566a719dcf423d1c512376cd8d291f2037e
Simon Oxenford <oxenfordsimon@gmail.com>
2021-09-20 10:57:53 +0200

    REF: initial refactor for reconstruction. Working until ea_map_coords

--- ea_reconstruction2mni.m
+++ ea_reconstruction2mni.m
@@ -3,46 +3,44 @@ function ea_reconstruction2mni(options)
 directory=[options.root,options.patientname,filesep];
 load([directory,filesep,'ea_reconstruction.mat'],'reco');
 
-%[~,template]=ea_whichnormmethod(directory);
-options=ea_assignpretra(options);
-nii=ea_load_nii([directory,options.prefs.prenii_unnormalized]);
+nii = ea_load_nii(options.subj.coreg.anat.preop.(options.subj.AnchorModality));
 
 if ~isfield(options,'elspec')
-    options.elmodel=reco.props.elmodel;
-    options=ea_resolve_elspec(options);
+    options.elmodel = reco.props.elmodel;
+    options = ea_resolve_elspec(options);
 end
 
-if exist([options.root,options.patientname,filesep,'scrf',filesep,'scrf_converted.mat'],'file')
+if isfile(options.subj.brainshift.transform.converted)
     usenative='scrf';
 else
     usenative='native';
 end
 
 % apply native to scrf matrix if available
-if exist([options.root,options.patientname,filesep,'scrf',filesep,'scrf_converted.mat'],'file')
-    d=load([options.root,options.patientname,filesep,'scrf',filesep,'scrf_converted.mat']);
-    reco.scrf=ea_applyscrfmat(d.mat,reco.native,options.sides);
-elseif exist([options.root,options.patientname,filesep,'scrf',filesep,'scrf.mat'],'file') % legacy
-    mat=ea_getscrfmat(options);
-    save([directory,'scrf',filesep,'scrf_converted.mat'],'mat');
-    reco.scrf=ea_applyscrfmat(mat,reco.native,options.sides);
+if isfile(options.subj.brainshift.transform.converted)
+    d = load(options.subj.brainshift.transform.converted);
+    reco.scrf = ea_applyscrfmat(d.mat, reco.native, options.sides);
+elseif isfile(options.subj.brainshift.transform.scrf)
+    mat = ea_getscrfmat(options);
+    save(options.subj.brainshift.transform.converted, 'mat');
+    reco.scrf = ea_applyscrfmat(mat,reco.native,options.sides);
 else
     if isfield(reco,'scrf')
-        reco=rmfield(reco,'scrf'); % delete subcortical transform if user apparently deleted the transform file.
+        reco = rmfield(reco,'scrf'); % delete subcortical transform if user apparently deleted the transform file.
     end
 end
 
-towarp=cell(0);
+towarp = cell(0);
 for side=options.sides
-    towarp{end+1}=reco.(usenative).coords_mm{side};
-    towarp{end+1}=reco.(usenative).markers(side).head;
-    towarp{end+1}=reco.(usenative).markers(side).tail;
-    towarp{end+1}=reco.(usenative).trajectory{side};
+    towarp{end+1} = reco.(usenative).coords_mm{side};
+    towarp{end+1} = reco.(usenative).markers(side).head;
+    towarp{end+1} = reco.(usenative).markers(side).tail;
+    towarp{end+1} = reco.(usenative).trajectory{side};
 end
-towarp=cell2mat(towarp');
-warpedcoord=ea_warpcoord(towarp,nii,options);
+towarp = cell2mat(towarp');
+warpedcoord = ea_warpcoord(towarp,nii,options);
 
-cnt=1;
+cnt = 1;
 for side=options.sides
     offset=size(reco.(usenative).coords_mm{side},1);
     reco.mni.coords_mm{side}=warpedcoord(cnt:cnt+offset-1,:); cnt=cnt+offset;
@@ -73,9 +71,9 @@ save([directory,filesep,'ea_reconstruction.mat'],'reco');
 
 
 function c=ea_warpcoord(c,nii,options)
-c=[c,ones(size(c,1),1)]';
+c = [c,ones(size(c,1),1)]';
 % to template voxel space:
-c=nii(1).mat\c;
+c = nii(1).mat\c;
 try
     whichnormmethod=ea_whichnormmethod([options.root,options.patientname,filesep]);
     if ~ismember(whichnormmethod,ea_getantsnormfuns)
@@ -87,7 +85,7 @@ try
 end
 
 c=ea_map_coords(c(1:3,:), ...
-    [options.root,options.patientname,filesep,options.prefs.prenii_unnormalized], ...
+    options.subj.coreg.anat.preop.(options.subj.AnchorModality), ...
     [options.root,options.patientname,filesep,'y_ea_inv_normparams.nii'], ...
     '');
 c=c';
