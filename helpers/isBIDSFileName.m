7c5c856b2edff524a9fc0a8da749aa5aee7c6854
Ningfei Li <ningfei.li@gmail.com>
2021-09-21 15:55:51 +0200

    REF: Refine checking BIDS path.

--- helpers/isBIDSFileName.m
+++ helpers/isBIDSFileName.m
@@ -1,9 +1,17 @@
 function flag = isBIDSFileName(filePath)
 % Check if file name is BIDS-like
+%
+% regexp will match:
+%     sub-XX_key1-value1_key2-value2.[ext]
+%     sub-XX_key1-value1_key2-value2_[modality].nii
+%     sub-XX_key1-value1_key2-value2_[modality].nii.gz
 
-[~, fileName] = fileparts(filePath);
+[~, fileName, fileExt] = fileparts(filePath);
+fullFileName = [fileName, fileExt];
 
-if isempty(regexp(fileName, '^sub-.*_[a-zA-Z0-9]+(\.nii)?$', 'once'))
+pattern = '^sub-[^\W_]+(_[^\W_]+-[^\W_]+){1,}(\.[^\W_]+|_[^\W_]+\.nii(\.gz)?)$';
+
+if isempty(regexp(fullFileName, pattern, 'once'))
     flag = 0;
 else
     flag = 1;
