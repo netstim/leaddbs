529bb7c46648baafd227b06a515b58cdaef3289a
Ningfei Li <ningfei.li@gmail.com>
2021-09-21 16:27:27 +0200

    ENH: BIDSPath parser also works with non-NIfTI files now.

--- helpers/parseBIDSFilePath.m
+++ helpers/parseBIDSFilePath.m
@@ -3,18 +3,37 @@ function parsedStruct = parseBIDSFilePath(filePath)
 
 if ~isBIDSFileName(filePath)
     error('Seems not a BIDS-like file name.')
+else
+    filePath = GetFullPath(filePath);
 end
 
 % Split file path into stripped path, file name and extension
-[strippedPath, fileName, ext] = ea_niifileparts(GetFullPath(filePath));
+strippedPath = regexp(filePath, ['.+(?=\', filesep, '[^\', filesep, ']+)'], 'match', 'once');
+fullName = strrep(filePath, [strippedPath, filesep], '');
+fileName = regexp(fullName, '[^.]+', 'match', 'once');
+fileExt = strrep(fullName, fileName, '');
+
 parsedStruct.dir = fileparts(strippedPath);
 
 % Parse file name
 entities = strsplit(fileName, '_');
-for i=1:length(entities)-1
-    pair = regexp(entities{i}, '-', 'split', 'once');
-    parsedStruct.(pair{1}) = pair{2};
-end
-parsedStruct.suffix = entities{end}; % Last one should be modality
+if ~contains(entities{end}, '-')
+    % filePath has the following patterns:
+    % sub-XX_key1-value1_key2-value2_[modality].nii
+    % sub-XX_key1-value1_key2-value2_[modality].nii.gz
+    for i=1:length(entities)-1
+        pair = regexp(entities{i}, '-', 'split', 'once');
+        parsedStruct.(pair{1}) = pair{2};
+    end
+    parsedStruct.suffix = entities{end}; % Last one should be modality
+else
+    % filePath has the following patterns:
+	% sub-XX_key1-value1_key2-value2.[ext]
+    for i=1:length(entities)
+        pair = regexp(entities{i}, '-', 'split', 'once');
+        parsedStruct.(pair{1}) = pair{2};
+    end
+    parsedStruct.suffix = '';
+end 
 
-parsedStruct.ext = ext;
+parsedStruct.ext = fileExt;
