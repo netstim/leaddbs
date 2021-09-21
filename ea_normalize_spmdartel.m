e662079d8268b5f9f94ec6775f320720fcee1e25
Ningfei Li <ningfei.li@gmail.com>
2021-09-21 15:55:21 +0200

    REF: Refine regexp.
    Use [^\W_] instead of [a-zA-Z0-9]

--- ea_normalize_spmdartel.m
+++ ea_normalize_spmdartel.m
@@ -152,7 +152,7 @@ if ~isempty(lcit)
     cits=[cits; {lcit}];
 end
 
-modality = regexp(preopImages, '(?<=_)[a-zA-Z0-9]+(?=\.nii(\.gz)?$)', 'match', 'once');
+modality = regexp(preopImages, '(?<=_)[^\W_]+(?=\.nii(\.gz)?$)', 'match', 'once');
 
 ea_methods(options,['Pre- (and post-) operative acquisitions were spatially normalized into ',ea_getspace,' space ',scit,' based on preoperative acquisition(s) (',strjoin(modality, ', '),') using a'...
     ' fast diffeomorphic image registration algorithm (DARTEL) as implemented in SPM12 (Ashburner 2007; www.fil.ion.ucl.ac.uk/spm/software/).',...
