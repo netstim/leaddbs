aa0149700fcb5a2f453f850b3ef2763043e043fc
Ningfei Li <ningfei.li@gmail.com>
2021-09-21 15:55:21 +0200

    REF: Refine regexp.
    Use [^\W_] instead of [a-zA-Z0-9]

--- helpers/setBIDSEntity.m
+++ helpers/setBIDSEntity.m
@@ -16,12 +16,12 @@ for i=1:length(entities)
     entity = entities{i};
     value = values{i};
     if strcmp(entity, 'suffix') % Set suffix
-        filePath = regexprep(filePath, '_[a-zA-Z0-9]+(\.[a-zA-Z0-9\.]+$)', ['_', value, '$1']);
+        filePath = regexprep(filePath, '_[^\W_]+(\.[a-zA-Z0-9\.]+$)', ['_', value, '$1']);
     elseif strcmp(entity, 'ext') % Set extension
-        filePath = regexprep(filePath, '_([a-zA-Z0-9]+\.)[a-zA-Z0-9\.]+$', ['_$1', value]);
+        filePath = regexprep(filePath, '_([^\W_]+\.)[a-zA-Z0-9\.]+$', ['_$1', value]);
     elseif contains(filePath, [entity, '-']) % Entity already exist
-        filePath = regexprep(filePath, [entity, '-[a-zA-Z0-9]+'], [entity, '-', value]);
+        filePath = regexprep(filePath, [entity, '-[^\W_]+'], [entity, '-', value]);
     else % Append new entity
-        filePath = regexprep(filePath, '(_[a-zA-Z0-9]+\.[a-zA-Z0-9\.]+$)', ['_', entity, '-', value, '$1']);
+        filePath = regexprep(filePath, '(_[^\W_]+\.[a-zA-Z0-9\.]+$)', ['_', entity, '-', value, '$1']);
     end
 end
