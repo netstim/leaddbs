d6aa3bce485bbe5d8aa786c6fe2c0c8fee4749e5
Ningfei Li <ningfei.li@gmail.com>
2021-09-21 22:31:49 +0200

    REF: Refine rm and set BIDS file name entity.

--- helpers/setBIDSEntity.m
+++ helpers/setBIDSEntity.m
@@ -12,16 +12,29 @@ if length(entities) ~= length(values)
     error('Length of entities doesn''t match length of values!')
 end
 
+% Parse file path for further operation
+parsedStruct = parseBIDSFilePath(filePath);
+
 for i=1:length(entities)
     entity = entities{i};
     value = values{i};
     if strcmp(entity, 'suffix') % Set suffix
-        filePath = regexprep(filePath, '_[^\W_]+((\.[^\W_]+){1,})$', ['_', value, '$1']);
+        parsedStruct.suffix = value;
     elseif strcmp(entity, 'ext') % Set extension
-        filePath = regexprep(filePath, '(_[^\W_]+)(\.[^\W_]+){1,}$', ['$1', value]);
-    elseif contains(filePath, [entity, '-']) % Entity already exist
-        filePath = regexprep(filePath, [entity, '-[^\W_]+'], [entity, '-', value]);
+        parsedStruct.ext = value;
+    elseif isfield(parsedStruct, entity) % Entity already exist
+        parsedStruct.(entity) = value;
     else % Append new entity
-        filePath = regexprep(filePath, '(_[^\W_]+(\.[^\W_]+){1,})$', ['_', entity, '-', value, '$1']);
+        % Insert key-value pair before suffix
+        keys = fieldnames(parsedStruct);
+        keys = [keys(1:end-2); entity; keys(end-1:end)];
+        values = struct2cell(parsedStruct);
+        values = [values(1:end-2); value; values(end-1:end)];
+
+        % Construct new parsed struct
+        parsedStruct = cell2struct(values, keys, 1);
     end
 end
+
+% Join BIDS file path struct into file path
+filePath = joinBIDSFilePath(parsedStruct);
