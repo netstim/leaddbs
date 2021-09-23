7dfac46789cb0e78976017403a0b11396a56a496
Ningfei Li <ningfei.li@gmail.com>
2021-09-23 17:18:16 +0200

    REF: Refine 2D setting.

--- ea_spec2dwrite.m
+++ ea_spec2dwrite.m
@@ -62,12 +62,14 @@ guidata(hObject, handles);
 % uiwait(handles.ea_spec2dwrite);
 
 % add backdrops
+
 try
     options = varargin{1};
 catch
     options.prefs = ea_prefs('');
 end
-backdrops=ea_assignbackdrop('list',options);
+
+backdrops = ea_assignbackdrop('list',options);
 
 set(handles.tdbackdrop,'String',backdrops);
 if get(handles.tdbackdrop,'Value')>length(backdrops)
