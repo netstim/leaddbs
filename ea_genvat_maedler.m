7a3eabff501d6aee16e80c97747dc557319bdc05
Ningfei Li <ningfei.li@gmail.com>
2021-09-23 23:13:02 +0200

    REF: Adapt VTA calc functions for BIDS.
    OSS-DBS yet to be done.

--- ea_genvat_maedler.m
+++ ea_genvat_maedler.m
@@ -45,8 +45,8 @@ radius=repmat(1.5,options.elspec.numel,1); % some default setting.
 if ~isfield(S, 'sources')
     S.sources=1:4;
 end
-for source=S.sources
 
+for source=S.sources
     stimsource=S.([sidec,'s',num2str(source)]);
 
     for cnt=1:length(cnts)
@@ -89,11 +89,11 @@ XYZmm=[gvmm{1}(XYZv(1,:));...
     gvmm{3}(XYZv(3,:));...
     ones(1,length(XYZv))];
 mat=mldivide(XYZv',XYZmm')';
+
 for source=S.sources
     in=ea_intriangulation(VAT{source},K{source},XYZmm(1:3,:)');
     voxspace(sub2ind(size(voxspace),XYZv(1,in),XYZv(2,in),XYZv(3,in)))=1;
 end
-
 
 % write nifti of VAT
 Vvat.mat=mat;
@@ -102,30 +102,23 @@ Vvat.dim=size(voxspace);
 Vvat.dt=[4,0];
 Vvat.n=[1 1];
 Vvat.descrip='lead dbs - vat';
-if ~exist([options.root,options.patientname,filesep,'stimulations',filesep,ea_nt(options)],'file')
 
-    mkdir([options.root,options.patientname,filesep,'stimulations',filesep,ea_nt(options)]);
-end
-
-mkdir([options.root,options.patientname,filesep,'stimulations',filesep,ea_nt(options),stimname]);
-%S(side).volume=sum(volume);
-
+stimDir = fullfile(options.subj.stimDir, ea_nt(options), stimname);
+ea_mkdir(stimDir);
+filePrefix = ['sub-', options.subj.subjId, '_desc-'];
 
 if side == 1
-    Vvat.fname=[options.root,options.patientname,filesep,'stimulations',filesep,ea_nt(options),stimname,filesep,'vat_right.nii'];
-    stimfile=[options.root,options.patientname,filesep,'stimulations',filesep,ea_nt(options),stimname,filesep,'stimparameters_right.mat'];
+    Vvat.fname = [stimDir, filesep, filePrefix, 'stimvol_hemi-right.nii'];
 elseif side == 2
-    Vvat.fname=[options.root,options.patientname,filesep,'stimulations',filesep,ea_nt(options),stimname,filesep,'vat_left.nii'];
-    stimfile=[options.root,options.patientname,filesep,'stimulations',filesep,ea_nt(options),stimname,filesep,'stimparameters_left.mat'];
+    Vvat.fname = [stimDir, filesep, filePrefix, 'stimvol_hemi-left.nii'];
 end
+
 ea_savestimulation(S,options);
 spm_write_vol(Vvat,voxspace);
-
 
 varargout{1}=VAT;
 varargout{2}=volume;
 varargout{3}=radius;
-
 
 
 function r=maedler12_eq3(U,Im)
@@ -134,18 +127,12 @@ function r=maedler12_eq3(U,Im)
 % 500?1500 Ohm (Butson 2006).
 r=0; %
 if U %(U>0)
-
     k1=-1.0473;
     k3=0.2786;
     k4=0.0009856;
 
-
-    r=-(k4*Im-sqrt(k4^2*Im^2  +   2*k1*k4*Im    +   k1^2 +   4*k3*U)   +   k1)...
-        /...
-        (2*k3);
+    r=-(k4*Im-sqrt(k4^2*Im^2  +   2*k1*k4*Im    +   k1^2 +   4*k3*U)   +   k1) / (2*k3);
 end
-
-
 
 
 function [x,y,z,avgr] = psphere(n,rad)
@@ -297,7 +284,3 @@ avgr = min(tmp(indices));
 
 %Turn back on the default warning state.
 warning backtrace
-
-
-
-
