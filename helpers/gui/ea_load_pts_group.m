12088438ed6771534b9e609cf196e28df6f5e511
Ningfei Li <ningfei.li@gmail.com>
2021-09-24 22:35:59 +0200

    REF: GetFullPath when loading patients.

--- helpers/gui/ea_load_pts_group.m
+++ helpers/gui/ea_load_pts_group.m
@@ -1,6 +1,5 @@
 function ea_load_pts_group(handles,uipatdir)
 
-
 ea_busyaction('on',handles.leadfigure,'group');
 
 % check if Lead group is empty at present:
@@ -18,12 +17,12 @@ end
 
 if ~projectexists
     a=questdlg('Please choose a (potentially blank) folder in which you would like to store your new Lead-Group project...','New Lead-Group project','Okay','Cancel','Cancel');
-    if strcmp(lower(a),'cancel')
+    if strcmpi(a,'cancel')
         return
     end
-    %% init M and add folder:
-    
-    nudir=[uigetdir];
+
+    % init M and add folder:
+    nudir = uigetdir;
     
     if ~nudir % user pressed cancel
         return
@@ -31,47 +30,43 @@ if ~projectexists
     
     nudir=[nudir,filesep];
     M=ea_initializeM;
-    
-    
+
     set(handles.groupdir_choosebox,'String',nudir);
-    
-    
-    if exist([nudir,'LEAD_groupanalysis.mat'],'file');
+
+    if exist([nudir,'LEAD_groupanalysis.mat'],'file')
         answ=questdlg('In the folder you selected, an old Lead-Group project was already found. Continuing will OVERWRITE the old project. Please press cancel to abort.','Existing Lead-Group project','Overwrite','Cancel','Cancel');
         switch lower(answ)
             case 'cancel'
                 return
         end
     end
+
     % store blank M in folder.
     save([nudir,'LEAD_groupanalysis.mat'],'M','-v7.3');
-    
-    
-    
+
     M.ui.groupdir=nudir;
     setappdata(handles.leadfigure,'M',M);
     try
         setappdata(handles.leadfigure,'S',M.S);
         setappdata(handles.leadfigure,'vatmodel',M.S(1).model);
     end
-    
-    
+
     ea_refresh_lg(handles);
 end
 
-
-%% add patients:
-M=getappdata(handles.leadfigure,'M');
-folders=uipatdir; % folders to add to list.
+% add patients:
+M = getappdata(handles.leadfigure,'M');
+folders = GetFullPath(uipatdir); % folders to add to list.
 M.patient.list=[M.patient.list;folders'];
 M.patient.group=[M.patient.group;ones(length(folders),1)];
 
 setappdata(handles.leadfigure,'M',M);
 ea_refresh_lg(handles);
+
 % save M
 M=getappdata(handles.leadfigure,'M');
 save([get(handles.groupdir_choosebox,'String'),'LEAD_groupanalysis.mat'],'M','-v7.3');
 
-    ea_busyaction('off',handles.leadfigure,'group');
+ea_busyaction('off',handles.leadfigure,'group');
 
     
