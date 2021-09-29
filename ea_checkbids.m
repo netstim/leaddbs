c7b29dadff939aa34be4807d5a1844e529c60369
nrajamani3 <nrajamani3@gatech.edu>
2021-09-29 22:34:08 +0200

    STYLE: Better UX for migrate - lead

--- ea_checkbids.m
+++ ea_checkbids.m
@@ -36,9 +36,18 @@ if length(uipatdir) == 1 % Dragged single folder
         end
     end
     if ~isSubjFolder && ~isBIDSRoot
-        warndlg('Neither BIDS dataset nor patient folder found, opening in lead migrate!');
-        lead_migrate(uipatdir,options,handles)
-     
+        choosedialog(uipatdir,options,handles);
+        %answer = questdlg('This dataset appears to have an older lead-dbs data structure. Would you like to Migrate it to BIDS? Please note that support for older version will not provided following this version...','Migrate to BIDS','Yes','No','Read More','Read More');
+        %switch answer
+        %    case 'Yes'
+        %        close(gcf);
+        %        lead_migrate(uipatdir,options,handles);
+        %    case 'No'
+        %        return
+        %    case 'Read More'
+        %        disp(['Thank you for your interest in Lead-DBS. To better provide support to lead-DBS users and promote data sharing\n' ...
+        %               'we have reorganized the structure in which data is stored. This means changes to your input and output data']);
+        %end
     elseif isBIDSRoot % Is BIDS root folder but it is empty
         rawData = ea_regexpdir([uipatdir{1}, filesep, 'rawdata'], 'sub-', 0);
         rawData = regexprep(rawData, ['\', filesep, '$'], '');
@@ -67,5 +76,40 @@ else % Dragged multiple patient folders
     lead_migrate(uipatdir,options,handles)
     
 end
+
+function choosedialog(uipatdir,options,handles)
+
+     d = dialog('Position',[500 500 400 150],'Name','Migrate to BIDS');
+     %,'Color',[1 1 1]
+    
+     txt = uicontrol('Parent',d,'Style','text','Position',[45 80 320 50],'String','This dataset appears to have an older lead-dbs data structure. Would you like to Migrate it to BIDS? Please note that support for older version will not provided following this version!');
+        
+     btn1 = uicontrol('Parent',d,...
+            'Position',[50 20 70 25],...
+            'String','Yes',...
+            'Callback',{@callMigrate,d,uipatdir,options,handles});       
+     btn2 = uicontrol('Parent',d,...
+         'Position',[170 20 70 25],...
+         'String','No',...
+         'Callback','close(gcf)');     
+     btn3 = uicontrol('Parent',d,...
+         'Position', [290 20 70 25],...
+         'String','Read More',...
+         'Callback',{@callReadMore,d,txt});
+  
+        function callMigrate(btn1,event,d,uipatdir,options,handles)
+           close(d);
+           lead_migrate(uipatdir,options,handles);
+        function callReadMore(btn3,event,d,txt)
+            d.Position = [500 500 400 300];
+            txt.Position = [45 230 320 50];
+            version_message = ['Thank you for your interest in Lead-DBS! After version 2.5, we have re-organized the way Lead-DBS acesses and stores data.' ,...
+                               'This implies changes to the organization of your input and output data. The main objective to set standards for data organization was to promote' ,...
+                               'data sharing and open science initiatives. For more information and details on specific changes, please refer to our manual [insert url].' ,...
+                               'Lead-migrate is a tool developed to automatically assist you in moving your dataset from the classic lead-dbs to the bidsy-fied version.',...
+                               'If you wish to use it, please click on ''Yes''. You can exit this message by clicking on ''No'', but you will not be able to use Lead-DBS.'];
+            txt2 = uicontrol('Parent',d,'Style','text','Position',[45 50 320 150],'String',version_message);
+            
+
 
 
