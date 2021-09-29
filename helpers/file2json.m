29593ed67e7699db9578cc757cda190f9b1fc2db
nrajamani3 <nrajamani3@gatech.edu>
2021-09-29 15:45:11 +0200

    REF: normmethod and coreg json files

--- helpers/file2json.m
+++ helpers/file2json.m
@@ -4,7 +4,6 @@ function file2json(fname_in,fname_out)
     if endsWith(fname_in,'.mat')
         %special case for legacy dataset  
         %read the input mat
-        disp(fname_in);        
         %dealing with coregistration
         if contains(fname_in,'coreg_approved.mat')
             [filepath,filename,ext] = fileparts(fname_in);
@@ -16,87 +15,85 @@ function file2json(fname_in,fname_out)
                 json_mat.approval.(modality) = input_mat.(coreg_fieldnames{i});
                 if exist(fullfile(filepath,'ea_coregctmethod_applied.mat'),'file')
                     temp_mat = load(fullfile(filepath,'ea_coregctmethod_applied.mat'));
-                    if isfield(temp_mat,'coregct_method_applied')
-                        if contains(temp_mat.coregct_method_applied{1},'_ants')
-                            method_used = ea_normalize_ants('promt');
-                        elseif contains(temp_mat.coregct_method_applied{1},'BRAINSFit')
-                            method_used = 'BRAINSFit (Johnson 2007)';
-                        elseif contains(temp_mat.coregct_method_applied{1},'FLIRT')
-                            method_used = 'FLIRT (Jenkinson 2001 & 2002)';
-                        elseif contains(temp_mat.coregct_nethod_applied{1},'_spm')
-                            method_used = legacyMethods.containers('_spm');
-                            json_mat.method.(modality) = method_used;
-                        else
-                            json_mat.method.(modality) = '';
-                        end
-                    elseif exist(fullfile(filepath,'ea_coregmrmethod_applied.mat'),'file')
-                        temp_mat = load(fullfile(filepath,'ea_coregmrmethod_applied.mat'));
-                        if isfield(temp_mat,'coregmr_method_applied')
-                            if contains(temp_mat.coregmr_method_applied{1},'_ants')
-                                method_used = ea_normalize_ants('promt');
-                            elseif contains(temp_mat.coregmr_method_applied{1},'BRAINSFit')
-                                method_used = 'BRAINSFit (Johnson 2007)';
-                            elseif contains(temp_mat.coregmr_method_applied{1},'FLIRT')
-                                method_used = 'FLIRT (Jenkinson 2001 & 2002)';
-                            elseif contains(temp_mat.coregmr_method_applied{1},'BBR')
-                                method_used = 'FLIRT BBR (Greve and Fischl 2009)';
-                            elseif contains(temp_mat.coregmr_method_applied{1},'HybridSPMANTs')
-                                method_used = 'Hybrid SPM & ANTs';
-                            elseif contains(temp_mat.coregmr_method_applied{1},'HybridSPMBRAINSFIT')
-                                method_used = 'Hybrid SPM & BRAINSFIT';
-                            elseif contains(temp_mat.coregmr_method_applied{1},'_spm')
-                                method_used = 'SPM (Friston 2007)';
-                            elseif contains(temp_mat.coregmr_method_applied{1},'HybridSPMFLIRT')
-                                method_used = 'Hybrid SPM & FLIRT';
-                            end
-                            json_mat.method.(modality) = method_used;
-                        else
-                            json_mat.method.(modality) = '';
-                        end
-                    end
+                    method_used = generateMethod(temp_mat,'coregct_method_applied');
+                elseif exist(fullfile(filepath,'ea_coregmrmethod_applied.mat'),'file')
+                    temp_mat = load(fullfile(filepath,'ea_coregmrmethod_applied.mat'));
+                    method_used = generateMethod(temp_mat,'coregmr_method_applied');
                 end
-                encodejson = jsonencode(json_mat);
-                json_fid = fopen(fname_out,'w');
+                json_mat.method.(modality) = method_used;
             end
-        end
-        if contains(fname_in,'ea_normmethod_applied.mat')
+            encodejson = jsonencode(json_mat);
+            json_fid = fopen(fname_out,'w');
+            fprintf(json_fid,encodejson);
+        
+        %dealing with normalization
+        elseif contains(fname_in,'ea_normmethod_applied.mat')
             input_mat = load(fname_in);
-            coreg_fieldnames = fieldnames(input_mat);
-            for i=1:length(coreg_fieldnames)
-                modality = strsplit(coreg_fieldnames{i},'_');
-                modality = upper(modality{end});
-                json_mat.approval.(modality) = input_mat.(coreg_fieldnames{i});
+            normalize_fieldnames = fieldnames(input_mat);
+            for i=1:length(normalize_fieldnames)
+                method_used = generateMethod(input_mat,'norm_method_applied');
+                json_mat.approval = 1;
+                json_mat.method = method_used;
             end
+            encodejson = jsonencode(json_mat);
+            json_fid = fopen(fname_out,'w');
+            fprintf(json_fid,encodejson);
         end
+    end
+ function  method_used = generateMethod(input_mat,modality_field)
+     if iscell(modality_field)
+         modality_field = modality_field{1};
+     end
+     if isfield(input_mat,modality_field)
+         if contains(input_mat.(modality_field),'_ants')
+             method_used = ea_normalize_ants('promt');
+         elseif contains(input_mat.(modality_field),'BRAINSFit')
+             method_used = 'BRAINSFit (Johnson 2007)';
+         elseif contains(input_mat.(modality_field),'FLIRT')
+             method_used = 'FLIRT (Jenkinson 2001 & 2002)';
+         elseif contains(input_mat.(modality_field),'BBR')
+             method_used = 'FLIRT BBR (Greve and Fischl 2009)';
+         elseif contains(input_mat.(modality_field),'HybridSPMANTs')
+             method_used = 'Hybrid SPM & ANTs';
+         elseif contains(input_mat.(modality_field),'HybridSPMBRAINSFIT')
+             method_used = 'Hybrid SPM & BRAINSFIT';
+         elseif contains(input_mat.(modality_field),'_spm')
+             method_used = 'SPM (Friston 2007)';
+         elseif contains(input_mat.(modality_field),'HybridSPMFLIRT')
+             method_used = 'Hybrid SPM & FLIRT';
+         end
+     else
+         method_used = '';
+     end
+     return
+%%dealing with normalization
         
-        %%dealing with normalization
-        
-    elseif endsWith(fname_in,'.txt')
-        fid = fopen(fname_in,'rt');
-        json_fid = fopen(fname_out,'w');
-        text_cell = textscan(fid, '%s','Delimiter','');
-        for text = 1:(length(text_cell{1,1})-1)
-            if strcmp(text_cell{1,1}(text),'***')
-                    temp_var = strsplit(text_cell{1,1}{text+1},':');
-                    method_var = strtrim(temp_var{end});
-                    S.method.(method_var) = text_cell{1,1}{text+3};
-            end
-        end
-        if ~exist('S','var')
-            try
-                %try a type of manual resolution - not sure if it belongs to the catch statement?
-                temp_var = strsplit(text_cell{1,1}{1},':');
-                method_var = strtrim(temp_var{end});
-                S.method.(method_var) = text_cell{1,1}{3};
-            catch
-                disp(['You might have to convert this file to .json manually: ' fname_in]);
-            end
-        end
-        if exist('S','var')
-            encodejson = jsonencode(S);
-        end
-    end
-    if exist('encodejson','var') && exist('json_fid','var')
-        fprintf(json_fid,encodejson);
-    end
-    end
+%     elseif endsWith(fname_in,'.txt')
+%         fid = fopen(fname_in,'rt');
+%         json_fid = fopen(fname_out,'w');
+%         text_cell = textscan(fid, '%s','Delimiter','');
+%         for text = 1:(length(text_cell{1,1})-1)
+%             if strcmp(text_cell{1,1}(text),'***')
+%                     temp_var = strsplit(text_cell{1,1}{text+1},':');
+%                     method_var = strtrim(temp_var{end});
+%                     S.method.(method_var) = text_cell{1,1}{text+3};
+%             end
+%         end
+%         if ~exist('S','var')
+%             try
+%                 %try a type of manual resolution - not sure if it belongs to the catch statement?
+%                 temp_var = strsplit(text_cell{1,1}{1},':');
+%                 method_var = strtrim(temp_var{end});
+%                 S.method.(method_var) = text_cell{1,1}{3};
+%             catch
+%                 disp(['You might have to convert this file to .json manually: ' fname_in]);
+%             end
+%         end
+%         if exist('S','var')
+%             encodejson = jsonencode(S);
+%         end
+%     end
+%     if exist('encodejson','var') && exist('json_fid','var')
+%         fprintf(json_fid,encodejson);
+%     end
+%     end
