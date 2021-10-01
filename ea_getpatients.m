99e6b4da05eb99d18efcf95a8eeed9e2aa9c43b2
Ningfei Li <ningfei.li@gmail.com>
2021-10-01 14:17:06 +0200

    REF: Load patient function supports old dataset and dicom folder now.

--- ea_getpatients.m
+++ ea_getpatients.m
@@ -1,29 +1,29 @@
-function uipatdir=ea_getpatients(options,handles,uipatdir)
+function uipatdir = ea_getpatients(options,handles)
 
-p='/'; % default use root
+startFolder = ea_gethome; % default home folder
+
 try
-    p=pwd; % if possible use pwd instead (could not work if deployed)
-end
-try % finally use last patient parent dir if set.
-    load([ea_getearoot,'common',filesep,'ea_recentpatients.mat']);
-    p=fileparts(recentfolders{1});
+    startFolder = pwd; % if possible use pwd instead (could not work if deployed)
 end
 
-if ~exist('uipatdir','var')
-    uipatdir=ea_uigetdir(p,'Please choose patient folder(s)...');
+try % finally use last patient parent dir if set.
+    load([ea_getearoot,'common', filesep, 'ea_recentpatients.mat'], 'recentfolders');
+    startFolder = fileparts(recentfolders{1});
 end
+
+uipatdir = ea_uigetdir(startFolder, 'Please choose patient folder(s)...');
+
 if isempty(uipatdir)
     return
 end
 
-
 if exist('handles','var')
     ea_load_pts(handles,uipatdir);
-    
+
     if isfield(handles,'atlassetpopup') % not present in connectome mapper
         atlasset=get(handles.atlassetpopup,'String');
         atlasset=atlasset{get(handles.atlassetpopup,'Value')};
-        
+
         ea_listatlassets(options,handles,get(handles.vizspacepopup,'Value'),atlasset);
     end
 end
