4507c161b4bc874a50bcd8d96099ba37561b04f4
Ningfei Li <ningfei.li@gmail.com>
2021-10-05 23:44:55 +0200

    BUG: Fix recent popupmenu.
    Clarify logic.

--- helpers/ea_refreshrecent.m
+++ helpers/ea_refreshrecent.m
@@ -1,20 +1,23 @@
-function ea_refreshrecent(handles, type, idx)
+function ea_refreshrecent(handles, type)
 % Refresh recent patients/group analyses popupmenu
 
 earoot = ea_getearoot;
 load([earoot, 'common', filesep, 'ea_recent', type, '.mat'], 'recentfolders');
 
-if strcmp(type, 'patients')
-    recentfolders = regexp(recentfolders, ['(?<=\', filesep, 'leaddbs\', filesep, 'sub-).+'], 'match', 'once');
-elseif strcmp(type, 'groups')
-    dataset = regexp(recentfolders, ['(?<=\', filesep, ')[^\', filesep, ']+', '(?=\', filesep, 'derivatives)'], 'match', 'once');
-    analysis = regexp(recentfolders, ['(?<=\', filesep, 'leadgroup\', filesep, 'gs_).+'], 'match', 'once');
-    recentfolders = strcat('dataset-', dataset, '_analysis-', analysis);
+if ismember(['No recent ', type, ' found'], recentfolders) % No recent folder found, set to empty
+    recentfolders = {};
+else
+    if strcmp(type, 'patients')
+        recentfolders = regexp(recentfolders, ['(?<=\', filesep, 'leaddbs\', filesep, 'sub-).+'], 'match', 'once');
+    elseif strcmp(type, 'groups')
+        dataset = regexp(recentfolders, ['(?<=\', filesep, ')[^\', filesep, ']+', '(?=\', filesep, 'derivatives)'], 'match', 'once');
+        analysis = regexp(recentfolders, ['(?<=\', filesep, 'leadgroup\', filesep, 'gs_).+'], 'match', 'once');
+        recentfolders = strcat('dataset-', dataset, '_analysis-', analysis);
+    end
 end
 
 recentfolders = [['Recent ', type, ':']; recentfolders];
 
+% Refresh recent popupmenu, always set the value to 1
 handles.recent.String = recentfolders;
-if exist('idx', 'var')
-   handles.recent.Value = idx+1;
-end
+handles.recent.Value = 1;
