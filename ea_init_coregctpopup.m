d0c799a76d05507d65b86e24b5bc0f1b32fba698
Ningfei Li <ningfei.li@gmail.com>
2021-10-07 11:24:53 +0200

    REF: Refine patterns in regexpdir call.
    More robust. Use 'file' and 'dir' input parameter to filter results.

--- ea_init_coregctpopup.m
+++ ea_init_coregctpopup.m
@@ -8,7 +8,7 @@ if ~exist('handlestring','var')
 end
 
 % Get functions and names
-funcs = ea_regexpdir(ea_getearoot, 'ea_coregpostopct_.*.m', 0);
+funcs = ea_regexpdir(ea_getearoot, 'ea_coregpostopct_.*\.m$', 0);
 funcs = regexp(funcs, '(ea_coregpostopct_.*)(?=\.m)', 'match', 'once');
 names = cellfun(@(x) eval([x, '(''prompt'');']), funcs, 'Uni', 0);
 
