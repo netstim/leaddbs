d8ba521d659a02a285ca92a63a1561f601ff33b0
Ningfei Li <ningfei.li@gmail.com>
2021-10-19 23:29:25 +0200

    REF: Adapt mapper seeds file naming for BIDS.

--- helpers/gui/ea_menu_addvats.m
+++ helpers/gui/ea_menu_addvats.m
@@ -1,21 +1,15 @@
-function ea_menu_addvats(toolsmenu,handles)
-
+function ea_menu_addvats(toolsmenu, handles)
 
 v = uimenu(toolsmenu,'Label','VAT');
-uimenu(v,'Label','Aggregate all files of selected stimulation (Left VAT)','Callback',{@ea_aggregateVTA,handles,'vat_left'});
-uimenu(v,'Label','Aggregate all files of selected stimulation (Right VAT)','Callback',{@ea_aggregateVTA,handles,'vat_right'});
-uimenu(v,'Label','Aggregate all files of selected stimulation (Left E-Field)','Callback',{@ea_aggregateVTA,handles,'vat_efield_left'});
-uimenu(v,'Label','Aggregate all files of selected stimulation (Right E-Field)','Callback',{@ea_aggregateVTA,handles,'vat_efield_right'});
-uimenu(v,'Label','Aggregate all files of selected stimulation (Left normalized E-Field)','Callback',{@ea_aggregateVTA,handles,'vat_efield_gauss_left'});
-uimenu(v,'Label','Aggregate all files of selected stimulation (Right normalized E-Field)','Callback',{@ea_aggregateVTA,handles,'vat_efield_gauss_right'});
-uimenu(v,'Label','Aggregate all files of selected stimulation (Left fMRI seed)','Callback',{@ea_aggregateVTA,handles,'vat_seed_compound_fMRI_l'});
-uimenu(v,'Label','Aggregate all files of selected stimulation (Right fMRI seed)','Callback',{@ea_aggregateVTA,handles,'vat_seed_compound_fMRI_r'});
-uimenu(v,'Label','Aggregate all files of selected stimulation (Combined fMRI seed)','Callback',{@ea_aggregateVTA,handles,'vat_seed_compound_fMRI'});
-uimenu(v,'Label','Aggregate all files of selected stimulation (Left dMRI seed)','Callback',{@ea_aggregateVTA,handles,'vat_seed_compound_dMRI_l'});
-uimenu(v,'Label','Aggregate all files of selected stimulation (Right dMRI seed)','Callback',{@ea_aggregateVTA,handles,'vat_seed_compound_dMRI_r'});
-uimenu(v,'Label','Aggregate all files of selected stimulation (Combined dMRI seed)','Callback',{@ea_aggregateVTA,handles,'vat_seed_compound_dMRI'});
-
-
-        
-     
-
+uimenu(v,'Label','Aggregate all files of selected stimulation (Left VAT)','Callback',{@(src, evt) ea_aggregateVTA(handles,'sim-binary_hemi-L')});
+uimenu(v,'Label','Aggregate all files of selected stimulation (Right VAT)','Callback',{@(src, evt) ea_aggregateVTA(handles,'sim-binary_hemi-R')});
+uimenu(v,'Label','Aggregate all files of selected stimulation (Left E-Field)','Callback',{@(src, evt) ea_aggregateVTA(handles,'sim-efield_hemi-L')});
+uimenu(v,'Label','Aggregate all files of selected stimulation (Right E-Field)','Callback',{@(src, evt) ea_aggregateVTA(handles,'sim-efield_hemi-R')});
+uimenu(v,'Label','Aggregate all files of selected stimulation (Left normalized E-Field)','Callback',{@(src, evt) ea_aggregateVTA(handles,'sim-efieldgauss_hemi-R')});
+uimenu(v,'Label','Aggregate all files of selected stimulation (Right normalized E-Field)','Callback',{@(src, evt) ea_aggregateVTA(handles,'sim-efieldgauss_hemi-R')});
+uimenu(v,'Label','Aggregate all files of selected stimulation (Left fMRI seed)','Callback',{@(src, evt) ea_aggregateVTA(handles,'sim-binary_seed-fMRI_hemi-L')});
+uimenu(v,'Label','Aggregate all files of selected stimulation (Right fMRI seed)','Callback',{@(src, evt) ea_aggregateVTA(handles,'sim-binary_seed-fMRI_hemi-R')});
+uimenu(v,'Label','Aggregate all files of selected stimulation (Combined fMRI seed)','Callback',{@(src, evt) ea_aggregateVTA(handles,'sim-binary_seed-fMRI')});
+uimenu(v,'Label','Aggregate all files of selected stimulation (Left dMRI seed)','Callback',{@(src, evt) ea_aggregateVTA(handles,'sim-binary_seed-dMRI_hemi-L')});
+uimenu(v,'Label','Aggregate all files of selected stimulation (Right dMRI seed)','Callback',{@(src, evt) ea_aggregateVTA(handles,'sim-binary_seed-dMRI_hemi-R')});
+uimenu(v,'Label','Aggregate all files of selected stimulation (Combined dMRI seed)','Callback',{@(src, evt) ea_aggregateVTA(handles,'sim-binary_seed-dMRI')});
