48bbdda512e8fee27fc47c207654be0ea2976a7f
Ningfei Li <ningfei.li@gmail.com>
2021-10-19 16:36:45 +0200

    REF: Adapt split_nii_lr function for BIDS.

--- helpers/ea_split_nii_lr.m
+++ helpers/ea_split_nii_lr.m
@@ -4,12 +4,19 @@ function ea_split_nii_lr(fname,offset)
 if ~exist('offset','var')
     offset=0;
 end
+
 nii=ea_load_nii(fname);
 lnii=nii;
 rnii=nii;
-[pth,fn,ext]=fileparts(fname);
-lnii.fname=fullfile(pth,[fn,'_l',ext]);
-rnii.fname=fullfile(pth,[fn,'_r',ext]);
+
+if ~isBIDSFileName(fname)
+    [pth,fn,ext]=fileparts(fname);
+    lnii.fname=fullfile(pth,[fn,'_l',ext]);
+    rnii.fname=fullfile(pth,[fn,'_r',ext]);
+else
+    lnii.fname = setBIDSEntity(fname, 'hemi', 'L');
+    rnii.fname = setBIDSEntity(fname, 'hemi', 'R');
+end
 
 [xx,yy,zz]=ind2sub(size(nii.img),1:numel(nii.img));
 XYZ=[xx;yy;zz;ones(1,length(xx))];
