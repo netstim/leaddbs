d983d486db2e8c6f399f22625bc0a9ec271ee361
Ningfei Li <ningfei.li@gmail.com>
2021-10-19 23:29:25 +0200

    REF: Adapt mapper seeds file naming for BIDS.

--- common/ea_prefs_default.m
+++ common/ea_prefs_default.m
@@ -72,7 +72,7 @@ prefs.lc.func.bplowcutoff=0.009;
 prefs.lc.datadir=[ea_getearoot,'connectomes',filesep];
 
 %% connectome mapper settings:
-prefs.lcm.vatseed='binary'; % set to 'efield_gauss' to use weighted seed of normalized E-field (or 'efield' to use weighted seed of unmodified e-field - not recommended).
+prefs.lcm.vatseed='binary'; % set to 'efieldgauss' to use weighted seed of normalized E-field (or 'efield' to use weighted seed of unmodified e-field - not recommended).
 prefs.lcm.vat2fmrimethod='fsl'; % can be 'fsl' (default) or 'spm'. set to spm to use spm_reslice instead of fsl flirt -applyxfm
 prefs.lcm.chunk=10; % define how many fMRI seeds to handle in the same run. Can be 0 to handle all supplied. Depending on RAM available, 5-20 is a good option.
 prefs.lcm.includesurf=1; % if surface definitions are available for connectomes, include those, too
