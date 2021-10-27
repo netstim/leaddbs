e5ca9cf1222abceff1807b37b2a739ed919b97a6
Ningfei Li <ningfei.li@gmail.com>
2021-10-27 17:31:47 +0200

    BUG: Fix setting UI in case missing fields.

--- ea_options2handles.m
+++ ea_options2handles.m
@@ -3,7 +3,12 @@ function ea_options2handles(options,handles)
 % set handles
 set(handles.normalize_checkbox,'Value',options.normalize.do);
 
-index = find(ismember(handles.normmethod.String, options.normalize.method), 1);
+if ~isfield(options.normalize, 'method')
+    index = [];
+else
+    index = find(ismember(handles.normmethod.String, options.normalize.method), 1);
+end
+
 if ~isempty(index)
     set(handles.normmethod,'Value',index);
 else
@@ -18,7 +23,13 @@ end
 
 % CT coregistration
 set(handles.coreg_checkbox,'Value',options.coregct.do);
-index = find(ismember(handles.coregctmethod.String, options.coregct.method), 1);
+
+if ~isfield(options.coregct, 'method')
+    index = [];
+else
+    index = find(ismember(handles.coregctmethod.String, options.coregct.method), 1);
+end
+
 if ~isempty(index)
     set(handles.coregctmethod, 'Value', index);
 else
@@ -41,16 +52,28 @@ end
 
 set(handles.doreconstruction_checkbox,'Value',options.doreconstruction);
 
-if options.automask
+if isfield(options, 'automask') && options.automask
     set(handles.maskwindow_txt,'String','auto')
-else
+elseif isfield(options, 'maskwindow') && ~isempty(options.maskwindow)
     set(handles.maskwindow_txt,'String',num2str(options.maskwindow));
 end
 
 set(handles.writeout2d_checkbox,'Value',options.d2.write);
-set(handles.manualheight_checkbox,'Value',options.manualheightcorrection);
 set(handles.render_checkbox,'Value',options.d3.write);
-set(handles.targetpopup,'Value',options.entrypointn);
-set(handles.electrode_model_popup,'Value',options.elmodeln);
-set(handles.atlassetpopup,'Value',options.atlassetn);
 set(handles.exportservercheck,'Value',options.d3.autoserver);
+
+if isfield(options, 'manualheightcorrection')
+    set(handles.manualheight_checkbox,'Value',options.manualheightcorrection);
+end
+
+if isfield(options, 'entrypointn')
+    set(handles.targetpopup,'Value',options.entrypointn);
+end
+
+if isfield(options, 'elmodeln')
+    set(handles.electrode_model_popup,'Value',options.elmodeln);
+end
+
+if isfield(options, 'atlassetn')
+    set(handles.atlassetpopup,'Value',options.atlassetn);
+end
