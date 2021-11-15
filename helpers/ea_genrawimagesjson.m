367a343a171bc76f4d44f54a4c78e463c8290191
Ningfei Li <ningfei.li@gmail.com>
2021-11-15 12:01:08 +0100

    ENH: Add function to regenerate rawimages json file.

---
+++ helpers/ea_genrawimagesjson.m
@@ -0,0 +1,29 @@
+function json = ea_genrawimagesjson(BIDSRoot, subjId)
+% [Re-]generate rawimages json file in case it's not present in subject's
+% derivatives folder
+
+rawdataFolder = fullfile(BIDSRoot, 'rawdata', ['sub-', subjId]);
+
+sessionFolders = flip(ea_regexpdir(rawdataFolder, '^ses-.*', 0, 'dir'));
+sessions = regexp(sessionFolders, ['(?<=\', filesep, 'ses-)(.*)$'], 'match', 'once');
+
+for s=1:length(sessions)
+    datatypeFolders = ea_regexpdir(sessionFolders{s}, '[a-z]+', 0, 'dir');
+    datatypes = regexp(datatypeFolders, ['(?<=\', filesep, ')([a-z]+)$'], 'match', 'once');
+    for d=1:length(datatypes)
+        niftiFiles = ea_regexpdir(datatypeFolders{d}, '.*\.nii(\.gz)?$', 0, 'file');
+        for n=1:length(niftiFiles)
+            parsed = parseBIDSFilePath(niftiFiles{n});
+            if isfield(parsed, 'acq')
+                key = [parsed.acq, '_', parsed.suffix];
+            else
+                key = parsed.suffix;
+            end
+            [~, json.(sessions{s}).(datatypes{d}).(key)] = ea_niifileparts(niftiFiles{n});
+        end
+    end
+end
+
+prefsFolder = fullfile(BIDSRoot, 'derivatives', 'leaddbs', ['sub-', subjId], 'prefs');
+ea_mkdir(prefsFolder);
+savejson('', json, fullfile(prefsFolder, ['sub-', subjId, '_desc-rawimages.json']));
