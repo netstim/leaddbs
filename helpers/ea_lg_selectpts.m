04731ed9e4bff26419a20b31ec4be183cccc6b00
Ningfei Li <ningfei.li@gmail.com>
2021-11-18 22:00:37 +0100

    REF: Adapt new group analysis file name.

--- helpers/ea_lg_selectpts.m
+++ helpers/ea_lg_selectpts.m
@@ -99,4 +99,4 @@ for i=1:length(M.patient.list)
 end
 
 % Save modified group analysis file
-save(fullfile(outputfolder, 'LEAD_groupanalysis.mat'), 'M');
+save(ea_getGroupAnalysisFile(outputfolder), 'M');
