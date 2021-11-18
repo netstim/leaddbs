a92a7cfb58e5a08944d0d628c38ebcb56d315030
Ningfei Li <ningfei.li@gmail.com>
2021-11-18 22:00:37 +0100

    REF: Adapt new group analysis file name.

--- connectomics/discfibers/ea_disctract.m
+++ connectomics/discfibers/ea_disctract.m
@@ -274,7 +274,7 @@ classdef ea_disctract < handle
         end
         function refreshlg(obj)
             if ~exist(obj.leadgroup,'file')
-                msgbox('LEAD_groupanalysis file has vanished. Please select file.');
+                msgbox('Groupan alysis file has vanished. Please select file.');
                 [fn,pth]=uigetfile();
                 obj.leadgroup=fullfile(pth,fn);
             end
