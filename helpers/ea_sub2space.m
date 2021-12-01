2fbab6a6b767273ae2188f8925ef684ea6dfa5a3
Ningfei Li <ningfei.li@gmail.com>
2021-12-01 21:45:16 +0100

    REF: mlapp compatibility set to R2020b for now.
    Also use strrep instead of sub2space

--- helpers/ea_sub2space.m
+++
@@ -1,8 +0,0 @@
-function txt=ea_sub2space(txt) % replaces subscores with spaces
-if ~iscell(txt)
-    txt(txt=='_')=' ';
-else
-    for c=1:length(txt)
-        txt{c}(txt{c}=='_')=' ';
-    end
-end
