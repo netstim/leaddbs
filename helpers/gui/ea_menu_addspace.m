1ec0b0238f72fd8909cbded37065f6330c02b050
Ningfei Li <ningfei.li@gmail.com>
2022-01-14 17:37:03 +0100

    BUG: Fix switching space.

--- helpers/gui/ea_menu_addspace.m
+++ helpers/gui/ea_menu_addspace.m
@@ -4,17 +4,18 @@ space_menu = uimenu('Label','Space');
 change_space_menu = uimenu(space_menu,'Label','Change current space to');
 
 spaces = dir(fullfile(ea_getearoot,'templates','space','*','ea_space_def.mat'));
-[~,spaces] = cellfun(@(x) fileparts(strrep(x,'_',' ')), {spaces.folder}', 'uni', false);
-sspaces = cellfun(@(x) ea_underscore2space(x), spaces, 'uni', false);
+[~, spaces] = fileparts({spaces.folder}');
+spacesLabel = ea_underscore2space(spaces);
 
 current_space = ea_getspace;
 current_space_index = find(cellfun(@(x) strcmp(x, current_space), spaces));
 
 for i = 1:length(spaces)
-    c = uimenu(change_space_menu,'Label',sspaces{i},'Callback',{@ea_switchspace,spaces{i}});
+    c = uimenu(change_space_menu,'Label',spacesLabel{i},'Callback',{@ea_switchspace,spaces{i}});
     if i == current_space_index
         c.Checked = 'on';
     end
+
 end
 
 uimenu(space_menu,'Label','(Re-)generate aux files for current space, using selected atlas','Callback',{@ea_genauxspace,handles});
@@ -28,9 +29,9 @@ for i = 1:length(spaces)
     if i == current_space_index
         continue
     end
-    uimenu(import_atlas_menu,'Label',sspaces{i},'Callback',{@ea_importspaceassets,spaces{i},'atlases'});
-    uimenu(import_parcellation_menu,'Label',sspaces{i},'Callback',{@ea_importspaceassets,spaces{i},'labeling'});
-    uimenu(import_both_menu,'Label',sspaces{i},'Callback',{@ea_importspaceassets,spaces{i},'both'});
-    uimenu(import_custom_menu,'Label',sspaces{i},'Callback',{@ea_importspaceassets,spaces{i},'custom'});
+    uimenu(import_atlas_menu,'Label',spacesLabel{i},'Callback',{@ea_importspaceassets,spaces{i},'atlases'});
+    uimenu(import_parcellation_menu,'Label',spacesLabel{i},'Callback',{@ea_importspaceassets,spaces{i},'labeling'});
+    uimenu(import_both_menu,'Label',spacesLabel{i},'Callback',{@ea_importspaceassets,spaces{i},'both'});
+    uimenu(import_custom_menu,'Label',spacesLabel{i},'Callback',{@ea_importspaceassets,spaces{i},'custom'});
 end
 
