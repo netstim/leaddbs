20b02406f29521e81c2cc2ac36c2419589891e3d
Ningfei Li <ningfei.li@gmail.com>
2022-02-15 11:44:35 +0100

    REF: Refine checkSpecialChars.

--- helpers/gui/ea_menu_initmenu.m
+++ helpers/gui/ea_menu_initmenu.m
@@ -57,6 +57,7 @@ if isempty(menuprobe)
     uimenu(f,'Label','Clean folders from unnecessary/legacy files','Callback',{@ea_cleanlegacy,handles});
 
     uimenu(f,'Label','Calculate SNR ratio for selected subjects','Callback',{@ea_run_SNR,handles});
+
     uimenu(f,'Label','Anonymize files for selected subjects','Callback',{@ea_run_deface,handles});
         
     uimenu(f,'Label','Read in stimulation settings from move.base','Callback',{@ea_import_movebase_stimsettings,handles});
