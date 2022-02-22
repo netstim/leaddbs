699e2ac33c89bff88069920d19aaa434ace86766
Ningfei Li <ningfei.li@gmail.com>
2022-02-22 21:48:15 +0100

    ENH: Add function to override renderer of listbox and popupmenu.
    Now they can have dynamic tooltip.

---
+++ helpers/gui/ListBoxRenderer/ea_ListBoxRenderer.m
@@ -0,0 +1,15 @@
+function ea_ListBoxRenderer(control)
+% Add dynamic tooltip to ListBox and Popupmenu by overriding Renderer
+
+if exist('ListBoxRenderer','class') ~= 8
+    javaaddpath(fileparts(mfilename('fullpath')));
+end
+
+switch control.Style
+    case 'listbox'
+        jScrollPane = findjobj_fast(control);
+        jScrollPane.getViewport.getView.setCellRenderer(ListBoxRenderer);
+    case 'popupmenu'
+        jComboBox = findjobj_fast(control);
+        jComboBox.setRenderer(ListBoxRenderer);
+end
