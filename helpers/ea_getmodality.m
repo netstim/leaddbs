0fcfa5005e2aed19e3b4a7c7ca0def701a9767f8
lahart21 <66320336+lahart21@users.noreply.github.com>
2022-03-21 15:07:50 -0400

    BUG: Fix missing iso acq tag when checking modality.
    There was a bug that "iso" was not included in the modality variable. It
    originally said T2w, now it is correctly iso_T2w.
    
    This closes #727

--- helpers/ea_getmodality.m
+++ helpers/ea_getmodality.m
@@ -2,8 +2,8 @@ function modality = ea_getmodality(BIDSFilePath)
 % Extract image modality from BIDS file path and keep [ax|cor|sag] label
 % for post-op MRI
 
-if isempty(regexp(BIDSFilePath, '_acq-(ax|cor|sag)_', 'once'))
+if isempty(regexp(BIDSFilePath, '_acq-(ax|cor|sag|iso)_', 'once'))
     modality = regexp(BIDSFilePath, '(?<=_)([^\W_]+)(?=\.nii(\.gz)?$)', 'match', 'once');
 else % Keep plane label for post-op MRI
-    modality = regexp(BIDSFilePath, '(?<=_acq-)((ax|sag|cor)_[^\W_]+)(?=\.nii(\.gz)?$)', 'match', 'once');
+    modality = regexp(BIDSFilePath, '(?<=_acq-)((ax|sag|cor|iso)_[^\W_]+)(?=\.nii(\.gz)?$)', 'match', 'once');
 end
