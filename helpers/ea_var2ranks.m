a1801bc6f5c7b34d55d801cc761623acead74d12
Andreas Horn <andreas.horn@charite.de>
2022-03-23 17:53:43 -0400

    ENH support fibers when changing lighting

--- helpers/ea_var2ranks.m
+++ helpers/ea_var2ranks.m
@@ -1,8 +1,10 @@
-function ranks=ea_var2ranks(var)
+function ranks=ea_var2ranks(var,order)
 % function to convert variables to ranked data.
+if ~exist('order','var')
+    order='ascend';
+end
 
-
-        [varsort,idx]=sort(var);
+        [varsort,idx]=sort(var,order);
         ranks=zeros(length(varsort),1);
         for rank=1:length(ranks)
             ranks(idx(rank))=rank;
