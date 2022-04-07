562c7d7b1dead266ff9cce8ed5b90c44fee89f3a
nrajamani3 <nrajamani3@gatech.edu>
2022-04-07 14:34:21 +0200

    BUG: typo in ea_regexpdir fcn call

--- connectomics/discfibers/ea_discfibers_getvats.m
+++ connectomics/discfibers/ea_discfibers_getvats.m
@@ -16,8 +16,8 @@ disp('Construct VAT list...')
 for sub=1:numPatient
     % Original VAT E-field
     stimFolder = [pthprefix, obj.allpatients{sub}, filesep, 'stimulations', filesep, ea_nt(0), 'gs_', obj.M.guid];
-    vatlist(sub,1) = ea_regexpdir(stimFolder, 'sim-efield_model-',modelLabel,'_hemi-R\.nii$', 0);
-    vatlist(sub,2) = ea_regexpdir(stimFolder, 'sim-efield_model-',modelLabel,'_hemi-L\.nii$', 0);
+    vatlist(sub,1) = ea_regexpdir(stimFolder, ['sim-efield_model-',modelLabel,'_hemi-R\.nii$'], 0);
+    vatlist(sub,2) = ea_regexpdir(stimFolder, ['sim-efield_model-',modelLabel,'_hemi-L\.nii$'], 0);
 
     % Mirrored VAT E-field
     ea_genflippedjointnii(vatlist{sub,1}, vatlist{sub,2});
