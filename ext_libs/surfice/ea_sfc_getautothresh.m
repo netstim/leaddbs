2ab1dfe6958ec53196949c8d25036cc7bc81a3b4
Ningfei Li <ningfei.li@gmail.com>
2022-04-13 11:14:28 +0200

    REF: sfc autothresh supports both char and cell input.

--- ext_libs/surfice/ea_sfc_getautothresh.m
+++ ext_libs/surfice/ea_sfc_getautothresh.m
@@ -1,25 +1,29 @@
-function threshold = ea_sfc_getautothresh(fis)
+function threshold = ea_sfc_getautothresh(images)
 % Calculate thresholds for visualization in SufIce
 
-threshold = nan(length(fis), 4);
+if ischar(images)
+    images = {images};
+end
 
-for fi=1:length(fis)
-    nii = ea_load_nii(fis{fi});
+threshold = nan(length(images), 4);
+
+for f=1:length(images)
+    nii = ea_load_nii(images{f});
     nii.img(nii.img==0) = nan;
     imgstd = ea_nanstd(nii.img(:));
     if min(nii.img(:))<0 % has negative values, calculate 4 vals
-        threshold(fi,1) = 0.5*imgstd;
-        threshold(fi,2) = 2.7*imgstd;
-        threshold(fi,3) = -2.7*imgstd;
-        threshold(fi,4) = -0.5*imgstd;
+        threshold(f,1) = 0.5*imgstd;
+        threshold(f,2) = 2.7*imgstd;
+        threshold(f,3) = -2.7*imgstd;
+        threshold(f,4) = -0.5*imgstd;
     else % only positive vals
-        threshold(fi,1) = 0.04*imgstd;
-        threshold(fi,2) = 1.9*imgstd;
-        threshold(fi,3) = nan;
-        threshold(fi,4) = nan;
+        threshold(f,1) = 0.04*imgstd;
+        threshold(f,2) = 1.9*imgstd;
+        threshold(f,3) = nan;
+        threshold(f,4) = nan;
     end
 
     if ~any(nii.img(:)>0)
-        threshold(fi, 1:2) = nan;
+        threshold(f, 1:2) = nan;
     end
 end
