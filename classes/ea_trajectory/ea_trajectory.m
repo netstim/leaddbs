netstim/leaddbs

commit efe709f2a90980650cb5476d14ce1b92eedbce42

ENH secondary click robustness for planning fiducials

classes/ea_trajectory/ea_trajectory.m
@@ -543,7 +543,7 @@ end
 
 function rightcallback(src, evnt, obj)
     if evnt.getButton() == 3
-        ea_editfiducial(src,evnt, obj)
+        
     end
 end
 
@@ -553,7 +553,7 @@ function ea_editfiducial(Hobj, evt, obj)
 end
 
 
-function ea_trajvisible(~, evt, onoff, obj)
+function ea_trajvisible(src, evt, onoff, obj)
     if getappdata(obj.plotFigureH,'altpressed') % hide all
         el_render=getappdata(obj.plotFigureH,'el_render');
 
@@ -562,6 +562,8 @@ function ea_trajvisible(~, evt, onoff, obj)
             el_render(el).showMacro=ea_bool2onoff(onoff);
             el_render(el).showMicro=ea_bool2onoff('off');
         end
+    elseif getappdata(obj.plotFigureH,'cmdpressed') % rightclick
+        ea_editfiducial(src,evt,obj)
     else
         if ~isempty(obj.controlH) ...
            && ~strcmp(evt.Source.Type, 'uitoggletool') % From control figure

ea_elvis.m
@@ -723,6 +723,8 @@ if ismember('alt', event.Modifier)
     %    disp('Altpressed');
 elseif ismember('shift', event.Modifier)
     setappdata(resultfig, 'shiftpressed', 1);
+elseif ismember('command', event.Modifier)
+    setappdata(resultfig, 'cmdpressed', 1);
 end
 
 % If the MER Control window is open
@@ -808,6 +810,8 @@ end
 
 function ea_keyrelease(resultfig,event)
 setappdata(resultfig,'altpressed',0);
+setappdata(resultfig,'shiftpressed',0);
+setappdata(resultfig,'cmdpressed',0);
 %disp('Altunpressed');
 
 

helpers/ea_fuse_reconstructions.m
@@ -1,0 +1,33 @@
+function ea_fuse_reconstructions(recos,outputfilename)
+% simple function to fuse several reconstructions into one.
+% recos is a cell pointing to different ea_reconstruction.mat files
+
+for r=1:length(recos)
+
+    rec(r)=load(recos{r});
+    if r == 1
+        reco=rec(1).reco;
+    else
+        try reco.native.coords_mm=[reco.native.coords_mm,rec(r).reco.native.coords_mm]; end
+        try reco.mni.coords_mm=[reco.native.coords_mm,rec(r).reco.mni.coords_mm]; end
+        try reco.scrf.coords_mm=[reco.native.coords_mm,rec(r).reco.scrf.coords_mm]; end
+        try reco.acpc.coords_mm=[reco.native.coords_mm,rec(r).reco.acpc.coords_mm]; end
+
+
+        try reco.native.trajectory=[reco.native.trajectory,rec(r).reco.native.trajectory]; end
+        try reco.mni.trajectory=[reco.mni.trajectory,rec(r).reco.mni.trajectory]; end
+        try reco.scrf.trajectory=[reco.scrf.trajectory,rec(r).reco.scrf.trajectory]; end
+        try reco.acpc.trajectory=[reco.acpc.trajectory,rec(r).reco.acpc.trajectory]; end
+
+        try reco.native.markers(end+1:end+length(rec(r).reco.native.markers))=rec(r).reco.native.markers; end
+        try reco.mni.markers(end+1:end+length(rec(r).reco.mni.markers))=rec(r).reco.mni.markers; end
+        try reco.scrf.markers(end+1:end+length(rec(r).reco.scrf.markers))=rec(r).reco.scrf.markers; end       
+        try reco.acpc.markers(end+1:end+length(rec(r).acpc.scrf.markers))=rec(r).acpc.scrf.markers; end
+    end
+
+
+end
+
+save(outputfilename,'reco');
+
+

commit 0deede9f8ad02b56a03ad6f105ae6a9bde72a4a5

BUG: Fix openning trajectory control window.
BUG was introduced in efe709f.

classes/ea_trajectory/ea_trajectory.m
@@ -541,9 +541,9 @@ function fn=stripext(fn)
 end
 
 
-function rightcallback(src, evnt, obj)
-    if evnt.getButton() == 3
-        
+function rightcallback(src, evt, obj)
+    if evt.getButton() == 3
+        ea_editfiducial(src,evt,obj)
     end
 end
 

ea_elvis.m
@@ -720,7 +720,6 @@ function ea_keypress(resultfig, event)
 % listeners here.
 if ismember('alt', event.Modifier)
     setappdata(resultfig, 'altpressed', 1);
-    %    disp('Altpressed');
 elseif ismember('shift', event.Modifier)
     setappdata(resultfig, 'shiftpressed', 1);
 elseif ismember('command', event.Modifier)