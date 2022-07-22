620a47dbbef389df60a028952329b5b6b24eb0d7
Ningfei Li <ningfei.li@gmail.com>
2022-07-22 11:45:27 +0200

    REF: Only reslicing anchor for DBS patients (with postop images).

--- ea_autocoord.m
+++ ea_autocoord.m
@@ -79,9 +79,12 @@ if ~strcmp(options.patientname,'No Patient Selected') && ~isempty(options.patien
             % Run reorientation, cropping and bias field correction
             ea_anatpreprocess(options.subj.preopAnat.(fields{i}).preproc);
 
-            % Preprocessing only for pre-op anchor image
+            % Preprocessing steps only for pre-op anchor image
             if i==1
-                ea_resliceanat(options.subj.preopAnat.(fields{i}).preproc);
+                % Skip reslicing anchor image to 0.7^3 resolution when only pre-op images exist
+                if isfield(options.subj, 'postopAnat')
+                    ea_resliceanat(options.subj.preopAnat.(fields{i}).preproc);
+                end
                 % ea_acpcdetect(options.subj.preopAnat.(fields{i}).preproc);
             end
 
