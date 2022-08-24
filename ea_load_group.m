7b064972dc8b7d654be8c4006ee7e352e2b08472
Ningfei Li <ningfei.li@gmail.com>
2022-08-24 15:50:13 +0200

    REF: Refine filesep etc.

--- ea_load_group.m
+++ ea_load_group.m
@@ -11,15 +11,13 @@ if isempty(analysisFile) % Create new analysis file in case not found
 end
 load(analysisFile, 'M');
 
-%out = regexp(groupdir,'/','split');
-%rem_end = length(out{end}) + length(out{end-1}) + 1; % +1 for /
 derivative_folder = split(groupdir,'leadgroup');
-if isfile([derivative_folder{1},'leaddbs/Miniset_flag.json'])
+if isfile([derivative_folder{1}, 'leaddbs', filesep, 'Miniset_flag.json'])
     for i = 1:size(M.patient.list,1)
-        [~,patient_tag,~] = fileparts(M.patient.list{i});
-        M.patient.list{i} = [derivative_folder{1},'leaddbs/',patient_tag];
+        [~, patient_tag] = fileparts(M.patient.list{i});
+        M.patient.list{i} = [derivative_folder{1}, 'leaddbs', filesep, patient_tag];
     end
-    M.root = [groupdir, '/'];
+    M.root = fullfile(groupdir, filesep);
     save(analysisFile, 'M')
 end
 
--- helpers/ea_generate_min_dataset.m
+++ helpers/ea_generate_min_dataset.m
@@ -1,5 +1,4 @@
 function ea_generate_min_dataset(M, min_dataset_dir)
-
 % Generates minimal datasets that contain electrode reconstructions
 % and optionally VATs (including flipped)
 % Such datasets allow VAT calculations in MNI space and
@@ -10,53 +9,50 @@ function ea_generate_min_dataset(M, min_dataset_dir)
 
 % first we create varios directories to follow BIDS structure
 if ~exist('min_dataset_dir', 'var')
-    min_dataset_dir = [M.root,'Miniset'];
+    min_dataset_dir = fullfile(M.root, 'Miniset');
 end
 
 if exist(min_dataset_dir, 'dir')
-    disp('Overwriting the old miniset')
+    disp('Overwriting the old miniset!')
     rmdir(min_dataset_dir, 's');
 end
 
-mkdir(min_dataset_dir);
-mkdir([min_dataset_dir, '/derivatives']);
-mkdir([min_dataset_dir, '/derivatives/leaddbs']);
-mkdir([min_dataset_dir, '/derivatives/leadgroup']);
-mkdir([min_dataset_dir, '/derivatives/leadgroup/', M.guid]);
+mkdir(fullfile(min_dataset_dir, 'derivatives', 'leaddbs'));
+mkdir(fullfile(min_dataset_dir, 'derivatives', 'leadgroup', M.guid));
 
 % add the miniset flag
-fid = fopen([min_dataset_dir, '/derivatives/leaddbs/Miniset_flag.json'],'w');
-fid = fclose(fid);
-%edit([min_dataset_dir, '/derivatives/leaddbs/Miniset_flag.json'])
+fid = fopen(fullfile(min_dataset_dir, 'derivatives', 'leaddbs', 'Miniset_flag.json'), 'w');
+fclose(fid);
 
 % the leadgroup will be also stored, but detached
 M_mini = M;
-M_mini.root = [min_dataset_dir,'/derivatives/leadgroup/', M.guid];
+M_mini.root = fullfile(min_dataset_dir, 'derivatives', 'leadgroup', M.guid, filesep);
 
 for i = 1 : size(M.patient.list, 1)
+    [~, patient_tag] = fileparts(M.patient.list{i});
 
-    [filepath,patient_tag,ext] = fileparts(M.patient.list{i});
-    mkdir([min_dataset_dir, '/derivatives/leaddbs/', patient_tag]);
+    newPatientFolder = fullfile(min_dataset_dir, 'derivatives', 'leaddbs', patient_tag);
+    mkdir(newPatientFolder);
 
     % here min_dataset_dir should be substituted with 'local directory/Miniset'
-    M_mini.patient.list{i} = [min_dataset_dir, '/derivatives/leaddbs/', patient_tag];
+    M_mini.patient.list{i} = newPatientFolder;
     
-    newPrefsFolder = [min_dataset_dir, '/derivatives/leaddbs/', patient_tag, '/prefs'];
+    newPrefsFolder = fullfile(newPatientFolder, 'prefs');
     mkdir(newPrefsFolder);
-    newReconstFolder = [min_dataset_dir, '/derivatives/leaddbs/', patient_tag, '/reconstruction'];
+    newReconstFolder = fullfile(newPatientFolder, 'reconstruction');
     mkdir(newReconstFolder);
-    newStimFolder = [min_dataset_dir, '/derivatives/leaddbs/', patient_tag, '/stimulations/MNI152NLin2009bAsym/',M.S(i).label];
+    newStimFolder = fullfile(newPatientFolder, 'stimulations', ea_getspace, M.S(i).label);
     mkdir(newStimFolder);
     
     % now we copy some essential files
-    copyfile([M.patient.list{i},'/prefs/',patient_tag,'_desc-rawimages.json'], newPrefsFolder);
-    copyfile([M.patient.list{i},'/reconstruction/',patient_tag,'_desc-reconstruction.mat'], newReconstFolder);  
-    if isfile([M.patient.list{i},'/',patient_tag,'_desc-stats.mat'])
-        copyfile([M.patient.list{i},'/',patient_tag,'_desc-stats.mat'], M_mini.patient.list{i});
+    copyfile(fullfile(M.patient.list{i}, 'prefs', [patient_tag, '_desc-rawimages.json']), newPrefsFolder);
+    copyfile(fullfile(M.patient.list{i}, 'reconstruction', [patient_tag, '_desc-reconstruction.mat']), newReconstFolder);  
+    if isfile(fullfile(M.patient.list{i}, [patient_tag, '_desc-stats.mat'])
+        copyfile(fullfile(M.patient.list{i}, [patient_tag, '_desc-stats.mat']), M_mini.patient.list{i});
     end
 
     % check if VATs were already computed 
-    myVATs = dir(fullfile([M.patient.list{i}, '/stimulations/MNI152NLin2009bAsym/',M.S(i).label],[patient_tag,'_sim-efield_model*'])); %gets all mat files in struct
+    myVATs = dir(fullfile(M.patient.list{i}, 'stimulations', ea_getspace, M.S(i).label, [patient_tag,'_sim-efield_model*'])); %gets all mat files in struct
     % do not copy flipped VATs, they might be outdated!
     myVATs = myVATs(~endsWith({myVATs.name}, 'lippedFromLeft.nii'));
     myVATs = myVATs(~endsWith({myVATs.name}, 'lippedFromRight.nii'));
@@ -70,15 +66,13 @@ for i = 1 : size(M.patient.list, 1)
         end
         
         % flip VTAs
-        %ea_genflippedjointnii([newStimFolder,'/','*_hemi-R.nii'], [newStimFolder,'/','*_hemi-L.nii']); 
-
         if length(myVATs) == 2 % flipped will be overwritten
             % this order because L comes before R
-            ea_genflippedjointnii([newStimFolder,'/',myVATs(2).name], [newStimFolder,'/',myVATs(1).name]);     
+            ea_genflippedjointnii([newStimFolder, filesep, myVATs(2).name], [newStimFolder, filesep, myVATs(1).name]);     
         elseif length(myVATs) == 1 && contains(myVATs(k).name, 'hemi-R')
-            ea_genflippedjointnii([newStimFolder,'/',myVATs(2).name], 'no_VAT'); 
+            ea_genflippedjointnii([newStimFolder, filesep, myVATs(2).name], 'no_VAT'); 
         elseif length(myVATs) == 1 && contains(myVATs(k).name, 'hemi-L')
-            ea_genflippedjointnii('no_VAT', [newStimFolder,'/',myVATs(1).name]);
+            ea_genflippedjointnii('no_VAT', [newStimFolder, filesep, myVATs(1).name]);
         else
             waitfor(msgbox('Wrong files were copied?'));
         end       
@@ -89,12 +83,7 @@ for i = 1 : size(M.patient.list, 1)
 end
 
 % save the updated lead-group file
-M_orig = M;
 M = M_mini;
-[filepath,miniset_name,ext] = fileparts(min_dataset_dir);
-save([M_mini.root,'/dataset-',miniset_name,'_analysis-',M_mini.guid,'.mat'], 'M')
-M = M_orig;
-
+[~, miniset_name] = fileparts(min_dataset_dir);
+save(fullfile(M_mini.root, ['dataset-', miniset_name, '_analysis-', M_mini.guid, '.mat']), 'M')
 disp('Done')
-
-end
