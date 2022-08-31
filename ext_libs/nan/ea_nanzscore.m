070150e8dd62fc1b8a81419263fcccc77dedc376
GaranceMeyer <108418007+GaranceMeyer@users.noreply.github.com>
2022-08-31 11:50:54 -0400

    UPD: works on matrices not only column vectors
    This function is used on a matrix of clinical scores rather than vectors for the PCA option of the fiberfiltering tool, such as the z-score computation was wrong. Made the function work for both column vectors and matrices with one score per column.

--- ext_libs/nan/ea_nanzscore.m
+++ ext_libs/nan/ea_nanzscore.m
@@ -1,17 +1,25 @@
 function z=ea_nanzscore(varargin)
 
 data=varargin{1};
-datawonan = data(~isnan(data));
+
 dorobust=0;
 if nargin>1
-    if strcmp(varargin{2},'robust');
+    if strcmp(varargin{2},'robust')
         dorobust=1;
     end
 end
-if dorobust
-    datamean = ea_robustmean(datawonan);
-else
-    datamean = mean(datawonan);
-end
-datasd = std(datawonan);
-z = (data-datamean)/datasd;
+
+[datamean, datasd] = deal(zeros(1, size(data, 2)));
+for ci = 1:size(data, 2)
+    datawonan = data(:, ci); 
+    datawonan = datawonan(~isnan(datawonan));
+    if dorobust
+        datamean(ci) = ea_robustmean(datawonan);
+    else
+        datamean(ci) = mean(datawonan);
+    end
+    datasd(ci) = std(datawonan); 
+end 
+
+z = ( data - repmat(datamean, size(data, 1), 1) ) ...
+    ./ repmat(datasd, size(data, 1), 1); 
