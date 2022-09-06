abb3bfeaff34ae39eea1aafadffc7285da914f7a
Ningfei Li <ningfei.li@gmail.com>
2022-09-06 21:53:52 +0200

    REF: Unified getpeak function for peak and peak5.

--- connectomics/discfibers/ea_discfibers_getpeak.m
+++ connectomics/discfibers/ea_discfibers_getpeak.m
@@ -1,21 +1,46 @@
-function peak = ea_discfibers_getpeak(vals, posvisible, negvisible)
+function peak = ea_discfibers_getpeak(vals, posvisible, negvisible, peakmode)
 % Get peak fiber values
-% ea_discfibers_getpeak(vals, obj.posvisible, obj.negvisible)
+% ea_discfibers_getpeak(vals, obj.posvisible, obj.negvisible, 'peak')
 
 if ~exist('posvisible', 'var')
     posvisible = 1;
 end
 
 if ~exist('negvisible', 'var')
-    negvisible = 1;
+    negvisible = 0;
+end
+
+if ~exist('peakmode', 'var')
+    peakmode = 'peak';
+end
+
+switch peakmode
+    case {1, 'peak'}
+        endInd = 1;
+    case {0.05, 'peak5'}
+        endInd = ceil(size(vals,1).*0.05);
+    otherwise
+        if isnumeric(peakmode) && peakmode<1
+            endInd = ceil(size(vals,1).*peakmode);
+        else
+            error('Invalid peak mode!');
+        end
 end
 
 if posvisible && ~negvisible
-    peak = ea_nanmax(vals, 1);
+    vals = sort(vals, 'descend');
+    vals(vals<0) = nan; % Remove negative values
+    peak = ea_nansum(vals(1:endInd,:), 1);
 elseif ~posvisible && negvisible
-    peak = ea_nanmin(vals, 1);
+    vals = sort(vals, 'ascend');
+    vals(vals>0) = nan; % Remove positive values
+    peak = ea_nansum(vals(1:endInd,:), 1);
 elseif posvisible && negvisible
-    pospeak = ea_nanmax(vals, 1);
-    negpeak = ea_nanmin(vals, 1);
-    peak = ea_nansum([pospeak; negpeak]);
+    posvals = sort(vals, 'descend');
+    posvals(posvals<0) = nan; % Remove negative values
+    negvals = sort(vals, 'ascend');
+    negvals(negvals>0) = nan; % Remove negative values
+    peak = ea_nansum([posvals(1:endInd,:); negvals(1:endInd,:)], 1);
+else
+    error('Both positive and negative values are deselected!');
 end
