0fe7eddabecb2fa652ce02eac8766c11d0255bef
Ningfei Li <ningfei.li@gmail.com>
2022-09-13 12:02:27 +0200

    REF: fileparts already supports cell input.

--- ext_libs/ea_regexpdir.m
+++ ext_libs/ea_regexpdir.m
@@ -28,7 +28,7 @@ switch type
         isdir = 0;
     case {'d', 'dir', 'folder'}
         % Remove filesep from the end of the folder path
-        list = cellfun(@fileparts,list(isfolder(list)),'UniformOutput',false);
+        list = fileparts(list(isfolder(list)));
         isdir = 1;
     case {'a', 'all'}
         % Remove filesep from the end of the folder path
