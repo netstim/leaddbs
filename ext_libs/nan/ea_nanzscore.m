246e343cea96e88d07b9c8f1621f9ceb3ee75912
nrajamani3 <nrajamani3@gatech.edu>
2022-09-30 18:05:35 +0200

    BUG: rebase ea_nanzscore properly

--- ext_libs/nan/ea_nanzscore.m
+++ ext_libs/nan/ea_nanzscore.m
@@ -1,25 +1,27 @@
-function z=ea_nanzscore(varargin)
+function z = ea_nanzscore(data, robust, dim)
+% Computes zscores for input data
+%
+% data:   row vector, column vector, or matrix
+%         If data is a matrix, z-scores are computed over columns by default
+%         or by specified dim.
+% robust: if set to 'robust' as second argument, mean is computed excluding
+%         outliers
 
-data=varargin{1};
-
-dorobust=0;
-if nargin>1
-    if strcmp(varargin{2},'robust')
-        dorobust=1;
+if isrow(data)
+    dim = 2;
+elseif iscolumn(data)
+    dim = 1;
+else % matrix
+    if ~exist('dim', 'var')
+        dim = 1;
     end
 end
 
-[datamean, datasd] = deal(zeros(1, size(data, 2)));
-for ci = 1:size(data, 2)
-    datawonan = data(:, ci); 
-    datawonan = datawonan(~isnan(datawonan));
-    if dorobust
-        datamean(ci) = ea_robustmean(datawonan);
-    else
-        datamean(ci) = mean(datawonan);
-    end
-    datasd(ci) = std(datawonan); 
-end 
+if exist('robust', 'var') && strcmpi(robust, 'robust')
+    datamean = ea_robustmean(data, dim);
+else
+    datamean = mean(data, dim, 'omitnan');
+end
 
-z = ( data - repmat(datamean, size(data, 1), 1) ) ...
-    ./ repmat(datasd, size(data, 1), 1); 
+datasd = std(data, 0, dim, 'omitnan');
+z = (data-datamean) ./ datasd;
