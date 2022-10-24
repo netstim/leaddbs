8f498c1b617fac286e1f5da63428748d0ae34dd3
Ningfei Li <ningfei.li@gmail.com>
2022-10-25 00:00:20 +0200

    REF: getConnLabel only once.

--- connectomics/dbsnetworkmapping/ea_networkmapping_calcvals.m
+++ connectomics/dbsnetworkmapping/ea_networkmapping_calcvals.m
@@ -24,8 +24,8 @@ options.lcm.cmd = 1;
 ea_run('run', options);
 
 %% Load in nifti files as matrix
+connLabel = ea_getConnLabel(cfile);
 for s=1:size(vatlist,1)
-        connLabel = ea_getConnLabel(cfile);
         switch sf(ix)
             case 1 % structural
                 fingerprint = setBIDSEntity(vatlist{s}, 'conn', connLabel, 'suffix', 'strucmap');
