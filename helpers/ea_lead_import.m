netstim/leaddbs

commit 6b539a20beccd30ce19462053bf7866be5872d9a

REF: base rawimages.json entries of preproc folder special case

helpers/ea_lead_import.m
@@ -20,7 +20,7 @@ for pt = 1:length(source_folder)
         end
 
         %detect dataset type
-        [results,flag,dicom_conv,doMigrate,doOnlyRaw,dicom_source_folder] = enablerules(source_folder{pt},options);
+        [results,flag,dicom_conv,doMigrate,doOnlyRaw] = enablerules(source_folder{pt},options);
 
         if strcmp(results,'fail')
             if length(source_folder) == 1
@@ -49,7 +49,6 @@ for pt = 1:length(source_folder)
         end
     end
     run_prop.source = source_folder{pt};
-    run_prop.dicom_source = dicom_source_folder;
     run_prop.dest = dest_folder;
     run_prop.convert_using = options.prefs.migrate.DicomConversionTool;
     run_prop.flag = flag;
@@ -65,7 +64,6 @@ ea_busyaction('off',handles.leadfigure,'dbs');
 
 function runImport(run_prop)
     source_dir = run_prop.source;
-    dicom_source_folder = run_prop.dicom_source;
     dest_folder = run_prop.dest;
     convert_using = run_prop.convert_using;
     flag = run_prop.flag;
@@ -74,24 +72,25 @@ function runImport(run_prop)
     doOnlyRaw = run_prop.doOnlyRaw;
     if strcmp(flag,'onlyMigrate')
         disp('Migrating dataset...');
-        if dicom_conv && ~isempty(dicom_source_folder) && ~doOnlyRaw
-            ea_legacy2bids(source_dir,dest_folder,1,dicom_source_folder,1,0);
+        if dicom_conv && ~doOnlyRaw
+            ea_legacy2bids(source_dir,dest_folder,1,0);
         elseif ~dicom_conv && doMigrate && doOnlyRaw
-            ea_legacy2bids(source_dir,dest_folder,0,'',0,1);
+            ea_legacy2bids(source_dir,dest_folder,0,1);
         elseif doMigrate && ~dicom_conv && ~doOnlyRaw
-            ea_legacy2bids(source_dir,dest_folder,0,'',0,0);
+            ea_legacy2bids(source_dir,dest_folder,0,0);
         end
     end
     if strcmp(flag,'MigrateDCMconv')
         disp('Migrating dataset...')
+        ea_dataset_import(source_dir,dest_folder,convert_using, 1);
         %app.dicom_source: parent dicom,
         %implement checkpoint if folder is not empty
         if doOnlyRaw && dicom_conv
-            ea_legacy2bids(source_dir,dest_folder,1,dicom_source_folder,1,1);
+            ea_legacy2bids(source_dir,dest_folder,1,1);
         elseif dicom_conv && ~doOnlyRaw
-            ea_legacy2bids(source_dir,dest_folder,1,dicom_source_folder,1,0)
+            ea_legacy2bids(source_dir,dest_folder,1,0)
         end
-        ea_dataset_import(source_dir,dest_folder,convert_using, 1);
+        
     end
     % only DICOM conversion, no migration
     if strcmp(flag,'onlyDCMconv')
@@ -121,13 +120,12 @@ function count = checkValidity(folders)
    % end
 
 
-function [results,flag,dicom_conv,doMigrate,doOnlyRaw,dicom_source_folder] = enablerules(selection,options)
+function [results,flag,dicom_conv,doMigrate,doOnlyRaw] = enablerules(selection,options)
     [filepath,subject_name,~] = fileparts(selection);
     
     subfolder = dir_without_dots(selection);
     subfolder = {subfolder.name};
     results = 'pass';
-    dicom_source_folder = {};
     % detection function: if the patient has derivatives and
     % raw data, but not in BIDS format then it should be migrated.
     if endsWith(filepath, 'sourcedata')
@@ -135,7 +133,6 @@ function [results,flag,dicom_conv,doMigrate,doOnlyRaw,dicom_source_folder] = ena
         dicom_conv = 1;
         doMigrate = 0;
         doOnlyRaw = 0;
-        dicom_source_folder{1} = selection;
         flag = 'onlyDCMconv';
     
         % BIDS Compliant raw data nifti files are available! in this case,
@@ -164,19 +161,7 @@ function [results,flag,dicom_conv,doMigrate,doOnlyRaw,dicom_source_folder] = ena
         dicom_conv = 1;
         doOnlyRaw = 0;
         flag = 'MigrateDCMconv';
-        if isfolder(fullfile(selection,'DICOM'))
-            dicom_source_folder{1} = fullfile(selection,'DICOM');
-        else
-            if ~exist(fullfile(selection,'DICOM'),'dir')
-                mkdir(fullfile(selection,'DICOM'))
-            end
-            for i=1:length(subfolder)
-                if regexp(subfolder{i},'.*.dcm')
-                    movefile(fullfile(selection,subfolder{i}),fullfile(selection,'DICOM'))
-                end
-            end
-        end
-        dicom_source_folder{1} = fullfile(selection,'DICOM');
+        
     elseif checkIfOneExist(subfolder,'[^anat]*.nii$') && checkIfNoneExist(subfolder,'dicom||.*.dcm')
         doMigrate = 1;
         dicom_conv = 0;
@@ -188,59 +173,15 @@ function [results,flag,dicom_conv,doMigrate,doOnlyRaw,dicom_source_folder] = ena
         dicom_conv = 1;
         doOnlyRaw = 1;
         flag = 'MigrateDCMconv';
-        if isfolder(fullfile(selection,'DICOM'))
-            dicom_source_folder{1} = fullfile(selection,'DICOM');
-        else
-            if ~exist(fullfile(selection,'DICOM'),'dir')
-                mkdir(fullfile(selection,'DICOM'))
-            end
-            for i=1:length(subfolder)
-                if regexp(subfolder{i},'.*.dcm')
-                    movefile(fullfile(selection,subfolder{i}),fullfile(selection,'DICOM'))
-                end
-            end
-            dicom_source_folder{1} = fullfile(selection,'DICOM');
-        end
+        
     
         % only DICOM files available
-    elseif checkIfOneExist(subfolder,'dicom*||.*.dcm')
+    elseif checkIfOnlyExist(subfolder,'dicom*||.*.dcm')
         %todo: remove support for .dcm files inside
         flag = 'onlyDCMconv';
         doMigrate = 0;
         dicom_conv = 1;
         doOnlyRaw = 0;
-        if ~isempty(ea_regexpdir(selection, 'DICOM', 0, 'd'))
-            dcm_folder = ea_regexpdir(selection, 'DICOM', 0, 'd');
-            [~,dicom_folder,~] = fileparts(dcm_folder);
-            if ~strcmp(dicom_folder,'DICOM')
-    
-                dicom_conv = 1;
-                doOnlyRaw = 0;
-                folders_inside = dir_without_dots(dcm_folder);
-                subfolders_inside = folders_inside([folders_inside.isdir]); %only get the folders, since they have to be moved to DICOM]
-    
-                if ~exist(fullfile(selection,'DICOM'),'dir')
-                    mkdir(fullfile(selection,'DICOM'))
-                end
-                subfolder_names = {subfolders_inside.name};
-                for i=1:length(subfolder_names)
-                    movefile(fullfile(selection,dicom_folder,subfolder_names{i}), fullfile(selection,'DICOM',subfolder_names{i}))
-                end
-                dicom_source_folder{1} = fullfile(selection,'DICOM');
-    
-            else
-                if ~exist(fullfile(selection,'DICOM'),'dir')
-                    mkdir(fullfile(selection,'DICOM'))
-                end
-                for i=1:length(subfolder)
-                    if regexp(subfolder{i},'.*.dcm')
-                        movefile(fullfile(selection,subfolder{i}),fullfile(selection,'DICOM'))
-                    end
-                end
-                dicom_source_folder{1} = fullfile(selection,'DICOM');
-            end
-        end
-    
     elseif checkIfOneExist(subfolder,'^ea_.*.mat$') || ea_ismember(subfolder,"current_headmodel") || ea_ismember(subfolder,"stimulation") % for detached files only, experimental
         dicom_conv = 0;
         doMigrate = 1;
@@ -249,28 +190,13 @@ function [results,flag,dicom_conv,doMigrate,doOnlyRaw,dicom_source_folder] = ena
         % additional check for subfolders of dicom. Not just
         % folders inside which there might be dicom files.
     elseif ~isfolder(fullfile(selection,'DICOM')) && ~isfolder(fullfile(selection,'dicom'))
-        is_dicom_inside = dir(fullfile(selection,'**/*.dcm'));
-        if ~isempty(is_dicom_inside)
+            %todo: test whether there are actually dicom files inside
             dicom_conv = 1;
             doOnlyRaw = 0;
-            folders_inside = dir_without_dots(selection);
-            subfolders_inside = folders_inside([folders_inside.isdir]); %only get the folders, since they have to be moved to DICOM]
-            if ~exist(fullfile(selection,'DICOM'),'dir')
-                mkdir(fullfile(selection,'DICOM'))
-            end
-            subfolder_names = {subfolders_inside.name};
-            for i=1:length(subfolder_names)
-                movefile(fullfile(selection,subfolder_names{i}), fullfile(selection,'DICOM',subfolder_names{i}))
-            end
-            dicom_source_folder{1} = fullfile(selection,'DICOM');
             flag = 'onlyDCMconv';
-        else
-            ea_warndlg('%s: unable to detect the type of processing required, skipping for now', subject_name{1});
-            results = 'skip';
-        end
-    
     else
-        results = 'fail';%case not handled
+        ea_warndlg('%s: unable to detect the type of processing required, skipping for now', subject_name{1});
+        results = 'skip';
     end
     %compare the results with the user defined prefs:
     if  ~options.prefs.migrate.doDicomConversion

helpers/ea_create_bids_mapping.m
@@ -317,7 +317,8 @@ stimulations{:,2} = {'sim-efield_model-modelTag_hemi-R_hemidesc-FlippedFromLeft.
     'sim-efieldgauss_model-modelTag_seed-fMRI.nii';...
     'desc-stimparameters.mat'}; %deal with files inside connectome folder in the main code for now
     
-lead_mapper{:,1} = {'_vat_seed_compound_dMRI_struc_seed.nii';...
+lead_mapper{:,1} = {
+    '_vat_seed_compound_dMRI_struc_seed.nii';...
     '_vat_seed_compound_fMRI_func_seed_AvgR_Fz.nii';...
     '_vat_seed_compound_fMRI_func_seed_AvgR.nii';...
     '_vat_seed_compound_fMRI_func_seed_T.nii';...

helpers/ea_legacy2bids.m
@@ -1,12 +1,10 @@
-function ea_legacy2bids(source,dest,isdicom,dicom_source,doDcmConv,doOnlyRaw)
+function ea_legacy2bids(source,dest,doDcmConv,doOnlyRaw)
 %%This function migrates a classic LEAD-DBS dataset, whether fully
 %%processed or raw into a BIDS-STYLE dataset.The BIDSified version is
 %%integral for the future releases of BIDS.
 %% Parameters:
 %% i)  source: full path of the source dataset (classic lead-dbs), as a cell structure. Multiple entries may be provided
 %% ii) dest:   full path of the destination dataset (BIDSified lead-dbs), as a cell structure. One overarching directory should be specified
-%% iii)isdicom: Boolean value for whether the classic dataset contains dicom files, present inside a folder called "DICOM"
-%% iv) dicom_source: to be specified only if isdicom is true. Else, specify as an empty string
 %% v)  doDcmConv: Boolean, 1 if your dataset only contains DICOM folders, else 0
 %% vi) doOnlyRaw: Boolean, 1 if your dataset only contains RAW nifti files, else 0
 if ~iscell(source)
@@ -37,17 +35,10 @@ else
 end
 
 
-%if you have dicom files and have not provided a dicom source directory
-%(this can also just be source directory,so refactor this) then throw
-%an error
-if isdicom && ~exist('dicom_source','var')
-    warndlg("You have specified that you want dicom import, but have not specified a dicom source file. Please specific your dicom source directory!")
-end
-
 %define names of the new directorey structure
 
 pipelines = {'brainshift','coregistration','normalization','reconstruction','preprocessing','prefs','log','export','stimulations','headmodel','miscellaneous','ftracking'};
-
+fmri_keywords = {'(Yeo 2011)','HCP 612','HCP 1200','PPMI 74','Depression 38'};
 
 %mapping will allow quick reference of the files to move: also, certain
 %modalities have specific bids naming.
@@ -83,7 +74,7 @@ for patients = 1:length(source)
     modes = {'anat','func','dwi'};
     sessions = {'ses-preop','ses-postop'};
     if  doDcmConv && ~doOnlyRaw
-        subfolder_cell = {'sourcedata','legacy','derivatives'};
+        subfolder_cell = {'derivatives','legacy'};
     elseif doOnlyRaw && ~doDcmConv
         subfolder_cell = {'rawdata'};
     elseif  doOnlyRaw && doDcmConv
@@ -96,11 +87,7 @@ for patients = 1:length(source)
         end
     end
 
-    %dest directory is already specified
-    if isdicom
-        dicom_patient = dicom_source{patients};
-    end
-    
+
     %get and refactor patient names. specially, don't allow
     %'_' or '-'
     [~,patient_name,~] = fileparts(source_patient);
@@ -129,7 +116,7 @@ for patients = 1:length(source)
                continue
             else
                 if ~any(contains(files_to_move{j},'\w*(ct|tra|cor|sag)\w*')) %find a mapping between tags and modalities (for e.g., tag for T1w is ax, therefore tag = {'T1w.nii'}, mod = {'ax'})
-                    if any(regexpi(files_to_move{j},'raw_anat_.*.nii')) || any(regexpi(files_to_move{j},'^anat_.*.nii'))  %we already know their tags in the case of cor,tra,sag
+                    if any(regexpi(files_to_move{j},'raw_anat_.*.nii')) || any(regexpi(files_to_move{j},'^anat_.*.nii')) || doOnlyRaw  %we already know their tags in the case of cor,tra,sag
                         to_match = files_to_move{j};
                         bids_mod = add_mod(to_match,legacy_modalities,rawdata_containers);
                         tag = ea_checkacq(fullfile(source_patient,files_to_move{j})); %function for modalities, use of fslHD
@@ -191,12 +178,14 @@ for patients = 1:length(source)
             end
             dir_names{j} = '';
         elseif strcmp(dir_names{j},'DICOM')
-            if ~exist(fullfile(dest,'sourcedata',patient_name,'DICOM'),'dir')
-                mkdir(fullfile(dest,'sourcedata',patient_name,'DICOM'))
+            if ~doDcmConv
+                if ~exist(fullfile(dest,'sourcedata',patient_name,'DICOM'),'dir')
+                    mkdir(fullfile(dest,'sourcedata',patient_name,'DICOM'))
+                end
+
+                copyfile(fullfile(source_patient,'DICOM'),fullfile(dest,'sourcedata',patient_name,'DICOM'));
+                dir_names{j} = '';
             end
-            
-            copyfile(fullfile(source_patient,'DICOM'),fullfile(dest,'sourcedata',patient_name,'DICOM'));
-            dir_names{j} = '';
             
             %handle brainshift copy and rename: we already do this because
             %some of the filenames are similar to the coreg filenames and
@@ -269,22 +258,7 @@ for patients = 1:length(source)
     %handle raw,
     for subfolders = 1:length(subfolder_cell)
         switch subfolder_cell{subfolders}
-            case 'sourcedata'
-                new_path = fullfile(dest,subfolder_cell{subfolders},patient_name);
-                if ~exist(new_path,'dir')
-                    mkdir(new_path)
-                end
-                if ~isdicom
-                    disp("There are no dicom images, source data folder will be empty")
-                else
-                    if exist(dicom_patient, 'dir')
-                        disp("Copying DICOM folder...");
-                        copyfile(dicom_patient,new_path)
-                    elseif exist(fullfile(source_patient,'dicom'),'dir')
-                        disp("Copying dicom folder...");
-                        copyfile(fullfile(source_patient,'dicom'),new_path)
-                    end
-                end
+          
             case 'derivatives'
                 disp("Migrating Derivatives folder...");
                 new_path = fullfile(dest,subfolder_cell{subfolders},'leaddbs',patient_name);
@@ -552,13 +526,13 @@ for patients = 1:length(source)
                                     is_it_aconnectome = ea_regexpdir(leadMapper_folder{k},'.*vat_seed_compound_[df]MRI.*',0,'f');
                                     if ~isempty(is_it_aconnectome)
                                         [~,connectome_filename,ext] = fileparts(leadMapper_folder{k});
-                                        if ~contains(connectome_filename, '_') % Structural connectome
-                                            bids_connectome_name = ea_getConnLabel(connectome_filename);
-                                        else % Functional connectome, subset in the name
+                                        if cellfun(@(x)contains(x,connectome_filename),fmri_keywords)% Structural connectome
                                             connectome_filename = strsplit(connectome_filename, '_');
                                             connectome = connectome_filename{1};
                                             subset = connectome_filename{2};
-                                            bids_connectome_name = ea_getConnLabel(connectome, subset);
+                                            bids_connectome_name = ea_getConnLabel(connectome, subset);                                            
+                                        else % Functional connectome, subset in the name
+                                            bids_connectome_name = ea_getConnLabel(connectome_filename);
                                         end
                                         model_name = add_model(all_gs_folders{i});
                                         stimulation_folder = all_gs_folders{i};
@@ -568,7 +542,7 @@ for patients = 1:length(source)
                                         tag_struct.subjID = patient_name;
                                         tag_struct.modeltag = model_name;
                                         tag_struct.conntag = bids_connectome_name;
-                                        generate_bidsConnectome_name(stimulation_folder,connectome_folder,lead_mapper,tag_struct)
+                                        generate_bidsConnectome_name(stimulation_folder,connectome_folder,lead_mapper,stimulations,tag_struct)
                                     end
                                 end
                             end
@@ -709,18 +683,8 @@ for patients = 1:length(source)
                         
                     end
                 end
-                raw_path = fullfile(dest,subfolder_cell{subfolders});
-                if ~isempty(dir_without_dots(raw_path))
-                    if exist('postop_modality','var')
-                        ea_generate_datasetDescription(raw_path,'raw',postop_modality);
-                    else
-                        ea_generate_datasetDescription(raw_path,'raw')
-                    end
-                    if ~isdicom
-                        %generate raw image json in the raw data folder
-                        generate_rawImagejson(patient_name,dest)
-                    end
-                end
+                
+                generate_rawImagejson(patient_name,dest,doDcmConv)
         end
     end
     
@@ -870,19 +834,24 @@ function move_mni2bids(mni_files,native_files,~,headmodel,which_pipeline,patient
         
     end
     
-function generate_rawImagejson(patient_name,dest)
+function generate_rawImagejson(patient_name,dest,doDcmConv)
     output_dir = fullfile(dest,'derivatives','leaddbs',patient_name,'prefs');
     if ~exist(output_dir,'dir')
         mkdir(output_dir)
     end
+    if doDcmConv
+        raw_data_path = fullfile(dest,'rawdata','legacy',patient_name);
+    else
+        raw_data_path = fullfile(dest,'rawdata',patient_name);
+    end
     coreg_dir = fullfile(dest,'derivatives','leaddbs',patient_name,'coregistration','anat');
-    raw_preop_dir = fullfile(dest,'rawdata',patient_name,'ses-preop','anat');
-    raw_postop_dir = fullfile(dest,'rawdata',patient_name,'ses-postop','anat');
+    raw_preop_dir = fullfile(raw_data_path,'ses-preop','anat');
+    raw_postop_dir = fullfile(raw_data_path,'ses-postop','anat');
     preprocessing_dir = fullfile(dest,'derivatives','leaddbs',patient_name,'preprocessing','anat');
     opt.FileName = fullfile(dest,'derivatives','leaddbs',patient_name,'prefs',[patient_name,'_','desc-rawimages.json']);
     %special_case
     
-    preop_files = dir_without_dots(fullfile(dest,'rawdata',patient_name,'ses-preop','anat','*.nii.gz'));
+    preop_files = dir_without_dots(fullfile(raw_preop_dir,'*.nii.gz'));
     preop_files = {preop_files.name};
     preop_modalities = {};
     for comparing_files = 1:length(preop_files)
@@ -891,8 +860,8 @@ function generate_rawImagejson(patient_name,dest)
         preop_mod = preop_mod{end};
         preop_modalities{end+1} = preop_mod;
     end
-    coreg_t1 = dir(fullfile(dest,'derivatives','leaddbs',patient_name,'coregistration','anat','sub-*_ses-preop_space-anchorNative_*_acq-*_T1w.nii'));
-    rawdata_t1 = dir(fullfile(dest,'rawdata',patient_name,'ses-preop','anat','sub-*_ses-preop_acq-*_T1w.nii'));
+    coreg_t1 = dir(fullfile(coreg_dir,'sub-*_ses-preop_space-anchorNative_*_acq-*_T1w.nii'));
+    rawdata_t1 = dir(fullfile(raw_preop_dir,'sub-*_ses-preop_acq-*_T1w.nii'));
     if ~isempty(coreg_t1) && isempty(rawdata_t1)
         transfer_t1 = 1;
     else
@@ -900,7 +869,7 @@ function generate_rawImagejson(patient_name,dest)
     end
     if isempty(preop_files) || transfer_t1
         %other preop files
-        coreg_preop_files = dir(fullfile(dest,'derivatives','leaddbs',patient_name,'coregistration','anat','sub-*_ses-preop_space-anchorNative_*_acq-*.nii'));
+        coreg_preop_files = dir(fullfile(coreg_dir,'sub-*_ses-preop_space-anchorNative_*_acq-*.nii'));
         coreg_preop_files = {coreg_preop_files.name};
         for coreg_files = 1:length(coreg_preop_files)
             coreg_file = regexprep(coreg_preop_files{coreg_files}, '(.nii)|(.gz)', '');
@@ -941,7 +910,7 @@ function generate_rawImagejson(patient_name,dest)
         end
     end
     
-    postop_files = dir_without_dots(fullfile(dest,'rawdata',patient_name,'ses-postop','anat','*.nii.gz'));
+    postop_files = dir_without_dots(fullfile(raw_postop_dir,'*.nii.gz'));
     postop_files = {postop_files.name};
     postop_modalities = {};
     for comparing_files = 1:length(postop_files)
@@ -951,7 +920,7 @@ function generate_rawImagejson(patient_name,dest)
         postop_modalities{end+1} = postop_mod;
     end
     if isempty(postop_files)
-        coreg_postop_files = dir(fullfile(dest,'derivatives','leaddbs',patient_name,'coregistration','anat','sub-*_ses-postop_space-anchorNative_desc-preproc_*.nii'));
+        coreg_postop_files = dir(fullfile(coreg_dir,'sub-*_ses-postop_space-anchorNative_desc-preproc_*.nii'));
         coreg_postop_files = {coreg_postop_files.name};
         for coreg_files = 1:length(coreg_postop_files)
             coreg_file = regexprep(coreg_postop_files{coreg_files}, '(.nii)|(.gz)', '');
@@ -987,6 +956,8 @@ function generate_rawImagejson(patient_name,dest)
         end
     end
     savejson('',anat_files_selected,opt);
+    
+    
 
 function bids_name = add_tag(try_bids_name,mod_cell,tag_cell)
     bids_mod = strsplit(try_bids_name,'_');
@@ -1095,7 +1066,7 @@ function model_name = add_model(stimFolder)
         model_name = 'simbio';
     end
 return
-function generate_bidsConnectome_name(mni_folder,connectome_folder,lead_mapper,tag_struct)
+function generate_bidsConnectome_name(mni_folder,connectome_folder,lead_mapper,stimulations,tag_struct)
    %connectome_folder should be the full path of the connectome files,
    %mni_folder =
    %fullfile(new_path,pipeline,'MNI152NLinAsym','stimulations',stimulation
@@ -1104,9 +1075,9 @@ function generate_bidsConnectome_name(mni_folder,connectome_folder,lead_mapper,t
    mapper_output_files = {mapper_output_files(~[mapper_output_files.isdir]).name};
    
    for mapper_file = 1:length(mapper_output_files)
-       matching_file = regexprep(mapper_output_files{mapper_file},'(fl_|efield_|efield_gauss)', '');
-       matching_file = ['_' matching_file];
-       if ismember(matching_file,lead_mapper{:,1})
+       matching_stim_file = regexprep(mapper_output_files{mapper_file},'(fl_|efield_|efield_gauss)', '');
+       matching_lm_file = ['_' matching_stim_file];
+       if ismember(matching_lm_file,lead_mapper{:,1})
            if contains(mapper_output_files{mapper_file},'efield')
                tag_struct.simtag = 'efield';
            elseif contains(mapper_output_files{mapper_file},'efield_gauss')
@@ -1116,7 +1087,7 @@ function generate_bidsConnectome_name(mni_folder,connectome_folder,lead_mapper,t
            end
            extract_old_hemisdesc_str = strsplit(mapper_output_files{mapper_file},'_');
            
-           indx = cellfun(@(x)strcmp(x,matching_file),lead_mapper{:,1});
+           indx = cellfun(@(x)strcmp(x,matching_lm_file),lead_mapper{:,1});
            bids_name = lead_mapper{1,2}{indx};
            %replace hemidesc tag
            if strcmp(extract_old_hemisdesc_str{1},'fl')
@@ -1134,6 +1105,33 @@ function generate_bidsConnectome_name(mni_folder,connectome_folder,lead_mapper,t
            movefile(fullfile(mni_folder,mapper_output_files{mapper_file}),fullfile(mni_folder,[tag_struct.subjID,'_',bids_name]));
            fprintf('Renaming file %s as %s.\n',mapper_output_files{mapper_file},[tag_struct.subjID,'_',bids_name]);
            
+       elseif ismember(matching_stim_file,stimulations{:,1})
+            if contains(mapper_output_files{mapper_file},'efield')
+               tag_struct.simtag = 'efield';
+           elseif contains(mapper_output_files{mapper_file},'efield_gauss')
+               tag_struct.simtag = 'efieldgauss';
+           else %doesn't contain either
+               tag_struct.simtag = 'binary';
+           end
+           extract_old_hemisdesc_str = strsplit(mapper_output_files{mapper_file},'_');
+           
+           indx = cellfun(@(x)strcmp(x,matching_stim_file),stimulations{:,1});
+           bids_name = stimulations{1,2}{indx};
+           %replace hemidesc tag
+           if strcmp(extract_old_hemisdesc_str{1},'fl')
+               bids_name = strrep(bids_name,'flippedTag','flipped');
+           else
+               bids_name = strrep(bids_name,'_hemidesc-flippedTag','');
+           end
+           %replace sim tag
+           bids_name = strrep(bids_name,'simTag',tag_struct.simtag);
+           %replace model tag
+           bids_name = strrep(bids_name,'modelTag',tag_struct.modeltag);
+           %replace connectome tag
+           bids_name = strrep(bids_name,'conTag',tag_struct.conntag);
+           copyfile(fullfile(connectome_folder,mapper_output_files{mapper_file}),fullfile(mni_folder,mapper_output_files{mapper_file}));
+           movefile(fullfile(mni_folder,mapper_output_files{mapper_file}),fullfile(mni_folder,[tag_struct.subjID,'_',bids_name]));
+           fprintf('Renaming file %s as %s.\n',mapper_output_files{mapper_file},[tag_struct.subjID,'_',bids_name]);
        else
            evalin('base','WARNINGSILENT=1;');
            ea_warning(sprintf('BIDS tag could not be assigned for %s. Please rename manually',mapper_output_files{mapper_file}));

commit 33e66059c65c1d1d00aa74013b93319f84825a75

BUG: Fix regression introduced in 6b539a2 (legacy2bids interface).
Handle importing 'DICOM' folder (using parent folder as subjId). Was broken
due to recent changes. Need more neat way to set the flags in the app
@nrajamani3.

helpers/ea_lead_import.m
@@ -128,7 +128,12 @@ function [results,flag,dicom_conv,doMigrate,doOnlyRaw] = enablerules(selection,o
     results = 'pass';
     % detection function: if the patient has derivatives and
     % raw data, but not in BIDS format then it should be migrated.
-    if endsWith(filepath, 'sourcedata')
+    if options.isDICOMFolder
+        dicom_conv = 1;
+        doMigrate = 0;
+        doOnlyRaw = 0;
+        flag = 'onlyDCMconv';
+    elseif endsWith(filepath, 'sourcedata')
         % Input is sourcedata folder under a bids dataset
         dicom_conv = 1;
         doMigrate = 0;
@@ -192,6 +197,7 @@ function [results,flag,dicom_conv,doMigrate,doOnlyRaw] = enablerules(selection,o
     elseif ~isfolder(fullfile(selection,'DICOM')) && ~isfolder(fullfile(selection,'dicom'))
             %todo: test whether there are actually dicom files inside
             dicom_conv = 1;
+            doMigrate = 0;
             doOnlyRaw = 0;
             flag = 'onlyDCMconv';
     else

helpers/gui/ea_load_pts.m
@@ -9,6 +9,8 @@ uipatdir = GetFullPath(uipatdir);
 isSubjFolder = 0;
 isBIDSRoot = 0;
 
+isDICOMFolder = 0; % Will be true in case a 'DICOM' folder dragged in, will then only do dcm conversion, subjId will be the parent folder name
+
 if length(uipatdir) == 1 % Single folder
     if contains(uipatdir{1}, ['derivatives', filesep, 'leaddbs']) % Is patient folder under derivatives
         isSubjFolder = 1;
@@ -64,6 +66,7 @@ if length(uipatdir) == 1 % Single folder
         % DICOM folder detected
         elseif endsWith(uipatdir{1}, 'dicom', 'IgnoreCase',true)
             uipatdir = {fileparts(uipatdir{1})};
+            isDICOMFolder = 1;
             folder_type = 'patient_folder_dicom_folder';
 
         % DICOM folder detected inside the folder
@@ -159,6 +162,7 @@ if length(uipatdir) == 1 % Single folder
                 end
             case  'patient_folder_dicom_folder'
                 options.prefs = ea_prefs;
+                options.isDICOMFolder = isDICOMFolder;
                 msg = sprintf('DICOM folder found,\n should we run DICOM to NIfTI conversion?');
                 waitfor(ea_selectdataset(msg,handles.leadfigure));
                 dest_folder = getappdata(handles.leadfigure, 'BIDSRoot');