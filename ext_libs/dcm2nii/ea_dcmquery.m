465f20b2df19ae34820012ad9950d7a7be1c0a47
Ningfei Li <ningfei.li@gmail.com>
2022-10-28 13:53:59 +0200

    REF: Add list option in ea_querydcm.

--- ext_libs/dcm2nii/ea_dcmquery.m
+++ ext_libs/dcm2nii/ea_dcmquery.m
@@ -1,5 +1,12 @@
-function [numDICOM, isCompressed] = ea_dcmquery(inputFolder)
+function [numDICOM, fileList, isCompressed] = ea_dcmquery(inputFolder, queryOption)
 % Check the number of DICOM file(s) in the specified folder
+%
+% queryOption can be 'y' (only show number of DICOMs found) or 'l'(show
+% number of DICOMs found and list of DICOMs)
+
+if ~exist('queryOption', 'var')
+    queryOption = 'y';
+end
 
 basedir = fullfile(ea_getearoot, 'ext_libs', 'dcm2nii', filesep);
 
@@ -9,7 +16,7 @@ else
     dcm2niix = [basedir, 'dcm2niix.', computer('arch')];
 end
 
-cmd = [dcm2niix, ' -q y', ' ', ea_path_helper(inputFolder)];
+cmd = [dcm2niix, ' -q ', queryOption, ' ', ea_path_helper(inputFolder)];
 
 if ~ispc
     [status, cmdout] = system(['bash -c "', cmd, '"']);
@@ -17,6 +24,7 @@ else
     [status, cmdout] = system(cmd);
 end
 
+fileList = {};
 isCompressed = false;
 
 if status ~= 0
@@ -25,6 +33,11 @@ if status ~= 0
 else
     ea_cprintf('CmdWinWarnings', [regexp(strip(cmdout), '(?<=\n)Found[^\n]+', 'match', 'once'), '\n']);
     numDICOM = str2double(regexp(cmdout, '(?<=Found )\d+(?= DICOM)', 'match', 'once'));
+
+    if strcmpi(queryOption, 'l')
+        fileList = splitlines(regexp(cmdout, '(?<=List of DICOM file\(s\):\n).*(?=\nEnd of list)', 'match', 'once'));
+    end
+
     if contains(cmdout, 'Decompression', 'IgnoreCase', true)
         isCompressed = true;
     end
