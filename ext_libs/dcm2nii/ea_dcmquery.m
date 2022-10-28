d92fbc5b2ced7258b80e550909e9cfb277e72886
Ningfei Li <ningfei.li@gmail.com>
2022-10-28 08:51:39 +0200

    REF: Refine regexp for dcmquery.
    Add newline in CmdWinWarnings.
    Set isCompressed to logical.

--- ext_libs/dcm2nii/ea_dcmquery.m
+++ ext_libs/dcm2nii/ea_dcmquery.m
@@ -17,15 +17,15 @@ else
     [status, cmdout] = system(cmd);
 end
 
-isCompressed = 0;
+isCompressed = false;
 
 if status ~= 0
-    ea_cprintf('CmdWinWarnings', regexp(strip(cmdout), '(?<=\n)Error[^\r\n]+', 'match', 'once'));
+    ea_cprintf('CmdWinWarnings', [regexp(strip(cmdout), '(?<=\n)Error[^\n]+', 'match', 'once'), '\n']);
     numDICOM = 0;
 else
-    ea_cprintf('CmdWinWarnings', regexp(strip(cmdout), '(?<=\n)Found[^\r\n]+', 'match', 'once'));
+    ea_cprintf('CmdWinWarnings', [regexp(strip(cmdout), '(?<=\n)Found[^\n]+', 'match', 'once'), '\n']);
     numDICOM = str2double(regexp(cmdout, '(?<=Found )\d+(?= DICOM)', 'match', 'once'));
     if contains(cmdout, 'Decompression', 'IgnoreCase', true)
-        isCompressed = 1;
+        isCompressed = true;
     end
 end
