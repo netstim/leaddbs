a128ef6105916f9347e6532eb9629c0e112b512c
Konstantin Butenko <konstantin.butenko@charite.de>
2022-12-03 20:49:48 +0100

    UPD: Import Model enabled in GUI
    Also small fixes for legacy support and GUI adjustments

--- connectomics/discfibers/ea_save_fibscore_model.m
+++ connectomics/discfibers/ea_save_fibscore_model.m
@@ -1,24 +1,19 @@
-function ea_save_fibscore_model(obj, patientsel, training, Iperm)
+function ea_save_fibscore_model(obj, ExternalModelFile)
+
+    % this function is called from the GUI
+
+    % we want to define the model outside of cross-validation
+    if ~exist('patsel','var') % patsel can be supplied directly (in this case, obj.patientselection is ignored), e.g. for cross-validations.
+        patientsel = obj.patientselection;
+    end
 
     % get fiber model (vals) and corresponding indices (usedidx)
-    if ~exist('Iperm', 'var')
-        if obj.cvlivevisualize
-            [vals,fibcell,usedidx] = ea_discfibers_calcstats(obj, patientsel(training));
-            obj.draw(vals,fibcell,usedidx)
-            %obj.draw(vals,fibcell);
-            drawnow;
-        else
-            [vals,~,usedidx] = ea_discfibers_calcstats(obj, patientsel(training));
-        end
+    if obj.cvlivevisualize
+        [vals,fibcell,usedidx] = ea_discfibers_calcstats(obj, patientsel);
+        obj.draw(vals,fibcell,usedidx)
+        drawnow;
     else
-        if obj.cvlivevisualize
-            [vals,fibcell,usedidx] = ea_discfibers_calcstats(obj, patientsel(training), Iperm);
-            obj.draw(vals,fibcell,usedidx)
-            %obj.draw(vals,fibcell);
-            drawnow;
-        else
-            [vals,~,usedidx] = ea_discfibers_calcstats(obj, patientsel(training), Iperm);
-        end
+        [vals,~,usedidx] = ea_discfibers_calcstats(obj, patientsel);
     end
 
     % maps fiber model (vals) into the full connectome space
@@ -35,10 +30,41 @@ function ea_save_fibscore_model(obj, patientsel, training, Iperm)
         end
     end
 
+    % get Ihats and fit the linear model
+    % here we compute Ihats for all patietsel at once
+    disp('Computing the linear model...')
+    fibsval = full(obj.results.(ea_conn2connid(obj.connectome)).(ea_method2methodid(obj)).fibsval);
+    % Ihat is the estimate of improvements (not scaled to real improvements)
+    if strcmp(obj.multitractmode,'Single Tract Analysis')
+        Ihat = nan(length(patientsel),2);
+        Ihat_ph = nan(length(patientsel),2);
+    else
+        Ihat = nan(length(patientsel),2,length(obj.subscore.vars));
+        Ihat_ph = nan(length(patientsel),2,length(obj.subscore.vars));
+    end
+    training = 1:size(patientsel,2);
+    test = training;
+    Improvement = obj.responsevar(patientsel,:);
+    [Ihat, ~, ~,~] = ea_compute_fibscore_model(1, obj.adj_scaler, obj, fibsval, Ihat, Ihat_ph, patientsel, training, test);
+    predictor=squeeze(ea_nanmean(Ihat,2));
+    mdl=fitglm(predictor(training),Improvement(training),lower(obj.predictionmodel));
+    Intercept = mdl.Coefficients.Estimate(1);
+    Slope = mdl.Coefficients.Estimate(2);
+    plotName = 'Fitting of a linear model for the stored Fiber Model';
+    empiricallabel = 'Variable of Intrest';
+    pred_label = 'Fiber (Ihat) score';    
+    LM_values_slope = ['Slope: ' num2str(Slope)];
+    LM_values_intercept = ['Intercept: ' num2str(Intercept)];
+    ftr.mdl = mdl;
+
+    h=ea_corrbox(Improvement,predictor,'permutation',{plotName,empiricallabel,pred_label, plotName, LM_values_slope, LM_values_intercept});
+
     % add the connectome name for recognition
     ftr.vals_all = vals_all;
     ftr.connectome = ea_conn2connid(obj.connectome);
+    ftr.conn_type = obj.connectivity_type;
     ftr.fibsvalType = ea_method2methodid(obj);
-    save([obj.M.root,'vals_all_model.mat'], '-struct', 'ftr');
+
+    save(ExternalModelFile, '-struct', 'ftr');
 
 end
