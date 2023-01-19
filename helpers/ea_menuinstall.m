3750bc2e728d6e2be95d6a594f20edb1b046a282
Ningfei Li <ningfei.li@gmail.com>
2023-01-19 23:28:12 +0100

    REF: Update menu check status after successful installation.

--- helpers/ea_menuinstall.m
+++ helpers/ea_menuinstall.m
@@ -1,4 +1,4 @@
-function ea_menuinstall(~,~,cmd)
+function ea_menuinstall(src, ~, cmd)
 
 choice=questdlg(['Please confirm to start downloading: ',cmd],'Download additional data','Proceed','Cancel','Proceed');
 
@@ -9,9 +9,10 @@ end
 success=ea_checkinstall(cmd);
 
 if ~(success==-1) % user aborted.
-if ~success
-    errordlg([cmd,' dataset could not be installed.']);
-else
-    msgbox([cmd,' successfully installed.']);
-end
+    if ~success
+        errordlg([cmd,' dataset could not be installed.']);
+    else
+        msgbox([cmd,' successfully installed.']);
+        src.Checked = 1;
+    end
 end
