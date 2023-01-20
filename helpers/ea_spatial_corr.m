1c68278c2c607dd87ba18b41c8531dad98599836
Min Jae Kim <51182118+peterpan0716@users.noreply.github.com>
2023-01-20 15:02:27 -0500

    Add files via upload

--- helpers/ea_spatial_corr.m
+++ helpers/ea_spatial_corr.m
@@ -1,59 +1,48 @@
-function [rho,pval] = ea_spatial_corr(ROI1,ROI2,method)
+function [rho,pval] = ea_spatial_corr(ROI1_fname,ROI2_fname,method)
 % This code calculates spatial correlation between two binary / non-binary
 % ROIS
-ROI_1_tit=char(ROI1); % Path of the 1st image (First Image is Template) 
+ROI_1_tit=char(ROI1_fname); % Path of the 1st image (First Image is Template) 
 ROI_1=ea_load_nii(ROI_1_tit);
+%r_ROI_1_tit=char("re_"+ROI_1_tit);
 
-ROI_2_tit=char(ROI2); % Path of the 2nd image (First Image is Template) 
-ROI_2=ea_load_nii(ROI_2_tit); 
+ROI_2_tit=char(ROI2_fname); % Path of the 2nd image (First Image is Template) 
+r_ROI_2_tit=char("re_"+ROI_2_tit);
 
 %% Reslicing 2nd Image to Template Space (First Image)
-ea_reslice_nii(ROI_2_tit,ROI_2_tit,ROI_1.voxsize);
-ROI_2=ea_load_nii(ROI_2_tit);
-%% Checking If NaN Exists (Transforming 0's in Binary Volumes to NaNs)
-if isempty(find(isnan(ROI_1.img))) % If NaN 
-    mini=min(ROI_1.img(:));
-    if mini==0
-        ROI_1.img(find(ROI_1.img==0))=NaN;
-    end 
-end 
-
-if isempty(find(isnan(ROI_2.img))) % If NaN 
-    mini=min(ROI_2.img(:));
-    if mini==0
-        ROI_2.img(find(ROI_2.img==0))=NaN;
-    end 
-end 
+ea_reslice_nii(ROI_2_tit,r_ROI_2_tit,ROI_1.voxsize);
+ROI_2=ea_load_nii(r_ROI_2_tit);
 %% Transforming from Voxel to World Space 
-nii_2=ROI_1;
-mat_2=nii_2.mat;
+%max_val=max(nii.img(:));
+%min_val=min(nii.img(:));
 
-hand=cell(2,1);
-hand{1,1}=ROI_1_tit;
-hand{2,1}=ROI_2_tit;
+if sum(isnan(ROI_2.img(:)))==0
+    max_val=max(ROI_2.img(:));
+    min_val=min(ROI_2.img(:));
+    [X,Y,Z]=ind2sub(size(ROI_2.img),find(ROI_2.img(:)>=min_val));
+elseif sum(isnan(ROI_2.img(:)))~=0
+    [X,Y,Z]=ind2sub(size(ROI_2.img),find(~isnan(ROI_2.img(:))));
+end 
 
-pcavox_cell=cell(2,1);
-for kk=1:2
-nii=ea_load_nii(hand{kk,1}); 
-max_val=max(nii.img(:));
-min_val=min(nii.img(:));
-[X,Y,Z]=ind2sub(size(nii.img),find(nii.img(:)>min_val));
+%[X,Y,Z]=ind2sub(size(ROI_2.img),find(~isnan(ROI_2.img(:))));
 XYZvox=[X,Y,Z,ones(size(X,1),1)];
-XYZmm=nii.mat*XYZvox';
+XYZmm=ROI_2.mat*XYZvox';
 %% Transforming Back from World Space to Voxel Space 
-pcavox = zeros(nii_2.dim);
+mat_2=ROI_1.mat;
+pcavox = NaN(ROI_1.dim);
 transf=inv(mat_2);
 for i = 1:size(XYZmm,2)
     coors=transf*XYZmm(:,i);
     coors=floor(coors(1:3));
-    pcavox(coors(1),coors(2),coors(3))=1;  
-end 
-pcavox_cell{kk,1}=pcavox;
+    pcavox(coors(1),coors(2),coors(3))=ROI_2.img(X(i),Y(i),Z(i));  
 end 
 %% Calculating Spatial Correlation
-vec1=pcavox_cell{1,1};
-vec2=pcavox_cell{2,1};
-[rho,pval]=corr(vec1(:),vec2(:),'Type',method,'Rows','complete');
-disp("R-Value is "+string(rho)+", and p-value is "+string(pval)+". Method used is "+string(method))
+vec1=ROI_1.img(:);
+vec2=pcavox(:);
 
+idxi_rmv=find(isnan(vec2));
+vec1(idxi_rmv)=[];
+vec2(idxi_rmv)=[];
+
+[rho,pval]=corr(vec1,vec2,'Type',method);
+disp("R-Value is "+string(rho)+", and p-value is "+string(pval)+". Method used is "+string(method))
 end 
