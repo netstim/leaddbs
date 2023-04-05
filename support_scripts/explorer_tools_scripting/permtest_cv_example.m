d032654ef26ea9f6070409b9b266383d46aa1ff3
Andreas Horn <andreas.horn@charite.de>
2023-04-05 17:34:25 -0400

    ADD support script examples

---
+++ support_scripts/explorer_tools_scripting/permtest_cv_example.m
@@ -0,0 +1,56 @@
+clear
+lead path
+load('path/to/fibfilt.fibfilt','-mat'); % change to your fibfilt name
+
+nperm=1000; % number of permutations
+corrtype='Spearman'; % correlation type
+
+
+
+% k-fold
+for kfold=[5,10] % run it for 5 fold and 10-fold crossvalidations
+    tractset.kfold=kfold;
+
+    tractset.multitractmode = 'Single Tract Analysis';
+    rng(tractset.rngseed);
+    cvp = cvpartition(length(tractset.patientselection),'KFold',tractset.kfold);
+    ea_dispercent(0,[num2str(kfold),'-fold: Iterating permutations']);
+    R=zeros(nperm+1,1);
+    for perm=0:nperm
+        if perm % actual permutation run
+            tractset.responsevar=tractset.responsevar(randpermute(length(tractset.responsevar))); % permute improvements
+            [I,Ihat] = crossval(tractset,cvp,[],0,silent);
+            R(perm+1)=corr(I,Ihat,'rows','pairwise','type',corrtype);
+            ea_dispercent(0/nperm);
+        end
+    end
+    ea_dispercent(1,'end');
+    [h,p]=ea_plothistperm([num2str(kfold),'-fold: Null-Distribution'],R,'Unpermuted Set',1);
+    h.Position(3:4)=[1400,200]; % make figure long
+    saveas(h,[num2str(kfold),'fold_nulldistribution.png']);
+    saveas(h,[num2str(kfold),'fold_nulldistribution.fig']);
+    saveas(h,[num2str(kfold),'fold_nulldistribution.eps']);
+    close(h);
+end
+
+% leave one out
+
+rng(obj.rngseed);
+cvp = cvpartition(length(obj.patientselection), 'LeaveOut');
+    ea_dispercent(0,['Leave-One-Out: Iterating permutations']);
+    R=zeros(nperm+1,1);
+    for perm=0:nperm
+        if perm % actual permutation run
+            tractset.responsevar=tractset.responsevar(randpermute(length(tractset.responsevar))); % permute improvements
+            [I,Ihat] = crossval(tractset,cvp,[],0,silent);
+            R(perm+1)=corr(I,Ihat,'rows','pairwise','type',corrtype);
+            ea_dispercent(0/nperm);
+        end
+    end
+    ea_dispercent(1,'end');
+    [h,p]=ea_plothistperm(['Leave-One-Out: Null-Distribution'],R,'Unpermuted Set',1);
+    h.Position(3:4)=[1400,200]; % make figure long
+    saveas(h,['loo_nulldistribution.png']);
+    saveas(h,['loo_nulldistribution.fig']);
+    saveas(h,['loo_nulldistribution.eps']);
+    close(h);
