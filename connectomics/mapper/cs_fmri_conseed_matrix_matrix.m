856f17c1177d6643099c68ba4e8c49d8307c6aad
Ningfei Li <ningfei.li@gmail.com>
2023-04-17 21:08:58 +0800

    REF: Optimize connmat calc perf when using matrix connectome.
    Also save roi used in the result.
    Add more doc comments.

--- connectomics/mapper/cs_fmri_conseed_matrix_matrix.m
+++ connectomics/mapper/cs_fmri_conseed_matrix_matrix.m
@@ -1,5 +1,5 @@
 function cs_fmri_conseed_matrix_matrix(dfold,cname,sfile,outputfolder)
-% Create fMRI connectivity matrix based on matrix type of connectome
+% ROI based fMRI connectivity matrix based on matrix type of connectome
 
 tic
 
@@ -13,17 +13,20 @@ end
 
 disp(['Connectome dataset: ',cname,'.']);
 if contains(cname, '>')
-    subset = regexprep(cname, '.*> *', '');
     cname = regexprep(cname, ' *>.*', '');
 end
+
+connLabel = ea_getConnLabel(cname, 'FullSet');
 
 try
     options = evalin('caller','options');
     if strcmp(options.lcm.seeddef, 'parcellation')
         prefix = 'parc_';
-        roi = options.lcm.parcSeedName;
+        % Parcellation as roi definition
+        roi = [fileparts(options.lcm.parcSeedFolder), '.nii'];
     else
         prefix = 'seed_';
+        % Seeds as roi definition 
         roi = sfile;
     end
 catch
@@ -31,59 +34,11 @@ catch
     roi = sfile;
 end
 
-connLabel = ea_getConnLabel(cname, 'FullSet');
-
-dataset = load([dfold,'fMRI',filesep,cname,filesep,'dataset_volsurf.mat']);
-dataset.connLabel = connLabel;
-dinfo = loadjson([dfold,'fMRI',filesep,cname,filesep,'dataset_info.json']);
-dataset.type = dinfo.type;
-
 if isempty(outputfolder)
-    warning('off', 'backtrace');
-    warning('Custom output folder not specified! Will save result to current folder.');
-    warning('on', 'backtrace');
-    outputfolder = [pwd, filesep];
-elseif ~strcmp(outputfolder(end),filesep)
-    outputfolder = [outputfolder,filesep];
+    ea_cprintf('CmdWinWarnings', 'Custom output folder not specified! Will save result to current folder.\n');
+    outputfolder = pwd;
 end
 
-if length(sfile) == 1 % Manually chosen parcellation: single NIfTI
-    parc = ea_load_nii(sfile{1});
-    roiID = unique(parc.img(parc.img~=0));
-    roiInd = cell(size(roiID));
-    for i=1:length(roiID)
-        roiInd{i} = intersect(find(parc.img==roiID(i)), dataset.vol.outidx);
-        roiInd{i} = sort(arrayfun(@(x) find(dataset.vol.outidx==x), roiInd{i}));
-    end
-else % Manually chosen seeds: multiple NIfTIs or text file with NIfTI entries
-    [~, roiID] = cellfun(@ea_niifileparts, sfile, 'UniformOutput', false);
-    roiInd = cell(size(roiID));
-    sfile = ea_resolveseeds(sfile, dataset.vol.space);
-    for i=1:length(sfile)
-        seed = ea_load_nii(sfile{i});
-        roiInd{i} = intersect(find(seed.img==1), dataset.vol.outidx);
-        roiInd{i} = sort(arrayfun(@(x) find(dataset.vol.outidx==x), roiInd{i}));
-    end
-end
-
-db = matfile([dfold,'fMRI',filesep,cname,filesep,'AllX.mat'], 'Writable', false);
-connMat = nan(length(roiID));
-
-for i=1:length(roiID)
-    for j=i:length(roiID)
-        block = zeros(length(roiInd{i}), length(roiInd{j}));
-        for r=1:size(block,1)
-            for c=1:size(block,2)
-                block(r,c) = db.X(roiInd{i}(r), roiInd{j}(c));
-            end
-        end
-        connMat(i, j) = mean(block, 'all');
-    end
-end
-
-conn.roi = roi;
-conn.roiID = roiID;
-conn.connMat = connMat + triu(connMat', 1);
 outputFile = fullfile(outputfolder, [prefix, 'conn-', connLabel, '_funcmatrix.mat']);
 i = 1; 
 while isfile(outputFile)
@@ -91,7 +46,64 @@ while isfile(outputFile)
     i = i + 1;
 end
 
-ea_cprintf('CmdWinWarnings', 'Saved to %s\n', outputFile);
+dataset = load([dfold,'fMRI',filesep,cname,filesep,'dataset_volsurf.mat']);
+dataset.connLabel = connLabel;
+dinfo = loadjson([dfold,'fMRI',filesep,cname,filesep,'dataset_info.json']);
+dataset.type = dinfo.type;
+
+if length(sfile) == 1 % Manually chosen parcellation: single NIfTI
+    parc = ea_load_nii(sfile{1});
+    % Unique value in parcellation as roiID
+    roiID = unique(parc.img(parc.img~=0));
+    roiInd = cell(size(roiID));
+    for i=1:length(roiID)
+        % Intersect roi indices and connectome outidx
+        roiInd{i} = intersect(find(parc.img==roiID(i)), dataset.vol.outidx);
+        % Find corresponding indices of connectome X matrix
+        roiInd{i} = sort(arrayfun(@(x) find(dataset.vol.outidx==x), roiInd{i}));
+    end
+else % Manually chosen seeds: multiple NIfTIs or text file with NIfTI entries
+    % NIfTI file name as roiID
+    [~, roiID] = cellfun(@ea_niifileparts, sfile, 'UniformOutput', false);
+    roiInd = cell(size(roiID));
+    sfile = ea_resolveseeds(sfile, dataset.vol.space);
+    for i=1:length(sfile)
+        seed = ea_load_nii(sfile{i});
+        % Intersect roi indices and connectome outidx
+        roiInd{i} = intersect(find(seed.img==1), dataset.vol.outidx);
+        % Find corresponding indices of connectome X matrix
+        roiInd{i} = sort(arrayfun(@(x) find(dataset.vol.outidx==x), roiInd{i}));
+    end
+end
+
+db = matfile([dfold,'fMRI',filesep,cname,filesep,'AllX.mat'], 'Writable', false);
+connMat = zeros(length(roiID));
+
+for i=1:length(roiID)
+    for j=i:length(roiID)
+        % First read a larger block from connectome X matrix since ranges
+        % for MatFile object must increase in equally spaced intervals
+        rowBlockInd = roiInd{i}(1):roiInd{i}(end);
+        colBlockInd = roiInd{j}(1):roiInd{j}(end);
+        blockConnMat = db.X(rowBlockInd, colBlockInd);
+
+        % Get the real (smaller) connectivity matrix block
+        rowInd = arrayfun(@(x) find(rowBlockInd==x), roiInd{i});
+        colInd = arrayfun(@(x) find(colBlockInd==x), roiInd{j});
+        blockConnMat = blockConnMat(rowInd, colInd);
+
+        % Grand mean of the connectivity matrix block
+        connMat(i, j) = mean(blockConnMat, 'all');
+    end
+end
+
+conn.roi = roi;
+conn.roiID = roiID;
+
+% Symmetrize the roi based connectivity matrix
+conn.connMat = connMat + tril(connMat', -1);
+
+ea_cprintf('CmdWinWarnings', 'ROI based connectivity matrix saved to:\n%s\n', outputFile);
 save(outputFile, '-struct', 'conn', '-v7.3');
 
 toc
