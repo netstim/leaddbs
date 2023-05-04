b59222183b90d196272dcfea3b488f0e77367356
Ningfei Li <ningfei.li@gmail.com>
2023-05-04 11:47:49 +0200

    BUG: Fix checking 2D export existence.

--- tools/export/pat2pdf/ea_pat2pdf.m
+++ tools/export/pat2pdf/ea_pat2pdf.m
@@ -12,7 +12,7 @@ options.d3.expdf=1;
 options.d3.verbose='off';
 
 if ~ea_checkslicespresent(options)
-    cuts=ea_writeplanes(options);
+    ea_writeplanes(options);
 end
 
 ea_elvis(options);
@@ -22,21 +22,46 @@ movefile([fname, '.u3d'], [uipatdir, filesep, 'export', filesep, 'pdf']);
 movefile([fname, '.pdf'], [uipatdir, filesep, 'export', filesep, 'pdf']);
 
 
-function present=ea_checkslicespresent(options)
-present=1;
-directory=[options.root,options.patientname,filesep];
-for el=1:length(options.elspec.contactnames)
-    for tracor=1:3
-        switch tracor
-            case 1
-                ppend='_axial.png';
-            case 2
-                ppend='_coronal.png';
-            case 3
-                ppend='_sagittal.png';
-        end
-        if ~exist([directory,options.elspec.contactnames{el},ppend],'file');
-            present=0;
-        end
+function present = ea_checkslicespresent(options)
+present = 1;
+fileprefix = fullfile(options.root,options.patientname,'export', '2D', [options.patientname, '_desc-2D_view-']);
+
+for tracor=1:3
+    switch tracor
+        case 1
+            view = 'ax_';
+            for elcnt=1:(options.elspec.numel)
+                el = [elcnt, elcnt + options.elspec.numel];
+                contactTag = strjoin(options.elspec.contactnames(el), '');
+                contactTag = erase(contactTag, {' ', '(', ')'});
+
+                if ~isfile([fileprefix, view, contactTag, '.png'])
+                    present=0;
+                    return;
+                end
+            end
+        case 2
+            view = 'cor_';
+            for elcnt=1:(options.elspec.numel)
+                el = [elcnt, elcnt + options.elspec.numel];
+                contactTag = strjoin(options.elspec.contactnames(el), '');
+                contactTag = erase(contactTag, {' ', '(', ')'});
+
+                if ~isfile([fileprefix, view, contactTag, '.png'])
+                    present=0;
+                    return;
+                end
+            end
+        case 3
+            view = 'sag_';
+            for el=1:length(options.elspec.contactnames)
+                contactTag = strjoin(options.elspec.contactnames(el), '');
+                contactTag = erase(contactTag, {' ', '(', ')'});
+
+                if ~isfile([fileprefix, view, contactTag, '.png'])
+                    present=0;
+                    return;
+                end
+            end
     end
 end
