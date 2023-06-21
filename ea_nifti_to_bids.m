aa62fe70d00f2f63ea4adbf33560ced728b9d6ba
Ningfei Li <ningfei.li@gmail.com>
2023-06-21 22:14:07 +0200

    REF: Use func handle from nii_viewer I/O hard-coded func in nifti2bids.

--- ea_nifti_to_bids.m
+++ ea_nifti_to_bids.m
@@ -51,6 +51,7 @@ N_fnames = length(niiFiles);
 imgs = cell(N_fnames,1);
 imgs_resolution = cell(N_fnames,1);
 h_wait = waitbar(0, 'Please wait while NIfTI images are being loaded');
+read_nii = nii_viewer('func_handle', 'read_nii');
 for image_idx = 1:N_fnames
     imgs{image_idx} = struct();
     [imgs{image_idx}.p, imgs{image_idx}.frm, imgs{image_idx}.rg, imgs{image_idx}.dim] = read_nii(niiFiles{image_idx}, [], 0);
@@ -1000,78 +1001,8 @@ end
 axesTag = [];
 end
 
-function [p, frm, rg, dim] = read_nii(fname, ask_code, reOri)
-if nargin<2, ask_code = []; end
-if ischar(fname), p.nii = nii_tool('load', fname);
-else, p.nii = fname; fname = p.nii.hdr.file_name;
-end
-p.hdr0 = p.nii.hdr; % original hdr
-c = p.nii.hdr.intent_code;
-if c>=3000 && c<=3099 && isfield(p.nii, 'ext') && any([p.nii.ext.ecode] == 32)
-    p.nii = cii2nii(p.nii);
-end
-
-if nargin<3 || reOri
-    [p.nii, p.perm, p.flip] = nii_reorient(p.nii, 0, ask_code);
-else
-    p.perm = 1:3;
-    p.flip = false(1,3);
-end
-
-dim = p.nii.hdr.dim(2:8);
-dim(dim<1 | mod(dim,1)~=0) = 1;
-if p.nii.hdr.dim(1)>4 % 4+ dim, put all into dim4
-    if sum(dim(4:7)>1)>1
-        warndlg([fname ' has 5 or more dimension. Dimension above 4 are ' ...
-            'all treated as volumes for visualization']);
-    end
-    dim(4) = prod(dim(4:7)); dim(5:7) = 1;
-    p.nii.img = reshape(p.nii.img, [dim size(p.nii.img, 8)]);
-end
-
-[p.R, frm] = nii_xform_mat(p.nii.hdr, ask_code);
-dim = dim(1:3);
-p.pixdim = p.nii.hdr.pixdim(2:4);
-
-if size(p.nii.img,4)<4 && ~isfloat(p.nii.img)
-    p.nii.img = single(p.nii.img);
-end
-if p.nii.hdr.scl_slope==0, p.nii.hdr.scl_slope = 1; end
-if p.nii.hdr.scl_slope~=1 || p.nii.hdr.scl_inter~=0
-    if isfloat(p.nii.img)
-        p.nii.img = p.nii.img * p.nii.hdr.scl_slope + p.nii.hdr.scl_inter;
-    else
-        p.scl_slope = p.nii.hdr.scl_slope;
-        p.scl_inter = p.nii.hdr.scl_inter;
-    end
-end
-
-% check if ROI labels available: the same file name with .txt extension
-if c == 1002 % Label
-    [pth, nam, ext] = fileparts(p.nii.hdr.file_name);
-    nam1 = fullfile(pth, [nam '.txt']);
-    if strcmpi(ext, '.gz') && ~exist(nam1, 'file')
-        [~, nam] = fileparts(nam);
-        nam1 = fullfile(pth, [nam '.txt']);
-    end
-    if exist(nam1, 'file') % each line format: 1 ROI_1
-        fid = fopen(nam1);
-        while 1
-            ln = fgetl(fid);
-            if ~ischar(ln), break; end
-            [ind, a] = strtok(ln);
-            ind = str2double(ind);
-            try p.labels{ind} = strtrim(a); catch, end
-        end
-        fclose(fid);
-    end
-end
-rg = get_range(p.nii, isfield(p, 'labels'));
-try, p.map = p.nii.NamedMap{1}.map; end
-end
 
 %% helper functions for lookup_table
-
 function lookup_table_data = convert_lookup_struct_to_table(lookup_table)
 types = fieldnames(lookup_table);
 
@@ -1114,155 +1045,12 @@ for type_idx = 1:length(types)
             temp_table.Modality = string(modality);
 
             lookup_table_data = [lookup_table_data; temp_table];
-
         end
     end
 end
 
 end
 
-%% helper functions for nii reading
-function [nii, perm, flp] = nii_reorient(nii, leftHand, ask_code)
-if nargin<3, ask_code = []; end
-[R, frm] = nii_xform_mat(nii.hdr, ask_code);
-dim = nii.hdr.dim(2:4);
-pixdim = nii.hdr.pixdim(2:4);
-[R, perm, flp] = reorient(R, dim, leftHand);
-fps = bitand(nii.hdr.dim_info, [3 12 48]) ./ [1 4 16];
-if ~isequal(perm, 1:3)
-    nii.hdr.dim(2:4) = dim(perm);
-    nii.hdr.pixdim(2:4) = pixdim(perm);
-    nii.hdr.dim_info = [1 4 16] * fps(perm)' + bitand(nii.hdr.dim_info, 192);
-    nii.img = permute(nii.img, [perm 4:8]);
-end
-sc = nii.hdr.slice_code;
-if sc>0 && flp(fps==3)
-    nii.hdr.slice_code = sc+mod(sc,2)*2-1; % 1<->2, 3<->4, 5<->6
-end
-if isequal(perm, 1:3) && ~any(flp), return; end
-if frm(1) == nii.hdr.sform_code % only update matching form
-    nii.hdr.srow_x = R(1,:);
-    nii.hdr.srow_y = R(2,:);
-    nii.hdr.srow_z = R(3,:);
-end
-if frm(1) == nii.hdr.qform_code
-    nii.hdr.qoffset_x = R(1,4);
-    nii.hdr.qoffset_y = R(2,4);
-    nii.hdr.qoffset_z = R(3,4);
-    R0 = normc(R(1:3, 1:3));
-    dcm2quat = dicm2nii('', 'dcm2quat', 'func_handle');
-    [q, nii.hdr.pixdim(1)] = dcm2quat(R0);
-    nii.hdr.quatern_b = q(2);
-    nii.hdr.quatern_c = q(3);
-    nii.hdr.quatern_d = q(4);
-end
-for i = find(flp), nii.img = flip(nii.img, i); end
-
-
-end
-
-function [R, frm] = nii_xform_mat(hdr, ask_code)
-% [R, form] = nii_xform_mat(hdr, asked_code);
-% Return the transformation matrix from a NIfTI hdr. By default, this returns
-% the sform if available. If the optional second input, required form code, is
-% provided, this will try to return matrix for that form code. The second
-% optional output is the form code of the actually returned matrix.
-fs = [hdr.sform_code hdr.qform_code]; % sform preferred
-if fs(1)==fs(2), fs = fs(1); end % sform if both are the same
-f = fs(fs>=1 & fs<=4); % 1/2/3/4 only
-if isempty(f) || ~strncmp(hdr.magic, 'n', 1) % treat it as Analyze
-    frm = 0;
-    try % try spm style Analyze
-        [pth, nam, ext] = fileparts(hdr.file_name);
-        if strcmpi(ext, '.gz'), [~, nam] = fileparts(nam); end
-        R = load(fullfile(pth, [nam '.mat']));
-        R = R.M;
-    catch % make up R for Analyze: suppose xyz order with left storage
-        R = [diag(hdr.pixdim(2:4)) -(hdr.dim(2:4).*hdr.pixdim(2:4)/2)'; 0 0 0 1];
-        R(1,:) = -R(1,:); % use left handed
-    end
-    return;
-end
-
-if numel(f)==1 || nargin<2 || isempty(ask_code) % only 1 avail or no ask_code
-    frm = f;
-else % numel(f) is 2, numel(ask_code) can be 1 or 2
-    frm = f(f == ask_code(1));
-    if isempty(frm) && numel(ask_code)>1, frm = f(f == ask_code(2)); end
-    if isempty(frm) && any(f==2), frm = 2; end % use confusing code 2
-    if isempty(frm), frm = f(1); end % no match to ask_code, use sform
-end
-
-if frm(1) == fs(1) % match sform_code or no match
-    R = [hdr.srow_x; hdr.srow_y; hdr.srow_z; 0 0 0 1];
-else % match qform_code
-    quat2R = nii_viewer('func_handle', 'quat2R');
-    R = quat2R(hdr);
-end
-end
-
-function [R, perm, flp] = reorient(R, dim, leftHand)
-% [R, perm, flip] = reorient(R, dim, leftHand)
-% Re-orient transformation matrix R (4x4), so it will be diagonal major and
-% positive at diagonal, unless the optional third input is true, which requires
-% left-handed matrix, where R(1,1) will be negative.
-% The second input is the img space dimension (1x3).
-% The perm output, like [1 2 3] or a permutation of it, indicates if input R was
-% permuted for 3 axis. The third output, flip (1x3 logical), indicates an axis
-% (AFTER perm) is flipped if true.
-a = abs(R(1:3,1:3));
-[~, ixyz] = max(a);
-if ixyz(2) == ixyz(1), a(ixyz(2),2) = 0; [~, ixyz(2)] = max(a(:,2)); end
-if any(ixyz(3) == ixyz(1:2)), ixyz(3) = setdiff(1:3, ixyz(1:2)); end
-[~, perm] = sort(ixyz);
-R(:,1:3) = R(:,perm);
-flp = R([1 6 11]) < 0; % diag(R(1:3, 1:3))
-if nargin>2 && leftHand, flp(1) = ~flp(1); end
-rotM = diag([1-flp*2 1]);
-rotM(1:3, 4) = (dim(perm)-1) .* flp; % 0 or dim-1
-R = R / rotM; % xform matrix after flip
-end
-
-%  Estimate lower and upper bound of img display
-function rg = get_range(nii, isLabel)
-if size(nii.img, 8)>2 || any(nii.hdr.datatype == [128 511 2304]) % RGB / RGBA
-    if max(nii.img(:))>2, rg = [0 255]; else, rg = [0 1]; end
-    return;
-elseif nii.hdr.cal_max~=0 && nii.hdr.cal_max>min(nii.img(:))
-    rg = [nii.hdr.cal_min nii.hdr.cal_max];
-    return;
-end
-
-img = nii.img(:,:,:,1);
-img = img(:);
-img(isnan(img) | isinf(img)) = [];
-if ~isreal(img), img = abs(img); end
-if ~isfloat(img)
-    slope = nii.hdr.scl_slope; if slope==0, slope = 1; end
-    img = single(img) * slope + nii.hdr.scl_inter;
-end
-
-mi = min(img); ma = max(img);
-if nii.hdr.intent_code > 1000 || (nargin>1 && isLabel)
-    rg = [mi ma]; return;
-end
-
-ind = abs(img)>50;
-if sum(ind)<numel(img)/10, ind = abs(img)>std(img)/2; end
-im = img(ind);
-mu = mean(im);
-sd = std(im);
-rg = mu + [-2 2]*sd;
-if rg(1)<=0, rg(1) = sd/5; end
-if rg(1)<mi || isnan(rg(1)), rg(1) = mi; end
-if rg(2)>ma || isnan(rg(2)), rg(2) = ma; end
-if rg(1)==rg(2), rg(1) = mi; if rg(1)==rg(2), rg(1) = 0; end; end
-% rg = round(rg, 2, 'significant'); % since 2014b
-rg = str2num(sprintf('%.2g ', rg)); %#ok<*ST2NM>
-if rg(1)==rg(2), rg(1) = mi; end
-if abs(rg(1))>10, rg(1) = floor(rg(1)/2)*2; end % even number
-if abs(rg(2))>10, rg(2) = ceil(rg(2)/2)*2; end % even number
-end
 
 %% misc helper functions
 function bids_fname = generate_bids_filename(subjID, session, run, task, acq, modality, phase_enc_dir)
@@ -1284,6 +1072,7 @@ else
 end
 
 end
+
 
 function ped = get_phase_encoding_direction(json_sidecar)
 
@@ -1319,8 +1108,5 @@ for i = 1:height(niitable.Data)
             && niitable.Data.Include(i) == 1
         dupl_row = i;
     end
-
 end
 end
-
-
