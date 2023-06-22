c7008afcea6aafa82fc2ee61f344c1ba411f73aa
Ningfei Li <ningfei.li@gmail.com>
2023-06-22 22:10:48 +0200

    ENH: opendir supports file path input.
    Will open parent folder with the file selected.

--- helpers/gui/ea_opendir.m
+++ helpers/gui/ea_opendir.m
@@ -1,20 +1,59 @@
-function ea_opendir(directory)
-% For single directory, change current directory to it and open it in OS
-% file manager. For multiple directories, open them in OS file manager.
+function ea_opendir(path, options)
+% For single directory, change current working directory and open it in OS
+% file manager.
+%
+% For single file, change current working directory to its parent folder
+% and open the folder in OS file manager with the file selected.
+%
+% For multiple directories, open them in OS file manager.
+%
+% for multiple files, open their parent folders in OS file manager with the
+% files selected.
+%
+% Do not change current working directory in case 'chdir' is set to false.
 
-if ischar(directory)    % Single directory
-    if ~isdeployed
-        cd(directory);
-    end
-    directory = {directory};
+arguments
+    path    {mustBeText}
+    options.chdir   {mustBeNumericOrLogical} = true
 end
 
-for d=1:length(directory)
+
+if ischar(path)    % Single directory/file
+    if ~isdeployed && options.chdir
+        if isfolder(path)
+            cd(path);
+        elseif isfile(path)
+            cd(fileparts(path));
+        end
+    end
+    path = {path};
+end
+
+cellfun(@opendir, path);
+
+
+function opendir(path)
+
+path = GetFullPath(path);
+
+if isfolder(path)
     if ismac
-        system(['open "', directory{d}, '"']);
+        system(['open "', path, '"']);
     elseif isunix
-        system(['xdg-open "', directory{d}, '"']);
+        system(['xdg-open "', path, '"']);
     elseif ispc
-        system(['explorer "', directory{d}, '"']);
+        system(['explorer "', path, '"']);
+    end
+elseif isfile(path)
+    if ismac
+        system(['open -R "', path, '"']);
+    elseif isunix
+        try
+            system(['gdbus call --session --dest org.freedesktop.FileManager1 --object-path /org/freedesktop/FileManager1 --method org.freedesktop.FileManager1.ShowItems "[''file://', path, ''']" ""']);
+        catch
+            ea_cprintf('CmdWinWarnings', 'Failed to open directory:\n%s\n', fileparts(path));
+        end
+    elseif ispc
+        system(['explorer /select,"', path, '"']);
     end
 end
