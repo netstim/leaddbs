fa000364722a26493f47adcd73d45919977072ae
Ningfei Li <ningfei.li@gmail.com>
2023-07-28 12:22:22 +0200

    REF: Refine submitcmd, handle output properly.

--- helpers/ea_submitcmd.m
+++ helpers/ea_submitcmd.m
@@ -1,7 +1,14 @@
-function status = ea_submitcmd(cmd)
+function varargout = ea_submitcmd(cmd)
 
-if ~ispc
-    status = system(['bash -c "', cmd, '"']);
-else
-    status = system(cmd);
+if isunix
+    cmd = ['bash -c "', cmd, '"'];
+end
+
+if nargout == 0
+    system(cmd);
+elseif nargout == 1
+    varargout{1} = system(cmd);
+elseif nargout == 2
+    [varargout{1}, varargout{2}] = system(cmd);
+    varargout{2} = strip(varargout{2});
 end
