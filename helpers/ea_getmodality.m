cf39e3c41bf00cd53a22f0ab309009d1e76f8ec1
Ningfei Li <ningfei.li@gmail.com>
2023-08-09 14:43:20 +0200

    FIX: getModality returns correct value for CT (acq skipped).
    Fixes #1059

--- helpers/ea_getmodality.m
+++ helpers/ea_getmodality.m
@@ -6,7 +6,7 @@ if ~exist('acq', 'var')
     acq = 1;
 end
 
-if ~acq % Skip plane label
+if ~acq || ~isempty(regexp(BIDSFilePath, '_CT\.nii(.gz)?$', 'once')) % Skip plane label
     modality = regexp(BIDSFilePath, '(?<=_)([^\W_]+)(?=\.nii(\.gz)?$)', 'match', 'once');
 else % Keep plane label
     modality = regexp(BIDSFilePath, '(?<=_acq-)((ax|sag|cor|iso)\d*_[^\W_]+)(?=\.nii(\.gz)?$)', 'match', 'once');
