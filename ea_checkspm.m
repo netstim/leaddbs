677179e39e583631bdff5ed6064f09ccc2727ec9
Ningfei Li <ningfei.li@gmail.com>
2023-08-15 19:05:36 +0200

    REF: Add patched spm cfg files on lead startup.

--- ea_checkspm.m
+++ ea_checkspm.m
@@ -13,12 +13,15 @@ if ~isdeployed
         if strcmp(computer('arch'), 'maca64') && contains(ME.message, 'MEX files')
             % Add maca64 MEX files from upstream
             ea_cprintf('CmdWinWarnings', 'Adding missing maca64 MEX files for SPM12...\n');
-            unzip(fullfile(ea_getearoot, 'ext_libs', 'spm_mexmaca64.zip'), fileparts(which('spm')));
+            unzip(fullfile(ea_getearoot, 'ext_libs', 'spm', 'spm_mexmaca64.zip'), fileparts(which('spm')));
         else
             rethrow(ME);
         end
     end
-    
+
+    % Patch SPM cfg files
+    ea_patch_spm;
+
     if ~any(ismember(ver(8:11),'.')) % old version format
         if str2double(ver(8:11))<6906
             msgbox('Some functions (such as SPM SHOOT and DARTEL) may not be available using your SPM version. Please upgrade SPM12 to at least revision 6906 (or simply update to newest release).');
