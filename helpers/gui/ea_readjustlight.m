79761cf85f939a71bcadd9dcd17907d9c54ca42a
Ningfei Li <ningfei.li@gmail.com>
2023-08-18 13:46:47 +0200

    REF: Set appdata after auto-adjust light.

--- helpers/gui/ea_readjustlight.m
+++ helpers/gui/ea_readjustlight.m
@@ -9,17 +9,21 @@ CamLight = getappdata(hfig, 'CamLight');
 prefs = ea_prefs;
 
 try
-    camlight(CamLight, 'headlight'); % move light object.
+    CamLight = camlight(CamLight, 'headlight'); % move light object.
+    setappdata(hfig, 'CamLight', CamLight);
 end
 
 try
     set(CeilingLight, 'Position', [0 0 10], 'style', 'local', 'Color', prefs.d3.ceilinglightcolor); % not modifiable, infinite light.
+    setappdata(hfig, 'CeilingLight', CeilingLight);
 end
 
 try
     set(RightLight, 'Position', [-100 0 0], 'style', 'infinite', 'Color', prefs.d3.rightlightcolor); % not modifiable, infinite light.
+    setappdata(hfig, 'RightLight', RightLight);
 end
 
 try
     set(LeftLight, 'Position', [100 0 0], 'style', 'infinite', 'Color', prefs.d3.leftlightcolor); % not modifiable, infinite light.
+    setappdata(hfig, 'LeftLight', LeftLight);
 end
