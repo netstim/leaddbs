c49ecca05aadc945e6aff01c2c8e1b96502be140
Ningfei Li <ningfei.li@gmail.com>
2023-08-20 11:19:00 +0200

    STYLE: cosmetic.

--- helpers/gui/ea_editprefs.m
+++ helpers/gui/ea_editprefs.m
@@ -3,23 +3,23 @@ function ea_editprefs(varargin)
 if ~exist([ea_gethome,'.ea_prefs',ea_prefsext],'file')
     copyfile([ea_getearoot,'common',filesep,'ea_prefs_default',ea_prefsext],[ea_gethome,'.ea_prefs',ea_prefsext]);
 end
+
 if ~isdeployed
     edit([ea_gethome,'.ea_prefs.m']);
 else
-
     msgbox('Open ~/.ea_prefs.json with a text editor to edit preferences.');
 
-%     prefs = ea_prefs;
-%     if ~isfield(prefs, 'textedit')
-%         mb = msgbox('Choose app to open .json file');
-%         uiwait(mb);
-%         file = uigetfile('/Applications/*.app');
-%         if file
-%             ea_setprefs('textedit', file(1:end-4), 'user');
-%             prefs = ea_prefs;
-%         else
-%             return
-%         end
-%     end
-%     system(['open -a ','''',prefs.textedit,'''',' ',fullfile(ea_gethome,'.ea_prefs.json')])
+    % prefs = ea_prefs;
+    % if ~isfield(prefs, 'textedit')
+    %     mb = msgbox('Choose app to open .json file');
+    %     uiwait(mb);
+    %     file = uigetfile('/Applications/*.app');
+    %     if file
+    %         ea_setprefs('textedit', file(1:end-4), 'user');
+    %         prefs = ea_prefs;
+    %     else
+    %         return
+    %     end
+    % end
+    % system(['open -a ','''',prefs.textedit,'''',' ',fullfile(ea_gethome,'.ea_prefs.json')])
 end
