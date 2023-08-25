ae72604c1de12d139a4b5de23859302c3c5fcf67
Konstantin Butenko <35195806+Kinway25@users.noreply.github.com>
2023-08-25 16:25:09 +0200

    FIX: check grounding on all VC sources

--- helpers/ea_getStimVector.m
+++ helpers/ea_getStimVector.m
@@ -50,6 +50,16 @@ if (current_control(1) == 0 && current_control(2) == 0) || (isnan(current_contro
     for i=1:eleNum
         for j=1:numSources
             amp(i,j) = S.amplitude{i}(j);
+
+            % check grounding for active sources
+            if amp(i,j) ~= 0
+                % if any source for the electrode is grounded, then all grounded
+                stimSource = S.([sideCode, 's', num2str(j)]);
+                if stimSource.case.perc == 100
+                    Case_grounding(i) = 1;
+                end
+            end
+            
         end
     end
 else
