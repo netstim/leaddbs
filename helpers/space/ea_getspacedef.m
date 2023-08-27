0990e27c9daa9c84454180cebe07e24388c9a5ae
Ningfei Li <ningfei.li@gmail.com>
2023-08-27 19:46:54 +0200

    ENH: restoreprefs automatically when incompatible prefs found.
    Solve the issue after installing new version without explicitly restoring prefs.

--- helpers/space/ea_getspacedef.m
+++ helpers/space/ea_getspacedef.m
@@ -1,3 +1,14 @@
 function spacedef = ea_getspacedef
 
-load([ea_space,'spacedef.mat'], 'spacedef');
+try
+    load([ea_space, 'spacedef.mat'], 'spacedef');
+catch ME
+    if ~isempty(which('lead_dbs.mlapp')) && contains(ea_getspace, '_')
+        % LeadDBS v3.0+ installed but prefs from classic version found.
+        ea_restoreprefs(1, 1);
+        ea_cprintf('CmdWinWarnings', 'Incompatible prefs found! Restored to default.\n');
+        load([ea_space, 'spacedef.mat'], 'spacedef');
+    else
+        rethrow(ME);
+    end
+end
