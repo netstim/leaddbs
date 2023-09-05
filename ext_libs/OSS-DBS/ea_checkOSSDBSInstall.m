e6dd3e351bbe0ee3aa510bfbeb46eeb168cd420a
Ningfei Li <ningfei.li@gmail.com>
2023-09-05 10:44:42 +0200

    ENH: Add check docker image function.

--- ext_libs/OSS-DBS/ea_checkOSSDBSInstall.m
+++ ext_libs/OSS-DBS/ea_checkOSSDBSInstall.m
@@ -18,40 +18,21 @@ end
 
 binPath = getenv('PATH'); % Current PATH
 
-% Check docker installation
-dockerPath = ea_findBinPath('docker');
-if isempty(dockerPath)
-    if ismac
-        % /usr/local/bin might not be in the system path env on macOS
-        ea_error(sprintf(['docker not found!\nIf it''s already installed, ', ...
-            'please run the line below in your terminal (not MATLAB Command Window!) and reboot:\n', ...
-            'sudo launchctl config user path /usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin']), ...
-            showdlg = 0, simpleStack = 1);
+% Check docker image
+ea_checkDocker('ningfei/oss-dbs:latest');
+
+if isunix && ~ismac % Use local built image for Linux to solve permission issue
+    [~, id] = system('docker images -q ningfei/oss-dbs:custom');
+    if ~isempty(id)
+        fprintf('docker image found: ningfei/oss-dbs:custom\n');
+        fprintf('\nRebuilding local docker image...\n');
     else
-        ea_error('docker not found!', showdlg = 0, simpleStack = 1);
+        fprintf('\nBuilding local docker image...\n');
     end
-else
-    if ismac || ispc % Use upstream image since there's no permission issue
-        [~, id] = system('docker images -q ningfei/oss-dbs:latest');
-        if ~isempty(id)
-            fprintf('docker image found: ningfei/oss-dbs:latest\n');
-        end
-        fprintf('\nPulling docker image...\n'); % Always pull to update local image
-        system('docker pull ningfei/oss-dbs:latest');
-    else % Use local built image
-        [~, id] = system('docker images -q ningfei/oss-dbs:custom');
-        if ~isempty(id)
-            fprintf('docker image found: ningfei/oss-dbs:custom\n');
-            fprintf('\nRebuilding docker image...\n');
-        else
-            fprintf('\nBuilding docker image...\n');
-        end
-        currentPath = pwd;
-        cd([ea_getearoot, 'ext_libs/OSS-DBS']);
-        system('docker pull ningfei/oss-dbs:latest'); % Pull to update base image
-        system('docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) -t ningfei/oss-dbs:custom -f custom.Dockerfile .');
-        cd(currentPath);
-    end
+    currentPath = pwd;
+    cd([ea_getearoot, 'ext_libs/OSS-DBS']);
+    system('docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) -t ningfei/oss-dbs:custom -f custom.Dockerfile .');
+    cd(currentPath);
 end
 
 % Check conda installation
@@ -65,7 +46,7 @@ if ~condaenv.is_created
     condaenv.create;
 end
 
-fprintf('\nOSS-DBS dependencies installed under: %s\n', condaenv.path);
+fprintf('\n\nOSS-DBS dependencies installed under: %s\n', condaenv.path);
 
 % Set PATH
 if ispc
