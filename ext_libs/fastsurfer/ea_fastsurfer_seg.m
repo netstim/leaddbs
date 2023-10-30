699134ab18f7c8bd58a50e5e02cae12ece6fda6d
Ningfei Li <ningfei.li@gmail.com>
2023-10-30 09:48:38 +0100

    REF: Refine fastsurfer workflow.
    Update conda env when necessary. Add parameter to update FastSurfer script.

--- ext_libs/fastsurfer/ea_fastsurfer_seg.m
+++ ext_libs/fastsurfer/ea_fastsurfer_seg.m
@@ -1,5 +1,9 @@
-function ea_fastsurfer_seg(input, subjID, outputFolder)
+function ea_fastsurfer_seg(input, subjID, outputFolder, updateScript)
 % Wrapper to run FastSurfer seg_only pipeline
+
+if ~exist('updateScript', 'var')
+    updateScript = false; % Update FastSurfer script or not
+end
 
 input = GetFullPath(input);
 
@@ -40,17 +44,22 @@ if ~condaenv.is_created
     ea_cprintf('CmdWinWarnings', 'Initializing FastSurfer reconsurf environment...\n')
     condaenv.create;
     ea_cprintf('CmdWinWarnings', 'FastSurfer conda environment initialized.\n')
+elseif ~condaenv.is_up_to_date
+    ea_cprintf('CmdWinWarnings', 'Updating FastSurfer reconsurf environment...\n')
+    condaenv.update;
+    ea_cprintf('CmdWinWarnings', 'FastSurfer conda environment initialized.\n')
 end
 
 % Check FastSurfer
 runner = fullfile(fastsurferFolder, 'run_fastsurfer.sh');
-if ~isfile(runner)
+if ~isfile(runner) || updateScript
     ea_cprintf('CmdWinWarnings', 'Downloading FastSurfer...\n')
+    ea_delete(fastsurferFolder);
     downloadFile = fullfile(ea_prefsdir, 'FastSurfer.zip');
     websave(downloadFile, 'https://github.com/Deep-MI/FastSurfer/archive/refs/heads/stable.zip');
     unzip(downloadFile, ea_prefsdir);
     movefile(fullfile(ea_prefsdir, 'FastSurfer-stable'), fastsurferFolder);
-    delete(downloadFile);
+    ea_delete(downloadFile);
     ea_cprintf('CmdWinWarnings', 'FastSurfer downloaded.\n')
 end
 
