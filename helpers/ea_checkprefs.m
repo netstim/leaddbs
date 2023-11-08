d964fadca438f8a2ee223e75c0c16a0a94a9c980
Ningfei Li <ningfei.li@gmail.com>
2023-11-08 12:32:57 +0100

    REF: Force moving prefs in checkprefs func.

--- helpers/ea_checkprefs.m
+++ helpers/ea_checkprefs.m
@@ -7,19 +7,19 @@ prefs = ea_regexpdir(ea_gethome, '^\.ea_prefs.*', 0, 'f', 1);
 
 if ~isempty(prefs)
     newprefs = replace(prefs, [ea_gethome, '.'], [prefsDir, filesep]);
-    cellfun(@(x,y) movefile(x,y), prefs, newprefs);
+    cellfun(@(x,y) movefile(x,y,'f'), prefs, newprefs);
 end
 
 recent = ea_regexpdir([ea_getearoot, 'common'], '^\ea_recent.*', 0, 'f');
 if ~isempty(recent)
     newrecent = replace(recent, [ea_getearoot, 'common'], prefsDir);
-    cellfun(@(x,y) movefile(x,y), recent, newrecent);
+    cellfun(@(x,y) movefile(x,y,'f'), recent, newrecent);
 end
 
 ui = ea_regexpdir(ea_getearoot, '^\ea_ui.*', 0, 'f');
 if ~isempty(ui)
     newui = replace(ui, ea_getearoot, [prefsDir, filesep]);
-    cellfun(@(x,y) movefile(x,y), ui, newui);
+    cellfun(@(x,y) movefile(x,y,'f'), ui, newui);
 end
 
 extFolder = [ea_getearoot, 'ext_libs', filesep];
