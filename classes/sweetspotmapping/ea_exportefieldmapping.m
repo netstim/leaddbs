7eb96400282c397157a675021ae8d8c04b456e5a
Till-Dembek <31889931+Till-Dembek@users.noreply.github.com>
2023-11-20 19:54:00 -0500

    Revert "GenerateTemplateSpace for Sweetspotexplorer"
    This reverts commit 576673533aa1cf5d900bfb0abad3ff68611e9c6f.

--- classes/sweetspotmapping/ea_exportefieldmapping.m
+++ classes/sweetspotmapping/ea_exportefieldmapping.m
@@ -1,45 +1,56 @@
 function [AllX,space]=ea_exportefieldmapping(vatlist,obj)
-EFthresh = 50;
-templateresolution = .5;
+
+disp('Need to export Efields in proper format, this may take a while');
+outdir=[fileparts(obj.leadgroup),filesep,'sweetspots',filesep,obj.ID,filesep];
+ea_mkdir(outdir);
+copyfile([ea_space,'bb.nii'],[outdir,'bb_nan.nii']);
+nii=ea_load_nii([outdir,'bb_nan.nii']);
+nii.dt(1) = 16;
+nii.img(:)=nan;
+ea_write_nii(nii);
+allV{1}=[outdir,'bb_nan.nii'];
+
+cnt=2;
+for vat=1:size(vatlist,1)
+    for side=1:size(vatlist,2)
+        allV{cnt}=vatlist{vat,side};
+        cnt=cnt+1;
+    end
+end
+
+% export mean to get bounding box
+matlabbatch{1}.spm.util.imcalc.input = allV';
+matlabbatch{1}.spm.util.imcalc.output = 'efield_bb.nii';
+matlabbatch{1}.spm.util.imcalc.outdir = {outdir};
+matlabbatch{1}.spm.util.imcalc.expression = 'ea_nansum(X)';
+matlabbatch{1}.spm.util.imcalc.var = struct('name', {}, 'value', {});
+matlabbatch{1}.spm.util.imcalc.options.dmtx = 1;
+matlabbatch{1}.spm.util.imcalc.options.mask = 0;
+matlabbatch{1}.spm.util.imcalc.options.interp = 1;
+matlabbatch{1}.spm.util.imcalc.options.dtype = 16;
+
+spm_jobman('run',{matlabbatch});
+clear matlabbatch
+
 if size(vatlist,2)>1
     sidesuffices={'_r','_l'};
+    ea_split_nii_lr([outdir,'efield_bb.nii']);
 else
     sidesuffices={''};
 end
-disp('Need to export Efields in proper format, this may take a while');
-outdir=[fileparts(obj.leadgroup),filesep,'sweetspots',filesep,obj.ID,filesep];
-mkdir(outdir)
 
-disp(['Creating Voxel Template: EFthreshold = ' num2str(EFthresh) ' V/m; Resolution = ' num2str(templateresolution) ' mm.']);
-mins = repmat({[]},1,size(vatlist,2));
-maxs = repmat({[]},1,size(vatlist,2));
+% create final space:
 for side=1:size(vatlist,2)
-    for vat=1:size(vatlist,1)
-        if ~isempty(vatlist{vat,side})
-            xyz=[];
-            nii=ea_load_nii(vatlist{vat,side});
-            nii.img=nii.img>EFthresh;
-            [xyz(1,:),xyz(2,:),xyz(3,:)]=ind2sub(size(nii.img),find(nii.img));
-            xyz(4,:)=1;
-            xyz=nii.mat*xyz;
-            mins{side} = vertcat(mins{side},min(xyz(1:3,:),[],2)');
-            maxs{side} = vertcat(maxs{side},max(xyz(1:3,:),[],2)');
-        end
+    nii=ea_load_nii([outdir,'efield_bb',sidesuffices{side},'.nii']);
+    if ~isfield(obj.M,'pseudoM')
+        nii.img(nii.img<150)=nan;
+    else
+        ea_warndlg("PseudoM VTAs are used, low field filtering disabled! This can result in a large RAM consumption")
     end
-    mins{side}=min(mins{side});
-    maxs{side}=max(maxs{side});
-end
-
-for side=1:size(vatlist,2)
-    templatesize=[];
-    templatecenter=[];
-    for dim=1:3
-        templatesize(dim) = numel(mins{side}(dim):templateresolution:maxs{side}(dim))+1;
-        templatecenter(dim) = (mins{side}(dim)+maxs{side}(dim))./2;
-    end
-    templatecenter = round(templatecenter/templateresolution)*templateresolution;
-    ea_createTemplateSpace(templatecenter,templatesize,repmat(templateresolution,1,3),outdir,['template',sidesuffices{side},'.nii']);   
-    space{side}=ea_load_nii([outdir,'template',sidesuffices{side},'.nii']);
+    ea_write_nii(nii);
+    ea_crop_nii(nii.fname);
+    nii=ea_load_nii(nii.fname); % reload for space function.
+    space{side}=nii;
 end
 
 % now conform each VTA to space
@@ -47,20 +58,19 @@ AllX=cell(size(vatlist,2),1);
 for vat=1:size(vatlist,1)
     for side=1:size(vatlist,2)
         copyfile(vatlist{vat,side},[outdir,'tmp_efield.nii']);
-        ea_conformspaceto([outdir,'template',sidesuffices{side},'.nii'],...
+        ea_conformspaceto([outdir,'efield_bb',sidesuffices{side},'.nii'],...
             [outdir,'tmp_efield.nii'],0);
         nii=ea_load_nii([outdir,'tmp_efield.nii']);
-        % [~,vatnametmp,~]=fileparts(vatlist{vat,side});
-        % copyfile([outdir,'tmp_efield.nii'],[outdir,vatnametmp,'.nii'])
-        clear vatnametmp
-        if ~exist('AllX','var')
-            AllX{side}=zeros(size(vatlist,1),numel(nii.img));
-        end
-        AllX{side}(vat,:)=nii.img(:);
+
+       if ~exist('AllX','var')
+          AllX{side}=zeros(size(vatlist,1),numel(nii.img));
+       end
+       AllX{side}(vat,:)=nii.img(:);
     end
 end
-ea_delete([outdir,'template.nii']);
-ea_delete([outdir,'template_l.nii']);
-ea_delete([outdir,'teamplate_r.nii']);
+ea_delete([outdir,'efield_bb.nii']);
+ea_delete([outdir,'efield_bb_l.nii']);
+ea_delete([outdir,'efield_bb_r.nii']);
 ea_delete([outdir,'tmp_efield.nii']);
+ea_delete([outdir,'bb_nan.nii']);
 ea_delete(outdir);
