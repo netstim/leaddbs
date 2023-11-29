27a980523287fbcff1492fbdaec2a3393e5fce2d
Ningfei Li <ningfei.li@gmail.com>
2023-11-29 09:09:50 +0800

    REF: Standardize user prefs file name.

--- helpers/ea_switch2classic.m
+++ helpers/ea_switch2classic.m
@@ -33,22 +33,22 @@ if isfile([ea_prefsdir, filesep, 'ea_ui.mat.classic'])
 end
 
 if isfile(ea_prefspath)
-    disp('Backup ea_prefs.m from develop branch ...');
+    disp('Backup ea_prefs_user.m from develop branch ...');
     movefile(ea_prefspath,  ea_prefspath('.m.dev'))
 end
 
 if isfile(ea_prefspath('.m.classic'))
-    disp('Restore ea_prefs.m from classic branch  ...');
+    disp('Restore ea_prefs_user.m from classic branch  ...');
     movefile(ea_prefspath('.m.classic'), ea_prefspath)
 end
 
 if isfile(ea_prefspath('mat'))
-    disp('Backup ea_prefs.mat from develop branch ...');
+    disp('Backup ea_prefs_user.mat from develop branch ...');
     movefile(ea_prefspath('mat'),  ea_prefspath('.mat.dev'))
 end
 
 if isfile(ea_prefspath('.mat.classic'))
-    disp('Restore ea_prefs.mat from classic branch  ...');
+    disp('Restore ea_prefs_user.mat from classic branch  ...');
     movefile(ea_prefspath('.mat.classic'), ea_prefspath('mat'))
     % Fix possible wrong template string
     load(ea_prefspath('mat'), 'machine');
@@ -58,12 +58,12 @@ if isfile(ea_prefspath('.mat.classic'))
 end
 
 if isfile(ea_prefspath('json'))
-    disp('Backup ea_prefs.json from develop branch  ...');
+    disp('Backup ea_prefs_user.json from develop branch  ...');
     movefile(ea_prefspath('json'),  ea_prefspath('.json.dev'))
 end
 
 if isfile(ea_prefspath('.json.classic'))
-    disp('Restore ea_prefs.json from classic branch  ...');
+    disp('Restore ea_prefs_user.json from classic branch  ...');
     movefile(ea_prefspath('.json.classic'), ea_prefspath('json'))
 end
 
