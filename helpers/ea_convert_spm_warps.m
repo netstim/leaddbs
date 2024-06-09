6e521dc56a2dab08ffcab47654f5ae71f78b0413
Ningfei Li <ningfei.li@gmail.com>
2024-06-09 22:49:20 +0200

    ENH: Add wrapper func to convert SPM deformation field to ITK.
    Can apply to legacy patient folder before running normalization.
    Input 'subj' is the bids struct.

---
+++ helpers/ea_convert_spm_warps.m
@@ -0,0 +1,13 @@
+function ea_convert_spm_warps(subj)
+% Convert SPM deformation field into ITK format
+
+forwardBaseName = subj.norm.transform.forwardBaseName;
+inverseBaseName = subj.norm.transform.inverseBaseName;
+
+if isfile([forwardBaseName, 'spm.nii'])
+    ea_spm_fwd_displacement_field_to_ants([forwardBaseName, 'spm.nii'], [forwardBaseName, 'ants.nii.gz']);
+    ea_slicer_invert_transform([forwardBaseName, 'ants.nii.gz'], subj.coreg.anat.preop.(subj.AnchorModality), [inverseBaseName, 'ants.nii.gz']);
+
+    delete([forwardBaseName, 'spm.nii']);
+    ea_delete([inverseBaseName, 'spm.nii']);
+end
