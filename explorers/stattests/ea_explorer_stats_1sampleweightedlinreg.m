bd7e4fdc8dcb28227df3f4c666f200565d84e443
Andreas Horn <andreas.horn@charite.de>
2024-06-25 16:10:18 -0400

    ENH: Drastically sped up weighted lin regression functions

--- explorers/stattests/ea_explorer_stats_1sampleweightedlinreg.m
+++ explorers/stattests/ea_explorer_stats_1sampleweightedlinreg.m
@@ -1,4 +1,4 @@
-function varargout=ea_explorer_stats_1sampleweightedlinreg(varargin)
+function varargout = ea_explorer_stats_1sampleweightedlinreg(varargin)
 % Function to estimate a certain mass univariate test (see below) as used
 % in explorer apps.
 % - Expects valsin as V x N matrix where 
@@ -10,62 +10,95 @@ function varargout=ea_explorer_stats_1sampleweightedlinreg(varargin)
 % - Expects H0 as the value to test against {'Zero','Average' or a number}
 % - Outputs valsout (test results) and psout (p-values of test results) as V x 1 or 2.
 
-if nargin>0
-    % map inputs
-    valsin=varargin{1};
-    outcomein=varargin{2};
-    H0=varargin{3};
+if nargin > 0
+    % Map inputs
+    valsin = varargin{1};
+    outcomein = varargin{2};
+    H0 = varargin{3};
 else
-    varargout{1}.name="1-Sample Weighted Regression";
+    varargout{1}.name = "1-Sample Weighted Regression";
     varargout{1}.file = mfilename;
     varargout{1}.type = "1-Sample Tests";
     varargout{1}.outcometype = {'gradual'};
-    varargout{1}.compatibility = {'Electric Field','Sigmoid Field','VTA'};    
-    return
+    varargout{1}.compatibility = {'Electric Field', 'Sigmoid Field', 'VTA'};    
+    return;
 end
 
 % Actual test:
 if ischar(H0)
     switch H0
         case 'Average'
-            H0=mean(outcomein,'all','omitnan');
+            H0 = mean(outcomein, 'all', 'omitnan');
         case 'Zero'
-            H0=0;
+            H0 = 0;
     end
 end
-outcomein=repmat(outcomein',size(valsin,1),1);
+outcomein = repmat(outcomein', size(valsin, 1), 1);
 
-%valsout = ~valsin;
-%valsin_new = ones(size(valsin));
-%valsout_new = ones(size(valsin));
+group1 = outcomein;
+group1(isnan(valsin)) = NaN;
+group2 = repmat(H0, size(valsin));
+group2(isnan(group1)) = NaN;
 
-% zero entries are meaningless!
-%valsin_new(valsin==0) = nan;
-%valsout_new(valsout==0) = nan;
+% Initialize output arrays
+valsout = nan(size(valsin, 1), 1);
+psout = nan(size(valsin, 1), 1);
 
-group1=outcomein;
-group1(isnan(valsin))=NaN;
-group2=repmat(H0,size(valsin));
-group2(isnan(group1))=NaN;
-valsout=nan(size(valsin,1),1);
-psout=nan(size(valsin,1),1);
+% Check for Parallel Computing Toolbox
+if license('test', 'Distrib_Computing_Toolbox')
+    % Start parallel pool if not already started
+    if isempty(gcp('nocreate'))
+        parpool;
+    end
 
-mysyntax = 'outcome ~ 1 + condition';
-ea_dispercent(1/size(valsin,1),'Calculating Weighted regression')
-for i = 1:size(valsin,1)
-    ea_dispercent(i/size(valsin,1))
-    mytable = table;
-    mytable.outcome= vertcat(group2(i,:)',group1(i,:)');
-    mytable.weight = vertcat(valsin(i,:)',valsin(i,:)');
-    mytable.condition = vertcat(zeros(size(valsin(i,:)')),ones(size(valsin(i,:)')));
-    mymdl = fitlm(mytable,mysyntax,'Weights',mytable.weight);
-    %% storing statitic values in maps
-    psout(i) = mymdl.Coefficients.pValue(2);
-    valsout(i) = mymdl.Coefficients.tStat(2);
+    % Parallel processing
+    parfor i = 1:size(valsin, 1)
+        % Prepare data for regression
+        Y = [group2(i, :)'; group1(i, :)'];
+        X = [ones(size(Y)), [zeros(size(valsin(i, :)')); ones(size(valsin(i, :)'))]];
+        W = [valsin(i, :)'; valsin(i, :)'];
+
+        % Perform weighted least squares regression
+        Wsqrt = diag(sqrt(W));
+        Xw = Wsqrt * X;
+        Yw = Wsqrt * Y;
+        b = Xw \ Yw;
+        residuals = Yw - Xw * b;
+        sigma2 = (residuals' * residuals) / (size(X, 1) - size(X, 2));
+        C = sigma2 * inv(Xw' * Xw);
+        se = sqrt(diag(C));
+
+        % Store results
+        tStat = b(2) / se(2);  % t-statistic
+        valsout(i) = tStat;
+        psout(i) = 2 * (1 - tcdf(abs(tStat), size(X, 1) - size(X, 2))); % p-value
+    end
+else
+    % Standard processing
+    for i = 1:size(valsin, 1)
+        % Prepare data for regression
+        Y = [group2(i, :)'; group1(i, :)'];
+        X = [ones(size(Y)), [zeros(size(valsin(i, :)')); ones(size(valsin(i, :)'))]];
+        W = [valsin(i, :)'; valsin(i, :)'];
+
+        % Perform weighted least squares regression
+        Wsqrt = diag(sqrt(W));
+        Xw = Wsqrt * X;
+        Yw = Wsqrt * Y;
+        b = Xw \ Yw;
+        residuals = Yw - Xw * b;
+        sigma2 = (residuals' * residuals) / (size(X, 1) - size(X, 2));
+        C = sigma2 * inv(Xw' * Xw);
+        se = sqrt(diag(C));
+
+        % Store results
+        tStat = b(2) / se(2);  % t-statistic
+        valsout(i) = tStat;
+        psout(i) = 2 * (1 - tcdf(abs(tStat), size(X, 1) - size(X, 2))); % p-value
+    end
 end
-ea_dispercent(i/size(valsin,1),'end')
 
-% map outputs
-varargout{1}=valsout;
-varargout{2}=psout;
+% Map outputs
+varargout{1} = valsout;
+varargout{2} = psout;
 end
--- explorers/stattests/ea_explorer_stats_2sampleweightedlinreg.m
+++ explorers/stattests/ea_explorer_stats_2sampleweightedlinreg.m
@@ -1,4 +1,4 @@
-function varargout=ea_explorer_stats_2sampleweightedlinreg(varargin)
+function varargout = ea_explorer_stats_2sampleweightedlinreg(varargin)
 % Function to estimate a certain mass univariate test (see below) as used
 % in explorer apps.
 % - Expects valsin as V x N matrix where 
@@ -9,45 +9,82 @@ function varargout=ea_explorer_stats_2sampleweightedlinreg(varargin)
 % - Expects outcomein as improvement values with dimension N x 1 or 2.
 % - Outputs valsout (test results) and psout (p-values of test results) as V x 1 or 2.
 
-if nargin>0
-    % map inputs
-    valsin=varargin{1};
-    outcomein=varargin{2};
+if nargin > 0
+    % Map inputs
+    valsin = varargin{1};
+    outcomein = varargin{2};
 else
-    varargout{1}.name="2-Sample Weighted Regression";
+    varargout{1}.name = "2-Sample Weighted Regression";
     varargout{1}.file = mfilename;
     varargout{1}.type = "2-Sample Tests";
     varargout{1}.outcometype = {'gradual'};
-    varargout{1}.compatibility = {'Electric Field','Sigmoid Field','VTA'};    
-    return
+    varargout{1}.compatibility = {'Electric Field', 'Sigmoid Field', 'VTA'};    
+    return;
 end
 
 % Actual test:
-outcomein=repmat(outcomein',size(valsin,1),1);
-group1=outcomein;
+outcomein = repmat(outcomein', size(valsin, 1), 1);
+group1 = outcomein;
 
-% we don't need group 2 in this test!
-%group2=outcomein;
-%group2(~isnan(valsin))=NaN;
-valsout=nan(size(valsin,1),1);
-psout=nan(size(valsin,1),1);
+% Initialize output arrays
+valsout = nan(size(valsin, 1), 1);
+psout = nan(size(valsin, 1), 1);
 
-mysyntax = 'outcome ~ 1 + condition';
-ea_dispercent(1/size(valsin,1),'Calculating Weighted regression')
-for i = 1:size(valsin,1)
-    ea_dispercent(i/size(valsin,1))
-    mytable = table;
-    mytable.outcome= vertcat(group1(i,:)',group1(i,:)');
-    mytable.weight = vertcat(1-valsin(i,:)',valsin(i,:)');
-    mytable.condition = vertcat(zeros(size(valsin(i,:)')),ones(size(valsin(i,:)')));
-    mymdl = fitlm(mytable,mysyntax,'Weights',mytable.weight);
-    %% storing statitic values in maps
-    psout(i) = mymdl.Coefficients.pValue(2);
-    valsout(i) = mymdl.Coefficients.tStat(2);
+% Check for Parallel Computing Toolbox
+if license('test', 'Distrib_Computing_Toolbox')
+    % Start parallel pool if not already started
+    if isempty(gcp('nocreate'))
+        parpool;
+    end
+
+    % Parallel processing
+    parfor i = 1:size(valsin, 1)
+        % Prepare data for regression
+        Y = [group1(i, :)'; group1(i, :)'];
+        X = [ones(size(Y)), [zeros(size(valsin(i, :)')); ones(size(valsin(i, :)'))]];
+        W = [1 - valsin(i, :)'; valsin(i, :)'];
+
+        % Perform weighted least squares regression
+        Wsqrt = diag(sqrt(W));
+        Xw = Wsqrt * X;
+        Yw = Wsqrt * Y;
+        b = Xw \ Yw;
+        residuals = Yw - Xw * b;
+        sigma2 = (residuals' * residuals) / (size(X, 1) - size(X, 2));
+        C = sigma2 * inv(Xw' * Xw);
+        se = sqrt(diag(C));
+
+        % Store results
+        tStat = b(2) / se(2);  % t-statistic
+        valsout(i) = tStat;
+        psout(i) = 2 * (1 - tcdf(abs(tStat), size(X, 1) - size(X, 2))); % p-value
+    end
+else
+    % Standard processing
+    for i = 1:size(valsin, 1)
+        % Prepare data for regression
+        Y = [group1(i, :)'; group1(i, :)'];
+        X = [ones(size(Y)), [zeros(size(valsin(i, :)')); ones(size(valsin(i, :)'))]];
+        W = [1 - valsin(i, :)'; valsin(i, :)'];
+
+        % Perform weighted least squares regression
+        Wsqrt = diag(sqrt(W));
+        Xw = Wsqrt * X;
+        Yw = Wsqrt * Y;
+        b = Xw \ Yw;
+        residuals = Yw - Xw * b;
+        sigma2 = (residuals' * residuals) / (size(X, 1) - size(X, 2));
+        C = sigma2 * inv(Xw' * Xw);
+        se = sqrt(diag(C));
+
+        % Store results
+        tStat = b(2) / se(2);  % t-statistic
+        valsout(i) = tStat;
+        psout(i) = 2 * (1 - tcdf(abs(tStat), size(X, 1) - size(X, 2))); % p-value
+    end
 end
-ea_dispercent(i/size(valsin,1),'end')
 
-% map outputs
-varargout{1}=valsout;
-varargout{2}=psout;
+% Map outputs
+varargout{1} = valsout;
+varargout{2} = psout;
 end
