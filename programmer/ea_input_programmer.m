8d1215b6d2e13656963ea0955b99b897c2d6cde6
Ningfei Li <ningfei.li@gmail.com>
2024-09-05 21:02:00 +0200

    REF: Refine input programmer func.
    Fix dissimilar S structures issue.

--- programmer/ea_input_programmer.m
+++ programmer/ea_input_programmer.m
@@ -5,22 +5,15 @@ function [file_path, releaseDir, input_file_path] = ea_input_programmer(options,
 programmerDir = fullfile(options.earoot, 'programmer');
 
 file_path = fullfile(programmerDir, 'data.json');
-input_file_path = fullfile(programmerDir, 'inputData.json');
-
-% Loop through each file path and create the file if it does not exist
-file_paths = {file_path, input_file_path};
-for i = 1:length(file_paths)
-    if ~isfile(file_paths{i})
-        % Create an empty file
-        fid = fopen(file_paths{i}, 'w');
-        if fid == -1
-            error('Could not create file: %s', file_paths{i});
-        end
-        fclose(fid);
+if ~isfile(file_path)
+    try
+        savejson('', struct, file_path);
+    catch ME
+        error('Failed to create file: %s', file_path);
     end
 end
 
-releaseDir = fullfile(programmerDir, 'app', 'release', 'build');
+releaseDir = fullfile(programmerDir, 'app', 'release');
 
 %% Convert stimparameters.mat to json, handle inputData.json
 inputStruct.patientname = options.patientname;
@@ -29,28 +22,35 @@ inputStruct.electrodeModel = options.elmodel;
 inputStruct.stimDir = fullfile(options.subj.stimDir, 'MNI152NLin2009bAsym');
 
 stimDir = fullfile(options.subj.stimDir, ea_getspace);
-if ~exist(stimDir, 'dir')
-    mkdir(stimDir); % Create the directory if it doesn't exist
-end
+ea_mkdir(stimDir);
 
 stimFileName = [options.patientname, '_desc-stimparameters'];
 stimMatFile = ea_regexpdir(stimDir, ['^', stimFileName, '\.mat$'], 1, 'f');
 
 if isempty(stimMatFile)
     % Create new stimulation label, set S to empty
-%     inputStruct.labels = {[char(datetime('now', 'Format', 'yyyyMMddHHmmSS')) ea_genid_rand(1,6)]};
     inputStruct.labels = {[char(datetime('now', 'Format', 'yyyyMMddHHmmSS'))]};
     inputStruct.S = {};
 else
     % Aggregate existing stimulations
     for i=1:numel(stimMatFile)
         load(stimMatFile{i}, 'S');
+
         if isempty(S.label)
-            % Generate a random label using datetime and a random ID
             S.label = [char(datetime('now', 'Format', 'yyyyMMddHHmmSS'))];
         end
         inputStruct.labels{i} = S.label;
+
+        if ~isfield(S, 'sources')
+            S.sources = 1:4;
+        end
+
+        if ~isfield(S, 'volume')
+            S.volume = [];
+        end
+
         inputStruct.S(i) = S;
+
         stimJsonFile = strrep(stimMatFile{i}, '.mat', '.json');
         savejson('', S, stimJsonFile);
         fprintf('Converted and saved stimparameters.mat to %s\n', stimJsonFile);
