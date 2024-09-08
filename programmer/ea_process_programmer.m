e01783c8ca0531baba0454ddec888301d0f2f60d
SavirMadan13 <136630218+SavirMadan13@users.noreply.github.com>
2024-09-08 16:30:12 -0400

    FIX: New MacOS binaries - cleaned up ea_process_programmer code

--- programmer/ea_process_programmer.m
+++ programmer/ea_process_programmer.m
@@ -1,19 +1,27 @@
 function [S] = ea_process_programmer(file_path, options)
+    %% Initialize Paths, Read in data from programmer through data.json, Delete helper files (inputData, data)
+
     stimDir = fullfile(options.subj.stimDir, ea_getspace);
     file_path = fullfile(stimDir, 'data.json');
     input_file_path = fullfile(stimDir, 'inputData.json');
-    new_data = fileread(file_path);
-%     fid = fopen(file_path, 'w');
-%     fclose(fid);
+
+    importedS = loadjson(file_path);
+
+    if isfield(importedS, 'message')
+        disp(['Message: ', importedS.message]);
+        % Exit the script
+        return;
+    end
+
     delete(file_path);
     delete(input_file_path);
-    importedS = jsondecode(new_data);
+
+    %% Reorganize input for programmer to support lead-dbs convention
+
     S = importedS.S;
-    % S.model = 'OSS-DBS (Butenko 2020)';
     numRows = size(S.activecontacts, 2);
     numCols = size(S.activecontacts, 2);
     
-    % Initialize the cell array
     newVariable = cell(2, 4);
     
     % Fill the cell array
@@ -22,26 +30,23 @@ function [S] = ea_process_programmer(file_path, options)
             newVariable{i, j} = S.activecontacts((i-1)*4+j, :);
         end
     end
+
     S.activecontacts = newVariable;
+
     firstTerm=S.activecontacts{1,1} + S.activecontacts{1,2} + S.activecontacts{1,3} + S.activecontacts{1,4};
     secondTerm=S.activecontacts{2,1} + S.activecontacts{2,2} + S.activecontacts{2,3} + S.activecontacts{2,4};
+
     S.activecontacts={secondTerm, firstTerm};
-    % Given data
-    S.activecontacts = {secondTerm, firstTerm};  % Assuming these are arrays
+    S.activecontacts = {secondTerm, firstTerm};
     
-    % Process each term in the cell array
     for i = 1:length(S.activecontacts)
-        term = S.activecontacts{i};  % Extract the term (either secondTerm or firstTerm)
+        term = S.activecontacts{i};
         
-        % Check if any element is greater than 1 and change it to 1
         term(term > 1) = 1;
         
-        % Save the modified term back to the cell array
         S.activecontacts{i} = term;
     end
-    
-    % S.label = formattedDate;
-    %             S.model = S_old.model;
+
     S.amplitude = {S.amplitude.rightAmplitude.', S.amplitude.leftAmplitude.'};
     S.monopolarmodel = 0;
     S.sources=[1, 2, 3, 4];
