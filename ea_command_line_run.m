448772d4954bd8f10c0ec34932ed7e4044d8a0cd
Ningfei Li <ningfei.li@gmail.com>
2024-09-16 12:09:26 +0200

    BUG: Fix command line interface.
    Adapted to new AppDesigner figure handle.
    BIDS structure has to be set.
    coregct method has to be set.

--- ea_command_line_run.m
+++ ea_command_line_run.m
@@ -15,70 +15,75 @@ switch varargin{1}
         leadprod = 'connectome';
 end
 
-set(h, 'Visible', 'off'); drawnow;
-handles = guihandles(h);
+h.leadfigure.Visible = 'off';
+drawnow;
+handles = guihandles(h.leadfigure);
 
 % reset all checkboxes to 0
 handles_names = fieldnames(handles);
 for i = 1:length(handles_names)
-    if isprop(handles.(handles_names{i}), 'Style') && strcmp(handles.(handles_names{i}).Style,'checkbox')
+    if isa(handles.(handles_names{i}), 'matlab.ui.control.CheckBox')
         handles.(handles_names{i}).Value = 0;
     end
 end
 
-tryBIDS = false; % flag to try BIDS import when calling with -process
-dirs = {};
+patdirs = {};
 
 for i = 2:nargin
-    switch varargin{i}(1) % first character of str
-        case filesep % directory
-            patient_dir = varargin{i};
-            if isfolder(patient_dir)
-                dirs{end+1} = patient_dir;
-            else
-                error([patient_dir ' is not a folder'])
-            end
-        case '-' % option
-            opt = varargin{i}(2:end);
-            if isfield(handles, opt)
-                set(handles.(opt),'Value',1); % set default option to 1
-            elseif strcmp(opt,'process')
-                % run basic lead dbs pipeline with defaults
-                handles.coreg_checkbox.Value = 1;
-                handles.coregmrmethod.Value = 1;
-                handles.normalize_checkbox.Value = 1;
-                handles.normmethod.Value = 9;
-                handles.scrf.Value = 1;
-                handles.scrfmask.Value = 2;
-                handles.doreconstruction.Value = 1;
-                handles.reconmethod.Value = 3;
-                tryBIDS = true;
-            else
-                error(['Unrecognized field: ' opt])
-            end
-        otherwise
-            set(handles.(opt),'Value',str2double(varargin{i}));
+    if isfolder(varargin{i})
+        patdirs{end+1} = patient_dir;
+    elseif startsWith(varargin{i}, '-')
+        opt = varargin{i}(2:end);
+        if strcmp(opt,'process')
+            % run basic lead dbs pipeline with defaults
+            handles.coreg_checkbox.Value = 1;
+            handles.coregctmethod.Value = 1;
+            handles.coregmrmethod.Value = 1;
+            handles.normalize_checkbox.Value = 1;
+            handles.normmethod.Value = 1;
+            handles.scrf.Value = 1;
+            handles.scrfmask.Value = 2;
+            handles.doreconstruction.Value = 1;
+            handles.reconmethod.Value = 3;
+        elseif isfield(handles, opt)
+            set(handles.(opt), 'Value', 1); % set default option to 1   
+        else
+            error(['Unrecognized field: ' opt])
+        end
+    else
+        set(handles.(opt), 'Value', str2double(varargin{i}));
     end
 end
 
+
 options = ea_handles2options(handles);
-options.uipatdirs = dirs;
 options.leadprod = leadprod;
-setappdata(h,'handles',handles);
-options.leadfigure = h;
+options.leadfigure = h.leadfigure;
+
+% Erase last filesep
+patdirs = erase(patdirs, filesep + textBoundary('end'));
+% All patient folder should be under the same dataset
+datasetDir = regexp(patdirs{1}, ['.*(?=\' filesep 'derivatives\' filesep 'leaddbs\' filesep 'sub-)'], 'match', 'once');
+subjId = erase(patdirs, fullfile(datasetDir, 'derivatives', 'leaddbs', 'sub-'));
+bids = BIDSFetcher(datasetDir);
+
+options.uipatdirs = patdirs;
+setappdata(options.leadfigure, 'bids', bids);
+setappdata(options.leadfigure, 'subjId', subjId);
 
 % dont show pop up methods
 umachine = load(ea_prefspath('mat'));
 machine = umachine.machine;
 machine.methods_show = 0;
-save(ea_prefspath('mat'),'machine');
+save(ea_prefspath('mat'), 'machine');
 
 % run
 ea_run('run',options);
 
 % restore previous pop up config
 machine.methods_show = umachine.machine.methods_show;
-save(ea_prefspath('mat'),'machine');
+save(ea_prefspath('mat'), 'machine');
 
-set(h, 'Visible', 'on'); drawnow; % make gui visible so that close request function is executed
+h.leadfigure.Visible = 'on';
+drawnow; % make gui visible so that close request function is executed
 close(h)
