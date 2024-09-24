42770dfdd9434771333c45dcf615ebcce24bfd40
Ningfei Li <ningfei.li@gmail.com>
2024-09-24 15:41:59 +0200

    REF: Simplify segmask clean up function.

--- ext_libs/OSS-DBS/genvat_butenko/ea_segmask_cleanup.m
+++ ext_libs/OSS-DBS/genvat_butenko/ea_segmask_cleanup.m
@@ -7,17 +7,9 @@ arguments
     options     % Lead-DBS options for the subject
 end
 
-if ispc
-    filelist = dir(fullfile(options.subj.subjDir, '**\*.*'));  % get list of files and folders in any subfolder
-else
-    filelist = dir(fullfile(options.subj.subjDir, '**/*.*'));  % get list of files and folders in any subfolder
-end
-filelist = filelist(~[filelist.isdir]);  %remove folders from list
-
-% delete all segmask entries from the patient folder
-for files_idx = 1:size(filelist,1)
-    if contains(filelist(files_idx).name, 'segmask') || contains(filelist(files_idx).name, '-C123_mod') || contains(filelist(files_idx).name, 'label-GM_mask') || contains(filelist(files_idx).name, 'label-WM_mask') || contains(filelist(files_idx).name, 'label-CSF_mask')
-        disp("here")
-        ea_delete(fullfile(filelist(files_idx).folder,filelist(files_idx).name))
-    end
+pattern = '(segmask|-C123_mod|label-(GM|WM|CSF)_mask)';
+maskFiles = ea_regexpdir(options.subj.subjDir, pattern, 1, 'f');
+if ~isempty(maskFiles)
+    ea_delete(maskFiles);
+    ea_cprintf('Comments', 'Segmask cleaned up for "%s".\n', options.subj.subjId);
 end
