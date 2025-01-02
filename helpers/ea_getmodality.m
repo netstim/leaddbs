7109b8495d2137bf64d94ec941be6abf3c58764f
Ningfei Li <ningfei.li@gmail.com>
2025-01-02 12:30:35 +0100

    ENH: getmodality supports cell input now.

--- helpers/ea_getmodality.m
+++ helpers/ea_getmodality.m
@@ -1,13 +1,27 @@
-function modality = ea_getmodality(BIDSFilePath, acq)
+function modality = ea_getmodality(BIDSFilePath, opts)
 % Extract image modality from BIDS file path
-
-% Keep acq label by default
-if ~exist('acq', 'var')
-    acq = 1;
+arguments
+    BIDSFilePath {mustBeText}
+    opts.acq {mustBeNumericOrLogical} = true % Keep acq label by default
 end
 
-if ~acq || ~isempty(regexp(BIDSFilePath, '_CT\.nii(.gz)?$', 'once')) % Skip plane label
-    modality = regexp(BIDSFilePath, '(?<=_)([^\W_]+)(?=\.nii(\.gz)?$)', 'match', 'once');
-else % Keep plane label
-    modality = regexp(BIDSFilePath, '(?<=_acq-)((ax|sag|cor|iso)\d*_[^\W_]+)(?=\.nii(\.gz)?$)', 'match', 'once');
+if ~iscell(BIDSFilePath)
+    wasChar = 1;
+    BIDSFilePath = {BIDSFilePath};
+else
+    wasChar = 0;
+end
+
+modality = cell(size(BIDSFilePath));
+
+for i=1:length(BIDSFilePath)
+    if ~opts.acq || ~isempty(regexp(BIDSFilePath{i}, '_CT\.nii(.gz)?$', 'once')) % Skip plane label
+        modality{i} = regexp(BIDSFilePath{i}, '(?<=_)([^\W_]+)(?=\.nii(\.gz)?$)', 'match', 'once');
+    else % Keep plane label
+        modality{i} = regexp(BIDSFilePath{i}, '(?<=_acq-)((ax|sag|cor|iso)\d*_[^\W_]+)(?=\.nii(\.gz)?$)', 'match', 'once');
+    end
+end
+
+if wasChar
+    modality = modality{1};
 end
