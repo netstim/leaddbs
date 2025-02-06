a594d1ae4ced5bed2ab011560ff5d5ce2f3f53d0
Ningfei Li <ningfei.li@gmail.com>
2025-02-06 23:46:34 +0100

    BUG: Check if atlas is none when exporting ply.
    Also mkdir before exporting.

--- tools/export/pat2ply/ea_pat2ply.m
+++ tools/export/pat2ply/ea_pat2ply.m
@@ -9,17 +9,24 @@ else
     atlasset=atlasset{get(handles.atlassetpopup,'Value')};
 end
 
-cfv(1)=ea_atlas2ply({atlasset},[uipatdir,filesep,'export',filesep,'ply',filesep,'anatomy.ply'],target);
+exportFolder = [uipatdir,filesep,'export',filesep,'ply'];
+ea_mkdir(exportFolder);
+
+if isfolder(fullfile(ea_space, 'atlases', atlasset))
+    cfv(1)=ea_atlas2ply({atlasset},[exportFolder,filesep,'anatomy.ply'],target);
+    cnt = 1;
+else
+    cnt = 0;
+end
 
 options=ea_detsides(ea_getptopts(uipatdir));
-cnt=1;
 for side=options.sides
     cfv(1+cnt)=ea_electrode2ply(uipatdir,side,handles);
     cnt=cnt+1;
 end
 
 cfvel=ea_concatfv(cfv(2:end));
-plywrite([uipatdir,filesep,'export',filesep,'ply',filesep,'combined_electrodes.ply'],cfvel.faces,cfvel.vertices,cfvel.facevertexcdata,repmat(100,size(cfvel.facevertexcdata,1),1));
+plywrite([exportFolder,filesep,'combined_electrodes.ply'],cfvel.faces,cfvel.vertices,cfvel.facevertexcdata,repmat(100,size(cfvel.facevertexcdata,1),1));
 
 cfv=ea_concatfv(cfv);
-plywrite([uipatdir,filesep,'export',filesep,'ply',filesep,'combined_scene.ply'],cfv.faces,cfv.vertices,cfv.facevertexcdata,repmat(100,size(cfv.facevertexcdata,1),1));
+plywrite([exportFolder,filesep,'combined_scene.ply'],cfv.faces,cfv.vertices,cfv.facevertexcdata,repmat(100,size(cfv.facevertexcdata,1),1));
