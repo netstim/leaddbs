19e7b38820d2f174609924a7eff6438a3719a404
Konstantin Butenko <35195806+Kinway25@users.noreply.github.com>
2026-01-08 13:51:43 +0100

    UPD: WSL path from prefs (#1296)
    * UPD: placeholder stimProtocol when not provided
    
    * UPD: WSL path from prefs

--- ext_libs/OSS-DBS/genvat_butenko/ea_prepare_ossdbs.m
+++ ext_libs/OSS-DBS/genvat_butenko/ea_prepare_ossdbs.m
@@ -9,7 +9,7 @@ arguments
 end
 
 % Check OSS-DBS installation, set env
-if ~isfield(options.prefs,'ext_oss_env') || strcmp(options.prefs.ext_oss_env,'None')
+if (~isfield(options.prefs,'ext_oss_env') || strcmp(options.prefs.ext_oss_env,'None')) && (~isfield(options.prefs,'wsl_env') || ~options.prefs.wsl_env)
     env = ea_conda_env('OSS-DBSv2');
     ea_checkOSSDBSInstallv2(env);
 end
--- helpers/vta_runwslcommand.m
+++ helpers/vta_runwslcommand.m
@@ -1,10 +1,13 @@
-function [status, output] = vta_runwslcommand(cmd)
-    % Run shell commands via WSL
-    % by Till Dembek
+function [status, output] = vta_runwslcommand(oss_env_path,cmd)
+% Run shell commands via WSL
+% by Till Dembek
 
-    % use OSS env within WSL. ToDo: pass from prefs
-    oss_env_path = '/mnt/c/Users/netstim/ossv2';  
-    full_cmd = sprintf('wsl bash -l -c "source %s/bin/activate && %s"', oss_env_path, cmd);
-    %full_cmd = sprintf('wsl bash -l -c "%s"', cmd);
-    [status, output] = system(full_cmd);
+arguments
+    oss_env_path     % (Linux-format) path to the OSS conda environment
+    cmd % command to run in the terminal
+end
+
+full_cmd = sprintf('wsl bash -l -c "source %s/bin/activate && %s"', oss_env_path, cmd);
+%full_cmd = sprintf('wsl bash -l -c "%s"', cmd);
+[status, output] = system(full_cmd);
 end
